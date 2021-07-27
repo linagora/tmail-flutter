8772126a3fd9a4b7a3b97d85e58fa95ef64a1441
dab246 <tdvu@linagora.com>
2021-07-27 12:55:55 +0700

    Init mailbox feature add data layer

---
+++ lib/features/mailbox/data/datasource_impl/mailbox_datasource_impl.dart
@@ -0,0 +1,21 @@
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
+import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
+import 'package:tmail_ui_user/features/mailbox/data/datasource/mailbox_datasource.dart';
+import 'package:tmail_ui_user/features/mailbox/data/network/mailbox_api.dart';
+
+class MailboxDataSourceImpl extends MailboxDataSource {
+
+  final MailboxAPI mailboxAPI;
+
+  MailboxDataSourceImpl(this.mailboxAPI,);
+
+  @override
+  Future<List<Mailbox>> getAllMailbox(AccountId accountId, {Properties? properties}) {
+    return Future.sync(() async {
+      return await mailboxAPI.getAllMailbox(accountId, properties: properties);
+    }).catchError((error) {
+      throw error;
+    });
+  }
+}
