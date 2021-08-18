d2d58ea8299def50242efe6233fc11f8c920dc21
dab246 <tdvu@linagora.com>
2021-08-18 17:42:15 +0700

    Configure file to run on android device

---
+++ android/app/src/main/kotlin/com/linagora/android/tmail/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.linagora.android.tmail
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
--- android/app/src/main/kotlin/com/linagora/android/tmail_ui_user/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.linagora.android.tmail_ui_user
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
