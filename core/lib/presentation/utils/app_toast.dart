b2ec590aa24657b54f4c090ed4a904e4851e34c3
dab246 <tdvu@linagora.com>
2021-09-21 10:07:16 +0700

    TF-80 Undo when move email to mailbox success from email detailed view

--- core/lib/presentation/utils/app_toast.dart
+++ core/lib/presentation/utils/app_toast.dart
@@ -1,6 +1,8 @@
 import 'package:core/presentation/extensions/color_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:getwidget/components/toast/gf_toast.dart';
+import 'package:getwidget/getwidget.dart';
 
 class AppToast {
   void showToast(String message) {
@@ -31,5 +33,26 @@ class AppToast {
         backgroundColor: AppColor.toastErrorBackgroundColor,
         toastLength: Toast.LENGTH_SHORT,
         gravity: ToastGravity.BOTTOM);
+  }
+
+  void showToastWithAction(BuildContext context, String message, String actionName, Function onActionClick) {
+    GFToast.showToast(
+      message,
+      context,
+      toastPosition: GFToastPosition.BOTTOM,
+      textStyle: TextStyle(fontSize: 16, color: Colors.white),
+      backgroundColor: AppColor.toastWithActionBackgroundColor,
+      trailing: GFButton(
+        onPressed: () {
+          ToastView.dismiss();
+          onActionClick();
+        },
+        text: actionName,
+        type: GFButtonType.transparent,
+        color: AppColor.buttonActionToastWithActionColor,
+      ),
+      toastBorderRadius: 5.0,
+      toastDuration: 3
+    );
   }
 }
