4a52c56504de3e30bd254bce3e907bba03b0bd3d
dab246 <tdvu@linagora.com>
2021-09-21 15:23:01 +0700

    TF-60 Add data get device contact suggestion

---
+++ lib/features/composer/data/repository/contact_repository_impl.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:model/model.dart';
+import 'package:tmail_ui_user/features/composer/data/datasource/contact_datasource.dart';
+import 'package:tmail_ui_user/features/composer/domain/model/auto_complete_pattern.dart';
+import 'package:tmail_ui_user/features/composer/domain/repository/contact_repository.dart';
+
+class ContactRepositoryImpl implements ContactRepository {
+  final ContactDataSource _contactDataSource;
+
+  ContactRepositoryImpl(this._contactDataSource);
+
+  @override
+  Future<List<Contact>> getContactSuggestions(AutoCompletePattern autoCompletePattern) {
+    return _contactDataSource.getContactSuggestions(autoCompletePattern);
+  }
+}
