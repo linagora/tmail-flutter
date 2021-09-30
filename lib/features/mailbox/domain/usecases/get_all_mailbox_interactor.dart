45b5f418e2cdb073f8607ac98d83f174a632fd1e
Dat PHAM HOANG <dphamhoang@linagora.com>
2021-09-30 20:31:32 +0700

    Unit-test for GetAllMailboxRepository

--- lib/features/mailbox/domain/usecases/get_all_mailbox_interactor.dart
+++ lib/features/mailbox/domain/usecases/get_all_mailbox_interactor.dart
@@ -2,10 +2,11 @@ import 'package:core/core.dart';
 import 'package:dartz/dartz.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
+import 'package:model/model.dart';
+import 'package:tmail_ui_user/features/mailbox/data/model/mailbox_response.dart';
+import 'package:tmail_ui_user/features/mailbox/domain/extensions/list_mailbox_extension.dart';
 import 'package:tmail_ui_user/features/mailbox/domain/repository/mailbox_repository.dart';
 import 'package:tmail_ui_user/features/mailbox/domain/state/get_all_mailboxes_state.dart';
-import 'package:tmail_ui_user/features/mailbox/domain/extensions/list_mailbox_extension.dart';
-import 'package:model/model.dart';
 
 class GetAllMailboxInteractor {
   final MailboxRepository _mailboxRepository;
@@ -16,19 +17,22 @@ class GetAllMailboxInteractor {
     try {
       yield Right<Failure, Success>(LoadingState());
 
-      final streamListMailbox = _mailboxRepository.getAllMailbox(accountId, properties: properties);
-
-      final streamResult = streamListMailbox.asyncExpand((response) async* {
-        final tupleList = response.mailboxes?.splitMailboxList((mailbox) => mailbox.hasRole()) ?? Tuple2([], []);
-        yield Right<Failure, Success>(GetAllMailboxSuccess(
-          defaultMailboxList: tupleList.value1,
-          folderMailboxList: tupleList.value2,
-          currentMailboxState: response.state));
-      });
-
-      yield* streamResult;
+      yield* _mailboxRepository
+        .getAllMailbox(accountId, properties: properties)
+        .map(_toGetMailboxState);
     } catch (e) {
       yield Left<Failure, Success>(GetAllMailboxFailure(e));
     }
+  }
+
+  Either<Failure, Success> _toGetMailboxState(MailboxResponse mailboxResponse) {
+    final tupleList = mailboxResponse.mailboxes
+      ?.splitMailboxList((mailbox) => mailbox.hasRole()) ?? Tuple2([], []);
+
+    return Right<Failure, Success>(GetAllMailboxSuccess(
+      defaultMailboxList: tupleList.value1,
+      folderMailboxList: tupleList.value2,
+      currentMailboxState: mailboxResponse.state)
+    );
   }
 }
---
+++ test/fixtures/account_fixtures.dart
@@ -0,0 +1,6 @@
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
+
+class AccountFixtures {
+  static final aliceAccountId = AccountId(Id('411ce'));
+}
