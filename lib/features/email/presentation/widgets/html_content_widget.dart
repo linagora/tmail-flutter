8e8f82cfaf90a82abe858485158e5f5e2b2174b2
Võ Thanh Long <long@mission.plus>
2021-10-01 00:03:45 +0700

    update height for InAppWebView

---
+++ lib/features/email/presentation/widgets/html_content_widget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class HtmlContentView extends StatefulWidget {
+
+  HtmlContentView(
+    this.htmlContent
+  );
+  final String htmlContent;
+
+  @override
+  State<StatefulWidget> createState() {
+     return _HtmlContentViewState();
+  }
+}
+
+class _HtmlContentViewState extends State<HtmlContentView> {
+
+    InAppWebViewController? webViewController;
+    InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+        enableViewportScale: true,
+      ));
+   double webViewContentHeight = 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+
+      return Container(
+        child: InAppWebView(
+        initialOptions: options,
+        initialData: InAppWebViewInitialData(data: widget.htmlContent),
+        onWebViewCreated: (InAppWebViewController controller) { 
+          webViewController = controller; 
+        },
+        onLoadStop: (controller, uri) {
+          controller.evaluateJavascript(source: '''(() => { return document.body.scrollHeight;})()''').then((value) {
+                if(value == null || value == '') {
+                  return;
+                }
+                this.setState(() {
+                    webViewContentHeight = double.parse('$value');
+                });
+                   
+          });
+        }),
+        width: double.infinity,
+        height: webViewContentHeight,
+      );
+  }
+}
--- lib/features/email/presentation/widgets/message_content_tile_builder.dart
+++ lib/features/email/presentation/widgets/message_content_tile_builder.dart
@@ -2,13 +2,14 @@
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-// import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
 import 'package:model/model.dart';
 import 'package:tmail_ui_user/features/email/presentation/model/message_content.dart';
 import 'package:tmail_ui_user/features/email/presentation/model/text_format.dart';
+import 'package:tmail_ui_user/features/email/presentation/widgets/html_content_widget.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:sanitize_html/sanitize_html.dart' show sanitizeHtml; 
 
 class MessageContentTileBuilder {
 
@@ -17,6 +18,19 @@ class MessageContentTileBuilder {
   final Session? session;
   final AccountId? accountId;
   final HtmlMessagePurifier htmlMessagePurifier;
+  InAppWebViewController? webViewController;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+        enableViewportScale: true,
+      ));
 
   MessageContentTileBuilder({
     required this.htmlMessagePurifier,
@@ -33,43 +47,19 @@ class MessageContentTileBuilder {
         highlightColor: Colors.transparent),
       child: MediaQuery(
         data: MediaQueryData(padding: EdgeInsets.zero),
-        child: Center(
-           
-          child: messageContent.textFormat == TextFormat.PLAIN
+        child: messageContent.textFormat == TextFormat.PLAIN
             ? Text('${messageContent.content}', style: TextStyle(fontSize: 12, color: AppColor.mailboxTextColor))
-            // : HtmlWidget(
-            //     getHtmlMessageText(),
-            //     textStyle: TextStyle(fontSize: 12, color: AppColor.mailboxTextColor),
-            //     buildAsync: true,
-            //     buildAsyncBuilder: (_, snapshot) => snapshot.data ?? SizedBox(
-            //       height: 30,
-            //       width: 30,
-            //       child: CircularProgressIndicator(color: AppColor.primaryColor)),
-            //     webViewJs: false),
-            : SizedBox(
-              child:InAppWebView(
-                    initialData: InAppWebViewInitialData(
-                      data: getHtmlMessageText()
-                    ),
-                    initialOptions: InAppWebViewGroupOptions(ios: IOSInAppWebViewOptions(enableViewportScale: true)),
-                ),
-            width: double.infinity,
-            height: 500,
-            )
-        )
+            : HtmlContentView(getHtmlMessageText())
       )
     );
   }
 
   String getHtmlMessageText() {
-    final message = (attachmentInlines.isNotEmpty && session != null && accountId != null && messageContent.hasImageInlineWithCid())
+    final htmlString = (attachmentInlines.isNotEmpty && session != null && accountId != null && messageContent.hasImageInlineWithCid())
       ? '${messageContent.getContentHasInlineAttachment(session!.getDownloadUrl(), accountId!, attachmentInlines)}'
       : '${messageContent.content}';
-
-    final trustAsHtml = htmlMessagePurifier.purifyHtmlMessage(
-        message,
-        allowAttributes: {'style', 'input', 'form'});
-
-    return trustAsHtml;
+    
+    final trustedHtml = sanitizeHtml(htmlString);
+    return trustedHtml;
   }
 }
