f152f83ca27d835edb6e2e7acb2261c782d502e6
dab246 <tdvu@linagora.com>
2021-10-01 11:38:21 +0700

    TF-129 Unit-test for refresh all mailbox interactor

--- test/features/mailbox/mock/mock_mailbox_repository.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mockito/mockito.dart';
-import 'package:tmail_ui_user/features/mailbox/domain/repository/mailbox_repository.dart';
-
-class MockMailboxRepository extends Mock implements MailboxRepository {}
