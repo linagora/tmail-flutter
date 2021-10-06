9592bb6974e5a42e257e9b18db3856e050a03590
dab246 <tdvu@linagora.com>
2021-10-07 00:20:56 +0700

    TF-145 Create object EmailCache
    (cherry picked from commit f5874a7e49ecb37cb2b6b15aa43e90011dd9e9c3)

---
+++ lib/features/thread/data/model/email_address_hive_cache.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
+import 'package:tmail_ui_user/features/caching/utils/caching_constants.dart';
+
+part 'email_address_hive_cache.g.dart';
+
+@HiveType(typeId: CachingConstants.EMAIL_ADDRESS_HIVE_CACHE_IDENTIFY)
+class EmailAddressHiveCache extends HiveObject with EquatableMixin {
+
+  @HiveField(0)
+  final String? name;
+
+  @HiveField(1)
+  final String? email;
+
+  EmailAddressHiveCache(this.name, this.email);
+
+  @override
+  List<Object?> get props => [name, email];
+}
