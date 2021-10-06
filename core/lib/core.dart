28bf691fa894b45214fe104f2c30ef6e2ac2b3aa
dab246 <tdvu@linagora.com>
2021-10-06 09:56:54 +0700

    TF-105 Create HtmlContentViewer widget
    (cherry picked from commit a9d83852d0db06c286abe6e8c93140bfdc453420)

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -38,6 +38,7 @@ export 'presentation/views/context_menu/simple_context_menu_action_builder.dart'
 export 'presentation/views/dialog/loading_dialog_builder.dart';
 export 'presentation/views/dialog/downloading_file_dialog_builder.dart';
 export 'presentation/views/background/background_widget_builder.dart';
+export 'presentation/views/html_viewer/html_content_viewer_widget.dart';
 
 // Resources
 export 'presentation/resources/assets_paths.dart';
