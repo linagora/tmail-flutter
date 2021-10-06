b9ef8e3adbed42c8e200907f3b960ab5ac8bad32
dab246 <tdvu@linagora.com>
2021-10-07 00:20:56 +0700

    TF-145 Create object EmailCache
    (cherry picked from commit f5874a7e49ecb37cb2b6b15aa43e90011dd9e9c3)

--- lib/features/mailbox/data/model/mailbox_rights_cache.dart
+++ lib/features/mailbox/data/model/mailbox_rights_cache.dart
@@ -1,7 +1,7 @@
 
 import 'package:equatable/equatable.dart';
 import 'package:hive/hive.dart';
-import 'package:tmail_ui_user/features/mailbox/data/utils/caching_constants.dart';
+import 'package:tmail_ui_user/features/caching/utils/caching_constants.dart';
 
 part 'mailbox_rights_cache.g.dart';
 
---
+++ lib/features/thread/data/model/email_cache.dart
@@ -0,0 +1,85 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
+import 'package:tmail_ui_user/features/caching/utils/caching_constants.dart';
+import 'package:tmail_ui_user/features/thread/data/model/email_address_hive_cache.dart';
+
+part 'email_cache.g.dart';
+
+@HiveType(typeId: CachingConstants.EMAIL_CACHE_IDENTIFY)
+class EmailCache extends HiveObject with EquatableMixin {
+
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final Map<String, bool>? keywords;
+
+  @HiveField(2)
+  final int? size;
+
+  @HiveField(3)
+  final DateTime? receivedAt;
+
+  @HiveField(4)
+  final bool? hasAttachment;
+
+  @HiveField(5)
+  final String? preview;
+
+  @HiveField(6)
+  final String? subject;
+
+  @HiveField(7)
+  final DateTime? sentAt;
+
+  @HiveField(8)
+  final List<EmailAddressHiveCache>? from;
+
+  @HiveField(9)
+  final List<EmailAddressHiveCache>? to;
+
+  @HiveField(10)
+  final List<EmailAddressHiveCache>? cc;
+
+  @HiveField(11)
+  final List<EmailAddressHiveCache>? bcc;
+
+  @HiveField(12)
+  final List<EmailAddressHiveCache>? replyTo;
+
+  EmailCache(
+    this.id,
+    {
+      this.keywords,
+      this.size,
+      this.receivedAt,
+      this.hasAttachment,
+      this.preview,
+      this.subject,
+      this.sentAt,
+      this.from,
+      this.to,
+      this.cc,
+      this.bcc,
+      this.replyTo,
+    }
+  );
+
+  @override
+  List<Object?> get props => [
+    id,
+    subject,
+    from,
+    to,
+    cc,
+    bcc,
+    keywords,
+    size,
+    receivedAt,
+    sentAt,
+    replyTo,
+    preview,
+    hasAttachment
+  ];
+}
