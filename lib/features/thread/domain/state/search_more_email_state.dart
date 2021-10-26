17eeefbb39828286d9b099ce2ad76efdc474a3d3
dab246 <tdvu@linagora.com>
2021-10-26 13:59:25 +0700

    TF-138 Implement load more for search email

---
+++ lib/features/thread/domain/state/search_more_email_state.dart
@@ -0,0 +1,30 @@
+
+import 'package:core/core.dart';
+import 'package:model/email/presentation_email.dart';
+
+class SearchingMoreState extends UIState {
+
+  SearchingMoreState();
+
+  @override
+  List<Object> get props => [];
+}
+
+
+class SearchMoreEmailSuccess extends UIState {
+  final List<PresentationEmail> emailList;
+
+  SearchMoreEmailSuccess(this.emailList);
+
+  @override
+  List<Object> get props => [emailList];
+}
+
+class SearchMoreEmailFailure extends FeatureFailure {
+  final exception;
+
+  SearchMoreEmailFailure(this.exception);
+
+  @override
+  List<Object> get props => [exception];
+}
