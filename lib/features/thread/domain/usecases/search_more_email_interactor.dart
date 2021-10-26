27223d3e231e177794d010296d0987f97268ad5b
dab246 <tdvu@linagora.com>
2021-10-26 13:59:25 +0700

    TF-138 Implement load more for search email
    (cherry picked from commit 17eeefbb39828286d9b099ce2ad76efdc474a3d3)

---
+++ lib/features/thread/domain/usecases/search_more_email_interactor.dart
@@ -0,0 +1,50 @@
+
+import 'package:core/core.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:model/model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/filter/filter.dart';
+import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
+import 'package:jmap_dart_client/jmap/core/unsigned_int.dart';
+import 'package:jmap_dart_client/jmap/core/sort/comparator.dart';
+import 'package:tmail_ui_user/features/thread/domain/repository/thread_repository.dart';
+import 'package:tmail_ui_user/features/thread/domain/state/search_more_email_state.dart';
+
+class SearchMoreEmailInteractor {
+
+  final ThreadRepository threadRepository;
+
+  SearchMoreEmailInteractor(this.threadRepository);
+
+  Stream<Either<Failure, Success>> execute(
+    AccountId accountId,
+    {
+      UnsignedInt? limit,
+      Set<Comparator>? sort,
+      Filter? filter,
+      Properties? properties,
+      EmailId? lastEmailId
+    }
+  ) async* {
+    try {
+      yield Right(SearchingMoreState());
+
+      final emailList = await threadRepository.searchEmails(
+        accountId,
+        limit: limit,
+        sort: sort,
+        filter: filter,
+        properties: properties);
+
+      final presentationEmailList = emailList
+        .where((email) => email.id != lastEmailId)
+        .map((email) => email.toPresentationEmail())
+        .toList();
+
+      yield Right(SearchMoreEmailSuccess(presentationEmailList));
+    } catch (e) {
+      yield Left(SearchMoreEmailFailure(e));
+    }
+  }
+}
