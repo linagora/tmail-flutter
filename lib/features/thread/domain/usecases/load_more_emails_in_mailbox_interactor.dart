ac0c905bdac2dae6ab5e346311e4094da899857c
dab246 <tdvu@linagora.com>
2021-10-27 13:53:52 +0700

    [BUG] Fix duplicate load more emails
    (cherry picked from commit 32b8e714a32ab81bd973d6f55c383ba5931612c1)

--- lib/features/thread/domain/usecases/load_more_emails_in_mailbox_interactor.dart
+++ lib/features/thread/domain/usecases/load_more_emails_in_mailbox_interactor.dart
@@ -5,6 +5,7 @@ import 'package:jmap_dart_client/jmap/core/filter/filter.dart';
 import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
 import 'package:jmap_dart_client/jmap/core/sort/comparator.dart';
 import 'package:jmap_dart_client/jmap/core/unsigned_int.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email.dart';
 import 'package:tmail_ui_user/features/thread/domain/model/email_response.dart';
 import 'package:tmail_ui_user/features/thread/domain/repository/thread_repository.dart';
 import 'package:model/model.dart';
@@ -21,7 +22,8 @@ class LoadMoreEmailsInMailboxInteractor {
       UnsignedInt? limit,
       Set<Comparator>? sort,
       Filter? filter,
-      Properties? properties
+      Properties? properties,
+      EmailId? lastEmailId,
     }
   ) async* {
     try {
@@ -33,7 +35,8 @@ class LoadMoreEmailsInMailboxInteractor {
           limit: limit,
           sort: sort,
           filter: filter,
-          properties: properties)
+          properties: properties,
+          lastEmailId: lastEmailId)
         .map(_toGetEmailState);
     } catch (e) {
       yield Left(LoadMoreEmailsFailure(e));
