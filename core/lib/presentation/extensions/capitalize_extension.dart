4526107004601c410a60b1425b0f0b513657858b
dab246 <tdvu@linagora.com>
2022-02-08 18:00:56 +0700

    TF-248 Update AppBar of ThreadView
    (cherry picked from commit 4a49ce81502a75b6f345c4952f288bab81d2eb51)

--- core/lib/presentation/extensions/capitalize_extension.dart
+++ core/lib/presentation/extensions/capitalize_extension.dart
@@ -1,6 +1,6 @@
 
 extension CapitalizeExtension on String {
-  String get inCaps => this.length > 0 ?'${this[0].toUpperCase()}${this.substring(1)}':'';
+  String get inCaps => this.length > 0 ?'${this[0].toUpperCase()}${this.toLowerCase().substring(1)}':'';
   String get allInCaps => this.toUpperCase();
   String get capitalizeFirstEach => this.replaceAll(RegExp(' +'), ' ').split(" ").map((str) => str.inCaps).join(" ");
 }
