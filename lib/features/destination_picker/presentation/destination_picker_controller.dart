ebb170399cf2ad5f4b707ebf5f595c593bdb4ead
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-03-11 14:19:07 +0700

    TF-189 Refactor destination picker components: observe only Tree variable
    (cherry picked from commit 19aacb27898eb3d7c382b53d92ea6451654a4386)

--- lib/features/destination_picker/presentation/destination_picker_controller.dart
+++ lib/features/destination_picker/presentation/destination_picker_controller.dart
@@ -1,35 +1,27 @@
 
 import 'package:core/core.dart';
 import 'package:dartz/dartz.dart';
+import 'package:get/get.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:model/model.dart';
-import 'package:tmail_ui_user/features/base/base_controller.dart';
+import 'package:tmail_ui_user/features/base/base_mailbox_controller.dart';
 import 'package:tmail_ui_user/features/destination_picker/presentation/model/destination_picker_arguments.dart';
 import 'package:tmail_ui_user/features/mailbox/domain/state/get_all_mailboxes_state.dart';
 import 'package:tmail_ui_user/features/mailbox/domain/usecases/get_all_mailbox_interactor.dart';
 import 'package:tmail_ui_user/features/mailbox/presentation/model/mailbox_actions.dart';
-import 'package:tmail_ui_user/features/mailbox/presentation/model/mailbox_node.dart';
-import 'package:tmail_ui_user/features/mailbox/presentation/model/mailbox_tree.dart';
-import 'package:tmail_ui_user/features/mailbox/presentation/model/mailbox_tree_builder.dart';
-import 'package:tmail_ui_user/features/mailbox/presentation/extensions/list_mailbox_node_extension.dart';
-import 'package:get/get.dart';
 import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 
-class DestinationPickerController extends BaseController {
+class DestinationPickerController extends BaseMailboxController {
 
   final GetAllMailboxInteractor _getAllMailboxInteractor;
-  final TreeBuilder _treeBuilder;
 
-  MailboxTree folderMailboxTree = MailboxTree(MailboxNode.root());
-  final defaultMailboxList = <PresentationMailbox>[].obs;
-  final folderMailboxNodeList = <MailboxNode>[].obs;
   final mailboxAction = Rxn<MailboxActions>();
   AccountId? accountId;
 
   DestinationPickerController(
     this._getAllMailboxInteractor,
-    this._treeBuilder,
-  );
+    treeBuilder,
+  ) : super(treeBuilder);
 
   @override
   void onReady() {
@@ -47,18 +39,13 @@ class DestinationPickerController extends BaseController {
     super.onData(newState);
     newState.map((success) {
       if (success is GetAllMailboxSuccess) {
-        _buildTree(success.folderMailboxList);
+        buildTree(success.mailboxList);
       }
     });
   }
 
   @override
   void onDone() {
-    viewState.value.map((success) {
-      if (success is GetAllMailboxSuccess) {
-        defaultMailboxList.value = success.defaultMailboxList;
-      }
-    });
   }
 
   @override
@@ -68,23 +55,6 @@ class DestinationPickerController extends BaseController {
     if (accountId != null) {
       consumeState(_getAllMailboxInteractor.execute(accountId!));
     }
-  }
-
-  void _buildTree(List<PresentationMailbox> folderMailboxList) async {
-    folderMailboxTree = await _treeBuilder.generateMailboxTree(folderMailboxList);
-    folderMailboxNodeList.value = folderMailboxTree.root.childrenItems ?? [];
-  }
-
-  void toggleMailboxFolder(MailboxNode selectedMailboxNode) {
-    final newExpandMode = selectedMailboxNode.expandMode == ExpandMode.COLLAPSE
-      ? ExpandMode.EXPAND
-      : ExpandMode.COLLAPSE;
-
-    final newMailboxNodeList = folderMailboxNodeList.updateNode(
-        selectedMailboxNode.item.id,
-        selectedMailboxNode.copyWith(newExpandMode: newExpandMode));
-
-    folderMailboxNodeList.value = newMailboxNodeList;
   }
 
   void selectMailboxAction(PresentationMailbox? destinationMailbox) {
