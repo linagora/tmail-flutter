af8af232cedef7b727abfdaa778958c774eabc87
dab246 <tdvu@linagora.com>
2022-03-25 16:55:56 +0700

    TF-348 Fix preserve the selection state when opening an email
    (cherry picked from commit f639fdc0d8789dbb5960d1a56332d59b5757f10d)

--- model/lib/extensions/list_presentation_email_extension.dart
+++ model/lib/extensions/list_presentation_email_extension.dart
@@ -1,5 +1,6 @@
 
-import 'package:model/email/presentation_email.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:model/model.dart';
 
 extension ListPresentationEmailExtension on List<PresentationEmail> {
   bool get isAllEmailRead => every((email) => email.isReadEmail());
@@ -7,4 +8,23 @@ extension ListPresentationEmailExtension on List<PresentationEmail> {
   bool get isAllEmailStarred => every((email) => email.isFlaggedEmail());
 
   List<PresentationEmail> get allEmailUnread => where((email) => email.isUnReadEmail()).toList();
+
+  PresentationEmail? findEmail(EmailId emailId) {
+    try {
+      return firstWhere((email) => email.id == emailId);
+    } catch(e) {
+      return null;
+    }
+  }
+
+  List<PresentationEmail> combine(List<PresentationEmail> listEmailBefore)  {
+    return map((presentationEmail) {
+      final emailBefore = listEmailBefore.findEmail(presentationEmail.id);
+      if (emailBefore != null) {
+        return presentationEmail.toSelectedEmail(selectMode: emailBefore.selectMode);
+      } else {
+        return presentationEmail;
+      }
+    }).toList();
+  }
 }
