d96a59a560bd4dc89cb1eed67f5ab97b31fbe1ee
dab246 <tdvu@linagora.com>
2022-03-29 18:37:34 +0700

    TF-333 Create 2 mailbox with the same name in MailboxCreatorView
    (cherry picked from commit 869053c6eb3886a99c5b8c0e4cdc0af244fd890d)

--- lib/features/mailbox_creator/presentation/model/mailbox_creator_arguments.dart
+++ lib/features/mailbox_creator/presentation/model/mailbox_creator_arguments.dart
@@ -1,14 +1,15 @@
 
 import 'package:equatable/equatable.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
-import 'package:model/mailbox/presentation_mailbox.dart';
+import 'package:tmail_ui_user/features/mailbox/presentation/model/mailbox_tree.dart';
 
 class MailboxCreatorArguments with EquatableMixin{
   final AccountId accountId;
-  final List<PresentationMailbox> allMailboxes;
+  final MailboxTree folderMailboxTree;
+  final MailboxTree defaultMailboxTree;
 
-  MailboxCreatorArguments(this.accountId, this.allMailboxes);
+  MailboxCreatorArguments(this.accountId, this.defaultMailboxTree, this.folderMailboxTree);
 
   @override
-  List<Object?> get props => [accountId, allMailboxes];
+  List<Object?> get props => [accountId, defaultMailboxTree, folderMailboxTree];
 }
