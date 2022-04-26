4d6fdca187893d17deea53e0af9f423b3f6da488
dab246 <tdvu@linagora.com>
2022-04-26 09:30:38 +0700

    TF-460 Add domain layer for implement get all identities

---
+++ lib/features/manage_account/domain/repository/manage_account_repository.dart
@@ -0,0 +1,6 @@
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/identities_response.dart';
+
+abstract class ManageAccountRepository {
+  Future<IdentitiesResponse> getAllIdentities(AccountId accountId);
+}
