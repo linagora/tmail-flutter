f3a0c8f7fea1be47d52b153ad395b0a269cc1874
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-04-26 19:37:40 +0700

    Clean up the cache whenever start the app
    (cherry picked from commit ff9bc6d7e5b23538011f7eacf44b05e6ac509138)

--- lib/features/cleanup/data/repository/cleanup_repository_impl.dart
+++ lib/features/cleanup/data/repository/cleanup_repository_impl.dart
@@ -1,5 +1,4 @@
 
-import 'package:core/core.dart';
 import 'package:tmail_ui_user/features/cleanup/data/datasource/cleanup_datasource.dart';
 import 'package:tmail_ui_user/features/cleanup/domain/model/cleanup_rule.dart';
 import 'package:tmail_ui_user/features/cleanup/domain/repository/cleanup_repository.dart';
@@ -12,12 +11,9 @@ class CleanupRepositoryImpl extends CleanupRepository {
 
   @override
   Future<void> cleanEmailCache(CleanupRule cleanupRule) async {
-    final lastTimeCleanup = await cleanupDataSource.getTimeCleanEmail();
-    if (lastTimeCleanup == null || !lastTimeCleanup.isToday()) {
-      final isSuccess = await cleanupDataSource.cleanEmailCache(cleanupRule);
-      if (isSuccess) {
-        await cleanupDataSource.saveTimeCleanEmail(DateTime.now());
-      }
+    final isSuccess = await cleanupDataSource.cleanEmailCache(cleanupRule);
+    if (isSuccess) {
+      await cleanupDataSource.saveTimeCleanEmail(DateTime.now());
     }
   }
 }
