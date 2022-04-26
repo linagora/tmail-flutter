64eafaab74b7705bb87636a94463f2f73575e560
dab246 <tdvu@linagora.com>
2022-04-26 09:30:45 +0700

    TF-460 Add data layer for implement get all identities
    (cherry picked from commit 4764f9eb901bef231b891812ee6a9388b867aba6)

---
+++ lib/features/manage_account/data/repository/manage_account_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:tmail_ui_user/features/manage_account/data/datasource/manage_account_datasource.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/identities_response.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/repository/manage_account_repository.dart';
+
+class ManageAccountRepositoryImpl extends ManageAccountRepository {
+
+  final ManageAccountDataSource dataSource;
+
+  ManageAccountRepositoryImpl(this.dataSource);
+
+  @override
+  Future<IdentitiesResponse> getAllIdentities(AccountId accountId) {
+    return dataSource.getAllIdentities(accountId);
+  }
+}
