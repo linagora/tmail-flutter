4bb812ee2a37de539253325499bcec3001a6a829
dab246 <tdvu@linagora.com>
2022-05-05 15:07:52 +0700

    TF-537 Add domain layer for delete a identity
    (cherry picked from commit 3e4908ab9804a8999a61b0bf837f503207b0a61a)

--- lib/features/manage_account/domain/repository/manage_account_repository.dart
+++ lib/features/manage_account/domain/repository/manage_account_repository.dart
@@ -1,6 +1,9 @@
 import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/identities/identity.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/identities_response.dart';
 
 abstract class ManageAccountRepository {
   Future<IdentitiesResponse> getAllIdentities(AccountId accountId);
+
+  Future<bool> deleteIdentity(AccountId accountId, IdentityId identityId);
 }
