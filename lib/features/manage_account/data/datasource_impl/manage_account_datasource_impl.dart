754d716af39b2a063ca70fb56b7e93f17997bef1
dab246 <tdvu@linagora.com>
2022-05-05 09:09:39 +0700

    TF-527 Add data layer in add new identity
    (cherry picked from commit 32f99fcca275a44f4ce8ad2bd6198cb494e6b492)

--- lib/features/manage_account/data/datasource_impl/manage_account_datasource_impl.dart
+++ lib/features/manage_account/data/datasource_impl/manage_account_datasource_impl.dart
@@ -2,6 +2,7 @@ import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/identities/identity.dart';
 import 'package:tmail_ui_user/features/manage_account/data/datasource/manage_account_datasource.dart';
 import 'package:tmail_ui_user/features/manage_account/data/network/manage_account_api.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_identity_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/identities_response.dart';
 
 class ManageAccountDataSourceImpl extends ManageAccountDataSource {
@@ -14,6 +15,15 @@ class ManageAccountDataSourceImpl extends ManageAccountDataSource {
   Future<IdentitiesResponse> getAllIdentities(AccountId accountId) {
     return Future.sync(() async {
       return await manageAccountAPI.getAllIdentities(accountId);
+    }).catchError((error) {
+      throw error;
+    });
+  }
+
+  @override
+  Future<Identity> createNewIdentity(AccountId accountId, CreateNewIdentityRequest identityRequest) {
+    return Future.sync(() async {
+      return await manageAccountAPI.createNewIdentity(accountId, identityRequest);
     }).catchError((error) {
       throw error;
     });
