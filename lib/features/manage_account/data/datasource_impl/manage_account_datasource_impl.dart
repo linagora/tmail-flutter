31279df9a3ab233324f9f6cd1a2cdb109c33bb73
dab246 <tdvu@linagora.com>
2022-05-05 15:07:57 +0700

    TF-537 Add data layer for delete a identity
    (cherry picked from commit c40be824ff2baa20281f8e77e65f7949045d472b)

--- lib/features/manage_account/data/datasource_impl/manage_account_datasource_impl.dart
+++ lib/features/manage_account/data/datasource_impl/manage_account_datasource_impl.dart
@@ -1,4 +1,5 @@
 import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/identities/identity.dart';
 import 'package:tmail_ui_user/features/manage_account/data/datasource/manage_account_datasource.dart';
 import 'package:tmail_ui_user/features/manage_account/data/network/manage_account_api.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/identities_response.dart';
@@ -13,6 +14,15 @@ class ManageAccountDataSourceImpl extends ManageAccountDataSource {
   Future<IdentitiesResponse> getAllIdentities(AccountId accountId) {
     return Future.sync(() async {
       return await manageAccountAPI.getAllIdentities(accountId);
+    }).catchError((error) {
+      throw error;
+    });
+  }
+
+  @override
+  Future<bool> deleteIdentity(AccountId accountId, IdentityId identityId) {
+    return Future.sync(() async {
+      return await manageAccountAPI.deleteIdentity(accountId, identityId);
     }).catchError((error) {
       throw error;
     });
