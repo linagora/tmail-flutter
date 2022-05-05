32f99fcca275a44f4ce8ad2bd6198cb494e6b492
dab246 <tdvu@linagora.com>
2022-05-05 09:09:39 +0700

    TF-527 Add data layer in add new identity

--- lib/features/manage_account/data/datasource/manage_account_datasource.dart
+++ lib/features/manage_account/data/datasource/manage_account_datasource.dart
@@ -1,9 +1,13 @@
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/identities/identity.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_identity_request.dart';
+import 'package:jmap_dart_client/jmap/identities/identity.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/identities_response.dart';
 
 abstract class ManageAccountDataSource {
   Future<IdentitiesResponse> getAllIdentities(AccountId accountId);
+
+  Future<Identity> createNewIdentity(AccountId accountId, CreateNewIdentityRequest identityRequest);
 
   Future<bool> deleteIdentity(AccountId accountId, IdentityId identityId);
 }
