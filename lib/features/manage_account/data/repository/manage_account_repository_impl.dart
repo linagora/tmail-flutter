c44bf1f075601dbc33e502a3a4352e61ddb03fbb
dab246 <tdvu@linagora.com>
2022-05-06 15:13:27 +0700

    TF-536 Add data layer for edit identity
    (cherry picked from commit 15de506b974aa39de80442177ce86d20a1571601)

--- lib/features/manage_account/data/repository/manage_account_repository_impl.dart
+++ lib/features/manage_account/data/repository/manage_account_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/identities/identity.dart';
 import 'package:tmail_ui_user/features/manage_account/data/datasource/manage_account_datasource.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_identity_request.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/edit_identity_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/identities_response.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/repository/manage_account_repository.dart';
 
@@ -24,5 +25,10 @@ class ManageAccountRepositoryImpl extends ManageAccountRepository {
   @override
   Future<bool> deleteIdentity(AccountId accountId, IdentityId identityId) {
     return dataSource.deleteIdentity(accountId, identityId);
+  }
+
+  @override
+  Future<Identity> editIdentity(AccountId accountId, EditIdentityRequest identityRequest) {
+    return dataSource.editIdentity(accountId, identityRequest);
   }
 }
