47be42e6813a6c4f922e6ff0c074afcbd0490a86
dab246 <tdvu@linagora.com>
2022-05-12 13:48:43 +0700

    TF-548 Implement datasource for getAutoComplete
    (cherry picked from commit ae830544dee658b010fa7aa4b78d2166a2939343)

---
+++ contact/test/datasource/tmail_contact_datasource_impl_test.dart
@@ -0,0 +1,51 @@
+
+import 'package:contact/contact/model/tmail_contact.dart';
+import 'package:contact/data/datasource_impl/tmail_contact_datasource_impl.dart';
+import 'package:contact/data/network/contact_api.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
+import 'tmail_contact_datasource_impl_test.mocks.dart';
+
+@GenerateMocks([ContactAPI])
+void main() {
+  final contact1 = TMailContact(
+      '2',
+      '',
+      '',
+      'marie@otherdomain.tld'
+  );
+
+  final contact2 = TMailContact(
+      '4',
+      'Marie',
+      'Dupond',
+      'mdupond@linagora.com'
+  );
+
+  group('tmail_contact_datasource_impl_test', () {
+    late ContactAPI _contactAPI;
+    late TMailContactDataSourceImpl _tmailContactDataSourceImpl;
+
+    setUp(() {
+      _contactAPI = MockContactAPI();
+      _tmailContactDataSourceImpl = TMailContactDataSourceImpl(_contactAPI);
+    });
+
+    test('getAutoComplete should return success with valid data', () async {
+      when(_contactAPI.getAutoComplete(
+          AccountId(Id('29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6')),
+          'marie'
+      )).thenAnswer((_) async => [contact1, contact2]);
+
+      final result = await _tmailContactDataSourceImpl.getAutoComplete(
+          AccountId(Id('29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6')),
+          'marie'
+      );
+      expect(result, [contact1, contact2]);
+    });
+  });
+}
