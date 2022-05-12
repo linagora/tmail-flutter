fa8aff78a76542af3d5caff3f94b5cc0ac5efb19
dab246 <tdvu@linagora.com>
2022-05-12 12:42:32 +0700

    TF-548 Add AutoComplete method/response for TMailContact

---
+++ contact/lib/contact_module.dart
@@ -0,0 +1,8 @@
+library contact_module;
+
+export 'package:contact/contact/model/contact.dart';
+export 'package:contact/contact/model/contact_filter.dart';
+export 'package:contact/contact/model/tmail_contact.dart';
+
+export 'package:contact/contact/autocomplete/autocomplete_tmail_contact_method.dart';
+export 'package:contact/contact/autocomplete/autocomplete_tmail_contact_response.dart';
