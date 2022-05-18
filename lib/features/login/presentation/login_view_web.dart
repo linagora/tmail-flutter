d1bbaae2123237cf783bbe185efb509501d135d9
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-05-18 15:52:45 +0700

    Add padding for the login form in web
    (cherry picked from commit a16fab5562b563e4324893b4231e487cc1fd0431)

--- lib/features/login/presentation/login_view_web.dart
+++ lib/features/login/presentation/login_view_web.dart
@@ -72,73 +72,75 @@ class LoginView extends BaseLoginView {
   Widget _buildWebForm(BuildContext context) {
     return Stack(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 86),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    AppLocalizations.of(context).jmapBasedMailSolution,
-                    style: const TextStyle(fontSize: 36, color: AppColor.colorNameEmail, fontWeight: FontWeight.w900)
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 24),
-                    child: (SloganBuilder(arrangedByHorizontal: true, )
-                        ..setLogoSVG(imagePaths.icJMAPStandard)
-                        ..setSizeLogo(48.0)
-                        ..setPadding(const EdgeInsets.only(left: 12))
-                        ..setSloganText(AppLocalizations.of(context).jmapStandard)
-                        ..setSloganTextStyle(const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, color: AppColor.colorNameEmail)))
-                      .build()
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: (SloganBuilder(arrangedByHorizontal: true, )
-                        ..setLogoSVG(imagePaths.icEncrypted)
-                        ..setSizeLogo(48.0)
-                        ..setPadding(const EdgeInsets.only(left: 12))
-                        ..setSloganText(AppLocalizations.of(context).encryptedMailbox)
-                        ..setSloganTextStyle(const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, color: AppColor.colorNameEmail)))
-                      .build()
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: (SloganBuilder(arrangedByHorizontal: true, )
-                        ..setLogoSVG(imagePaths.icTeam)
-                        ..setSizeLogo(48.0)
-                        ..setPadding(const EdgeInsets.only(left: 12))
-                        ..setSloganText(AppLocalizations.of(context).manageEmailAsATeam)
-                        ..setSloganTextStyle(const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, color: AppColor.colorNameEmail)))
-                      .build()
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: (SloganBuilder(arrangedByHorizontal: true, )
-                        ..setLogoSVG(imagePaths.icIntegration)
-                        ..setSizeLogo(48.0)
-                        ..setPadding(const EdgeInsets.only(left: 12))
-                        ..setSloganText(AppLocalizations.of(context).multipleIntegrations)
-                        ..setSloganTextStyle(const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, color: AppColor.colorNameEmail)))
-                      .build()
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 44),
-                    child: Image(
-                      image: AssetImage(imagePaths.loginGraphic),
-                      fit: BoxFit.fill,
-                      alignment: Alignment.center
+        Padding(
+          padding: const EdgeInsets.only(top: 60, bottom: 60),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 86),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context).jmapBasedMailSolution,
+                      style: const TextStyle(fontSize: 36, color: AppColor.colorNameEmail, fontWeight: FontWeight.w900)
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 24),
+                      child: (SloganBuilder(arrangedByHorizontal: true, )
+                          ..setLogoSVG(imagePaths.icJMAPStandard)
+                          ..setSizeLogo(48.0)
+                          ..setPadding(const EdgeInsets.only(left: 12))
+                          ..setSloganText(AppLocalizations.of(context).jmapStandard)
+                          ..setSloganTextStyle(const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, color: AppColor.colorNameEmail)))
+                        .build()
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: (SloganBuilder(arrangedByHorizontal: true, )
+                          ..setLogoSVG(imagePaths.icEncrypted)
+                          ..setSizeLogo(48.0)
+                          ..setPadding(const EdgeInsets.only(left: 12))
+                          ..setSloganText(AppLocalizations.of(context).encryptedMailbox)
+                          ..setSloganTextStyle(const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, color: AppColor.colorNameEmail)))
+                        .build()
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: (SloganBuilder(arrangedByHorizontal: true, )
+                          ..setLogoSVG(imagePaths.icTeam)
+                          ..setSizeLogo(48.0)
+                          ..setPadding(const EdgeInsets.only(left: 12))
+                          ..setSloganText(AppLocalizations.of(context).manageEmailAsATeam)
+                          ..setSloganTextStyle(const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, color: AppColor.colorNameEmail)))
+                        .build()
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: (SloganBuilder(arrangedByHorizontal: true, )
+                          ..setLogoSVG(imagePaths.icIntegration)
+                          ..setSizeLogo(48.0)
+                          ..setPadding(const EdgeInsets.only(left: 12))
+                          ..setSloganText(AppLocalizations.of(context).multipleIntegrations)
+                          ..setSloganTextStyle(const TextStyle(fontSize: 24, fontWeight: FontWeight.w400, color: AppColor.colorNameEmail)))
+                        .build()
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 44),
+                      child: Image(
+                        image: AssetImage(imagePaths.loginGraphic),
+                        fit: BoxFit.fill,
+                        alignment: Alignment.center
+                      )
                     )
-                  )
-                ],
-              )
-            ),
-            Column(
+                  ],
+                )
+              ),
+              Column(
               children: [
                 SizedBox(
                   height: 690,
@@ -184,7 +186,8 @@ class LoginView extends BaseLoginView {
                   child: SvgPicture.asset(imagePaths.icPowerByLinagora, width: 97, height: 44, fit: BoxFit.fill))
               ]
             )
-          ],
+            ],
+          )
         )
       ],
     );
