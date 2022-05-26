75e50ece269d88a8ea09b0e2a81da22fe60c0dc4
dab246 <tdvu@linagora.com>
2022-05-26 16:29:22 +0700

    TF-583 Disable scroll bar in email view
    (cherry picked from commit 299fde93f3d942e4369b3593a0829beb301dc20f)

--- core/lib/presentation/views/html_viewer/html_content_viewer_widget.dart
+++ core/lib/presentation/views/html_viewer/html_content_viewer_widget.dart
@@ -55,7 +55,7 @@ class _HtmlContentViewState extends State<HtmlContentViewer> {
   }
 
   String _generateHtmlDocument(String content) {
-    final htmlTemplate = generateHtml(content);
+    final htmlTemplate = generateHtml(content, hideScrollBar: true);
     return htmlTemplate;
   }
 
