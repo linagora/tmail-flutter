fc5f6aa9d41010310de48712b05a5bc467871ea0
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-05-31 14:36:44 +0700

    TF-571 Injection and configuration for Hive cache
    (cherry picked from commit 8c96a486d135ffa931217dc428e0353736d5e6e0)

--- lib/features/caching/utils/caching_constants.dart
+++ lib/features/caching/utils/caching_constants.dart
@@ -7,4 +7,6 @@ class CachingConstants {
   static const int EMAIL_CACHE_IDENTIFY = 5;
   static const int EMAIL_ADDRESS_HIVE_CACHE_IDENTIFY = 6;
   static const int RECENT_SEARCH_HIVE_CACHE_IDENTIFY = 7;
+  static const int TOKEN_OIDC_HIVE_CACHE_IDENTIFY = 8;
+  static const int ACCOUNT_HIVE_CACHE_IDENTIFY = 9;
 }
