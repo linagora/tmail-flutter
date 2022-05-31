328ff2c4f09e3bd4f2d51aeba5616ec3badf6939
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-05-31 14:32:07 +0700

    TF-571 Store account after authenticate user with basicAuth

--- lib/features/login/domain/usecases/get_credential_interactor.dart
+++ lib/features/login/domain/usecases/get_credential_interactor.dart
@@ -1,10 +1,10 @@
+import 'dart:core';
+
 import 'package:core/core.dart';
 import 'package:dartz/dartz.dart';
 import 'package:tmail_ui_user/features/login/domain/exceptions/authentication_exception.dart';
-import 'dart:core';
-
-import 'package:tmail_ui_user/features/login/domain/repository/credential_repository.dart';
 import 'package:tmail_ui_user/features/login/domain/extensions/uri_extension.dart';
+import 'package:tmail_ui_user/features/login/domain/repository/credential_repository.dart';
 import 'package:tmail_ui_user/features/login/domain/state/get_credential_state.dart';
 
 class GetCredentialInteractor {
@@ -20,7 +20,7 @@ class GetCredentialInteractor {
       if (isCredentialValid(baseUrl)) {
         return Right(GetCredentialViewState(baseUrl, userName, password));
       } else {
-        return Left(GetCredentialFailure(BadCredentials()));
+        return Left(GetCredentialFailure(const BadCredentials()));
       }
     } catch (exception) {
       return Left(GetCredentialFailure(exception));
