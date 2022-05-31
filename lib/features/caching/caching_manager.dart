8c96a486d135ffa931217dc428e0353736d5e6e0
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-05-31 14:36:44 +0700

    TF-571 Injection and configuration for Hive cache

--- lib/features/caching/caching_manager.dart
+++ lib/features/caching/caching_manager.dart
@@ -1,9 +1,11 @@
 
 import 'package:flutter/foundation.dart';
+import 'package:tmail_ui_user/features/caching/account_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/email_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/mailbox_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/recent_search_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/state_cache_client.dart';
+import 'package:tmail_ui_user/features/caching/token_oidc_cache_client.dart';
 
 class CachingManager {
 
@@ -11,12 +13,16 @@ class CachingManager {
   final StateCacheClient _stateCacheClient;
   final EmailCacheClient _emailCacheClient;
   final RecentSearchCacheClient _recentSearchCacheClient;
+  final TokenOidcCacheClient _tokenOidcCacheClient;
+  final AccountCacheClient _accountCacheClient;
 
   CachingManager(
     this._mailboxCacheClient,
     this._stateCacheClient,
     this._emailCacheClient,
     this._recentSearchCacheClient,
+    this._tokenOidcCacheClient,
+    this._accountCacheClient
   );
 
   Future<void> clearAll() async {
@@ -26,6 +32,8 @@ class CachingManager {
         _mailboxCacheClient.clearAllData(),
         _emailCacheClient.clearAllData(),
         _recentSearchCacheClient.clearAllData(),
+        _tokenOidcCacheClient.clearAllData(),
+        _accountCacheClient.clearAllData()
       ]);
     } else {
       await Future.wait([
@@ -33,6 +41,8 @@ class CachingManager {
         _mailboxCacheClient.deleteBox(),
         _emailCacheClient.deleteBox(),
         _recentSearchCacheClient.deleteBox(),
+        _tokenOidcCacheClient.deleteBox(),
+        _accountCacheClient.deleteBox()
       ]);
     }
   }
--- lib/main/bindings/local/local_bindings.dart
+++ lib/main/bindings/local/local_bindings.dart
@@ -1,12 +1,16 @@
 
 import 'package:core/core.dart';
 import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/caching/account_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/caching_manager.dart';
 import 'package:tmail_ui_user/features/caching/email_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/mailbox_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/recent_search_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/state_cache_client.dart';
+import 'package:tmail_ui_user/features/caching/token_oidc_cache_client.dart';
 import 'package:tmail_ui_user/features/cleanup/data/local/recent_search_cache_manager.dart';
+import 'package:tmail_ui_user/features/login/data/local/account_cache_manager.dart';
+import 'package:tmail_ui_user/features/login/data/local/token_oidc_cache_manager.dart';
 import 'package:tmail_ui_user/features/mailbox/data/local/mailbox_cache_manager.dart';
 import 'package:tmail_ui_user/features/thread/data/local/email_cache_manager.dart';
 
@@ -30,11 +34,17 @@ class LocalBindings extends Bindings {
     Get.put(EmailCacheManager(Get.find<EmailCacheClient>()));
     Get.put(RecentSearchCacheClient());
     Get.put(RecentSearchCacheManager(Get.find<RecentSearchCacheClient>()));
+    Get.put(TokenOidcCacheClient());
+    Get.put(TokenOidcCacheManager(Get.find<TokenOidcCacheClient>()));
+    Get.put(AccountCacheClient());
+    Get.put(AccountCacheManager(Get.find<AccountCacheClient>()));
     Get.put(CachingManager(
       Get.find<MailboxCacheClient>(),
       Get.find<StateCacheClient>(),
       Get.find<EmailCacheClient>(),
       Get.find<RecentSearchCacheClient>(),
+      Get.find<TokenOidcCacheClient>(),
+      Get.find<AccountCacheClient>()
     ));
   }
 }
