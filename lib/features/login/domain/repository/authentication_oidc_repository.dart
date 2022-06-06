f00d3c75696986e43b9b2c02eb93d2765cf65275
dab246 <tdvu@linagora.com>
2022-06-06 13:17:03 +0700

    TF-624 Remove baseUri param not use in GetOIDCConfigurationInteractor

--- lib/features/login/domain/repository/authentication_oidc_repository.dart
+++ lib/features/login/domain/repository/authentication_oidc_repository.dart
@@ -4,7 +4,7 @@ import 'package:model/model.dart';
 abstract class AuthenticationOIDCRepository {
   Future<OIDCResponse> checkOIDCIsAvailable(OIDCRequest oidcRequest);
 
-  Future<OIDCConfiguration> getOIDCConfiguration(Uri baseUri, OIDCResponse oidcResponse);
+  Future<OIDCConfiguration> getOIDCConfiguration(OIDCResponse oidcResponse);
 
   Future<TokenOIDC> getTokenOIDC(String clientId, String redirectUrl, String discoveryUrl, List<String> scopes);
 
