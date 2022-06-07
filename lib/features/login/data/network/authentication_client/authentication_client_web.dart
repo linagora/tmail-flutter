c9f89958f36e6870581d0b27231e0a9c3c04121f
dab246 <tdvu@linagora.com>
2022-06-07 18:50:09 +0700

    TF-624 Add AuthenticationClient for mobile/web platform to authenticate oidc

---
+++ lib/features/login/data/network/authentication_client/authentication_client_web.dart
@@ -0,0 +1,47 @@
+
+import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/login/data/network/authentication_client/authentication_client_base.dart';
+import 'package:flutter_appauth_platform_interface/flutter_appauth_platform_interface.dart';
+import 'package:model/oidc/oidc_configuration.dart';
+import 'package:model/oidc/token_id.dart';
+import 'package:model/oidc/token_oidc.dart';
+
+import '../../utils/library_platform/app_auth_plugin/app_auth_plugin.dart';
+
+class AuthenticationClientWeb implements AuthenticationClientBase {
+
+  final AppAuthWebPlugin _appAuthWeb;
+
+  AuthenticationClientWeb(this._appAuthWeb);
+
+  @override
+  Future<TokenOIDC> getTokenOIDC(String clientId, String redirectUrl,
+      String discoveryUrl, List<String> scopes) async {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> logoutOidc(TokenId tokenId, OIDCConfiguration config) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<TokenOIDC> refreshingTokensOIDC(String clientId, String redirectUrl,
+      String discoveryUrl, List<String> scopes, String refreshToken) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future authenticateOidcOnBrowser(String clientId, String redirectUrl,
+      String discoveryUrl, List<String> scopes) async {
+    await _appAuthWeb.authorizeAndExchangeCode(
+        AuthorizationTokenRequest(
+            clientId,
+            redirectUrl,
+            discoveryUrl: discoveryUrl,
+            scopes: scopes));
+  }
+}
+
+AuthenticationClientBase getAuthenticationClientImplementation() =>
+    AuthenticationClientWeb(Get.find<AppAuthWebPlugin>());
---
+++ lib/features/login/data/utils/library_platform/app_auth_plugin/app_auth_plugin.dart
@@ -0,0 +1,2 @@
+
+export 'app_auth_mobile_plugin.dart' if (dart.library.html) 'app_auth_web_plugin.dart';
