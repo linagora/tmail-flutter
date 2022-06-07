98736afbe2b0bc278fea7e86e76e03b5e8cac59b
dab246 <tdvu@linagora.com>
2022-06-07 18:06:04 +0700

    TF-624 Set clientId for oidc on browser
    (cherry picked from commit 9092e30b96e3ac073eecee7b3fd9be1e5eea5c75)

--- lib/features/login/data/network/config/oidc_constant.dart
+++ lib/features/login/data/network/config/oidc_constant.dart
@@ -1,5 +1,10 @@
+import 'package:core/core.dart';
+
 class OIDCConstant {
   static String get mobileOidcClientId => 'teammail-mobile';
+  static String get webOidcClientId => 'teammail-web';
   static List<String> get oidcScope => ['openid', 'offline_access'];
   static const keyAuthorityOidc = 'KEY_AUTHORITY_OIDC';
+
+  static String get clientId => BuildUtils.isWeb ? webOidcClientId : mobileOidcClientId;
 }
