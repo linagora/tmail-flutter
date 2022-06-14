d96d7b989662ed0fd5a1f9f23ad2609b177461eb
dab246 <tdvu@linagora.com>
2022-06-14 12:25:12 +0700

    TF-644 Add query param for download url
    (cherry picked from commit a9624a33d43564912a3d572dd9d67802e4e8cba2)

--- model/lib/extensions/session_extension.dart
+++ model/lib/extensions/session_extension.dart
@@ -1,15 +1,19 @@
 
+import 'package:core/utils/app_logger.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
 import 'package:uri/uri.dart';
 
 extension SessionExtension on Session {
   String getDownloadUrl() {
-    var baseUrl = '${downloadUrl.origin}${downloadUrl.path}';
+    var baseUrl = '${downloadUrl.origin}${downloadUrl.path}?${downloadUrl.query}';
     if (baseUrl.endsWith('/')) {
       baseUrl = baseUrl.substring(0, baseUrl.length - 1);
     }
-    return Uri.decodeFull(baseUrl);
+    log('SessionExtension::getDownloadUrl(): $baseUrl');
+    final downloadUrlDecode = Uri.decodeFull(baseUrl);
+    log('SessionExtension::getDownloadUrl(): DECODE $downloadUrlDecode');
+    return downloadUrlDecode;
   }
 
   Uri getUploadUrl(AccountId accountId) {
