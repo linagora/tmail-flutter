a293f6839d1263fb862cf42b56cd06c96a382d93
dab246 <tdvu@linagora.com>
2022-06-15 10:06:48 +0700

    TF-641 Change color for status bar in Android device

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
@@ -13,6 +14,12 @@ import 'package:tmail_ui_user/main/routes/app_routes.dart';
 void main() async {
   initLogger(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+    ));
     await MainBindings().dependencies();
     await HiveCacheConfig().setUp();
     await dotenv.load(fileName: 'env.file');
