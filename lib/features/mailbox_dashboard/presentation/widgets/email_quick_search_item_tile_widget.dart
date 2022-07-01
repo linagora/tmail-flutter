aa0d6900711a61151c40c2e19e36f7c500a6ec60
dab246 <tdvu@linagora.com>
2022-07-01 19:54:49 +0700

    TF-682 Update padding quick search form for tablet landscape
    (cherry picked from commit 93177d042d7bb57f06dd9a35d3efe4bba69e02a3)

--- lib/features/mailbox_dashboard/presentation/widgets/email_quick_search_item_tile_widget.dart
+++ lib/features/mailbox_dashboard/presentation/widgets/email_quick_search_item_tile_widget.dart
@@ -22,66 +22,75 @@ class EmailQuickSearchItemTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 2),
-            child: SvgPicture.asset(
-                _presentationEmail.hasStarred ? imagePath.icStar : imagePath.icUnStar,
-                width: 18, height: 18, fit: BoxFit.fill),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final maxWidthItem = constraints.maxWidth;
+        log('EmailQuickSearchItemTileWidget::build(): maxWidthItem: $maxWidthItem');
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 2),
+                child: SvgPicture.asset(
+                    _presentationEmail.hasStarred ? imagePath.icStar : imagePath.icUnStar,
+                    width: 18, height: 18, fit: BoxFit.fill),
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                       Container(
+                         constraints: BoxConstraints(maxWidth: maxWidthItem / 3),
+                         child: Text(_getInformationSender(),
+                             maxLines: 1,
+                             overflow: BuildUtils.isWeb ? null : TextOverflow.ellipsis,
+                             style: const TextStyle(
+                                 fontSize: 15,
+                                 fontWeight: FontWeight.w600,
+                                 color: Colors.black)),
+                       ),
+                       const SizedBox(width: 16),
+                       Expanded(
+                         child: Text(_presentationEmail.getEmailTitle(),
+                             maxLines: 1,
+                             overflow: BuildUtils.isWeb ? null : TextOverflow.ellipsis,
+                             style: const TextStyle(
+                                 fontSize: 13,
+                                 fontWeight: FontWeight.normal,
+                                 color: Colors.black)),
+                       ),
+                       if (_presentationEmail.hasAttachment == true)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: SvgPicture.asset(imagePath.icAttachment, width: 14, height: 14, fit: BoxFit.fill),
+                        ),
+                       Text(_presentationEmail.getReceivedAt(Localizations.localeOf(context).toLanguageTag()),
+                           textAlign: TextAlign.right,
+                           style: const TextStyle(
+                               fontSize: 13,
+                               fontWeight: FontWeight.normal,
+                               color: Colors.black))
+                     ]),
+                    const SizedBox(height: 3),
+                    Text(_presentationEmail.getPartialContent(),
+                        maxLines: 1,
+                        overflow: BuildUtils.isWeb ? null : TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.normal,
+                            color: AppColor.colorContentEmail))
+                    ]
+                ),
+              ),
+            ],
           ),
-          const SizedBox(width: 8),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                 Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                   Text(_getInformationSender(),
-                       maxLines: 1,
-                       overflow: BuildUtils.isWeb ? null : TextOverflow.ellipsis,
-                       style: const TextStyle(
-                           fontSize: 15,
-                           fontWeight: FontWeight.w600,
-                           color: Colors.black)),
-                   const SizedBox(width: 16),
-                   Expanded(
-                     child: Text(_presentationEmail.getEmailTitle(),
-                         maxLines: 1,
-                         overflow: BuildUtils.isWeb ? null : TextOverflow.ellipsis,
-                         style: const TextStyle(
-                             fontSize: 13,
-                             fontWeight: FontWeight.normal,
-                             color: Colors.black)),
-                   ),
-                   if (_presentationEmail.hasAttachment == true)
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: SvgPicture.asset(imagePath.icAttachment, width: 14, height: 14, fit: BoxFit.fill),
-                    ),
-                   Text(_presentationEmail.getReceivedAt(Localizations.localeOf(context).toLanguageTag()),
-                       textAlign: TextAlign.right,
-                       style: const TextStyle(
-                           fontSize: 13,
-                           fontWeight: FontWeight.normal,
-                           color: Colors.black))
-                 ]),
-                const SizedBox(height: 3),
-                Text(_presentationEmail.getPartialContent(),
-                    maxLines: 1,
-                    overflow: BuildUtils.isWeb ? null : TextOverflow.ellipsis,
-                    style: const TextStyle(
-                        fontSize: 13,
-                        fontWeight: FontWeight.normal,
-                        color: AppColor.colorContentEmail))
-                ]
-            ),
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 
