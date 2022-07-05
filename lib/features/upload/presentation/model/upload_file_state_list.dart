aad4afbb43b5ac28bc8c1818c796163f8d664c61
dab246 <tdvu@linagora.com>
2022-07-05 19:31:57 +0700

    TF-675 Add UploadController to handle upload attachment

---
+++ lib/features/upload/presentation/model/upload_file_state_list.dart
@@ -0,0 +1,64 @@
+
+import 'package:collection/collection.dart';
+import 'package:tmail_ui_user/features/upload/domain/model/upload_task_id.dart';
+import 'package:tmail_ui_user/features/upload/presentation/model/upload_file_state.dart';
+import 'package:tmail_ui_user/features/upload/presentation/model/upload_file_status.dart';
+
+typedef UpdateFileUploadingState = UploadFileState? Function(UploadFileState? currentState);
+typedef MatchedState = bool Function(UploadFileState? state);
+
+class UploadFileStateList {
+
+  final List<UploadFileState?> _uploadingStateFiles = <UploadFileState?>[];
+
+  List<UploadFileState?> get uploadingStateFiles => _uploadingStateFiles.toList(growable: false);
+
+  UploadFileStateList add(UploadFileState element) {
+    _uploadingStateFiles.add(element);
+    return this;
+  }
+
+  UploadFileStateList updateElementBy(
+    MatchedState matchedState,
+    UpdateFileUploadingState updateFileUploadingState,
+  ) {
+    final matchIndex = _uploadingStateFiles.indexWhere((element) => matchedState(element));
+    if (matchIndex >= 0) {
+      _uploadingStateFiles[matchIndex] = updateFileUploadingState(_uploadingStateFiles[matchIndex]);
+    }
+    return this;
+  }
+
+  UploadFileStateList updateElementByUploadTaskId(
+    UploadTaskId uploadTaskId,
+    UpdateFileUploadingState updateFileUploadingState,
+  ) {
+    return updateElementBy(
+      (state) => state?.uploadTaskId == uploadTaskId,
+      updateFileUploadingState
+    );
+  }
+
+  bool get allSuccess {
+    if (_uploadingStateFiles.isEmpty) {
+      return false;
+    }
+    return _uploadingStateFiles
+        .whereNotNull()
+        .every((file) => file.uploadStatus == UploadFileStatus.succeed);
+  }
+
+  void clear() {
+    _uploadingStateFiles.clear();
+  }
+
+  void deleteElementByUploadTaskId(UploadTaskId uploadTaskId) {
+    final fileState = _uploadingStateFiles
+        .firstWhereOrNull((fileState) => fileState?.uploadTaskId == uploadTaskId);
+
+    if (fileState != null) {
+      fileState.cancelToken?.cancel();
+      _uploadingStateFiles.remove(fileState);
+    }
+  }
+}
