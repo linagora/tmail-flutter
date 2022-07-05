835ad79949e23f6a262d2259d5639fdaadcc413e
dab246 <tdvu@linagora.com>
2022-07-05 19:31:18 +0700

    TF-675 Change UpdateAttachmentInteractor and repository
    (cherry picked from commit 87beef9230252bf97c32ece223ca7ae3903c4ef2)

--- lib/features/composer/domain/usecases/upload_attachment_interactor.dart
+++ lib/features/composer/domain/usecases/upload_attachment_interactor.dart
@@ -1,22 +1,14 @@
-import 'package:core/core.dart';
-import 'package:dartz/dartz.dart';
-import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:dio/dio.dart';
 import 'package:model/model.dart';
 import 'package:tmail_ui_user/features/composer/domain/repository/composer_repository.dart';
-import 'package:tmail_ui_user/features/composer/domain/state/upload_attachment_state.dart';
+import 'package:tmail_ui_user/features/upload/domain/model/upload_attachment.dart';
 
 class UploadAttachmentInteractor {
   final ComposerRepository _composerRepository;
 
   UploadAttachmentInteractor(this._composerRepository);
 
-  Stream<Either<Failure, Success>> execute(FileInfo fileInfo, AccountId accountId, Uri uploadUrl) async* {
-    try {
-      final result = await _composerRepository.uploadAttachment(
-          UploadRequest(uploadUrl, accountId, fileInfo));
-      yield Right<Failure, Success>(UploadAttachmentSuccess(result));
-    } catch (e) {
-      yield Left<Failure, Success>(UploadAttachmentFailure(e));
-    }
+  UploadAttachment execute(FileInfo fileInfo, Uri uploadUri, {CancelToken? cancelToken}) {
+    return _composerRepository.uploadAttachment(fileInfo, uploadUri, cancelToken: cancelToken);
   }
 }
