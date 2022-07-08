1c95749741ec4903fbc5f60b8b196713a46e239e
dab246 <tdvu@linagora.com>
2022-07-08 15:09:57 +0700

    TF-297 Get stored language when launch app
    (cherry picked from commit cf4d05e3e10e677e56ff8323c80d72e66d6da70b)

--- lib/features/manage_account/domain/usecases/save_language_interactor.dart
+++ lib/features/manage_account/domain/usecases/save_language_interactor.dart
@@ -5,20 +5,20 @@ import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
 import 'package:dartz/dartz.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/repository/manage_account_repository.dart';
-import 'package:tmail_ui_user/features/manage_account/domain/state/save_language_app_state.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/state/save_language_state.dart';
 
-class SaveLanguageAppInteractor {
+class SaveLanguageInteractor {
   final ManageAccountRepository manageAccountRepository;
 
-  SaveLanguageAppInteractor(this.manageAccountRepository);
+  SaveLanguageInteractor(this.manageAccountRepository);
 
   Stream<Either<Failure, Success>> execute(Locale localeCurrent) async* {
     try {
-      yield Right(SavingLanguageApp());
+      yield Right(SavingLanguage());
       await manageAccountRepository.persistLanguage(localeCurrent);
-      yield Right(SaveLanguageAppSuccess());
+      yield Right(SaveLanguageSuccess(localeCurrent));
     } catch (exception) {
-      yield Left(SaveLanguageAppFailure(exception));
+      yield Left(SaveLanguageFailure(exception));
     }
   }
 }
--- lib/main/localizations/localization_service.dart
+++ lib/main/localizations/localization_service.dart
@@ -1,7 +1,9 @@
 
 import 'dart:ui';
 
+import 'package:core/utils/app_logger.dart';
 import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/manage_account/data/local/language_cache_manager.dart';
 
 class LocalizationService extends Translations {
 
@@ -25,17 +27,25 @@ class LocalizationService extends Translations {
   static final locale = _getLocaleFromLanguage();
 
   static void changeLocale(String langCode) {
+    log('LocalizationService::changeLocale(): langCode: $langCode');
     final locale = _getLocaleFromLanguage(langCode: langCode);
     Get.updateLocale(locale);
   }
 
   static Locale _getLocaleFromLanguage({String? langCode}) {
-    final lang = langCode ?? Get.deviceLocale?.languageCode;
+    final languageCacheManager = Get.find<LanguageCacheManager>();
+    final localeStored = languageCacheManager.getStoredLanguage();
 
-    final localeSelected = supportedLocales
-        .firstWhereOrNull((locale) => locale.languageCode == lang);
+    log('LocalizationService::_getLocaleFromLanguage(): localeStored: ${localeStored.toString()}');
 
-    return localeSelected ?? Get.locale ?? defaultLocale;
+    if (localeStored != null) {
+      return localeStored;
+    } else {
+      final languageCodeCurrent = langCode ?? Get.deviceLocale?.languageCode;
+      final localeSelected = supportedLocales
+          .firstWhereOrNull((locale) => locale.languageCode == languageCodeCurrent);
+      return localeSelected ?? Get.locale ?? defaultLocale;
+    }
   }
 
   @override
