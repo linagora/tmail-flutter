7ea33d833dd945ccb9a2d432c5280d59a9a1fc9b
dab246 <tdvu@linagora.com>
2022-07-08 13:56:42 +0700

    TF-297 Inject LocalizationService in main when launch app
    (cherry picked from commit fd7ef882465a89294f045df8b1c95a03f2acb5ad)

--- lib/main/localizations/app_localizations_delegate.dart
+++ lib/main/localizations/app_localizations_delegate.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+import 'package:tmail_ui_user/main/localizations/localization_service.dart';
 
 class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
   const AppLocalizationsDelegate();
 
   @override
   bool isSupported(Locale locale) {
-    return ['en', 'vi', 'fr', 'ru'].contains(locale.languageCode);
+    return LocalizationService.supportedLanguageCodes.contains(locale.languageCode);
   }
 
   @override
