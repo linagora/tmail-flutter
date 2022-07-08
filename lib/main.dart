fd7ef882465a89294f045df8b1c95a03f2acb5ad
dab246 <tdvu@linagora.com>
2022-07-08 13:56:42 +0700

    TF-297 Inject LocalizationService in main when launch app

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:tmail_ui_user/features/caching/config/hive_cache_config.dart';
 import 'package:tmail_ui_user/main/bindings/main_bindings.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations_delegate.dart';
+import 'package:tmail_ui_user/main/localizations/localization_service.dart';
 import 'package:tmail_ui_user/main/pages/app_pages.dart';
 import 'package:tmail_ui_user/main/routes/app_routes.dart';
 
@@ -35,12 +36,7 @@ class TMailApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: appTheme(),
-      supportedLocales: const [
-        Locale('en'),
-        Locale('vi'),
-        Locale('ru'),
-        Locale('fr')
-      ],
+      supportedLocales: LocalizationService.supportedLocales,
       localizationsDelegates: const [
         AppLocalizationsDelegate(),
         GlobalMaterialLocalizations.delegate,
@@ -55,7 +51,9 @@ class TMailApp extends StatelessWidget {
         }
         return supportedLocales.first;
       },
-      locale: Get.locale ?? const Locale('en'),
+      locale: LocalizationService.locale,
+      fallbackLocale: LocalizationService.fallbackLocale,
+      translations: LocalizationService(),
       onGenerateTitle: (context) => AppLocalizations.of(context).page_name,
       defaultTransition: Transition.fade,
       initialRoute: AppRoutes.HOME,
