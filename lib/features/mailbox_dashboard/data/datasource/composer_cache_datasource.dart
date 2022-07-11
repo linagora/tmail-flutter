bd0be89dce0d11eb6c7a4b4ba4d37a559a4819a9
ManhNTX <tmnguyen@linagora.com>
2022-07-11 10:31:49 +0700

    refactor name folder

---
+++ lib/features/mailbox_dashboard/data/datasource/composer_cache_datasource.dart
@@ -0,0 +1,10 @@
+import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/data/model/composer_cache.dart';
+
+abstract class ComposerCacheDataSource {
+  void saveComposerCacheOnWeb(Email email);
+
+  ComposerCache? getComposerCacheOnWeb();
+
+  void removeComposerCacheOnWeb();
+}
