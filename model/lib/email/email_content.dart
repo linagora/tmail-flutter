db4a5b2a4d3433b7df892d7e4483b8941df88cf7
dab246 <tdvu@linagora.com>
2022-07-11 12:31:52 +0700

    TF-332 Get original email content as html
    (cherry picked from commit bce3f0a27af2f9da605e40a05defa815609e6b1d)

--- model/lib/email/email_content.dart
+++ model/lib/email/email_content.dart
@@ -1,9 +1,12 @@
 
+import 'package:core/utils/app_logger.dart';
 import 'package:equatable/equatable.dart';
 import 'package:model/email/email_content_type.dart';
 import 'package:model/model.dart';
 
 class EmailContent with EquatableMixin {
+
+  static const defaultHtmlTagsWrapContent = '<html><head></head><body></body></html>';
 
   final String content;
   final EmailContentType type;
@@ -23,6 +26,31 @@ extension EmailContentExtension on EmailContent {
           .replaceAll('\n', '<br/>')
           .replaceAll('\n\n', '<br/>');
     }
+    final newContent = _getContentOriginal(content);
+    return newContent;
+  }
+
+  String _getContentOriginal(String content) {
+    if (content == EmailContent.defaultHtmlTagsWrapContent ||
+        content.trim().isEmpty) {
+      return '';
+    }
+
+    final firstTags = '<html><head></head><body>';
+    final latestTags = '</body></html>';
+    if (content.startsWith(firstTags) && content.endsWith(latestTags)) {
+      final firstIndex = firstTags.length;
+      final latestIndex = content.length - latestTags.length;
+      log('EmailContentExtension::_getContentOriginal(): firstIndex: $firstIndex');
+      log('EmailContentExtension::_getContentOriginal(): latestIndex: $latestIndex');
+
+      if (latestIndex > firstIndex) {
+        final contentOriginal = content.substring(firstIndex, latestIndex);
+        log('EmailContentExtension::_getContentOriginal(): contentOriginal: $contentOriginal');
+        return contentOriginal;
+      }
+    }
+
     return content;
   }
 }
