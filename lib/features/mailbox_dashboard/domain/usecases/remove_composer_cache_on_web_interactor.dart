8431968eb5eeea39299726ddc91f994c561343ca
ManhNTX <tmnguyen@linagora.com>
2022-07-12 07:08:47 +0700

    add either and state in interactor
    (cherry picked from commit ef436c35570761d9f05426ad64216f422fb7f0b2)

---
+++ lib/features/mailbox_dashboard/domain/usecases/remove_composer_cache_on_web_interactor.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/repository/composer_cache_repository.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/state/remove_composer_cache_state.dart';
+
+class RemoveComposerCacheOnWebInteractor {
+  final ComposerCacheRepository composerCacheRepository;
+
+  RemoveComposerCacheOnWebInteractor(this.composerCacheRepository);
+
+  execute() {
+    try {
+      composerCacheRepository.removeComposerCacheOnWeb();
+      return const Right(RemoveComposerCacheSuccess);
+    } catch (exception) {
+      return Left(RemoveComposerCacheFailure(exception));
+    }
+  }
+}
