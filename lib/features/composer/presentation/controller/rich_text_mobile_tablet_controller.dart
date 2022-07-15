57907d56b6e4b9cbcde2e5c319d6ce7f2aadfc1c
ManhNTX <tmnguyen@linagora.com>
2022-07-15 16:16:56 +0700

    Compose email with text style for mobile

---
+++ lib/features/composer/presentation/controller/rich_text_mobile_tablet_controller.dart
@@ -0,0 +1,37 @@
+import 'package:enough_html_editor/enough_html_editor.dart';
+import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/composer/presentation/model/rich_text_style_type.dart';
+
+class RichTextMobileTabletController extends GetxController {
+  HtmlEditorApi? htmlEditorApi;
+  final listTextStyleApply = RxList<RichTextStyleType>();
+
+  void listenHtmlEditorApi() {
+    htmlEditorApi?.onFormatSettingsChanged = (formatSettings) {
+      listTextStyleApply.clear();
+      if (formatSettings.isBold) {
+        listTextStyleApply.add(RichTextStyleType.bold);
+      }
+
+      if (formatSettings.isItalic) {
+        listTextStyleApply.add(RichTextStyleType.italic);
+      }
+
+      if (formatSettings.isUnderline) {
+        listTextStyleApply.add(RichTextStyleType.underline);
+      }
+    };
+  }
+
+  selectTextStyleType(RichTextStyleType richTextStyleType) {
+    if (listTextStyleApply.contains(richTextStyleType)) {
+      listTextStyleApply.remove(richTextStyleType);
+    } else {
+      listTextStyleApply.add(richTextStyleType);
+    }
+  }
+
+  bool isTextStyleTypeSelected(RichTextStyleType richTextStyleType) {
+    return listTextStyleApply.contains(richTextStyleType);
+  }
+}
