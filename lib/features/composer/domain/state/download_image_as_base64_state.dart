e62f13292bcda51eb763f9eb0699cc9c15d680b0
dab246 <tdvu@linagora.com>
2022-07-20 16:48:28 +0700

    TF-725 Download inline image as base64 data
    (cherry picked from commit 74901821eedd4a61a498b6f32bb5b7bd0fc4d490)

---
+++ lib/features/composer/domain/state/download_image_as_base64_state.dart
@@ -0,0 +1,33 @@
+
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:model/upload/file_info.dart';
+
+class DownloadingImageAsBase64 extends UIState {
+
+  DownloadingImageAsBase64();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class DownloadImageAsBase64Success extends UIState {
+
+  final String imageAsBase64;
+  final String cid;
+  final FileInfo fileInfo;
+
+  DownloadImageAsBase64Success(this.imageAsBase64, this.cid, this.fileInfo);
+
+  @override
+  List<Object?> get props => [imageAsBase64, cid, fileInfo];
+}
+
+class DownloadImageAsBase64Failure extends FeatureFailure {
+  final dynamic exception;
+
+  DownloadImageAsBase64Failure(this.exception);
+
+  @override
+  List<Object> get props => [exception];
+}
