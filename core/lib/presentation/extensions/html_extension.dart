0a5809e9a69272e4e7165148677c8c4423668670
dab246 <tdvu@linagora.com>
2022-07-22 18:33:24 +0700

    TF-736 Remove string not use
    (cherry picked from commit db967b00bb9d2cfcd27eae19577c3ff52d3070a9)

--- core/lib/presentation/extensions/html_extension.dart
+++ core/lib/presentation/extensions/html_extension.dart
@@ -3,21 +3,10 @@ extension HtmlExtension on String {
 
   static const String editorStartTags = '<p><br></p>';
 
-  String removeFontSizeZeroPixel() => replaceAll(RegExp('font-size:0px;'), '');
-
-  String removeHeightZeroPixel() => replaceAll(RegExp('height:0px;'), '');
-
-  String removeWidthZeroPixel() => replaceAll(RegExp('width:0px;'), '');
-
-  String removeMaxWidthZeroPixel() => replaceAll(RegExp('max-width:0px;'), '');
-
-  String removeMaxHeightZeroPixel() => replaceAll(RegExp('max-height:0px;'), '');
-
-  String changeStyleBackground() => replaceAll(RegExp('background:'), 'background-color:');
-
-  String addBorderLefForBlockQuote() => replaceAll(RegExp('<blockquote'), '<blockquote style=\"margin-left:8px;margin-right:8px;padding-left:12px;padding-right:12px;border-left:6px solid #EFEFEF;\"');
-
-  String addBlockTag(String tag, {String? attribute}) => attribute != null ? '<$tag $attribute>$this</$tag>' : '<$tag>$this</$tag>';
+  String addBlockTag(String tag, {String? attribute}) =>
+      attribute != null
+          ? '<$tag $attribute>$this</$tag>'
+          : '<$tag>$this</$tag>';
 
   String append(String value) => this + value;
 
