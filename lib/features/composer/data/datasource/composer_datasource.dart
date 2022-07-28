8e22bec3f1d662d03fad9beac75a132d11d58e90
dab246 <tdvu@linagora.com>
2022-07-28 14:34:28 +0700

    TF-761 Update repository/datasource/interactor of downloadImageAsBase64

--- lib/features/composer/data/datasource/composer_datasource.dart
+++ lib/features/composer/data/datasource/composer_datasource.dart
@@ -1,4 +1,6 @@
 
+import 'package:model/upload/file_info.dart';
+
 abstract class ComposerDataSource {
-  Future<String?> downloadImageAsBase64(String url);
+  Future<String?> downloadImageAsBase64(String url, String cid, FileInfo fileInfo, {double? maxWidth, bool? compress});
 }
