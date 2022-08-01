d4daa8cda0a8aef1f6a596ba86fe0a390cdc46ef
dab246 <tdvu@linagora.com>
2022-08-01 13:19:33 +0700

    TF-694 Fixed sort compare emails
    (cherry picked from commit bdeefcf55f0a7c294116fc742d478919d60d7daa)

--- model/lib/extensions/list_email_extension.dart
+++ model/lib/extensions/list_email_extension.dart
@@ -18,14 +18,13 @@ extension ListEmailExtension on List<Email> {
 
   void sortBy(Comparator comparator) {
     sort((email1, email2) {
-      if (comparator.property == EmailComparatorProperty.sentAt
-          || comparator.property == EmailComparatorProperty.receivedAt) {
-        final emailTime1 = email1.sentAt ?? email1.receivedAt;
-        final emailTime2 = email2.sentAt ?? email2.receivedAt;
-        return emailTime1.compareToSort(emailTime2, comparator.isAscending);
-      } if (comparator.property == EmailComparatorProperty.subject) {
+      if (comparator.property == EmailComparatorProperty.receivedAt) {
+        return email1.receivedAt.compareToSort(email2.receivedAt, comparator.isAscending);
+      } else if (comparator.property == EmailComparatorProperty.sentAt) {
+        return email1.sentAt.compareToSort(email2.sentAt, comparator.isAscending);
+      } else if (comparator.property == EmailComparatorProperty.subject) {
         return email1.subject.compareToSort(email2.subject, comparator.isAscending);
-      } if (comparator.property == EmailComparatorProperty.size) {
+      } else if (comparator.property == EmailComparatorProperty.size) {
         return email1.size.compareToSort(email2.size, comparator.isAscending);
       } else {
         return 0;
