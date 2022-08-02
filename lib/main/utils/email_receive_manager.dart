6a2d046d06df8edc8309409327ce37fb794b016e
ManhNTX <tmnguyen@linagora.com>
2022-08-02 10:59:04 +0700

    open composer view when selected share file
    (cherry picked from commit 7d33899aba90121ede883dc02eaa7d5925e84470)

--- lib/main/utils/email_receive_manager.dart
+++ lib/main/utils/email_receive_manager.dart
@@ -8,13 +8,16 @@ class EmailReceiveManager {
   BehaviorSubject<EmailAddress?> _pendingEmailAddressInfo = BehaviorSubject.seeded(null);
   BehaviorSubject<EmailAddress?> get pendingEmailAddressInfo => _pendingEmailAddressInfo;
 
+  BehaviorSubject<List<SharedMediaFile>> _pendingFileInfo = BehaviorSubject.seeded(List.empty(growable: true));
+  BehaviorSubject<List<SharedMediaFile>> get pendingFileInfo => _pendingFileInfo;
+
   Stream<Uri?> get receivingSharingStream {
     return Rx.merge([
       Stream.fromFuture(ReceiveSharingIntent.getInitialTextAsUri()),
       ReceiveSharingIntent.getTextStreamAsUri()
     ]);
   }
-  Stream<List<SharedMediaFile>> get receivingSharingStream2 {
+  Stream<List<SharedMediaFile>> get receivingFileSharingStream {
     return Rx.merge([
       Stream.fromFuture(ReceiveSharingIntent.getInitialMedia()),
       ReceiveSharingIntent.getMediaStream()
@@ -36,5 +39,22 @@ class EmailReceiveManager {
 
   void closeEmailReceiveManagerStream() {
     _pendingEmailAddressInfo.close();
+  }
+
+  void setPendingFileInfo(List<SharedMediaFile> list) async {
+    clearPendingFileInfo();
+    _pendingFileInfo.add(list);
+  }
+
+  void clearPendingFileInfo() {
+    if(_pendingFileInfo.isClosed) {
+      _pendingFileInfo = BehaviorSubject.seeded(List.empty(growable: true));
+    } else {
+      _pendingFileInfo.add(List.empty(growable: true));
+    }
+  }
+
+  void closeFileSharingStream() {
+    _pendingFileInfo.close();
   }
 }
