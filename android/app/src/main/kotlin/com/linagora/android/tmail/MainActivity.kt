145f4230699d922eacde85a422281a09a7c286cf
ManhNTX <tmnguyen@linagora.com>
2022-08-02 10:56:38 +0700

    add share file to TMail on android
    (cherry picked from commit 4a94d1edbd5110e8a6b776cf5a1ca45371c31ea7)

--- android/app/src/main/kotlin/com/linagora/android/tmail/MainActivity.kt
+++ android/app/src/main/kotlin/com/linagora/android/tmail/MainActivity.kt
@@ -1,10 +1,23 @@
 package com.linagora.android.tmail
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 
 class MainActivity: FlutterFragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        if (intent.getIntExtra("org.chromium.chrome.extra.TASK_ID", -1) == this.taskId) {
+            this.finish()
+            intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
