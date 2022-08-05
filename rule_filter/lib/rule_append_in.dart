1da44e6bcdacb9f52b2e3cb806e1291e25066344
ManhNTX <tmnguyen@linagora.com>
2022-08-05 16:24:56 +0700

    create module rule_filter for tmail

---
+++ rule_filter/lib/rule_append_in.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:jmap_dart_client/http/converter/mailbox_id_converter.dart';
+import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'rule_append_in.g.dart';
+
+@MailboxIdConverter()
+@JsonSerializable()
+class RuleAppendIn with EquatableMixin {
+  final List<MailboxId> mailboxIds;
+
+  RuleAppendIn(this.mailboxIds);
+
+  @override
+  List<Object?> get props => [
+        mailboxIds,
+      ];
+
+  factory RuleAppendIn.fromJson(Map<String, dynamic> json) =>
+      _$RuleAppendInFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RuleAppendInToJson(this);
+
+}
