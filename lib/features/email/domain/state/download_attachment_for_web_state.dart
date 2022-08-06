b4c1ee91f6533a25175d08787525517911725338
dab246 <tdvu@linagora.com>
2022-08-06 19:51:25 +0700

    TF-654 Update datasource/repository for download attachment on web

--- lib/features/email/domain/state/download_attachment_for_web_state.dart
+++ lib/features/email/domain/state/download_attachment_for_web_state.dart
@@ -1,18 +1,64 @@
+import 'dart:typed_data';
+
 import 'package:core/core.dart';
+import 'package:model/model.dart';
+
+class StartDownloadAttachmentForWeb extends UIState {
+
+  final DownloadTaskId taskId;
+  final Attachment attachment;
+
+  StartDownloadAttachmentForWeb(this.taskId, this.attachment);
+
+  @override
+  List<Object> get props => [taskId, attachment];
+}
+
+class DownloadingAttachmentForWeb extends UIState {
+
+  final DownloadTaskId taskId;
+  final Attachment attachment;
+  final double progress;
+  final int downloaded;
+  final int total;
+
+  DownloadingAttachmentForWeb(
+    this.taskId,
+    this.attachment,
+    this.progress,
+    this.downloaded,
+    this.total
+  );
+
+  @override
+  List<Object> get props => [
+    taskId,
+    attachment,
+    progress,
+    downloaded,
+    total
+  ];
+}
 
 class DownloadAttachmentForWebSuccess extends UIState {
 
-  DownloadAttachmentForWebSuccess();
+  final DownloadTaskId taskId;
+  final Attachment attachment;
+  final Uint8List bytes;
+
+  DownloadAttachmentForWebSuccess(this.taskId, this.attachment, this.bytes);
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [taskId, attachment, bytes];
 }
 
 class DownloadAttachmentForWebFailure extends FeatureFailure {
+
+  final DownloadTaskId taskId;
   final dynamic exception;
 
-  DownloadAttachmentForWebFailure(this.exception);
+  DownloadAttachmentForWebFailure(this.taskId, this.exception);
 
   @override
-  List<Object> get props => [exception];
+  List<Object> get props => [taskId, exception];
 }
