ba0bd753c69626be0054e4a358bbaa4cfb7488bc
ManhNTX <tmnguyen@linagora.com>
2022-08-11 10:36:51 +0700

    create isolate worker for empty
    (cherry picked from commit 64d6ad6272f2c31794efe01ee1354eb6d7ae7a7e)

---
+++ lib/features/thread/data/model/empty_trash_folder_arguments.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/state.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
+import 'package:tmail_ui_user/features/email/data/network/email_api.dart';
+import 'package:tmail_ui_user/features/thread/data/network/thread_api.dart';
+
+class EmptyTrashFolderArguments with EquatableMixin {
+  AccountId accountId;
+  MailboxId trashMailboxId;
+  Future<void> Function(State state) updateState;
+  Future<void> Function({
+    List<Email>? newUpdated,
+    List<Email>? newCreated,
+    List<EmailId>? newDestroyed,
+  }) updateEmailCache;
+  final ThreadAPI threadAPI;
+  final EmailAPI emailAPI;
+
+  EmptyTrashFolderArguments(this.threadAPI, this.emailAPI, this.accountId,
+      this.trashMailboxId, this.updateEmailCache, this.updateState);
+
+  @override
+  List<Object?> get props => [accountId, trashMailboxId];
+}
