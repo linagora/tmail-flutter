4b5e2383ea164998c7cc6bc458e8b8371c352367
dab246 <tdvu@linagora.com>
2022-08-11 15:33:46 +0700

    TF-612 Add data layer for edit email rule filter
    (cherry picked from commit 036a8612f83313cb1f02e3f8673465731aa3b826)

--- lib/features/manage_account/data/datasource/manage_account_datasource.dart
+++ lib/features/manage_account/data/datasource/manage_account_datasource.dart
@@ -7,6 +7,7 @@ import 'package:rule_filter/rule_filter/tmail_rule.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_identity_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/delete_email_rule_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_email_rule_filter_request.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/edit_email_rule_filter_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/edit_identity_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/identities_response.dart';
 
@@ -26,4 +27,6 @@ abstract class ManageAccountDataSource {
   Future<List<TMailRule>> deleteTMailRule(AccountId accountId, DeleteEmailRuleRequest deleteEmailRuleRequest);
 
   Future<List<TMailRule>> createNewEmailRuleFilter(AccountId accountId, CreateNewEmailRuleFilterRequest ruleFilterRequest);
+
+  Future<List<TMailRule>> editEmailRuleFilter(AccountId accountId, EditEmailRuleFilterRequest ruleFilterRequest);
 }
