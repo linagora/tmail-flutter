277c77d10fbea78214fe69fd79fa48924b17e8f7
ManhNTX <tmnguyen@linagora.com>
2022-08-12 14:47:29 +0700

    add delete_email_rule feature

--- lib/features/thread/data/model/empty_trash_folder_arguments.dart
+++ lib/features/thread/data/model/empty_trash_folder_arguments.dart
@@ -1,18 +1,21 @@
 import 'package:equatable/equatable.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
-import 'package:jmap_dart_client/jmap/core/state.dart';
-import 'package:jmap_dart_client/jmap/mail/email/email.dart';
 import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
 import 'package:tmail_ui_user/features/email/data/network/email_api.dart';
 import 'package:tmail_ui_user/features/thread/data/network/thread_api.dart';
 
 class EmptyTrashFolderArguments with EquatableMixin {
-  AccountId accountId;
-  MailboxId trashMailboxId;
+  final AccountId accountId;
+  final MailboxId trashMailboxId;
   final ThreadAPI threadAPI;
   final EmailAPI emailAPI;
 
-  EmptyTrashFolderArguments(this.threadAPI, this.emailAPI, this.accountId, this.trashMailboxId);
+  EmptyTrashFolderArguments(
+    this.threadAPI,
+    this.emailAPI,
+    this.accountId,
+    this.trashMailboxId,
+  );
 
   @override
   List<Object?> get props => [accountId, trashMailboxId];
