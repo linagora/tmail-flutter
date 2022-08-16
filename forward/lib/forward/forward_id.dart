de0950d8a029d6e397891a18a34887cce9625348
ManhNTX <tmnguyen@linagora.com>
2022-08-16 11:19:16 +0700

    TF-836: add unit_test in bitrise
    (cherry picked from commit a47d15c98f1a691401df36aabc7d030ae533e84a)

--- forward/lib/forward/forward_id.dart
+++ forward/lib/forward/forward_id.dart
@@ -1,6 +1,4 @@
 import 'package:equatable/equatable.dart';
-import 'package:forward/forward/converter/rule_filter_id_coverter.dart';
-import 'package:jmap_dart_client/http/converter/id_converter.dart';
 import 'package:jmap_dart_client/jmap/core/id.dart';
 
 enum ForwardIdType {
@@ -11,8 +9,6 @@ enum ForwardIdType {
   final String value;
 }
 
-@ForwardIdConverter()
-@IdConverter()
 class ForwardId with EquatableMixin {
   final Id id;
 
