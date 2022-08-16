db276f0c149ee34f9c674c63e651669cd6552520
ManhNTX <tmnguyen@linagora.com>
2022-08-16 10:56:06 +0700

    TF-836: Create module forward

---
+++ forward/lib/forward/forward_id.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:forward/forward/converter/rule_filter_id_coverter.dart';
+import 'package:jmap_dart_client/http/converter/id_converter.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
+
+enum ForwardIdType {
+  singleton('singleton');
+
+  const ForwardIdType(this.value);
+
+  final String value;
+}
+
+@ForwardIdConverter()
+@IdConverter()
+class ForwardId with EquatableMixin {
+  final Id id;
+
+  ForwardId({
+    required this.id,
+  });
+
+  @override
+  List<Object?> get props => [id];
+}
+
+extension ForwardIdSingleton on ForwardId {
+  static ForwardId get forwardIdSingleton => ForwardId(id: Id(ForwardIdType.singleton.value));
+}
