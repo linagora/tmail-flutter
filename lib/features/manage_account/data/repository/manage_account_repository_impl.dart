027590a47b22730a9abd4cf196a99a0af419a1a6
dab246 <tdvu@linagora.com>
2022-08-17 17:30:33 +0700

    TF-801 Add data layer for get all vacation
    (cherry picked from commit 8ab4e7bfc061a4408e0cf09a6c7cc68a29ea205b)

--- lib/features/manage_account/data/repository/manage_account_repository_impl.dart
+++ lib/features/manage_account/data/repository/manage_account_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:forward/forward/tmail_forward.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
 import 'package:jmap_dart_client/jmap/identities/identity.dart';
+import 'package:jmap_dart_client/jmap/mail/vacation/vacation_response.dart';
 import 'package:rule_filter/rule_filter/tmail_rule.dart';
 import 'package:tmail_ui_user/features/manage_account/data/datasource/manage_account_datasource.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_identity_request.dart';
@@ -68,5 +69,10 @@ class ManageAccountRepositoryImpl extends ManageAccountRepository {
   @override
   Future<TMailForward> getForward(AccountId accountId) {
     return dataSource.getForward(accountId);
+  }
+
+  @override
+  Future<List<VacationResponse>> getAllVacationResponse(AccountId accountId) {
+    return dataSource.getAllVacationResponse(accountId);
   }
 }
