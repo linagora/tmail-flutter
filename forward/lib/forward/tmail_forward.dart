ddd09ee45423d968a54bcb092952bd6cbd44d1a5
ManhNTX <tmnguyen@linagora.com>
2022-08-19 00:55:08 +0700

    TF-831: add set forward to module

--- forward/lib/forward/tmail_forward.dart
+++ forward/lib/forward/tmail_forward.dart
@@ -1,4 +1,5 @@
 import 'package:forward/forward/converter/forward_id_coverter.dart';
+import 'package:forward/forward/converter/forward_id_nullable_converter.dart';
 import 'package:forward/forward/forward.dart';
 import 'package:forward/forward/forward_id.dart';
 import 'package:json_annotation/json_annotation.dart';
@@ -6,13 +7,14 @@ import 'package:json_annotation/json_annotation.dart';
 part 'tmail_forward.g.dart';
 
 @ForwardIdConverter()
-@JsonSerializable()
+@ForwardIdNullableConverter()
+@JsonSerializable(explicitToJson: true, includeIfNull: false)
 class TMailForward extends Forward {
-  final ForwardId id;
+  final ForwardId? id;
   final bool localCopy;
   final Set<String> forwards;
   TMailForward({
-    required this.id,
+    this.id,
     required this.localCopy,
     required this.forwards,
   });
