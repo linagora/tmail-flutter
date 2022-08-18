b285d6ba1e79522386df4b35fca754d455c4c203
dab246 <tdvu@linagora.com>
2022-08-18 15:35:47 +0700

    TF-802 Disable vacation responder in setting
    (cherry picked from commit 610d5771b4313c85df40ea98993b165e075e2d7a)

--- lib/features/manage_account/presentation/extensions/vacation_response_extension.dart
+++ lib/features/manage_account/presentation/extensions/vacation_response_extension.dart
@@ -1,6 +1,7 @@
 
 import 'package:core/utils/app_logger.dart';
 import 'package:flutter/material.dart';
+import 'package:jmap_dart_client/jmap/core/utc_date.dart';
 import 'package:jmap_dart_client/jmap/mail/vacation/vacation_response.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/model/vacation/vacation_presentation.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/model/vacation/vacation_responder_status.dart';
@@ -39,5 +40,23 @@ extension VacationResponseExtension on VacationResponse {
       }
     }
     return false;
+  }
+
+  VacationResponse copyWith({
+    bool? isEnabled,
+    UTCDate? fromDate,
+    UTCDate? toDate,
+    String? subject,
+    String? textBody,
+    String? htmlBody
+  }) {
+    return VacationResponse(
+      isEnabled: isEnabled ?? this.isEnabled,
+      fromDate: fromDate ?? this.fromDate,
+      toDate: toDate ?? this.toDate,
+      subject: subject ?? this.subject,
+      textBody: textBody ?? this.textBody,
+      htmlBody: htmlBody ?? this.htmlBody
+    );
   }
 }
