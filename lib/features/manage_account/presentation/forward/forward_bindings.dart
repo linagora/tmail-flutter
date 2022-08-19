7da79592c8ec2cbc74ba0ad2e6eb9cfcb9f49a5c
dab246 <tdvu@linagora.com>
2022-08-19 11:58:26 +0700

    TF-832 Implement delete an recipient in forwarding

--- lib/features/manage_account/presentation/forward/forward_bindings.dart
+++ lib/features/manage_account/presentation/forward/forward_bindings.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:tmail_ui_user/features/base/base_bindings.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/repository/manage_account_repository.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/usecases/delete_recipient_in_forwarding_interactor.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/usecases/get_forward_interactor.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/forward/forward_controller.dart';
 
@@ -10,6 +11,7 @@ class ForwardBindings extends BaseBindings {
   void bindingsController() {
     Get.lazyPut(() => ForwardController(
       Get.find<GetForwardInteractor>(),
+      Get.find<DeleteRecipientInForwardingInteractor>(),
     ));
   }
 
@@ -22,6 +24,7 @@ class ForwardBindings extends BaseBindings {
   @override
   void bindingsInteractor() {
     Get.lazyPut(() => GetForwardInteractor(Get.find<ManageAccountRepository>()));
+    Get.lazyPut(() => DeleteRecipientInForwardingInteractor(Get.find<ManageAccountRepository>()));
   }
 
   @override
