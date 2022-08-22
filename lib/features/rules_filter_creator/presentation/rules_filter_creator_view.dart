0727c0e3e05487d1a2ce445550086e0447c73e2c
dab246 <tdvu@linagora.com>
2022-08-22 16:37:51 +0700

    TF-855 Fix bottom padding of CreateNewRule dialog
    (cherry picked from commit 2a0fdd214d9cd74ca4df15a4a9f67c1ff07bc39b)

--- lib/features/rules_filter_creator/presentation/rules_filter_creator_view.dart
+++ lib/features/rules_filter_creator/presentation/rules_filter_creator_view.dart
@@ -32,31 +32,21 @@ class RuleFilterCreatorView extends GetWidget<RulesFilterCreatorController> {
             body: GestureDetector(
               onTap: () => FocusScope.of(context).unfocus(),
               child: SafeArea(
-                child: Align(
-                  alignment: Alignment.bottomCenter, 
-                  child: Card(
-                    color: Colors.transparent,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
+                bottom: false,
+                left: false,
+                right: false,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16)),
+                    color: Colors.white),
+                  margin: const EdgeInsets.only(top: BuildUtils.isWeb ? 70 : 0),
+                  child: ClipRRect(
+                      borderRadius: const  BorderRadius.only(
                           topLeft: Radius.circular(16),
-                          topRight: Radius.circular(16),
-                        )),
-                    child: Container(
-                        decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(16),
-                              topRight: Radius.circular(16),
-                            )),
-                        width: double.infinity,
-                        height: _responsiveUtils.getSizeScreenHeight(context) - 70,
-                        child: ClipRRect(
-                            borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(16),
-                                topRight: Radius.circular(16)),
-                            child: _buildRulesFilterFormOnMobile(context)
-                        )
-                    )
+                          topRight: Radius.circular(16)),
+                      child: SafeArea(child: _buildRulesFilterFormOnMobile(context))
                   )
                 ),
               ),
@@ -66,28 +56,20 @@ class RuleFilterCreatorView extends GetWidget<RulesFilterCreatorController> {
             backgroundColor: Colors.black38,
             body: GestureDetector(
               onTap: () => FocusScope.of(context).unfocus(),
-              child: Align(alignment: Alignment.bottomCenter, child: Card(
-                  color: Colors.transparent,
-                  shape: const RoundedRectangleBorder(
+              child: Align(alignment: Alignment.bottomCenter, child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
                       borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(16),
                         topRight: Radius.circular(16),
                       )),
-                  child: Container(
-                      decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(16),
-                            topRight: Radius.circular(16),
-                          )),
-                      width: double.infinity,
-                      height: _responsiveUtils.getSizeScreenHeight(context) * 0.7,
-                      child: ClipRRect(
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(16),
-                            topRight: Radius.circular(16)),
-                          child: _buildRulesFilterFormOnTablet(context)
-                      )
+                  width: double.infinity,
+                  height: _responsiveUtils.getSizeScreenHeight(context) * 0.7,
+                  child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16)),
+                      child: _buildRulesFilterFormOnTablet(context)
                   )
               )),
             )
@@ -111,8 +93,7 @@ class RuleFilterCreatorView extends GetWidget<RulesFilterCreatorController> {
                           child: _buildRulesFilterFormOnDesktop(context)
                       )
                   )
-              )
-              ),
+              )),
             )
         )
     );
