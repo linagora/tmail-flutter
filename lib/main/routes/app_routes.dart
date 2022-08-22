63071275d01c8c8aed1ffb08213db19f7d874203
dab246 <tdvu@linagora.com>
2022-08-22 16:38:57 +0700

    TF-855 Fix some app routes
    (cherry picked from commit a14a2a9f63402e20a1976053fcb3690f26ebfda0)

--- lib/main/routes/app_routes.dart
+++ lib/main/routes/app_routes.dart
@@ -8,8 +8,8 @@ abstract class AppRoutes {
   static const SESSION = '/session';
   static const COMPOSER = '/composer';
   static const DESTINATION_PICKER = '/destinationPicker';
-  static const MAILBOX_CREATOR = '/mailboxCreator';
+  static const MAILBOX_CREATOR = '$MAILBOX_DASHBOARD/mailboxCreator';
   static const MANAGE_ACCOUNT = '$MAILBOX_DASHBOARD/manage_account';
-  static const IDENTITY_CREATOR = '/identity_creator';
-  static const RULES_FILTER_CREATOR = '/rules_filter_creator';
+  static const IDENTITY_CREATOR = '$MANAGE_ACCOUNT/identity_creator';
+  static const RULES_FILTER_CREATOR = '$MANAGE_ACCOUNT/rules_filter_creator';
 }
