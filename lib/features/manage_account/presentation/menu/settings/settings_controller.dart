87f02ef7fd6a78690c11204d1337fe0989aaaed0
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-08-24 12:12:42 +0700

    TF-788 Build up Settings Screen with SettingsFirstLevelTileBuilder

---
+++ lib/features/manage_account/presentation/menu/settings/settings_controller.dart
@@ -0,0 +1,20 @@
+import 'package:core/utils/app_logger.dart';
+import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/manage_account/presentation/manage_account_dashboard_controller.dart';
+import 'package:tmail_ui_user/features/manage_account/presentation/model/account_menu_item.dart';
+import 'package:tmail_ui_user/features/manage_account/presentation/model/settings_page_level.dart';
+
+class SettingsController extends GetxController {
+  final manageAccountDashboardController = Get.find<ManageAccountDashBoardController>();
+
+  void selectSettings(AccountMenuItem accountMenuItem) {
+    log('SettingsController::selectSettings(): $accountMenuItem');
+    manageAccountDashboardController.selectAccountMenuItem(accountMenuItem);
+    manageAccountDashboardController.settingsPageLevel.value = SettingsPageLevel.level1;
+  }
+
+  void backToUniversalSettings() {
+    log('SettingsController::backToUniversalSettings()');
+    manageAccountDashboardController.settingsPageLevel.value = SettingsPageLevel.universal;
+  }
+}
