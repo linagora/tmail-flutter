38ccb215fce4fe7f870383738dd31fc4eff3a40f
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-09-14 17:33:06 +0700

    TF-939 Update Get/Update Vacation Interactors

--- lib/features/manage_account/domain/usecases/get_all_vacation_interactor.dart
+++ lib/features/manage_account/domain/usecases/get_all_vacation_interactor.dart
@@ -4,18 +4,18 @@ import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
 import 'package:dartz/dartz.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
-import 'package:tmail_ui_user/features/manage_account/domain/repository/manage_account_repository.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/repository/vacation_repository.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/state/get_all_vacation_state.dart';
 
 class GetAllVacationInteractor {
-  final ManageAccountRepository manageAccountRepository;
+  final VacationRepository vacationRepository;
 
-  GetAllVacationInteractor(this.manageAccountRepository);
+  GetAllVacationInteractor(this.vacationRepository);
 
   Stream<Either<Failure, Success>> execute(AccountId accountId) async* {
     try {
       yield Right<Failure, Success>(LoadingGetAllVacation());
-      final listVacationResponse = await manageAccountRepository.getAllVacationResponse(accountId);
+      final listVacationResponse = await vacationRepository.getAllVacationResponse(accountId);
       yield Right<Failure, Success>(GetAllVacationSuccess(listVacationResponse));
     } catch (exception) {
       yield Left<Failure, Success>(GetAllVacationFailure(exception));
--- lib/features/manage_account/domain/usecases/update_vacation_interactor.dart
+++ lib/features/manage_account/domain/usecases/update_vacation_interactor.dart
@@ -5,18 +5,18 @@ import 'package:core/presentation/state/success.dart';
 import 'package:dartz/dartz.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/mail/vacation/vacation_response.dart';
-import 'package:tmail_ui_user/features/manage_account/domain/repository/manage_account_repository.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/repository/vacation_repository.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/state/update_vacation_state.dart';
 
 class UpdateVacationInteractor {
-  final ManageAccountRepository manageAccountRepository;
+  final VacationRepository vacationRepository;
 
-  UpdateVacationInteractor(this.manageAccountRepository);
+  UpdateVacationInteractor(this.vacationRepository);
 
   Stream<Either<Failure, Success>> execute(AccountId accountId, VacationResponse vacationResponse) async* {
     try {
       yield Right<Failure, Success>(LoadingUpdateVacation());
-      final listVacationResponse = await manageAccountRepository.updateVacation(accountId, vacationResponse);
+      final listVacationResponse = await vacationRepository.updateVacation(accountId, vacationResponse);
       yield Right<Failure, Success>(UpdateVacationSuccess(listVacationResponse));
     } catch (exception) {
       yield Left<Failure, Success>(UpdateVacationFailure(exception));
