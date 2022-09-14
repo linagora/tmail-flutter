cd21465c0d0af19e7ffdf0707032be8ff18c5d65
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-09-14 11:22:47 +0700

    TF-939 Update Tmail contact capabilityIdentifier
    (cherry picked from commit 5f2c42004e50e2829d4a921a0eeee231de479259)

--- contact/lib/contact/model/capability_contact.dart
+++ contact/lib/contact/model/capability_contact.dart
@@ -1,4 +1,4 @@
 
 import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
 
-final capabilityContact = CapabilityIdentifier(Uri.parse('com:linagora:params:jmap:contact:autocomplete'));
+final tmailContactCapabilityIdentifier = CapabilityIdentifier(Uri.parse('com:linagora:params:jmap:contact:autocomplete'));
