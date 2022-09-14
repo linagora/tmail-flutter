a7f244fb5a9f70c142fa78da01a9bad65ca28716
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-09-14 17:27:49 +0700

    TF-939 Separate VacationRepository
    (cherry picked from commit 5605e22bbf3086eac2691b7f9e1688a780209aef)

--- lib/features/manage_account/data/repository/manage_account_repository_impl.dart
+++ lib/features/manage_account/data/repository/manage_account_repository_impl.dart
@@ -4,13 +4,12 @@ import 'package:forward/forward/tmail_forward.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
 import 'package:jmap_dart_client/jmap/identities/identity.dart';
-import 'package:jmap_dart_client/jmap/mail/vacation/vacation_response.dart';
 import 'package:rule_filter/rule_filter/tmail_rule.dart';
 import 'package:tmail_ui_user/features/manage_account/data/datasource/manage_account_datasource.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/add_recipients_in_forwarding_request.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_email_rule_filter_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_identity_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/delete_email_rule_request.dart';
-import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_email_rule_filter_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/delete_recipient_in_forwarding_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/edit_email_rule_filter_request.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/edit_identity_request.dart';
@@ -72,16 +71,6 @@ class ManageAccountRepositoryImpl extends ManageAccountRepository {
   @override
   Future<TMailForward> getForward(AccountId accountId) {
     return dataSource.getForward(accountId);
-  }
-
-  @override
-  Future<List<VacationResponse>> getAllVacationResponse(AccountId accountId) {
-    return dataSource.getAllVacationResponse(accountId);
-  }
-
-  @override
-  Future<List<VacationResponse>> updateVacation(AccountId accountId, VacationResponse vacationResponse) {
-    return dataSource.updateVacation(accountId, vacationResponse);
   }
 
   @override
