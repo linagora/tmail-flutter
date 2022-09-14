aea1cc3a5400da576f64d94872c32f24c1a5cc1b
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-09-14 17:27:49 +0700

    TF-939 Separate VacationRepository
    (cherry picked from commit 5605e22bbf3086eac2691b7f9e1688a780209aef)

---
+++ lib/features/manage_account/domain/repository/vacation_repository.dart
@@ -0,0 +1,8 @@
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/mail/vacation/vacation_response.dart';
+
+abstract class VacationRepository {
+  Future<List<VacationResponse>> getAllVacationResponse(AccountId accountId);
+
+  Future<List<VacationResponse>> updateVacation(AccountId accountId, VacationResponse vacationResponse);
+}
