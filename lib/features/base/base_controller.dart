a5fe381b33cc9257bf7e39fad3bbb4b1dd55faa1
dab246 <tdvu@linagora.com>
2022-09-22 19:05:36 +0700

    TF-971 Add method `injectAutoCompleteBindings` in `baseController`
    (cherry picked from commit 129118bd3c00427cfc107cdbb49062c841b8e270)

--- lib/features/base/base_controller.dart
+++ lib/features/base/base_controller.dart
@@ -1,9 +1,15 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:contact/contact/model/capability_contact.dart';
 import 'package:core/core.dart';
 import 'package:dartz/dartz.dart';
 import 'package:get/get.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
 import 'package:tmail_ui_user/features/base/mixin/message_dialog_action_mixin.dart';
 import 'package:tmail_ui_user/features/base/mixin/popup_context_menu_action_mixin.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/bindings/contact_autocomplete_bindings.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/bindings/tmail_autocomplete_bindings.dart';
+import 'package:tmail_ui_user/main/error/capability_validator.dart';
 
 abstract class BaseController extends GetxController with MessageDialogActionMixin, PopupContextMenuActionMixin {
   final viewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
@@ -64,6 +70,19 @@ abstract class BaseController extends GetxController with MessageDialogActionMix
     FpsManager().stop();
     if (fpsCallback != null) {
       FpsManager().removeFpsCallback(fpsCallback!);
+    }
+  }
+
+  void injectAutoCompleteBindings(Session? session, AccountId? accountId) {
+    if (session == null || accountId == null) {
+      return;
+    }
+    try {
+      ContactAutoCompleteBindings().dependencies();
+      requireCapability(session, accountId, [tmailContactCapabilityIdentifier]);
+      TMailAutoCompleteBindings().dependencies();
+    } catch (e) {
+      logError('BaseController::injectAutoCompleteBindings(): exception: $e');
     }
   }
 }
