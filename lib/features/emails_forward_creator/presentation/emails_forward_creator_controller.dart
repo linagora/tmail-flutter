7c25aca648bfe8c5a06bdde8a60f9645e337adc1
dab246 <tdvu@linagora.com>
2022-09-22 19:07:55 +0700

    TF-971 Implement method `injectAutoCompleteBindings` in all controller handle autocomplete
    (cherry picked from commit ac9f00c948a0154145a4e7e73750099d3780eb18)

--- lib/features/emails_forward_creator/presentation/emails_forward_creator_controller.dart
+++ lib/features/emails_forward_creator/presentation/emails_forward_creator_controller.dart
@@ -12,8 +12,6 @@ import 'package:tmail_ui_user/features/composer/domain/model/contact_suggestion_
 import 'package:tmail_ui_user/features/composer/domain/state/get_autocomplete_state.dart';
 import 'package:tmail_ui_user/features/composer/domain/usecases/get_autocomplete_interactor.dart';
 import 'package:tmail_ui_user/features/composer/domain/usecases/get_autocomplete_with_device_contact_interactor.dart';
-import 'package:tmail_ui_user/features/composer/presentation/extensions/session_extension.dart';
-import 'package:tmail_ui_user/features/emails_forward_creator/presentation/model/mails_forward_creator_arguments.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/forward/forward_controller.dart';
 import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 
@@ -23,21 +21,20 @@ class EmailsForwardCreatorController extends BaseController {
   final ContactSuggestionSource _contactSuggestionSource = ContactSuggestionSource.tMailContact;
   final _forwardController = Get.find<ForwardController>();
 
-  late AccountId _accountId;
-  late Session? _session;
-  late GetAutoCompleteWithDeviceContactInteractor _getAutoCompleteWithDeviceContactInteractor;
-  late GetAutoCompleteInteractor _getAutoCompleteInteractor;
+  GetAutoCompleteWithDeviceContactInteractor? _getAutoCompleteWithDeviceContactInteractor;
+  GetAutoCompleteInteractor? _getAutoCompleteInteractor;
 
   final listEmailForwards = RxSet<EmailAddress>();
 
-  EmailsForwardCreatorController(
-      this._getAutoCompleteWithDeviceContactInteractor,
-      this._getAutoCompleteInteractor,
-  );
+  EmailsForwardCreatorController();
+
+  AccountId? get _accountId => _forwardController.emailsForwardCreatorArguments.value?.accountId;
+
+  Session? get _session => _forwardController.emailsForwardCreatorArguments.value?.session;
 
   @override
   void onReady() {
-    _getArguments();
+    injectAutoCompleteBindings(_session, _accountId);
     super.onReady();
   }
 
@@ -53,35 +50,53 @@ class EmailsForwardCreatorController extends BaseController {
   @override
   void onError(error) {}
 
-  void _getArguments() {
-    final arguments = Get.arguments;
-    if (arguments is MailsForwardCreatorArguments) {
-      _accountId = arguments.accountId;
-      _session = arguments.session;
+  @override
+  void injectAutoCompleteBindings(Session? session, AccountId? accountId) {
+    try {
+      super.injectAutoCompleteBindings(session, accountId);
+      _getAutoCompleteWithDeviceContactInteractor = Get.find<GetAutoCompleteWithDeviceContactInteractor>();
+      _getAutoCompleteInteractor = Get.find<GetAutoCompleteInteractor>();
+    } catch (e) {
+      logError('EmailsForwardCreatorController::injectAutoCompleteBindings(): $e');
     }
   }
 
-  Future<List<EmailAddress>> getAutoCompleteSuggestion(
-      {required String word}) async {
+  Future<List<EmailAddress>> getAutoCompleteSuggestion(String word) async {
     log('EmailsForwardCreatorController::getAutoCompleteSuggestion(): $word | $_contactSuggestionSource');
-
-    if(_session?.hasSupportTmailAutoComplete != true) return <EmailAddress>[];
-
-    _getAutoCompleteWithDeviceContactInteractor = Get.find<GetAutoCompleteWithDeviceContactInteractor>();
-    _getAutoCompleteInteractor = Get.find<GetAutoCompleteInteractor>();
+    try {
+      _getAutoCompleteWithDeviceContactInteractor = Get.find<GetAutoCompleteWithDeviceContactInteractor>();
+      _getAutoCompleteInteractor = Get.find<GetAutoCompleteInteractor>();
+    } catch (e) {
+      logError('EmailsForwardCreatorController::getAutoCompleteSuggestion(): Exception $e');
+    }
 
     if (_contactSuggestionSource == ContactSuggestionSource.all) {
-      return await _getAutoCompleteWithDeviceContactInteractor
-          .execute(AutoCompletePattern(word: word, accountId: _accountId))
-          .then((value) => value.fold(
-              (failure) => <EmailAddress>[],
-              (success) => success is GetAutoCompleteSuccess ? success.listEmailAddress : <EmailAddress>[]));
+      if (_getAutoCompleteWithDeviceContactInteractor == null || _getAutoCompleteInteractor == null) {
+        logError('EmailsForwardCreatorController::_getAutoCompleteWithDeviceContactInteractor(): is null');
+        return <EmailAddress>[];
+      } else {
+        return await _getAutoCompleteWithDeviceContactInteractor!
+            .execute(AutoCompletePattern(word: word, accountId: _accountId))
+            .then((value) => value.fold(
+                (failure) => <EmailAddress>[],
+                (success) => success is GetAutoCompleteSuccess
+                    ? success.listEmailAddress
+                    : <EmailAddress>[]));
+      }
+    } else {
+      if (_getAutoCompleteInteractor == null) {
+        logError('EmailsForwardCreatorController::_getAutoCompleteInteractor(): is null');
+        return <EmailAddress>[];
+      } else {
+        return await _getAutoCompleteInteractor!
+            .execute(AutoCompletePattern(word: word, accountId: _accountId))
+            .then((value) => value.fold(
+                (failure) => <EmailAddress>[],
+                (success) => success is GetAutoCompleteSuccess
+                    ? success.listEmailAddress
+                    : <EmailAddress>[]));
+      }
     }
-    return await _getAutoCompleteInteractor
-        .execute(AutoCompletePattern(word: word, accountId: _accountId))
-        .then((value) => value.fold(
-            (failure) => <EmailAddress>[],
-            (success) => success is GetAutoCompleteSuccess ? success.listEmailAddress : <EmailAddress>[]));
   }
 
   void addToListEmailForwards(EmailAddress emailAddress) {
@@ -100,6 +115,8 @@ class EmailsForwardCreatorController extends BaseController {
 
   void closeView(BuildContext context) {
     FocusScope.of(context).unfocus();
+    clearAll();
+
     if(kIsWeb) {
       _clearListEmailForwards();
       _forwardController.accountDashBoardController.emailsForwardCreatorIsActive.toggle();
@@ -113,14 +130,18 @@ class EmailsForwardCreatorController extends BaseController {
     if(inputEmailForwardController.text.trim().isNotEmpty) {
       listEmailForwards.add(EmailAddress(null, inputEmailForwardController.text.trim()));
     }
-    inputEmailForwardController.clear();
+    clearAll();
+
+    if (listEmailForwards.isNotEmpty) {
+      final newListEmailForwards = List<EmailAddress>.from(listEmailForwards);
+      _forwardController.handleAddRecipients(newListEmailForwards);
+    }
 
     if(kIsWeb) {
-      _forwardController.accountDashBoardController.emailsForwardCreatorIsActive.toggle();
-      _forwardController.handleAddRecipients(listEmailForwards.toList());
       _clearListEmailForwards();
+      _forwardController.accountDashBoardController.emailsForwardCreatorIsActive.toggle();
     } else {
-      popBack(result: listEmailForwards.toList());
+      popBack();
     }
   }
 }
