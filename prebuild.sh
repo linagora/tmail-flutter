fb19c5a66c1eb36049c17b8496f56bac4ead3f92
Nguyen Thai <tknguyen@linagora.com>
2022-09-28 10:52:22 +0700

    Added workflow for CI/CD

--- prebuild.sh
+++ prebuild.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
-echo Pre-build ...
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
 
 cd core
 flutter pub get
 
-# Install necessary pods
-cd ../ios
-flutter pub get && pod install
+## Install necessary pods
+#cd ../ios
+#flutter pub get && pod install
 
 cd ../model
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
@@ -14,17 +17,14 @@ flutter pub get && flutter pub run build_runner build --delete-conflicting-outpu
 cd ../contact
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 
-cd ../rule_filter
-flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
-
 cd ../forward
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 
-cd ..
+cd ../rule_filter
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 
-flutter pub get && flutter pub run intl_generator:extract_to_arb --output-dir=./lib/l10n lib/main/localizations/app_localizations.dart
-
-flutter pub get && flutter pub run intl_generator:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/main/localizations/app_localizations.dart lib/l10n/intl*.arb
-
-echo \[Completed\] pre-build!!!
+cd ..
+flutter pub get \
+    && flutter pub run build_runner build --delete-conflicting-outputs \
+    && flutter pub run intl_generator:extract_to_arb --output-dir=./lib/l10n lib/main/localizations/app_localizations.dart \
+    && flutter pub run intl_generator:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/main/localizations/app_localizations.dart lib/l10n/intl*.arb
