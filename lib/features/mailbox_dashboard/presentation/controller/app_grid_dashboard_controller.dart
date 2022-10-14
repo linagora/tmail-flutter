e3aebc4bfb968edf0633276688398be4eb573b5d
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-10-15 00:14:51 +0700

    TF-1018 Add AppGridDashboardController to handle appGridDashboard

---
+++ lib/features/mailbox_dashboard/presentation/controller/app_grid_dashboard_controller.dart
@@ -0,0 +1,31 @@
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:core/utils/app_logger.dart';
+import 'package:dartz/dartz.dart';
+import 'package:get/get_rx/get_rx.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/app_dashboard/linagora_applications.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/usecases/get_app_dashboard_configuration_interactor.dart';
+import 'package:tmail_ui_user/main/utils/app_config.dart';
+
+class AppGridDashboardController {
+  final isAppGridDashboardOverlayOpen = false.obs;
+  final linagoraApplications = Rxn<LinagoraApplications>();
+
+  final GetAppDashboardConfigurationInteractor _getAppDashboardConfigurationInteractor;
+
+  AppGridDashboardController(this._getAppDashboardConfigurationInteractor);
+
+  void toggleAppGridDashboard() {
+    isAppGridDashboardOverlayOpen.toggle();
+  }
+
+  Stream<Either<Failure, Success>> showDashboardAction() {
+    return _getAppDashboardConfigurationInteractor.execute(AppConfig.appDashboardConfigurationPath);
+  }
+
+  void handleShowAppDashboard(LinagoraApplications linagoraApps) {
+    log('AppGridDashboardController::handleShowAppDashboard(): $linagoraApps');
+    isAppGridDashboardOverlayOpen.value = true;
+    linagoraApplications.value = linagoraApps;
+  }
+}
