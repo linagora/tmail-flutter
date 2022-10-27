bc102dd9ec9e312f166c86ad722ee8dc57f63692
ManhNTX <tmnguyen@linagora.com>
2022-10-27 16:23:36 +0700

    TF-996: add previous / Next message of a mailbox on mobile/tablet

---
+++ lib/features/email/domain/model/email_loaded.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:model/model.dart';
+
+class EmailLoaded with EquatableMixin {
+  final List<EmailContent> emailContents;
+  final List<EmailContent> emailContentsDisplayed;
+  final List<Attachment> attachments;
+  final Email? emailCurrent;
+
+  EmailLoaded(
+      this.emailContents,
+      this.emailContentsDisplayed,
+      this.attachments,
+      this.emailCurrent
+      );
+
+  @override
+  List<Object?> get props => [
+    emailContents,
+    emailContentsDisplayed,
+    attachments,
+    emailCurrent
+  ];
+}
