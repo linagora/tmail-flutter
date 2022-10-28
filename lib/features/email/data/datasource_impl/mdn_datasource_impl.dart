db6acf4020277f8ec51673587cf56acb9f89029f
dab246 <tdvu@linagora.com>
2022-10-28 12:09:04 +0700

    TF-1115 Implement catch exception for MdnDataSource

--- lib/features/email/data/datasource_impl/mdn_datasource_impl.dart
+++ lib/features/email/data/datasource_impl/mdn_datasource_impl.dart
@@ -5,19 +5,21 @@ import 'package:jmap_dart_client/jmap/mdn/mdn.dart';
 import 'package:tmail_ui_user/features/email/data/datasource/mdn_datasource.dart';
 import 'package:tmail_ui_user/features/email/data/network/mdn_api.dart';
 import 'package:tmail_ui_user/features/email/domain/model/send_receipt_to_sender_request.dart';
+import 'package:tmail_ui_user/main/exceptions/exception_thrower.dart';
 
 class MdnDataSourceImpl extends MdnDataSource {
 
   final MdnAPI _mdnAPI;
+  final ExceptionThrower _exceptionThrower;
 
-  MdnDataSourceImpl(this._mdnAPI);
+  MdnDataSourceImpl(this._mdnAPI, this._exceptionThrower);
 
   @override
   Future<MDN?> sendReceiptToSender(AccountId accountId, SendReceiptToSenderRequest request) {
     return Future.sync(() async {
       return await _mdnAPI.sendReceiptToSender(accountId, request);
     }).catchError((error) {
-      throw error;
+      _exceptionThrower.throwException(error);
     });
   }
 }
