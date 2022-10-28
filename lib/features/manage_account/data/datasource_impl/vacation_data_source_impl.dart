d0fcad1caca12224d8b596870278f9f5b1e42f52
dab246 <tdvu@linagora.com>
2022-10-28 12:45:06 +0700

    TF-1115 Implement catch exception for VacationDataSource

--- lib/features/manage_account/data/datasource_impl/vacation_data_source_impl.dart
+++ lib/features/manage_account/data/datasource_impl/vacation_data_source_impl.dart
@@ -2,18 +2,21 @@ import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/mail/vacation/vacation_response.dart';
 import 'package:tmail_ui_user/features/manage_account/data/datasource/vacation_data_source.dart';
 import 'package:tmail_ui_user/features/manage_account/data/network/vacation_api.dart';
+import 'package:tmail_ui_user/main/exceptions/exception_thrower.dart';
 
 class VacationDataSourceImpl extends VacationDataSource {
 
   final VacationAPI _vacationAPI;
-  VacationDataSourceImpl(this._vacationAPI);
+  final ExceptionThrower _exceptionThrower;
+
+  VacationDataSourceImpl(this._vacationAPI, this._exceptionThrower);
 
   @override
   Future<List<VacationResponse>> getAllVacationResponse(AccountId accountId) {
     return Future.sync(() async {
       return await _vacationAPI.getAllVacationResponse(accountId);
     }).catchError((error) {
-      throw error;
+      _exceptionThrower.throwException(error);
     });
   }
 
@@ -22,7 +25,7 @@ class VacationDataSourceImpl extends VacationDataSource {
     return Future.sync(() async {
       return await _vacationAPI.updateVacation(accountId, vacationResponse);
     }).catchError((error) {
-      throw error;
+      _exceptionThrower.throwException(error);
     });
   }
 }
