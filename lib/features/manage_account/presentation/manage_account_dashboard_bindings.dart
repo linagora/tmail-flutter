33cba8b1472e50027fdc3c6a408027382b3d4acb
dab246 <tdvu@linagora.com>
2022-10-28 12:46:49 +0700

    TF-1115 Implement catch exception for ManageAccountDataSource

--- lib/features/manage_account/presentation/manage_account_dashboard_bindings.dart
+++ lib/features/manage_account/presentation/manage_account_dashboard_bindings.dart
@@ -30,6 +30,7 @@ import 'package:tmail_ui_user/features/manage_account/presentation/manage_accoun
 import 'package:tmail_ui_user/features/manage_account/presentation/menu/manage_account_menu_bindings.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/menu/settings/settings_bindings.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/profiles/profiles_bindings.dart';
+import 'package:tmail_ui_user/main/exceptions/remote_exception_thrower.dart';
 
 class ManageAccountDashBoardBindings extends BaseBindings {
 
@@ -68,8 +69,9 @@ class ManageAccountDashBoardBindings extends BaseBindings {
         Get.find<OidcConfigurationCacheManager>()
     ));
     Get.lazyPut(() => ManageAccountDataSourceImpl(
-        Get.find<ManageAccountAPI>(),
-        Get.find<LanguageCacheManager>()));
+      Get.find<ManageAccountAPI>(),
+      Get.find<LanguageCacheManager>(),
+      Get.find<RemoteExceptionThrower>()));
   }
 
   @override
