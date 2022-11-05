095b2fb2bdadbb711a89b48059120353bff8e3c8
ManhNTX <tmnguyen@linagora.com>
2022-11-05 18:25:31 +0700

    TF-987: make email range selection function simple

--- lib/features/mailbox_dashboard/presentation/widgets/top_bar_thread_selection.dart
+++ lib/features/mailbox_dashboard/presentation/widgets/top_bar_thread_selection.dart
@@ -55,8 +55,8 @@ class TopBarThreadSelection {
       buildIconWeb(
           icon: SvgPicture.asset(
               listEmail.isAllEmailRead
-                  ? imagePaths.icUnread
-                  : imagePaths.icRead,
+                  ? imagePaths.icRead
+                  : imagePaths.icUnread,
               fit: BoxFit.fill),
           tooltip: listEmail.isAllEmailRead
               ? AppLocalizations.of(context).mark_as_unread
