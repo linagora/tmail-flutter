05a2a87dcadea00a969e34556b4d0b782ad9ced3
ManhNTX <tmnguyen@linagora.com>
2022-11-10 15:09:39 +0700

    TF-1014: change limit parameter

--- lib/features/quotas/presentation/quotas_controller.dart
+++ lib/features/quotas/presentation/quotas_controller.dart
@@ -2,25 +2,28 @@ import 'package:core/core.dart';
 import 'package:get/get.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
 import 'package:jmap_dart_client/jmap/quotas/data_types.dart';
 import 'package:tmail_ui_user/features/base/base_controller.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart';
 import 'package:tmail_ui_user/features/quotas/domain/state/get_quotas_state.dart';
 import 'package:tmail_ui_user/features/quotas/domain/use_case/get_quotas_interactor.dart';
 import 'package:tmail_ui_user/features/quotas/presentation/model/quotas_state.dart';
+import 'package:tmail_ui_user/main/error/capability_validator.dart';
 
 class QuotasController extends BaseController {
   final MailboxDashBoardController mailboxDashBoardController = Get.find<MailboxDashBoardController>();
   final GetQuotasInteractor _getQuotasInteractor;
   final usedCapacity = Rx<num>(0);
-  final softLimitCapacity = Rx<num>(0);
+  final limitCapacity = Rx<num>(0);
   final warningLimitCapacity = Rx<num>(0);
   final enableShowQuotas = false.obs;
   final quotasState = QuotasState.normal.obs;
+  final warningProgressConstant = 0.9;
   late Worker accountIdWorker;
 
-  double get progressUsedCapacity => softLimitCapacity.value != 0
-      ? (usedCapacity.value / softLimitCapacity.value)
+  double get progressUsedCapacity => limitCapacity.value != 0
+      ? (usedCapacity.value / limitCapacity.value)
       : 0;
 
   bool get enableShowWarningQuotas =>
@@ -35,18 +38,19 @@ class QuotasController extends BaseController {
 
   void _initWorker() {
     accountIdWorker = ever(mailboxDashBoardController.accountId, (accountId) {
-      if (accountId is AccountId) {
-        _getQuotasAction(accountId);
+      if (accountId is AccountId && mailboxDashBoardController.sessionCurrent!= null) {
+        _getQuotasAction(accountId, mailboxDashBoardController.sessionCurrent!);
       }
     });
   }
 
-  void _getQuotasAction(AccountId accountId) {
-    enableShowQuotas.value = mailboxDashBoardController.sessionCurrent != null &&
-        mailboxDashBoardController.sessionCurrent!.capabilities.containsValue(CapabilityIdentifier.jmapQuota);
-
-    if(enableShowQuotas.isTrue) {
+  void _getQuotasAction(AccountId accountId, Session session) {
+    try {
+      requireCapability(session, accountId, [CapabilityIdentifier.jmapQuota]);
+      enableShowQuotas.value = true;
       consumeState(_getQuotasInteractor.execute(mailboxDashBoardController.accountId.value!));
+    } catch (e) {
+      logError('QuotasController::_getQuotasAction():$e');
     }
   }
 
@@ -70,9 +74,9 @@ class QuotasController extends BaseController {
     try {
       final quotas = success.quotas.firstWhere((e) => e.resourceType == ResourceType.octets);
       usedCapacity.value = quotas.used.value;
-      warningLimitCapacity.value = quotas.warnLimit?.value ?? 0;
-      softLimitCapacity.value = quotas.softLimit?.value ?? 0;
-      if(usedCapacity.value >= softLimitCapacity.value) {
+      warningLimitCapacity.value = quotas.limit.value * warningProgressConstant;
+      limitCapacity.value = quotas.limit.value;
+      if(usedCapacity.value >= limitCapacity.value) {
         quotasState.value = QuotasState.runOutOfStorage;
       } else if (usedCapacity.value >= warningLimitCapacity.value) {
         quotasState.value = QuotasState.runningOutOfStorage;
