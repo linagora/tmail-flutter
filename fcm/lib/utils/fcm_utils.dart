3b2d0227e66ee75ef742929082f9be4ac45418e9
dab246 <tdvu@linagora.com>
2022-11-15 16:24:40 +0700

    TF-1204 Add `FirebaseRegistration/get` method/response and write unit test

--- fcm/lib/utils/fcm_utils.dart
+++
@@ -1,6 +0,0 @@
-
-import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
-
-class FCMUtils {
-  static final capabilityFCM = CapabilityIdentifier(Uri.parse('com:linagora:params:jmap:firebase:push'));
-}
