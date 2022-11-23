76cbf798ab04de0e1abc7693632bec23808d7a21
dab246 <tdvu@linagora.com>
2022-11-23 15:06:08 +0700

    TF-1217 Change fcm capability
    (cherry picked from commit 5ed8e85c66d7ddac29960f364d5e1a0b5cfbae04)

--- fcm/lib/model/firebase_capability.dart
+++ fcm/lib/model/firebase_capability.dart
@@ -1,8 +1,6 @@
 
 import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
 
-class FirebaseCapability extends CapabilityIdentifier {
-  static final identifier = FirebaseCapability(Uri.parse('com:linagora:params:jmap:firebase:push'));
-
-  FirebaseCapability(super.value);
+class FirebaseCapability {
+  static final fcmIdentifier = CapabilityIdentifier(Uri.parse('com:linagora:params:jmap:firebase:push'));
 }
