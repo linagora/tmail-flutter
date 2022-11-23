4008bc307743340ce02723c2a017836234080b00
dab246 <tdvu@linagora.com>
2022-11-23 15:12:21 +0700

    TF-1217 Convert fcm data message to StateChange
    (cherry picked from commit d73b2bc1be71deaf038f84ff7cc1be3b99032ac2)

---
+++ lib/features/push_notification/presentation/utils/fcm_utils.dart
@@ -0,0 +1,76 @@
+
+import 'package:core/utils/app_logger.dart';
+import 'package:dartz/dartz.dart';
+import 'package:fcm/model/type_name.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
+import 'package:jmap_dart_client/jmap/push/state_change.dart';
+import 'package:jmap_dart_client/jmap/push/type_state.dart';
+
+class FcmUtils {
+  FcmUtils._internal();
+
+  static final FcmUtils _instance = FcmUtils._internal();
+
+  static FcmUtils get instance => _instance;
+
+  StateChange? convertFirebaseDataMessageToStateChange(Map<String, dynamic> dataMessage) {
+    log('FcmUtils::convertFirebaseDataMessageToStateChange():dataMessage: $dataMessage');
+    Map<String, dynamic> mapData;
+    if (dataMessage.containsKey('data')) {
+      mapData = dataMessage['data'];
+    } else {
+      mapData = dataMessage;
+    }
+
+    final listKeys = mapData.keys;
+    Map<AccountId, TypeState> mapTypeStateChanged = {};
+
+    for (var key in listKeys) {
+      final tupleKey = _parseFromKeyFirebaseDataMessage(key);
+      final accountId = tupleKey.value1;
+      final typeName = tupleKey.value2;
+      final value = mapData[key];
+
+      if (accountId == null || typeName == null || isEmpty(value)) {
+        log('FcmUtils::convertFirebaseDataMessageToStateChange(): key or value is null');
+        continue;
+      }
+
+      if (mapTypeStateChanged.containsKey(accountId)) {
+        mapTypeStateChanged[accountId]!.typeState[typeName.value] = value;
+      } else {
+        mapTypeStateChanged[accountId] = TypeState({typeName.value: value});
+      }
+    }
+
+    if (mapTypeStateChanged.isEmpty) {
+      return null;
+    } else {
+      return StateChange('StateChange', mapTypeStateChanged);
+    }
+  }
+
+  Tuple2<AccountId?, TypeName?> _parseFromKeyFirebaseDataMessage(String key) {
+    if (_keyFirebaseDataMessageIsValid(key)) {
+      final listObject = key.split(':');
+      final accountId = AccountId(Id(listObject.first));
+      final typeName = TypeName(listObject.last);
+      return Tuple2(accountId, typeName);
+    } else {
+      return const Tuple2(null, null);
+    }
+  }
+
+  bool _keyFirebaseDataMessageIsValid(String key) {
+    if (key.contains(':')) {
+      return key.split(':').length == 2;
+    } else {
+      return false;
+    }
+  }
+
+  bool isEmpty(dynamic object) {
+    return object == null || (object is String && object.isEmpty);
+  }
+}
