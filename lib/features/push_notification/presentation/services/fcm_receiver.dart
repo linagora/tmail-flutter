2584e9dad5d6298f3564a671ca49d55b8136ae4c
dab246 <tdvu@linagora.com>
2022-11-24 14:35:05 +0700

    TF-438 Handle show local notification when type is `EmailDelivery` on background or terminate
    (cherry picked from commit d42756b4ea39f77335b50d4b6282c1e4b9dd28e6)

--- lib/features/push_notification/presentation/services/fcm_receiver.dart
+++ lib/features/push_notification/presentation/services/fcm_receiver.dart
@@ -3,16 +3,9 @@ import 'package:core/utils/app_logger.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:tmail_ui_user/features/push_notification/presentation/services/fcm_service.dart';
 
-int semaphoreBackground = 0;
-
 @pragma('vm:entry-point')
 Future<void> handleFirebaseBackgroundMessage(RemoteMessage message) async {
-  if (semaphoreBackground != 0) {
-    return;
-  }
-  semaphoreBackground = 1;
-  Future.delayed(const Duration(milliseconds: FcmService.durationMessageComing)).then((_) => semaphoreBackground = 0);
-
+  log('FcmReceiver::handleFirebaseBackgroundMessage():messageId: ${message.messageId}');
   log('FcmReceiver::handleFirebaseBackgroundMessage(): ${message.data}');
   FcmService.instance.handleFirebaseBackgroundMessage(message);
 }
