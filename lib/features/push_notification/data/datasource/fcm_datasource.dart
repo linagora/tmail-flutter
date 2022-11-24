46ce8ed635e3e91a1fa032f0f8a99cecee80b944
dab246 <tdvu@linagora.com>
2022-11-24 14:30:05 +0700

    TF-438 Add abstract for get/store/delete `StateToRefresh` and get email changes to push notification
    (cherry picked from commit 17dde9366f6f6df53d1b64f268bd21def220198d)

--- lib/features/push_notification/data/datasource/fcm_datasource.dart
+++ lib/features/push_notification/data/datasource/fcm_datasource.dart
@@ -1,5 +1,7 @@
 
+import 'package:fcm/model/type_name.dart';
 import 'package:model/fcm/fcm_token_dto.dart';
+import 'package:jmap_dart_client/jmap/core/state.dart' as jmap;
 
 abstract class FCMDatasource {
   Future<FCMTokenDto> getFCMToken(String accountId);
@@ -7,4 +9,10 @@ abstract class FCMDatasource {
   Future<void> setFCMToken(FCMTokenDto fcmToken);
 
   Future<void> deleteFCMToken(String accountId);
+
+  Future<bool> storeStateToRefresh(TypeName typeName, jmap.State newState);
+
+  Future<jmap.State> getStateToRefresh(TypeName typeName);
+
+  Future<bool> deleteStateToRefresh(TypeName typeName);
 }
