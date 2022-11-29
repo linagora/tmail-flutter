7b2f5fe195e647e137788e7868afd7f140b7eb80
dab246 <tdvu@linagora.com>
2022-11-29 16:23:35 +0700

    TF-439 Add abstract method to register new token
    (cherry picked from commit 92da2d51523215946a1abb526bfd999518c85b27)

--- lib/features/push_notification/data/datasource/fcm_datasource.dart
+++ lib/features/push_notification/data/datasource/fcm_datasource.dart
@@ -3,6 +3,7 @@ import 'package:fcm/model/firebase_subscription.dart';
 import 'package:fcm/model/type_name.dart';
 import 'package:model/fcm/fcm_token_dto.dart';
 import 'package:jmap_dart_client/jmap/core/state.dart' as jmap;
+import 'package:tmail_ui_user/features/push_notification/domain/model/register_new_token_request.dart';
 
 abstract class FCMDatasource {
   Future<FCMTokenDto> getFCMToken(String accountId);
@@ -20,4 +21,6 @@ abstract class FCMDatasource {
   Future<bool> storeDeviceId(String deviceId);
 
   Future<FirebaseSubscription> getFirebaseSubscriptionByDeviceId(String deviceId);
+
+  Future<FirebaseSubscription> registerNewToken(RegisterNewTokenRequest newTokenRequest);
 }
