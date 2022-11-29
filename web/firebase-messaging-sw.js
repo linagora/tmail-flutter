1c92d34d547385d1484ef8f3af4042ba1f6c0447
ManhNTX <tmnguyen@linagora.com>
2022-11-29 20:11:15 +0700

    TF-1246: Enable FCM for Web browser
    (cherry picked from commit 045c7383e645490bf94c63284d82a889cd7d1461)

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+    apiKey: "...",
+    authDomain: "...",
+    databaseURL: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "...",
+});
+const messaging = firebase.messaging();
