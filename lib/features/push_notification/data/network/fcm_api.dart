f06349f2d4598a490be87026a0dc945849f02841
dab246 <tdvu@linagora.com>
2022-11-29 16:24:04 +0700

    TF-439 Implement handle register new token when expired
    (cherry picked from commit d5e093150f34dc437caafc6e29ef40c79763bc99)

--- lib/features/push_notification/data/network/fcm_api.dart
+++ lib/features/push_notification/data/network/fcm_api.dart
@@ -1,10 +1,13 @@
 
+import 'package:fcm/method/set/firebase_subscription_set_method.dart';
+import 'package:fcm/method/set/firebase_subscription_set_response.dart';
 import 'package:fcm/method/get/firebase_subscription_get_method.dart';
 import 'package:fcm/method/get/firebase_subscription_get_response.dart';
 import 'package:fcm/model/firebase_subscription.dart';
 import 'package:jmap_dart_client/http/http_client.dart';
 import 'package:jmap_dart_client/jmap/jmap_request.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/exceptions/fcm_exception.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/model/register_new_token_request.dart';
 
 class FcmApi {
 
@@ -36,5 +39,27 @@ class FcmApi {
     } else {
       throw NotFoundFirebaseSubscriptionException();
     }
+  }
+
+  Future<FirebaseSubscription> registerNewToken(RegisterNewTokenRequest newTokenRequest) async {
+    final processingInvocation = ProcessingInvocation();
+    final requestBuilder = JmapRequestBuilder(_httpClient, processingInvocation);
+
+    final firebaseSubscriptionSetMethod = FirebaseSubscriptionSetMethod()
+      ..addCreate(
+        newTokenRequest.createRequestId,
+        newTokenRequest.firebaseSubscription
+      );
+    final firebaseSubscriptionSetInvocation = requestBuilder.invocation(firebaseSubscriptionSetMethod);
+    final response = await (requestBuilder
+        ..usings(firebaseSubscriptionSetMethod.requiredCapabilities))
+      .build()
+      .execute();
+
+    final firebaseSubscriptionSetResponse = response.parse<FirebaseSubscriptionSetResponse>(
+      firebaseSubscriptionSetInvocation.methodCallId,
+      FirebaseSubscriptionSetResponse.deserialize);
+
+    return firebaseSubscriptionSetResponse!.created![newTokenRequest.createRequestId]!;
   }
 }
