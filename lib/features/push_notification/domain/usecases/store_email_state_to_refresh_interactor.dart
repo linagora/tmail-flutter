f694f61d6e4a4676e341fbb9f46c7987f54a73e6
dab246 <tdvu@linagora.com>
2022-11-30 11:51:46 +0700

    TF-437 Store state change of mailbox in background/terminate

--- lib/features/push_notification/domain/usecases/store_email_state_to_refresh_interactor.dart
+++ lib/features/push_notification/domain/usecases/store_email_state_to_refresh_interactor.dart
@@ -4,17 +4,17 @@ import 'package:dartz/dartz.dart';
 import 'package:fcm/model/type_name.dart';
 import 'package:jmap_dart_client/jmap/core/state.dart' as jmap;
 import 'package:tmail_ui_user/features/push_notification/domain/repository/fcm_repository.dart';
-import 'package:tmail_ui_user/features/push_notification/domain/state/store_fcm_state_change_state.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/state/store_email_state_to_refresh_state.dart';
 
 class StoreEmailStateToRefreshInteractor {
   final FCMRepository _fcmRepository;
 
   StoreEmailStateToRefreshInteractor(this._fcmRepository);
 
-  Stream<Either<Failure, Success>> execute(TypeName typeName, jmap.State newState) async* {
+  Stream<Either<Failure, Success>> execute(jmap.State newState) async* {
     try {
       yield Right<Failure, Success>(StoreEmailStateToRefreshLoading());
-      await _fcmRepository.storeStateToRefresh(typeName, newState);
+      await _fcmRepository.storeStateToRefresh(TypeName.emailType, newState);
       yield Right<Failure, Success>(StoreEmailStateToRefreshSuccess());
     } catch (e) {
       yield Left<Failure, Success>(StoreEmailStateToRefreshFailure(e));
