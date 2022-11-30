84ac6728d2b63d877572a38b401e139773c8d82b
dab246 <tdvu@linagora.com>
2022-11-30 12:34:27 +0700

    TF-439 Delete all class not used

--- lib/features/caching/fcm_token_cache_client.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tmail_ui_user/features/caching/config/hive_cache_client.dart';
-import 'package:tmail_ui_user/features/push_notification/data/model/fcm_token_cache.dart';
-
-class FcmTokenCacheClient extends HiveCacheClient<FCMTokenCache> {
-
-  @override
-  String get tableName => 'FCMTokenCache';
-}
