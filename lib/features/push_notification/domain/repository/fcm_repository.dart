34021aca850502f8606c8f4832faa8013b25c6aa
HuyNguyen <quanghnguyen@linagora.com>
2022-12-29 15:22:54 +0700

    TF-1289: [Domain] add storeSubscription in fcm repository

--- lib/features/push_notification/domain/repository/fcm_repository.dart
+++ lib/features/push_notification/domain/repository/fcm_repository.dart
@@ -2,6 +2,7 @@ import 'package:fcm/model/firebase_subscription.dart';
 import 'package:fcm/model/type_name.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/model/fcm_subscription.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/model/register_new_token_request.dart';
 import 'package:tmail_ui_user/features/thread/domain/model/email_response.dart';
 import 'package:jmap_dart_client/jmap/core/state.dart' as jmap;
@@ -22,11 +23,9 @@ abstract class FCMRepository {
 
   Future<bool> deleteStateToRefresh(TypeName typeName);
 
-  Future<bool> storeDeviceId(String deviceId);
+  Future<void> storeSubscription(FCMSubscription fcmSubscription);
 
   Future<FirebaseSubscription> getFirebaseSubscriptionByDeviceId(String deviceId);
 
   Future<FirebaseSubscription> registerNewToken(RegisterNewTokenRequest newTokenRequest);
-
-  Future<String> getDeviceId();
 }
