b29a0c6123b492b01a4db9c1ba796673b1a409bb
HuyNguyen <quanghnguyen@linagora.com>
2022-12-29 17:53:58 +0700

    TF-1289: [Data] Implement DestroySubscription in fcm repository
    (cherry picked from commit 37e7013dd50384f976e486eec7538a5d9780a371)

--- lib/features/push_notification/data/network/fcm_api.dart
+++ lib/features/push_notification/data/network/fcm_api.dart
@@ -3,8 +3,11 @@ import 'package:fcm/method/set/firebase_subscription_set_method.dart';
 import 'package:fcm/method/set/firebase_subscription_set_response.dart';
 import 'package:fcm/method/get/firebase_subscription_get_method.dart';
 import 'package:fcm/method/get/firebase_subscription_get_response.dart';
+import 'package:fcm/model/firebase_capability.dart';
 import 'package:fcm/model/firebase_subscription.dart';
 import 'package:jmap_dart_client/http/http_client.dart';
+import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:jmap_dart_client/jmap/jmap_request.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/exceptions/fcm_exception.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/model/register_new_token_request.dart';
@@ -61,5 +64,30 @@ class FcmApi {
       FirebaseSubscriptionSetResponse.deserialize);
 
     return firebaseSubscriptionSetResponse!.created![newTokenRequest.createRequestId]!;
+  }
+
+  Future<bool> destroySubscription(String subscriptionId) async {
+    final processingInvocation = ProcessingInvocation();
+    final requestBuilder = JmapRequestBuilder(_httpClient, processingInvocation);
+
+    final firebaseSubscriptionSetMethod = FirebaseSubscriptionSetMethod();
+    final firebaseSubscriptionSetInvocation = requestBuilder.invocation(firebaseSubscriptionSetMethod);
+    final response = await (requestBuilder
+        ..usings({
+          CapabilityIdentifier.jmapCore,
+          FirebaseCapability.fcmIdentifier,
+          }))
+      .build()
+      .execute();
+
+    final firebaseSubscriptionSetResponse = response.parse<FirebaseSubscriptionSetResponse>(
+      firebaseSubscriptionSetInvocation.methodCallId,
+      FirebaseSubscriptionSetResponse.deserialize);
+
+     return Future.sync(() async {
+      return firebaseSubscriptionSetResponse?.destroyed?.contains(Id(subscriptionId)) == true;
+    }).catchError((error) {
+      throw error;
+    });
   }
 }
