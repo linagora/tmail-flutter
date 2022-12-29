f5f839afc6005db888cb09af1c33e070c81f30e1
HuyNguyen <quanghnguyen@linagora.com>
2022-12-29 15:49:41 +0700

    TF-1289: [Data] implement storeSubscription in fcm datasource
    (cherry picked from commit dd6079385d7f44edb982a3f51d5baeba986ce1e2)

---
+++ lib/features/push_notification/data/datasource_impl/cache_fcm_datasource_impl.dart
@@ -0,0 +1,62 @@
+import 'package:fcm/model/firebase_subscription.dart';
+import 'package:fcm/model/type_name.dart';
+import 'package:jmap_dart_client/jmap/core/state.dart' as jmap;
+import 'package:tmail_ui_user/features/push_notification/data/datasource/fcm_datasource.dart';
+import 'package:tmail_ui_user/features/push_notification/data/local/fcm_cache_manager.dart';
+import 'package:tmail_ui_user/features/push_notification/data/model/fcm_subscription.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/model/register_new_token_request.dart';
+import 'package:tmail_ui_user/main/exceptions/exception_thrower.dart';
+
+class CacheFCMDatasourceImpl extends FCMDatasource {
+
+  final FCMCacheManager _firebaseCacheManager;
+  final ExceptionThrower _exceptionThrower;
+
+  CacheFCMDatasourceImpl(this._firebaseCacheManager, this._exceptionThrower);
+
+  @override
+  Future<bool> storeStateToRefresh(TypeName typeName, jmap.State newState) {
+    return Future.sync(() async {
+      return await _firebaseCacheManager.storeStateToRefresh(typeName, newState);
+    }).catchError((error) {
+      _exceptionThrower.throwException(error);
+    });
+  }
+
+  @override
+  Future<jmap.State> getStateToRefresh(TypeName typeName) {
+    return Future.sync(() async {
+      return await _firebaseCacheManager.getStateToRefresh(typeName);
+    }).catchError((error) {
+      _exceptionThrower.throwException(error);
+    });
+  }
+
+  @override
+  Future<bool> deleteStateToRefresh(TypeName typeName) {
+    return Future.sync(() async {
+      return await _firebaseCacheManager.deleteStateToRefresh(typeName);
+    }).catchError((error) {
+      _exceptionThrower.throwException(error);
+    });
+  }
+
+  @override
+  Future<FirebaseSubscription> getFirebaseSubscriptionByDeviceId(String deviceId) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<FirebaseSubscription> registerNewToken(RegisterNewTokenRequest newTokenRequest) {
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> storeSubscription(FCMSubscriptionCache fcmSubscriptionCache) {
+   return Future.sync(() async {
+      return await _firebaseCacheManager.storeSubscription(fcmSubscriptionCache);
+    }).catchError((error) {
+       _exceptionThrower.throwException(error);
+    });
+  }
+}
