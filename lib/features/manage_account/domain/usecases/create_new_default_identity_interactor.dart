37d874e1f6f8ed1bf09cd90625fe3f2213b6de5e
sherlockvn <quangkhai28999@gmail.com>
2023-01-16 17:02:32 +0700

    TF-1194 create new default identity: add interactor and state for create new default identity
    (cherry picked from commit f188cd3b0df6dcfba62c06560e7dd8df80831a6a)

---
+++ lib/features/manage_account/domain/usecases/create_new_default_identity_interactor.dart
@@ -0,0 +1,56 @@
+import 'dart:core';
+
+import 'package:core/core.dart';
+import 'package:dartz/dartz.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
+import 'package:jmap_dart_client/jmap/identities/identity.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_default_identity_request.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/create_new_identity_request.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/repository/identity_repository.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/state/create_new_default_identity_state.dart';
+import 'package:tmail_ui_user/features/manage_account/presentation/profiles/identities/utils/identity_utils.dart';
+
+class CreateNewDefaultIdentityInteractor {
+  final IdentityRepository _identityRepository;
+  final IdentityUtils _identityUtils;
+
+  CreateNewDefaultIdentityInteractor(
+    this._identityRepository,
+    this._identityUtils);
+
+  Stream<Either<Failure, Success>> execute(AccountId accountId, CreateNewIdentityRequest identityRequest) async* {
+    try {
+      yield Right(CreateNewDefaultIdentityLoading());
+      final listDefaultIdentities = await _getDefaultIdentities(accountId);
+
+      final defaultRequest = _createNewIdentityDefault(identityRequest, listDefaultIdentities);
+      
+      final newIdentity = await _identityRepository.createNewIdentity(accountId, defaultRequest);
+      yield Right(CreateNewDefaultIdentitySuccess(newIdentity));
+    } catch (exception) {
+      yield Left(CreateNewDefaultIdentityFailure(exception));
+    }
+  }
+
+  Future<List<Identity>?> _getDefaultIdentities(AccountId accountId) async {
+    final listIdentities = await _identityRepository
+        .getAllIdentities(
+            accountId, 
+            properties: Properties({'sortOrder'})
+        );
+    return _identityUtils.getSmallestOrderedIdentity(listIdentities.identities);
+  }
+
+  CreateNewDefaultIdentityRequest _createNewIdentityDefault(
+    CreateNewIdentityRequest identityRequest,
+    List<Identity>? listDefaultIdentities,
+  ) {
+    return CreateNewDefaultIdentityRequest(
+      identityRequest.creationId, 
+      identityRequest.newIdentity,
+      oldDefaultIdentityIds: listDefaultIdentities
+          ?.map((identity) => identity.id!)
+          .toList());
+  }
+}
