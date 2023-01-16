e192e0d3bd0c1f9ea0eda598f991154424e17a2e
HuyNguyen <quanghnguyen@linagora.com>
2023-01-16 12:39:39 +0700

    TF-1363: [Domain] [Domain] Store, Get, Delete spamReportState in UseCase

--- lib/features/mailbox_dashboard/data/repository/spam_report_repository_impl.dart
+++ lib/features/mailbox_dashboard/data/repository/spam_report_repository_impl.dart
@@ -3,6 +3,7 @@ import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/unsigned_int.dart';
 import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox_filter_condition.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/data/datasource/spam_report_datasource.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/model/spam_report_state.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/model/unread_spam_emails_response.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/repository/spam_report_repository.dart';
 
@@ -38,5 +39,20 @@ class SpamReportRepositoryImpl extends SpamReportRepository {
         accountId,
         mailboxFilterCondition: mailboxFilterCondition,
         limit: limit);
+  }
+
+  @override
+  Future<SpamReportState> getSpamReportState() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> storeSpamReportState(SpamReportState spamReportState) {
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> deletestoreSpamReportState() {
+    throw UnimplementedError();
   }
 }
---
+++ lib/features/mailbox_dashboard/domain/usecases/delete_spam_report_state_interactor.dart
@@ -0,0 +1,21 @@
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:dartz/dartz.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/repository/spam_report_repository.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/state/delete_spam_report_state.dart';
+
+class DeleteSpamReportStateInteractor {
+  final SpamReportRepository _spamReportRepository;
+
+  DeleteSpamReportStateInteractor(this._spamReportRepository);
+
+  Stream<Either<Failure, Success>> execute() async* {
+    try {
+      yield Right<Failure, Success>(DeleteSpamReportStateLoading());
+        await _spamReportRepository.deletestoreSpamReportState();
+      yield Right<Failure, Success>(DeleteSpamReportStateSuccess());
+    } catch (e) {
+      yield Left<Failure, Success>(DeleteSpamReportStateFailure(e));
+    }
+  }
+}
