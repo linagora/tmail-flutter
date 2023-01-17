e7290fd2c5cd8612042a29bb1290b95c6c708ad2
dab246 <tdvu@linagora.com>
2023-01-17 17:03:00 +0700

    TF-1167 Improve more button in recipients of EmailView
    (cherry picked from commit 5424479526d916e6a7ff68e599b6a3e055e63789)

---
+++ lib/features/email/presentation/widgets/received_time_builder.dart
@@ -0,0 +1,39 @@
+
+import 'package:core/domain/extensions/datetime_extension.dart';
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:core/presentation/utils/style_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:model/email/presentation_email.dart';
+import 'package:model/extensions/presentation_email_extension.dart';
+
+class ReceivedTimeBuilder extends StatelessWidget {
+
+  final PresentationEmail emailSelected;
+
+  const ReceivedTimeBuilder(
+    this.emailSelected, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 100),
+      color: Colors.transparent,
+      child: Text(
+        emailSelected.getReceivedAt(
+          Localizations.localeOf(context).toLanguageTag(),
+          pattern: emailSelected.receivedAt?.value.toLocal().toPatternForEmailView()
+        ),
+        maxLines: 1,
+        overflow: CommonTextStyle.defaultTextOverFlow,
+        softWrap: CommonTextStyle.defaultSoftWrap,
+        style: const TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+          color: AppColor.colorEmailAddressFull
+        )
+      ),
+    );
+  }
+}
