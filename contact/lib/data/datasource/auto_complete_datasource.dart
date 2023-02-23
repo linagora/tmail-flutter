1cbb1a15bbf042ce3e46eb28f00236c765df42f6
dab246 <tdvu@linagora.com>
2023-02-23 21:45:31 +0700

    TF-1487 Upgrade latest dependencies version for `contact module`
    (cherry picked from commit 597f61a79dfe0f0cf08b76277bc898b949b9b3bc)

--- contact/lib/data/datasource/auto_complete_datasource.dart
+++ contact/lib/data/datasource/auto_complete_datasource.dart
@@ -1,6 +1,6 @@
 
 import 'package:jmap_dart_client/jmap/mail/email/email_address.dart';
-import 'package:model/model.dart';
+import 'package:model/autocomplete/auto_complete_pattern.dart';
 
 abstract class AutoCompleteDataSource {
   Future<List<EmailAddress>> getAutoComplete(AutoCompletePattern autoCompletePattern);
