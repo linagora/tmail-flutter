76c4ed08f3c7f1dcac4373fa5ecf372a471aa7ff
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-03-03 12:13:32 +0700

    Handle BadCredentialsException in BaseController

--- lib/features/base/base_controller.dart
+++ lib/features/base/base_controller.dart
@@ -18,24 +18,41 @@ import 'package:get/get.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:model/account/authentication_type.dart';
 import 'package:rule_filter/rule_filter/capability_rule_filter.dart';
 import 'package:tmail_ui_user/features/base/mixin/message_dialog_action_mixin.dart';
 import 'package:tmail_ui_user/features/base/mixin/popup_context_menu_action_mixin.dart';
 import 'package:tmail_ui_user/features/base/mixin/view_as_dialog_action_mixin.dart';
+import 'package:tmail_ui_user/features/caching/caching_manager.dart';
 import 'package:tmail_ui_user/features/email/presentation/mdn_interactor_bindings.dart';
+import 'package:tmail_ui_user/features/login/data/network/config/authorization_interceptors.dart';
+import 'package:tmail_ui_user/features/login/domain/usecases/delete_authority_oidc_interactor.dart';
+import 'package:tmail_ui_user/features/login/domain/usecases/delete_credential_interactor.dart';
+import 'package:tmail_ui_user/features/login/presentation/login_form_type.dart';
+import 'package:tmail_ui_user/features/login/presentation/model/login_arguments.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/bindings/contact_autocomplete_bindings.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/bindings/tmail_autocomplete_bindings.dart';
+import 'package:tmail_ui_user/features/manage_account/data/local/language_cache_manager.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/state/log_out_oidc_state.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/usecases/log_out_oidc_interactor.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/email_rules/bindings/email_rules_interactor_bindings.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/forward/bindings/forwarding_interactors_bindings.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/mailbox_visibility/bindings/mailbox_visibility_interactor_bindings.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/exceptions/fcm_exception.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/state/destroy_subscription_state.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/state/get_fcm_subscription_local.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/usecases/destroy_subscription_interactor.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/usecases/get_fcm_subscription_local_interactor.dart';
 import 'package:tmail_ui_user/features/push_notification/presentation/bindings/fcm_interactor_bindings.dart';
 import 'package:tmail_ui_user/features/push_notification/presentation/config/fcm_configuration.dart';
 import 'package:tmail_ui_user/features/push_notification/presentation/controller/fcm_controller.dart';
 import 'package:tmail_ui_user/features/push_notification/presentation/notification/local_notification_manager.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/services/fcm_receiver.dart';
+import 'package:tmail_ui_user/main/bindings/network/binding_tag.dart';
 import 'package:tmail_ui_user/main/error/capability_validator.dart';
 import 'package:tmail_ui_user/main/exceptions/remote_exception.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+import 'package:tmail_ui_user/main/routes/app_routes.dart';
 import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 import 'package:tmail_ui_user/main/utils/app_config.dart';
 import 'package:tmail_ui_user/main/utils/app_utils.dart';
@@ -44,6 +61,21 @@ abstract class BaseController extends GetxController
     with MessageDialogActionMixin,
         PopupContextMenuActionMixin,
         ViewAsDialogActionMixin {
+
+  final CachingManager cachingManager = Get.find<CachingManager>();
+  final languageCacheManager = Get.find<LanguageCacheManager>();
+  final authorizationInterceptors = Get.find<AuthorizationInterceptors>();
+  final authorizationIsolateInterceptors = Get.find<AuthorizationInterceptors>(tag: BindingTag.isolateTag);
+  final DeleteCredentialInteractor deleteCredentialInteractor = Get.find<DeleteCredentialInteractor>();
+  final LogoutOidcInteractor logoutOidcInteractor = Get.find<LogoutOidcInteractor>();
+  final DeleteAuthorityOidcInteractor deleteAuthorityOidcInteractor = Get.find<DeleteAuthorityOidcInteractor>();
+  final _fcmReceiver = FcmReceiver.instance;
+
+  GetFCMSubscriptionLocalInteractor? _getSubscriptionLocalInteractor;
+  DestroySubscriptionInteractor? _destroySubscriptionInteractor;
+
+  final AppToast _appToast = Get.find<AppToast>();
+
 
   final viewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
   FpsCallback? fpsCallback;
@@ -70,6 +102,47 @@ abstract class BaseController extends GetxController
 
   void onData(Either<Failure, Success> newState) {
     viewState.value = newState;
+    viewState.value.fold(
+      (failure) {
+        if (_handleCommonException(failure)) {
+          return;
+        }
+        if (failure is LogoutOidcFailure) {
+          log('BaseController::onData(): $failure');
+          _getSubscriptionLocalAction();
+        } else if (failure is GetFCMSubscriptionLocalFailure) {
+          checkAuthenticationTypeWhenLogout();
+        } else if (failure is DestroySubscriptionFailure) {
+          checkAuthenticationTypeWhenLogout();
+        }
+      },
+      (success) {
+        if (success is LogoutOidcSuccess) {
+          log('BaseController::onData(): $success');
+          _getSubscriptionLocalAction();
+        } else if (success is GetFCMSubscriptionLocalSuccess) {
+          final _subscriptionId = success.fcmSubscription.subscriptionId;
+          _destroySubscriptionAction(_subscriptionId);
+        } else if (success is DestroySubscriptionSuccess) {
+          checkAuthenticationTypeWhenLogout();
+        }
+      }
+    );
+  }
+
+  bool _handleCommonException(Failure failure) {
+    if (failure is FeatureFailure) {
+      log('BaseController::_handleCommonException(): ${failure.exception}');
+      if (failure.exception is NoNetworkError) {
+        return true;
+      } else if (failure.exception is BadCredentialsException) {
+        _appToast.showErrorToast(AppLocalizations.of(currentContext!).badCredentials);
+        cleanAppAtTheEnd();
+        checkAuthenticationTypeWhenLogout();
+        return true;
+      }
+    }
+    return false;
   }
 
   void onError(dynamic error) {
