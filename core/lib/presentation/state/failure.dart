00ca28c06129e79670be40e940cb207f687c7d89
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-03-03 12:10:11 +0700

    Add exception for FeatureFailure to handle exception in BaseController

--- core/lib/presentation/state/failure.dart
+++ core/lib/presentation/state/failure.dart
@@ -2,4 +2,8 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {}
 
-abstract class FeatureFailure extends Failure {}
+abstract class FeatureFailure extends Failure {
+  final dynamic exception;
+
+  FeatureFailure({this.exception});
+}
