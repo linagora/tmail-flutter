5e7e0f0cfb34510b5c341417044b8057cde2ca35
dab246 <tdvu@linagora.com>
2023-03-20 16:59:41 +0700

    TF-1606 Store `jmapUrl` on memory

--- core/lib/data/network/config/dynamic_url_interceptors.dart
+++ core/lib/data/network/config/dynamic_url_interceptors.dart
@@ -1,11 +1,18 @@
 import 'package:dio/dio.dart';
 
 class DynamicUrlInterceptors extends InterceptorsWrapper {
+  String? _jmapUrl;
   String? _baseUrl;
 
   void changeBaseUrl(String? url) {
     _baseUrl = url;
   }
+
+  void setJmapUrl(String? url) {
+    _jmapUrl = url;
+  }
+
+  String? get jmapUrl => _jmapUrl;
 
   String? get baseUrl => _baseUrl;
 
