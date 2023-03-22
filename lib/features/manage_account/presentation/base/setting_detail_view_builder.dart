943102f54285fc3c9291160cc685e42bbdfbf168
dab246 <tdvu@linagora.com>
2023-03-22 12:13:41 +0700

    TF-1611 Fix bottom padding too large in vacation view

--- lib/features/manage_account/presentation/base/setting_detail_view_builder.dart
+++ lib/features/manage_account/presentation/base/setting_detail_view_builder.dart
@@ -7,24 +7,32 @@ class SettingDetailViewBuilder extends StatelessWidget {
 
   final ResponsiveUtils responsiveUtils;
   final Widget child;
+  final EdgeInsets? padding;
+  final VoidCallback? onTapGestureDetector;
 
   const SettingDetailViewBuilder({
     super.key,
     required this.responsiveUtils,
-    required this.child
+    required this.child,
+    this.padding,
+    this.onTapGestureDetector
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: SettingsUtils.getBackgroundColor(context, responsiveUtils),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: SettingsUtils.getContentBackgroundColor(context, responsiveUtils),
-        decoration: SettingsUtils.getBoxDecorationForContent(context, responsiveUtils),
-        margin: SettingsUtils.getMarginSettingDetailsView(context, responsiveUtils),
-        child: child,
+      body: GestureDetector(
+        onTap: onTapGestureDetector,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: SettingsUtils.getContentBackgroundColor(context, responsiveUtils),
+          decoration: SettingsUtils.getBoxDecorationForContent(context, responsiveUtils),
+          margin: SettingsUtils.getMarginSettingDetailsView(context, responsiveUtils),
+          padding: padding,
+          child: child,
+        ),
       ),
     );
   }
