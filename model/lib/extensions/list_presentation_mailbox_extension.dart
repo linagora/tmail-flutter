a99c35dfeca3248acc35848dd0e044c840608b71
dab246 <tdvu@linagora.com>
2023-03-24 18:34:37 +0700

    TF-1645 Fix always show default mailbox in destination picker

--- model/lib/extensions/list_presentation_mailbox_extension.dart
+++ model/lib/extensions/list_presentation_mailbox_extension.dart
@@ -3,9 +3,6 @@ import 'package:model/mailbox/presentation_mailbox.dart';
 
 extension ListPresentationMailboxExtension on List<PresentationMailbox> {
 
-  List<PresentationMailbox> get listSubscribedMailboxes =>
-    where((mailbox) => mailbox.isSubscribedMailbox).toList();
-
   List<PresentationMailbox> get listSubscribedMailboxesAndDefaultMailboxes =>
     where((mailbox) => mailbox.isSubscribedMailbox || mailbox.isDefault).toList();
 
