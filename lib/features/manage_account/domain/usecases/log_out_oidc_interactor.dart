dce230c3793ffc994542c08a27056b41ca290735
Dat PHAM HOANG <dphamhoang@linagora.com>
2023-03-27 15:32:11 +0200

    [SLO] Implement for discoverOIDC method
    (cherry picked from commit 76ee41a238ad3e626148f3a236f20c6cd370df3a)

--- lib/features/manage_account/domain/usecases/log_out_oidc_interactor.dart
+++ lib/features/manage_account/domain/usecases/log_out_oidc_interactor.dart
@@ -1,10 +1,10 @@
-
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
 import 'package:core/utils/app_logger.dart';
 import 'package:dartz/dartz.dart';
 import 'package:model/account/authentication_type.dart';
 import 'package:model/oidc/oidc_configuration.dart';
+import 'package:model/oidc/response/oidc_discovery_response.dart';
 import 'package:model/oidc/token_oidc.dart';
 import 'package:tmail_ui_user/features/login/domain/exceptions/authentication_exception.dart';
 import 'package:tmail_ui_user/features/login/domain/repository/account_repository.dart';
@@ -12,27 +12,29 @@ import 'package:tmail_ui_user/features/login/domain/repository/authentication_oi
 import 'package:tmail_ui_user/features/manage_account/domain/state/log_out_oidc_state.dart';
 
 class LogoutOidcInteractor {
-
   final AccountRepository _accountRepository;
   final AuthenticationOIDCRepository _authenticationOIDCRepository;
 
-  LogoutOidcInteractor(this._accountRepository, this._authenticationOIDCRepository);
+  LogoutOidcInteractor(
+      this._accountRepository, this._authenticationOIDCRepository);
 
   Stream<Either<Failure, Success>> execute() async* {
     try {
       final currentAccount = await _accountRepository.getCurrentAccount();
       log('LogoutOidcInteractor::execute(): currentAccount: $currentAccount');
       if (currentAccount.authenticationType == AuthenticationType.oidc) {
-        final result = await Future.wait([
-          _authenticationOIDCRepository.getStoredTokenOIDC(currentAccount.id),
-          _authenticationOIDCRepository.getStoredOidcConfiguration()
-        ]).then((result) async {
-          final tokenOidc = result.first as TokenOIDC;
-          final oidcConfig = result.last as OIDCConfiguration;
-          log('LogoutOidcInteractor::execute(): tokenOidc: ${tokenOidc.tokenId.uuid}');
-          log('LogoutOidcInteractor::execute(): oidcConfig: $oidcConfig');
-          return await _authenticationOIDCRepository.logout(tokenOidc.tokenId, oidcConfig);
-        });
+        final result = await _authenticationOIDCRepository.getStoredOidcConfiguration()
+          .then((oidcConfig) => Future.wait([
+              Future.value(oidcConfig),
+              _authenticationOIDCRepository.getStoredTokenOIDC(currentAccount.id),
+              _authenticationOIDCRepository.discoverOIDC(oidcConfig)
+            ]))
+          .then((oidcParameters) async {
+            final oidcConfig = oidcParameters[0] as OIDCConfiguration;
+            final tokenOIDC = oidcParameters[1] as TokenOIDC;
+            final oidcDiscoveryResponse = oidcParameters[2] as OIDCDiscoveryResponse;
+            return await _authenticationOIDCRepository.logout(tokenOIDC.tokenId, oidcConfig, oidcDiscoveryResponse);
+          });
         log('LogoutOidcInteractor::execute(): statusSuccess: $result');
         if (result) {
           yield Right<Failure, Success>(LogoutOidcSuccess());
@@ -40,7 +42,8 @@ class LogoutOidcInteractor {
           yield Left<Failure, Success>(LogoutOidcFailure(null));
         }
       } else {
-        yield Left<Failure, Success>(LogoutOidcFailure(NotFoundAuthenticatedAccountException()));
+        yield Left<Failure, Success>(
+            LogoutOidcFailure(NotFoundAuthenticatedAccountException()));
       }
     } catch (e) {
       log('LogoutOidcInteractor::execute(): EXCEPTION: $e');
