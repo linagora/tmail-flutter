ba849b288e4e943648c35bf6a494eaa804b049b6
dab246 <tdvu@linagora.com>
2023-03-30 19:09:17 +0700

    TF-1600 Implement `GetMailboxesNotPutNotificationsInteractor`

--- lib/features/push_notification/data/repository/fcm_repository_impl.dart
+++ lib/features/push_notification/data/repository/fcm_repository_impl.dart
@@ -1,9 +1,13 @@
 import 'package:core/data/model/source_type/data_source_type.dart';
+import 'package:core/utils/app_logger.dart';
 import 'package:fcm/model/firebase_subscription.dart';
 import 'package:fcm/model/type_name.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:model/mailbox/presentation_mailbox.dart';
+import 'package:model/model.dart';
+import 'package:tmail_ui_user/features/mailbox/data/datasource/mailbox_datasource.dart';
 import 'package:tmail_ui_user/features/push_notification/data/datasource/fcm_datasource.dart';
 import 'package:tmail_ui_user/features/push_notification/data/extensions/fcm_subscription_extensions.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/model/fcm_subscription.dart';
@@ -18,10 +22,12 @@ class FCMRepositoryImpl extends FCMRepository {
 
   final Map<DataSourceType, FCMDatasource> _fcmDatasource;
   final ThreadDataSource _threadDataSource;
+  final Map<DataSourceType, MailboxDataSource> _mapMailboxDataSource;
 
   FCMRepositoryImpl(
     this._fcmDatasource,
-    this._threadDataSource
+    this._threadDataSource,
+    this._mapMailboxDataSource
   );
 
   @override
@@ -103,5 +109,27 @@ class FCMRepositoryImpl extends FCMRepository {
   @override
   Future<bool> destroySubscription(String subscriptionId) {
     return _fcmDatasource[DataSourceType.network]!.destroySubscription(subscriptionId);
+  }
+
+  @override
+  Future<List<PresentationMailbox>> getMailboxesNotPutNotifications(Session session, AccountId accountId) async {
+    final mailboxesCache = await _mapMailboxDataSource[DataSourceType.local]!.getAllMailboxCache();
+    final mailboxesCacheNotPutNotifications = mailboxesCache
+      .map((mailbox) => mailbox.toPresentationMailbox())
+      .where((presentationMailbox) => presentationMailbox.pushNotificationDeactivated)
+      .toList();
+    log('FCMRepositoryImpl::getMailboxesNotPutNotifications():mailboxesCacheNotPutNotifications: $mailboxesCacheNotPutNotifications');
+    if (mailboxesCacheNotPutNotifications.isNotEmpty && mailboxesCacheNotPutNotifications.length == 5) {
+      return mailboxesCacheNotPutNotifications;
+    } else {
+      final mailboxResponse = await _mapMailboxDataSource[DataSourceType.network]!.getAllMailbox(session, accountId);
+      final mailboxes = mailboxResponse.mailboxes ?? [];
+      final mailboxesNotPutNotifications = mailboxes
+        .map((mailbox) => mailbox.toPresentationMailbox())
+        .where((presentationMailbox) => presentationMailbox.pushNotificationDeactivated)
+        .toList();
+      log('FCMRepositoryImpl::getMailboxesNotPutNotifications():mailboxesNotPutNotifications: $mailboxesNotPutNotifications');
+      return mailboxesNotPutNotifications;
+    }
   }
 }
