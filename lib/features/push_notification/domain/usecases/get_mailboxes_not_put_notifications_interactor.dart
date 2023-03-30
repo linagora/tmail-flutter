7ed5a9f578b0c7303356329c9ad3e78012839042
dab246 <tdvu@linagora.com>
2023-03-30 19:09:17 +0700

    TF-1600 Implement `GetMailboxesNotPutNotificationsInteractor`
    (cherry picked from commit ba849b288e4e943648c35bf6a494eaa804b049b6)

---
+++ lib/features/push_notification/domain/usecases/get_mailboxes_not_put_notifications_interactor.dart
@@ -0,0 +1,23 @@
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:dartz/dartz.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/repository/fcm_repository.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/state/get_mailboxes_not_put_notifications_state.dart';
+
+class GetMailboxesNotPutNotificationsInteractor {
+  final FCMRepository _fcmRepository;
+
+  GetMailboxesNotPutNotificationsInteractor(this._fcmRepository);
+
+  Stream<Either<Failure, Success>> execute(Session session, AccountId accountId) async* {
+    try {
+      yield Right<Failure, Success>(GetMailboxesNotPutNotificationsLoading());
+      final mailboxes = await _fcmRepository.getMailboxesNotPutNotifications(session, accountId);
+      yield Right<Failure, Success>(GetMailboxesNotPutNotificationsSuccess(mailboxes));
+    } catch (e) {
+      yield Left<Failure, Success>(GetMailboxesNotPutNotificationsFailure(e));
+    }
+  }
+}
