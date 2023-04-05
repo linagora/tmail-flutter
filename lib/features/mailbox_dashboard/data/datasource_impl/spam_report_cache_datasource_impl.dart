ffe62c58aa46526ea34536bac17862b5c16aa9ac
dab246 <tdvu@linagora.com>
2023-04-05 15:30:34 +0700

    TF-1667 Implement `GetSpamMailboxCached` interactor

--- lib/features/mailbox_dashboard/data/datasource_impl/spam_report_cache_datasource_impl.dart
+++ lib/features/mailbox_dashboard/data/datasource_impl/spam_report_cache_datasource_impl.dart
@@ -4,49 +4,20 @@ import 'package:jmap_dart_client/jmap/core/session/session.dart';
 import 'package:jmap_dart_client/jmap/core/unsigned_int.dart';
 import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
 import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox_filter_condition.dart';
+import 'package:tmail_ui_user/features/mailbox/data/local/mailbox_cache_manager.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/data/datasource/spam_report_datasource.dart';
-import 'package:tmail_ui_user/features/mailbox_dashboard/data/network/spam_report_api.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/model/spam_report_state.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/model/unread_spam_emails_response.dart';
 import 'package:tmail_ui_user/main/exceptions/exception_thrower.dart';
 
-class SpamReportDataSourceImpl extends SpamReportDataSource {
-  final SpamReportApi _spamReportApi;
+class SpamReportCacheDataSourceImpl extends SpamReportDataSource {
+  final MailboxCacheManager _mailboxCacheManager;
   final ExceptionThrower _exceptionThrower;
 
-  SpamReportDataSourceImpl(this._spamReportApi, this._exceptionThrower);
+  SpamReportCacheDataSourceImpl(this._mailboxCacheManager, this._exceptionThrower);
 
   @override
   Future<bool> deleteLastTimeDismissedSpamReported() {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<UnreadSpamEmailsResponse> findNumberOfUnreadSpamEmails(
-    Session session,
-    AccountId accountId,
-    {
-      MailboxFilterCondition? mailboxFilterCondition,
-      UnsignedInt? limit
-    }
-  ) {
-     return Future.sync(() async {
-      final unreadSpamEmailsResponse = await _spamReportApi.getUnreadSpamEmailbox(
-        session,
-        accountId,
-        mailboxFilterCondition: mailboxFilterCondition,
-        limit: limit);
-      return unreadSpamEmailsResponse;
-    }).catchError(_exceptionThrower.throwException);
-  }
-
-  @override
-  Future<DateTime> getLastTimeDismissedSpamReported() {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<bool> storeLastTimeDismissedSpamReported(DateTime lastTimeDismissedSpamReported) {
     throw UnimplementedError();
   }
 
@@ -56,17 +27,34 @@ class SpamReportDataSourceImpl extends SpamReportDataSource {
   }
 
   @override
-  Future<SpamReportState> getSpamReportState() {
+  Future<UnreadSpamEmailsResponse> findNumberOfUnreadSpamEmails(Session session, AccountId accountId, {MailboxFilterCondition? mailboxFilterCondition, UnsignedInt? limit}) {
     throw UnimplementedError();
   }
 
   @override
-  Future<void> storeSpamReportState(SpamReportState spamReportState) {
+  Future<DateTime> getLastTimeDismissedSpamReported() {
     throw UnimplementedError();
   }
 
   @override
   Future<Mailbox> getSpamMailboxCached() {
+    return Future.sync(() async {
+      return await _mailboxCacheManager.getSpamMailbox();
+    }).catchError(_exceptionThrower.throwException);
+  }
+
+  @override
+  Future<SpamReportState> getSpamReportState() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> storeLastTimeDismissedSpamReported(DateTime lastTimeDismissedSpamReported) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> storeSpamReportState(SpamReportState spamReportState) {
     throw UnimplementedError();
   }
 }
---
+++ lib/features/mailbox_dashboard/domain/usecases/get_spam_mailbox_cached_interactor.dart
@@ -0,0 +1,38 @@
+
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:dartz/dartz.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/repository/spam_report_repository.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/state/get_number_of_unread_spam_emails_state.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/state/get_spam_mailbox_cached_state.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/utils/mailbox_dashboard_constant.dart';
+
+class GetSpamMailboxCachedInteractor {
+  final SpamReportRepository _spamReportRepository;
+
+  GetSpamMailboxCachedInteractor(this._spamReportRepository);
+
+   Stream<Either<Failure, Success>> execute() async* {
+    try {
+      yield Right<Failure, Success>(GetSpamMailboxCachedLoading());
+
+      final lastTimeDismissedSpamReported = await _spamReportRepository.getLastTimeDismissedSpamReported();
+      final timeLast = DateTime.now().difference(lastTimeDismissedSpamReported);
+      final checkTimeCondition = timeLast.inHours > MailboxDashboardConstant.spamReportBannerDisplayTimeOut;
+
+      if (checkTimeCondition) {
+        final spamMailbox =  await _spamReportRepository.getSpamMailboxCached();
+        final countUnreadSpamMailbox = spamMailbox.unreadEmails?.value.value.toInt() ?? 0;
+        if (countUnreadSpamMailbox > 0) {
+          yield Right<Failure, Success>(GetSpamMailboxCachedSuccess(spamMailbox));
+        } else {
+          yield Left<Failure, Success>(InvalidSpamReportCondition());
+        }
+      } else {
+        yield Left<Failure, Success>(InvalidSpamReportCondition());
+      }
+    } catch (e) {
+      yield Left<Failure, Success>(GetSpamMailboxCachedFailure(e));
+    }
+  }
+}
