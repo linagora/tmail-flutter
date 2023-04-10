f2937d503aa66edad2a875ccd4e1fe1a2920d539
dab246 <tdvu@linagora.com>
2023-04-10 18:38:17 +0700

    TF-1665 Remove blank in the app grip
    (cherry picked from commit 3913bb611b3200022a1b4d05f8337fe57101563a)

--- lib/features/mailbox_dashboard/presentation/widgets/app_dashboard/app_grid_dashboard_item.dart
+++ lib/features/mailbox_dashboard/presentation/widgets/app_dashboard/app_grid_dashboard_item.dart
@@ -17,40 +17,46 @@ class AppGridDashboardItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: LinkBrowserWidget(
-        uri: app.appUri,
+    return LinkBrowserWidget(
+      uri: app.appUri,
+      child: Material(
+        color: Colors.transparent,
         child: InkWell(
           onTap: _openApp,
-          child: Column(children: [
-            app.iconName.endsWith("svg")
-              ? SvgPicture.asset(
-                  _imagePaths.getConfigurationImagePath(app.iconName),
-                  width: 56,
-                  height: 56,
-                  fit: BoxFit.fill)
-              : Image.asset(
-                  _imagePaths.getConfigurationImagePath(app.iconName),
-                  width: 56,
-                  height: 56,
-                  fit: BoxFit.fill),
-            Padding(
-              padding: const EdgeInsets.only(top: 8),
-              child: Text(
-                app.appName,
-                maxLines: 1,
-                textAlign: TextAlign.center,
-                softWrap: CommonTextStyle.defaultSoftWrap,
-                overflow: CommonTextStyle.defaultTextOverFlow,
-                style: const TextStyle(
-                  color: AppColor.colorNameEmail,
-                  fontSize: 15
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          hoverColor: AppColor.colorItemSelected,
+          child: Container(
+            width: 98,
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Column(children: [
+              app.iconName.endsWith("svg")
+                ? SvgPicture.asset(
+                    _imagePaths.getConfigurationImagePath(app.iconName),
+                    width: 56,
+                    height: 56,
+                    fit: BoxFit.fill)
+                : Image.asset(
+                    _imagePaths.getConfigurationImagePath(app.iconName),
+                    width: 56,
+                    height: 56,
+                    fit: BoxFit.fill),
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Text(
+                  app.appName,
+                  maxLines: 1,
+                  textAlign: TextAlign.center,
+                  softWrap: CommonTextStyle.defaultSoftWrap,
+                  overflow: CommonTextStyle.defaultTextOverFlow,
+                  style: const TextStyle(
+                    color: AppColor.colorNameEmail,
+                    fontSize: 15
+                  ),
                 ),
               ),
-            ),
-          ]),
-        ),
+            ]),
+          ),
+        )
       )
     );
   }
