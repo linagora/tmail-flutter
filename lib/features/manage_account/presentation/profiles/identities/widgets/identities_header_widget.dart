aa939af0d136d3d11f13c14ca3b550fe1513d270
dab246 <tdvu@linagora.com>
2023-04-24 15:01:59 +0700

    TF-1711 Fix label displayed overlap for create identity button

--- lib/features/manage_account/presentation/profiles/identities/widgets/identities_header_widget.dart
+++ lib/features/manage_account/presentation/profiles/identities/widgets/identities_header_widget.dart
@@ -1,8 +1,8 @@
 import 'package:core/presentation/extensions/color_extension.dart';
 import 'package:core/presentation/resources/image_paths.dart';
-import 'package:core/presentation/utils/responsive_utils.dart';
-import 'package:core/presentation/views/button/button_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/base/widget/material_text_icon_button.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
 
 typedef OnAddNewIdentityAction = Function();
@@ -11,20 +11,18 @@ class IdentitiesHeaderWidget extends StatelessWidget {
 
   const IdentitiesHeaderWidget({
     Key? key,
-    required this.imagePaths,
-    required this.responsiveUtils,
-    this.onAddNewIdentityAction,
+    required this.onAddNewIdentityAction,
   }) : super(key: key);
 
-  final ImagePaths imagePaths;
-  final ResponsiveUtils responsiveUtils;
-  final OnAddNewIdentityAction? onAddNewIdentityAction;
+  final OnAddNewIdentityAction onAddNewIdentityAction;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.transparent,
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+    final imagePaths = Get.find<ImagePaths>();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
         Text(
           AppLocalizations.of(context).identities,
           style: const TextStyle(
@@ -39,24 +37,16 @@ class IdentitiesHeaderWidget extends StatelessWidget {
             fontWeight: FontWeight.normal,
             color: AppColor.colorSettingExplanation)),
         const SizedBox(height: 24),
-        (ButtonBuilder(imagePaths.icAddIdentity)
-          ..key(const Key('button_add_identity'))
-          ..decoration(BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: AppColor.colorCreateNewIdentityButton))
-          ..paddingIcon(const EdgeInsets.only(right: 8))
-          ..iconColor(AppColor.colorTextButton)
-          ..size(28)
-          ..radiusSplash(12)
-          ..padding(const EdgeInsets.symmetric(vertical: 10))
-          ..textStyle(const TextStyle(
-              fontSize: 16,
-              color: AppColor.colorTextButton,
-              fontWeight: FontWeight.w500))
-          ..onPressActionClick(() => onAddNewIdentityAction?.call())
-          ..text(AppLocalizations.of(context).createNewIdentity, isVertical: false)
-        ).build()
-      ]),
+        MaterialTextIconButton(
+          key: const Key('button_add_identity'),
+          label: AppLocalizations.of(context).createNewIdentity,
+          icon: imagePaths.icAddIdentity,
+          iconSize: 28,
+          padding: const EdgeInsets.all(16),
+          minimumSize: const Size(double.infinity, 44),
+          onTap: onAddNewIdentityAction
+        )
+      ]
     );
   }
 }
