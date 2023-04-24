2667ad8764ea94fdb84e878d6007ac09cb38045c
dab246 <dab246@users.noreply.github.com>
2023-04-24 09:40:44 +0000

    deploy: 68f5d0380a59da41eeeecee64f94c8f3f99199e6

---
+++ 1791/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+    apiKey: "...",
+    authDomain: "...",
+    databaseURL: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "...",
+});
