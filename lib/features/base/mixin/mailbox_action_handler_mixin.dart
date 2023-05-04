db3faed6d32b7c9c14095cbef3d71bfd050b5a5c
dab246 <tdvu@linagora.com>
2023-05-04 17:01:55 +0700

    TF-1792 Add option `EmptyTrash` for mailbox menu
    (cherry picked from commit 9c7a2c465e57d19d80f33a9cb916923f177ae129)

--- lib/features/base/mixin/mailbox_action_handler_mixin.dart
+++ lib/features/base/mixin/mailbox_action_handler_mixin.dart
@@ -1,8 +1,16 @@
 
+import 'package:core/core.dart';
+import 'package:core/presentation/views/bottom_popup/confirmation_dialog_action_sheet_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
 import 'package:model/mailbox/presentation_mailbox.dart';
+import 'package:pointer_interceptor/pointer_interceptor.dart';
 import 'package:tmail_ui_user/features/mailbox/domain/extensions/presentation_mailbox_extension.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart';
+import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 import 'package:tmail_ui_user/main/utils/app_utils.dart';
 
 mixin MailboxActionHandlerMixin {
@@ -34,6 +42,47 @@ mixin MailboxActionHandlerMixin {
       );
 
       onCallbackAction?.call(context);
+    }
+  }
+
+  void emptyTrashAction(
+    BuildContext context,
+    MailboxId mailboxId,
+    MailboxDashBoardController dashboardController
+  ) {
+    final responsiveUtils = Get.find<ResponsiveUtils>();
+    final imagePaths = Get.find<ImagePaths>();
+
+    if (responsiveUtils.isScreenWithShortestSide(context)) {
+      (ConfirmationDialogActionSheetBuilder(context)
+        ..messageText(AppLocalizations.of(context).emptyTrashMessageDialog)
+        ..onCancelAction(AppLocalizations.of(context).cancel, popBack)
+        ..onConfirmAction(AppLocalizations.of(context).delete, () {
+            popBack();
+            dashboardController.emptyTrashFolderAction(trashFolderId: mailboxId);
+        }))
+      .show();
+    } else {
+      showDialog(
+        context: context,
+        barrierColor: AppColor.colorDefaultCupertinoActionSheet,
+        builder: (context) => PointerInterceptor(child: (ConfirmDialogBuilder(imagePaths)
+          ..key(const Key('confirm_dialog_empty_trash'))
+          ..title(AppLocalizations.of(context).emptyTrash)
+          ..content(AppLocalizations.of(context).emptyTrashMessageDialog)
+          ..addIcon(SvgPicture.asset(imagePaths.icRemoveDialog, fit: BoxFit.fill))
+          ..colorConfirmButton(AppColor.colorConfirmActionDialog)
+          ..styleTextConfirmButton(const TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+              color: AppColor.colorActionDeleteConfirmDialog))
+          ..onCloseButtonAction(popBack)
+          ..onConfirmButtonAction(AppLocalizations.of(context).delete, () {
+            popBack();
+            dashboardController.emptyTrashFolderAction(trashFolderId: mailboxId);
+          })
+          ..onCancelButtonAction(AppLocalizations.of(context).cancel, popBack))
+        .build()));
     }
   }
 }
