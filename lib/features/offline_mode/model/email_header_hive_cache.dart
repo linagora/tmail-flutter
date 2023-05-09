eb855f859e9d77173137b602e1ff1d64720b5ae5
dab246 <tdvu@linagora.com>
2023-05-09 18:38:52 +0700

    TF-1810 Add hive cache object for detailed email
    (cherry picked from commit 0a8b2838205586c76e749d6b02496d22107726f4)

---
+++ lib/features/offline_mode/model/email_header_hive_cache.dart
@@ -0,0 +1,21 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
+import 'package:tmail_ui_user/features/caching/utils/caching_constants.dart';
+
+part 'email_header_hive_cache.g.dart';
+
+@HiveType(typeId: CachingConstants.EMAIL_HEADER_HIVE_CACHE_ID)
+class EmailHeaderHiveCache extends HiveObject with EquatableMixin {
+
+  @HiveField(0)
+  final String name;
+
+  @HiveField(1)
+  final String value;
+
+  EmailHeaderHiveCache({required this.name, required this.value});
+
+  @override
+  List<Object?> get props => [name, value];
+}
