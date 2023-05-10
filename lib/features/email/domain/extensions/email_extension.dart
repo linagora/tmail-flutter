4cb734b9d698ea117d9e991e29f4a12113dcb920
dab246 <tdvu@linagora.com>
2023-05-10 15:43:41 +0700

    TF-1810 Add GetDetailedEmailByIdInteractor to get detailed email
    (cherry picked from commit 765cebec00ae26458fe7f5e17ec6026f82f1fd0f)

---
+++ lib/features/email/domain/extensions/email_extension.dart
@@ -0,0 +1,15 @@
+
+import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:model/extensions/email_extension.dart';
+import 'package:tmail_ui_user/features/email/domain/model/detailed_email.dart';
+
+extension EmailExtension on Email {
+  DetailedEmail toDetailedEmail({String? htmlEmailContent}) {
+    return DetailedEmail(
+      emailId: id!,
+      attachments: allAttachments,
+      headers: headers?.toList(),
+      htmlEmailContent: htmlEmailContent
+    );
+  }
+}
