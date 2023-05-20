144e61009b717649106d76d1f633412d22e9c7a5
sakriwassim <sakriwassim@gmail.com>
2023-05-20 01:02:02 +0100

    fixup! TF-1489 Fix the Extension of EmailHeader

--- lib/features/email/domain/extensions/email_header_extension.dart
+++ lib/features/email/domain/extensions/email_header_extension.dart
@@ -3,5 +3,5 @@ import 'package:jmap_dart_client/jmap/mail/email/email_header.dart';
 import 'package:tmail_ui_user/features/offline_mode/model/email_header_hive_cache.dart';
 
 extension EmailHeaderExtension on EmailHeader {
-  EmailHeaderHiveCache toHiveCache() => EmailHeaderHiveCache(name: name ?? "", value: value ?? "");
+  EmailHeaderHiveCache toHiveCache() => EmailHeaderHiveCache(name: name, value: value);
 }
