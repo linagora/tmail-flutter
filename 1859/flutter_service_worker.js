c333ed8ae2c043bbc387222d6b62b6dc67cdd6a7
sakriwassim <sakriwassim@users.noreply.github.com>
2023-05-23 00:42:52 +0000

    deploy: c049b1cb277e700fdd50b9fc11d2ad0ee8826dfb

--- 1859/flutter_service_worker.js
+++ 1859/flutter_service_worker.js
@@ -248,8 +248,8 @@ const RESOURCES = {
 "assets/env.file": "4957181ef7fae9f1ac3bd566504c354d",
 "assets/FontManifest.json": "4dfd5e5693c0331263a39042fc45fa43",
 "assets/AssetManifest.json": "3cec02a67d0a3e720c04a286141c5e02",
-"index.html": "8a375de3d24ec7e735ce2678e4a5270c",
-"/": "8a375de3d24ec7e735ce2678e4a5270c",
+"index.html": "448e0c3f999bd79bedb0c04a368407b8",
+"/": "448e0c3f999bd79bedb0c04a368407b8",
 "main.dart.js_1.part.js": "de24f151f50271b8a3ffe7dff469aba6",
 "main.dart.js_10.part.js": "c60300365f1fbf74fab7f1393e7c7d15",
 "logout-callback.html": "999326dc9a0f37c9a9a4280b4a35dc51",
@@ -268,7 +268,7 @@ const RESOURCES = {
 "firebase-messaging-sw.js": "f76c3a61d564249a1734cf3487753856",
 "main.dart.js_8.part.js": "144c3f08cf07e5c5d7b76ca0836f5529",
 "main.dart.js_9.part.js": "32dd77ca0150dc908fb4740de632bb7a",
-"main.dart.js": "ddf5db75f64e23ee86ac9a8282241562",
+"main.dart.js": "3a18e7ee53e2fda05b5d893c04cae132",
 "manifest.json": "349bf3eb9699317dca24e90c8c657f3c",
 "main.dart.js_3.part.js": "19106e5291ce838468621e315a5025df"
 };
