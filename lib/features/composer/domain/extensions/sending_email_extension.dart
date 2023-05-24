5636d8b9de4ea082c421f0995ce7140cccb1c2b6
dab246 <tdvu@linagora.com>
2023-05-24 13:05:44 +0700

    TF-1812 Add SendingEmailHiveCache to store sending email

---
+++ lib/features/composer/domain/extensions/sending_email_extension.dart
@@ -0,0 +1,22 @@
+
+import 'package:model/extensions/email_id_extensions.dart';
+import 'package:model/extensions/identity_id_extension.dart';
+import 'package:model/extensions/mailbox_id_extension.dart';
+import 'package:tmail_ui_user/features/composer/domain/model/sending_email.dart';
+import 'package:tmail_ui_user/features/offline_mode/model/sending_email_hive_cache.dart';
+
+extension SendingEmailExtension on SendingEmail {
+  SendingEmailHiveCache toHiveCache() {
+    return SendingEmailHiveCache(
+      sendingId,
+      email.toJson(),
+      emailActionType.name,
+      sentMailboxId?.asString,
+      emailIdDestroyed?.asString,
+      emailIdAnsweredOrForwarded?.asString,
+      identityId?.asString,
+      mailboxNameRequest?.name,
+      creationIdRequest?.value
+    );
+  }
+}
---
+++ model/lib/extensions/identity_id_extension.dart
@@ -0,0 +1,6 @@
+
+import 'package:jmap_dart_client/jmap/identities/identity.dart';
+
+extension IdentityIdExtension on IdentityId {
+  String get asString => id.value;
+}
