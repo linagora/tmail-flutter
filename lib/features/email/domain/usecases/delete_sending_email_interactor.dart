b66931aa1b6cabb7ac32d9457d6d4ed69d3ea42d
HuyNguyen <quanghnguyen@linagora.com>
2023-05-31 00:57:45 +0700

    TF-1842: Create `deleteSendingEmail` in domain layer

---
+++ lib/features/email/domain/usecases/delete_sending_email_interactor.dart
@@ -0,0 +1,24 @@
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:dartz/dartz.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/user_name.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:tmail_ui_user/features/email/domain/repository/email_repository.dart';
+import 'package:tmail_ui_user/features/email/domain/state/delete_sending_email_state.dart';
+
+class DeleteSendingEmailInteractor {
+  final EmailRepository _emailRepository;
+
+  DeleteSendingEmailInteractor(this._emailRepository);
+
+  Stream<Either<Failure, Success>> execute(AccountId accountId, UserName userName, EmailId emailId) async* {
+    try {
+      yield Right<Failure, Success>(DeleteSendingEmailLoading());
+      await _emailRepository.deleteSendingEmail(accountId, userName, emailId);
+      yield Right<Failure, Success>(DeleteSendingEmailSuccess());
+    } catch (e) {
+      yield Left<Failure, Success>(DeleteSendingEmailFailure(e));
+    }
+  }
+}
