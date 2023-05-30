ba806581fd70dac9cb084c8fe32fcceb78b5a5a5
sakriwassim <sakriwassim@gmail.com>
2023-05-30 01:43:37 +0100

    TF-1489 Add the path of the icon

--- core/lib/presentation/resources/image_paths.dart
+++ core/lib/presentation/resources/image_paths.dart
@@ -179,6 +179,7 @@ class ImagePaths {
   String get icToastSuccessMessage => _getImagePath('ic_toast_success_message.svg');
   String get icForwarded => _getImagePath('ic_forwarded.svg');
   String get icReplyAndForward => _getImagePath('ic_reply_and_forward.svg');
+  String get icCalendarEvent => _getImagePath('ic_calendar_event.svg');
 
   String _getImagePath(String imageName) {
     return AssetsPaths.images + imageName;
