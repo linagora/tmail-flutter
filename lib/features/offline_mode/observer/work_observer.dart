1fbba21f0b659df1f59ed6a5bbfca74572321c9a
HuyNguyen <quanghnguyen@linagora.com>
2023-05-31 21:56:10 +0700

    TF-1842: Trigger `handleBackgroundTask` when finish task on `WorkObserver`

--- lib/features/offline_mode/observer/work_observer.dart
+++ lib/features/offline_mode/observer/work_observer.dart
@@ -1,13 +1,15 @@
 
+import 'dart:async';
+
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
 import 'package:core/utils/app_logger.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class WorkObserver {
-  Future<void> bindDI();
+  Future<void> bindDI(Completer<bool> completer);
 
-  Future<void> observe(String taskId, Map<String, dynamic> inputData);
+  Future<void> observe(String taskId, Map<String, dynamic> inputData, Completer<bool> completer);
 
   void consumeState(Stream<Either<Failure, Success>> newStateStream) {
     newStateStream.listen(
