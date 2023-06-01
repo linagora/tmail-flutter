a8bb19db058069c762ff4da334093b66528e1c8d
dab246 <tdvu@linagora.com>
2023-06-01 14:03:46 +0700

    TF-1813 Implement GetAllSendingEmails for domain and data layer

--- lib/features/offline_mode/manager/sending_email_cache_manager.dart
+++ lib/features/offline_mode/manager/sending_email_cache_manager.dart
@@ -5,6 +5,7 @@ import 'package:jmap_dart_client/jmap/core/user_name.dart';
 import 'package:model/extensions/account_id_extensions.dart';
 import 'package:tmail_ui_user/features/caching/clients/sending_email_hive_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/utils/cache_utils.dart';
+import 'package:tmail_ui_user/features/offline_mode/extensions/list_sending_email_hive_cache_extension.dart';
 import 'package:tmail_ui_user/features/offline_mode/model/sending_email_hive_cache.dart';
 
 class SendingEmailCacheManager {
@@ -21,5 +22,12 @@ class SendingEmailCacheManager {
     final keyCache = TupleKey(sendingEmailHiveCache.sendingId, accountId.asString, userName.value).encodeKey;
     log('SendingEmailCacheManager::storeSendingEmail():keyCache: $keyCache | sendingEmailHiveCache: $sendingEmailHiveCache');
     return _hiveCacheClient.insertItem(keyCache, sendingEmailHiveCache);
+  }
+
+  Future<List<SendingEmailHiveCache>> getAllSendingEmails(AccountId accountId, UserName userName) async {
+     final sendingEmailsCache = await _hiveCacheClient.getListByTupleKey(accountId.asString, userName.value);
+     log('SendingEmailCacheManager::getAllSendingEmails():COUNT: ${sendingEmailsCache.length}');
+     sendingEmailsCache.sortByLatestTime();
+     return sendingEmailsCache;
   }
 }
