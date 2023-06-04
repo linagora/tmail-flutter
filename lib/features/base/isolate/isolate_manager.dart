693dc0ef728867c3c514f3b933d7da17bc5e9b08
dab246 <tdvu@linagora.com>
2023-06-04 12:18:25 +0700

    TF-1900 Add IsolateManager to for data communication the isolate

---
+++ lib/features/base/isolate/isolate_manager.dart
@@ -0,0 +1,46 @@
+
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:core/presentation/utils/shims/dart_ui_real.dart';
+import 'package:core/utils/app_logger.dart';
+
+abstract class IsolateManager {
+
+  final _eventReceivePort = ReceivePort();
+  StreamSubscription? _streamSubscription;
+
+  String get isolateIdentityName;
+
+  void initial({
+    Function(dynamic)? onData,
+    Function? onError,
+    Function()? onDone,
+  }) {
+    try {
+      IsolateNameServer.registerPortWithName(_eventReceivePort.sendPort, isolateIdentityName);
+      _streamSubscription = _eventReceivePort.listen(onData, onError: onError, onDone: onDone);
+    } catch (e) {
+      logError('IsolateManager::initial():EXCEPTION: $e');
+    }
+  }
+
+  void addEvent(Object? value) {
+    log('IsolateManager::addEvent():value: $value');
+    try {
+      final sendPort = IsolateNameServer.lookupPortByName(isolateIdentityName);
+      if (sendPort != null) {
+        sendPort.send(value);
+      } else {
+        logError('IsolateManager::addEvent(): sendPort is null');
+      }
+    } catch (e) {
+      logError('IsolateManager::addEvent():EXCEPTION: $e');
+    }
+  }
+
+  void release() {
+    _eventReceivePort.close();
+    _streamSubscription?.cancel();
+  }
+}
