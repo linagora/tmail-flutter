d19cc77f5ce2de4f21a05ab65ab582bc62e67fbc
dab246 <tdvu@linagora.com>
2023-06-05 09:13:33 +0700

    TF-1866 Display BottomBar when on long press sending email item
    (cherry picked from commit dec400499e44c25a9f622424e5e26949f944b5a0)

---
+++ lib/features/sending_queue/presentation/extensions/list_sending_email_extension.dart
@@ -0,0 +1,17 @@
+
+import 'package:tmail_ui_user/features/composer/domain/extensions/sending_email_extension.dart';
+import 'package:tmail_ui_user/features/composer/domain/model/sending_email.dart';
+
+extension ListSendingEmailExtension on List<SendingEmail> {
+  List<SendingEmail> toggleSelection({required SendingEmail sendingEmailSelected}) =>
+    map((sendingEmail) => sendingEmailSelected.sendingId == sendingEmail.sendingId
+      ? sendingEmail.toggleSelection()
+      : sendingEmail
+    ).toList();
+
+  List<SendingEmail> unAllSelected() => map((sendingEmail) => sendingEmail.unSelected()).toList();
+
+  bool isAllSelected() => every((sendingEmail) => sendingEmail.isSelected);
+
+  bool isAllUnSelected() => every((sendingEmail) => !sendingEmail.isSelected);
+}
