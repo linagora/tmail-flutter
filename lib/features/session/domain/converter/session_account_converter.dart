3be2751b3eed1f6b0a7679cc6241527ad4a74b58
dab246 <tdvu@linagora.com>
2023-06-06 18:27:31 +0700

    TF-1899 Add SessionHiveObj to store session

---
+++ lib/features/session/domain/converter/session_account_converter.dart
@@ -0,0 +1,14 @@
+import 'package:jmap_dart_client/http/converter/account_id_converter.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/account/account.dart';
+import 'package:tmail_ui_user/features/session/domain/extensions/session_account_extension.dart';
+
+class SessionAccountConverter {
+
+  MapEntry<String, dynamic> convertToMapEntry(AccountId accountId, Account account) {
+    return MapEntry(
+      const AccountIdConverter().toJson(accountId),
+      account.toJson()
+    );
+  }
+}
---
+++ lib/features/session/domain/extensions/session_extensions.dart
@@ -0,0 +1,35 @@
+
+import 'package:jmap_dart_client/http/converter/state_converter.dart';
+import 'package:jmap_dart_client/http/converter/user_name_converter.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:tmail_ui_user/features/session/data/model/session_hive_obj.dart';
+import 'package:tmail_ui_user/features/session/domain/converter/session_account_converter.dart';
+import 'package:tmail_ui_user/features/session/domain/converter/session_capabilities_converter.dart';
+import 'package:tmail_ui_user/features/session/domain/converter/session_primary_account_converter.dart';
+
+extension SessionExtensions on Session {
+
+  Map<String, dynamic> toJson() {
+    final val = <String, dynamic>{};
+
+    void writeNotNull(String key, dynamic value) {
+      if (value != null) {
+        val[key] = value;
+      }
+    }
+
+    writeNotNull('capabilities', capabilities.map((key, value) =>  SessionCapabilitiesConverter().convertToMapEntry(key, value)));
+    writeNotNull('accounts', accounts.map((key, value) => SessionAccountConverter().convertToMapEntry(key, value)));
+    writeNotNull('primaryAccounts', primaryAccounts.map((key, value) => SessionPrimaryAccountConverter().convertToMapEntry(key, value)));
+    writeNotNull('username', const UserNameConverter().toJson(username));
+    writeNotNull('apiUrl', apiUrl.toString());
+    writeNotNull('downloadUrl', downloadUrl.toString());
+    writeNotNull('uploadUrl', uploadUrl.toString());
+    writeNotNull('eventSourceUrl', eventSourceUrl.toString());
+    writeNotNull('state', const StateConverter().toJson(state));
+
+    return val;
+  }
+
+  SessionHiveObj toHiveObj() => SessionHiveObj(values: toJson());
+}
