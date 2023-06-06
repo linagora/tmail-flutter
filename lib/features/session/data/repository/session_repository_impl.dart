a6b60484d7c64042f6d39e5b33ccb62e632b1741
dab246 <tdvu@linagora.com>
2023-06-06 22:20:35 +0700

    TF-1899 Implement store session to hive
    (cherry picked from commit e42b61f67a8352c9d9bc30207031989785295b65)

--- lib/features/session/data/repository/session_repository_impl.dart
+++ lib/features/session/data/repository/session_repository_impl.dart
@@ -1,15 +1,21 @@
+import 'package:core/data/model/source_type/data_source_type.dart';
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
 import 'package:tmail_ui_user/features/session/data/datasource/session_datasource.dart';
 import 'package:tmail_ui_user/features/session/domain/repository/session_repository.dart';
 
 class SessionRepositoryImpl extends SessionRepository {
 
-  final SessionDataSource sessionDataSource;
+  final Map<DataSourceType, SessionDataSource> sessionDataSource;
 
   SessionRepositoryImpl(this.sessionDataSource);
 
   @override
   Future<Session> getSession() {
-    return sessionDataSource.getSession();
+    return sessionDataSource[DataSourceType.network]!.getSession();
+  }
+
+  @override
+  Future<void> storeSession(Session session) {
+    return sessionDataSource[DataSourceType.hiveCache]!.storeSession(session);
   }
 }
