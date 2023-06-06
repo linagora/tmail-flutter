e42b61f67a8352c9d9bc30207031989785295b65
dab246 <tdvu@linagora.com>
2023-06-06 22:20:35 +0700

    TF-1899 Implement store session to hive

--- lib/features/session/domain/repository/session_repository.dart
+++ lib/features/session/domain/repository/session_repository.dart
@@ -2,4 +2,6 @@ import 'package:jmap_dart_client/jmap/core/session/session.dart';
 
 abstract class SessionRepository {
   Future<Session> getSession();
+
+  Future<void> storeSession(Session session);
 }
