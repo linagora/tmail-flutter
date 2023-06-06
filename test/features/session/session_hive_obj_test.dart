44bc6b20845d894da24e68322681a7718452591f
dab246 <tdvu@linagora.com>
2023-06-06 18:27:31 +0700

    TF-1899 Add SessionHiveObj to store session
    (cherry picked from commit de5c6f2c76e72ce85225e5a018e83dd815016733)

---
+++ test/features/session/session_hive_obj_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tmail_ui_user/features/session/data/extensions/session_hive_obj_extension.dart';
+import 'package:tmail_ui_user/features/session/domain/extensions/session_extensions.dart';
+
+import '../../fixtures/session_fixtures.dart';
+
+void main() {
+  group('SessionHiveObj test', () {
+    test('toSession() should return parsing correctly session', () {
+      final sessionHiveObj = SessionFixtures.aliceSession.toHiveObj();
+      final sessionParsed = sessionHiveObj.toSession();
+
+      expect(sessionParsed.accounts.length, equals(SessionFixtures.aliceSession.accounts.length));
+    });
+  });
+}
