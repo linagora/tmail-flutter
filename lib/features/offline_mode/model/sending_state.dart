84ed2654547e1532f8965bccb1655dd70e7be359
dab246 <tdvu@linagora.com>
2023-06-09 19:32:31 +0700

    TF-1915 Updating correctly state sending email when work manager running

--- lib/features/offline_mode/model/sending_state.dart
+++ lib/features/offline_mode/model/sending_state.dart
@@ -5,73 +5,98 @@ import 'package:flutter/material.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
 
 enum SendingState {
-  ready,
-  delivering,
-  error;
+  waiting,
+  running,
+  error,
+  success;
 
   String getTitle(BuildContext context) {
     switch(this) {
-      case SendingState.delivering:
+      case SendingState.waiting:
+      case SendingState.running:
         return AppLocalizations.of(context).delivering;
       case SendingState.error:
         return AppLocalizations.of(context).error;
-      case SendingState.ready:
+      case SendingState.success:
         return '';
     }
   }
 
   String getIcon(ImagePaths imagePaths) {
     switch(this) {
-      case SendingState.delivering:
+      case SendingState.waiting:
+      case SendingState.running:
         return imagePaths.icDelivering;
       case SendingState.error:
         return imagePaths.icError;
-      case SendingState.ready:
+      case SendingState.success:
         return '';
     }
   }
 
   String getAvatarGroup(ImagePaths imagePaths) {
     switch(this) {
-      case SendingState.delivering:
+      case SendingState.running:
         return imagePaths.icAvatarGroupDelivering;
+      case SendingState.waiting:
       case SendingState.error:
-        return imagePaths.icAvatarGroup;
-      case SendingState.ready:
+      case SendingState.success:
         return imagePaths.icAvatarGroup;
     }
   }
 
   String getAvatarPersonal(ImagePaths imagePaths) {
     switch(this) {
-      case SendingState.delivering:
+      case SendingState.running:
         return imagePaths.icAvatarPersonalDelivering;
+      case SendingState.waiting:
       case SendingState.error:
-        return imagePaths.icAvatarPersonal;
-      case SendingState.ready:
+      case SendingState.success:
         return imagePaths.icAvatarPersonal;
     }
   }
 
   Color getTitleColor() {
     switch(this) {
-      case SendingState.delivering:
+      case SendingState.waiting:
+      case SendingState.running:
+      case SendingState.success:
         return AppColor.colorTitleSendingItem;
       case SendingState.error:
         return AppColor.colorErrorState;
-      case SendingState.ready:
-        return Colors.transparent;
     }
   }
 
   Color getBackgroundColor() {
     switch(this) {
-      case SendingState.delivering:
+      case SendingState.waiting:
+      case SendingState.running:
+      case SendingState.success:
         return AppColor.colorBackgroundDeliveringState;
       case SendingState.error:
         return AppColor.colorBackgroundErrorState;
-      case SendingState.ready:
-        return Colors.transparent;
+    }
+  }
+
+  Color getTitleSendingEmailItemColor() {
+    switch(this) {
+      case SendingState.running:
+        return AppColor.colorDeliveringState;
+      case SendingState.waiting:
+      case SendingState.error:
+      case SendingState.success:
+        return Colors.black;
+    }
+  }
+
+  Color getSubTitleSendingEmailItemColor() {
+    switch(this) {
+      case SendingState.running:
+        return AppColor.colorDeliveringState;
+      case SendingState.waiting:
+      case SendingState.error:
+      case SendingState.success:
+        return AppColor.colorTitleSendingItem;
     }
   }
 }
