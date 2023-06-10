f1c191f690b268f3b91ded5fca085311666abb94
dab246 <tdvu@linagora.com>
2023-06-10 19:14:10 +0700

    TF-1897 Implement delete multiple sending email

--- lib/features/sending_queue/domain/usecases/delete_multiple_sending_email_interactor.dart
+++ lib/features/sending_queue/domain/usecases/delete_multiple_sending_email_interactor.dart
@@ -3,32 +3,19 @@ import 'package:core/presentation/state/success.dart';
 import 'package:dartz/dartz.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/user_name.dart';
+import 'package:tmail_ui_user/features/sending_queue/domain/repository/sending_queue_repository.dart';
 import 'package:tmail_ui_user/features/sending_queue/domain/state/delete_multiple_sending_email_state.dart';
-import 'package:tmail_ui_user/features/sending_queue/domain/usecases/delete_single_sending_email_interactor.dart';
 
 class DeleteMultipleSendingEmailInteractor {
-  final DeleteSingleSendingEmailInteractor _deleteSingleSendingEmailInteractor;
+  final SendingQueueRepository _sendingQueueRepository;
 
-  DeleteMultipleSendingEmailInteractor(this._deleteSingleSendingEmailInteractor);
+  DeleteMultipleSendingEmailInteractor(this._sendingQueueRepository);
 
   Stream<Either<Failure, Success>> execute(AccountId accountId, UserName userName, List<String> sendingIds) async* {
     try {
       yield Right<Failure, Success>(DeleteMultipleSendingEmailLoading());
-      final listResult = await Future.wait(
-        sendingIds.map((sendingId) => _deleteSingleSendingEmailInteractor.execute(accountId, userName, sendingId)),
-        eagerError: true
-      );
-
-      bool isAllSuccess = listResult.every((either) => either.isRight());
-      bool isAllFailure = listResult.every((either) => either.isLeft());
-
-      if (isAllSuccess) {
-        yield Right<Failure, Success>(DeleteMultipleSendingEmailAllSuccess());
-      } else if (isAllFailure) {
-        yield Left<Failure, Success>(DeleteMultipleSendingEmailAllFailure(null));
-      } else {
-        yield Right<Failure, Success>(DeleteMultipleSendingEmailHasSomeSuccess());
-      }
+      await _sendingQueueRepository.deleteMultipleSendingEmail(accountId, userName, sendingIds);
+      yield Right<Failure, Success>(DeleteMultipleSendingEmailSuccess(sendingIds));
     } catch (e) {
       yield Left<Failure, Success>(DeleteMultipleSendingEmailFailure(e));
     }
