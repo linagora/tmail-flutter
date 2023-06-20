fe45a6128ac6e607c1b3d4b0b6d405ef751056aa
dab246 <tdvu@linagora.com>
2023-06-20 23:32:53 +0700

    TF-1923 Fix logic store opened email
    (cherry picked from commit 8d9c8d3ee8ea519e683d5a7cbb5b080d2f78e292)

--- lib/features/email/domain/usecases/store_opened_email_interactor.dart
+++ lib/features/email/domain/usecases/store_opened_email_interactor.dart
@@ -1,8 +1,10 @@
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
+import 'package:core/utils/app_logger.dart';
 import 'package:dartz/dartz.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:tmail_ui_user/features/email/domain/exceptions/email_cache_exceptions.dart';
 import 'package:tmail_ui_user/features/email/domain/model/detailed_email.dart';
 import 'package:tmail_ui_user/features/email/domain/repository/email_repository.dart';
 import 'package:tmail_ui_user/features/email/domain/state/store_opened_email_state.dart';
@@ -19,10 +21,27 @@ class StoreOpenedEmailInteractor {
   ) async* {
     try {
       yield Right<Failure, Success>(StoreOpenedEmailLoading());
-      await _emailRepository.storeOpenedEmail(session, accountId, detailedEmail);
-      yield Right<Failure, Success>(StoreOpenedEmailSuccess());
+      final isOpenedEmailExist = await _isOpenedEmailAlreadyStored(session, accountId, detailedEmail);
+      log('StoreOpenedEmailInteractor::execute():isOpenedEmailExist: $isOpenedEmailExist');
+      if (!isOpenedEmailExist) {
+        await _emailRepository.storeOpenedEmail(session, accountId, detailedEmail);
+        yield Right<Failure, Success>(StoreOpenedEmailSuccess());
+      } else {
+        yield Left<Failure, Success>(StoreOpenedEmailFailure(OpenedEmailAlreadyStoredException()));
+      }
     } catch (e) {
       yield Left<Failure, Success>(StoreOpenedEmailFailure(e));
+    }
+  }
+
+  Future<bool> _isOpenedEmailAlreadyStored(Session session, AccountId accountId, DetailedEmail detailedEmail) async {
+    try {
+      final storedOpenEmail = await _emailRepository.getStoredOpenedEmail(session, accountId, detailedEmail.emailId);
+      log('StoreOpenedEmailInteractor::isOpenedEmailAlreadyStored():storedOpenEmail: $storedOpenEmail');
+      return true;
+    } catch (err) {
+      logError('StoreOpenedEmailInteractor::isOpenedEmailAlreadyStored():EXCEPTION: $err');
+      return false;
     }
   }
 }
