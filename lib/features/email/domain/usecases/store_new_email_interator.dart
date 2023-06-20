dd8cafc92d6c1f7d5c60f8f60e5a9a517912126e
dab246 <tdvu@linagora.com>
2023-06-21 01:53:38 +0700

    TF-1923 Fix duplicated store new email from push notification
    (cherry picked from commit 076bbe249cc870b944a2dc40bc31012e061e0897)

--- lib/features/email/domain/usecases/store_new_email_interator.dart
+++ lib/features/email/domain/usecases/store_new_email_interator.dart
@@ -5,6 +5,7 @@ import 'package:dartz/dartz.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:tmail_ui_user/features/email/domain/exceptions/email_cache_exceptions.dart';
 import 'package:tmail_ui_user/features/email/domain/model/detailed_email.dart';
 import 'package:tmail_ui_user/features/email/domain/repository/email_repository.dart';
 import 'package:tmail_ui_user/features/email/domain/state/store_new_email_state.dart';
@@ -22,14 +23,31 @@ class StoreNewEmailInteractor {
   ) async* {
     try {
       yield Right<Failure, Success>(StoreNewEmailLoading());
-      await Future.wait([
-        _emailRepository.storeEmail(session, accountId, email),
-        _emailRepository.storeDetailedNewEmail(session, accountId, detailedEmail),
-      ]);
-      log('StoreNewEmailInteractor::execute():Store Success | EMAIL: ${detailedEmail.htmlEmailContent} | TIME: ${detailedEmail.createdTime}');
-      yield Right<Failure, Success>(StoreNewEmailSuccess());
+      final isNewEmailExist = await _isNewEmailAlreadyStored(session, accountId, detailedEmail);
+      log('StoreNewEmailInteractor::execute():isNewEmailExist: $isNewEmailExist');
+      if (!isNewEmailExist) {
+        await Future.wait([
+          _emailRepository.storeEmail(session, accountId, email),
+          _emailRepository.storeDetailedNewEmail(session, accountId, detailedEmail),
+        ], eagerError: true);
+        log('StoreNewEmailInteractor::execute():Store Success | EMAIL: ${detailedEmail.emailId} | TIME: ${detailedEmail.createdTime}');
+        yield Right<Failure, Success>(StoreNewEmailSuccess());
+      } else {
+        log('StoreNewEmailInteractor::execute():Store Failure | EMAIL: ${detailedEmail.emailId} | TIME: ${detailedEmail.createdTime}');
+        yield Left<Failure, Success>(StoreNewEmailFailure(NewEmailAlreadyStoredException()));
+      }
     } catch (e) {
       yield Left<Failure, Success>(StoreNewEmailFailure(e));
+    }
+  }
+
+  Future<bool> _isNewEmailAlreadyStored(Session session, AccountId accountId, DetailedEmail detailedEmail) async {
+    try {
+      await _emailRepository.getStoredNewEmail(session, accountId, detailedEmail.emailId);
+      return true;
+    } catch (err) {
+      logError('StoreNewEmailInteractor::_isNewEmailAlreadyStored():EXCEPTION: $err');
+      return false;
     }
   }
 }
