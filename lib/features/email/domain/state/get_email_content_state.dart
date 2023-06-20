def6ae4645ed0245daa956ffd6d28665ebd70f38
dab246 <tdvu@linagora.com>
2023-06-20 23:32:53 +0700

    TF-1923 Fix logic store opened email

--- lib/features/email/domain/state/get_email_content_state.dart
+++ lib/features/email/domain/state/get_email_content_state.dart
@@ -2,46 +2,45 @@ import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email.dart';
 import 'package:model/email/attachment.dart';
-import 'package:model/email/email_content.dart';
 
 class GetEmailContentLoading extends LoadingState {}
 
 class GetEmailContentSuccess extends UIState {
-  final String emailContents;
-  final List<EmailContent> emailContentsDisplayed;
+  final String emailContent;
+  final String emailContentDisplayed;
   final List<Attachment> attachments;
   final Email? emailCurrent;
 
   GetEmailContentSuccess({
-    required this.emailContents,
-    required this.emailContentsDisplayed,
+    required this.emailContent,
+    required this.emailContentDisplayed,
     required this.attachments,
     required this.emailCurrent
   });
 
   @override
   List<Object?> get props => [
-    emailContents,
-    emailContentsDisplayed,
+    emailContent,
+    emailContentDisplayed,
     attachments,
     emailCurrent
   ];
 }
 
 class GetEmailContentFromCacheSuccess extends UIState {
-  final String emailContentString;
+  final String emailContent;
   final List<Attachment> attachments;
   final Email? emailCurrent;
 
   GetEmailContentFromCacheSuccess({
-    required this.emailContentString,
+    required this.emailContent,
     required this.attachments,
     this.emailCurrent
   });
 
   @override
   List<Object?> get props => [
-    emailContentString,
+    emailContent,
     attachments,
     emailCurrent,
   ];
