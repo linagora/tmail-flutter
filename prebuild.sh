26ee7af6706eaae755ed8644e43e493575e1e3c8
dab246 <tdvu@linagora.com>
2023-07-17 17:07:58 +0700

    Use `dart run` replace `flutter pub run` deprecated
    (cherry picked from commit c91871267d930213fa51b37e677a34dca1a89daa)

--- prebuild.sh
+++ prebuild.sh
@@ -12,22 +12,22 @@ flutter pub get
 # flutter pub get && pod install
 
 cd ../model
-flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../contact
-flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../forward
-flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../rule_filter
-flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../fcm
-flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ..
 flutter pub get \
-    && flutter pub run build_runner build --delete-conflicting-outputs \
-    && flutter pub run intl_generator:extract_to_arb --output-dir=./lib/l10n lib/main/localizations/app_localizations.dart \
-    && flutter pub run intl_generator:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/main/localizations/app_localizations.dart lib/l10n/intl*.arb
+    && dart run build_runner build --delete-conflicting-outputs \
+    && dart run intl_generator:extract_to_arb --output-dir=./lib/l10n lib/main/localizations/app_localizations.dart \
+    && dart run intl_generator:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/main/localizations/app_localizations.dart lib/l10n/intl*.arb
