a59c8c219460003a3e5f10abaf53a21d6625c068
dab246 <tdvu@linagora.com>
2023-07-20 15:59:32 +0700

    TF-2046 Add delay time for a while for GetX to delete the controller instance of the previous page
    (cherry picked from commit 466784af57d5c6be7b205e4b76e94a2ffa8f8347)

--- lib/features/base/base_controller.dart
+++ lib/features/base/base_controller.dart
@@ -279,8 +279,13 @@ abstract class BaseController extends GetxController
   bool _isFcmActivated(Session session, AccountId accountId) =>
     [FirebaseCapability.fcmIdentifier].isSupported(session, accountId) && AppConfig.fcmAvailable;
 
-  void goToLogin({LoginArguments? arguments}) {
-    pushAndPopAll(AppRoutes.login, arguments: arguments);
+  void goToLogin({LoginArguments? arguments}) async {
+    await Future.delayed(
+      const Duration(milliseconds: 500),
+      () async {
+        await pushAndPopAll(AppRoutes.login, arguments: arguments);
+      }
+    );
   }
 
   void logout(Session? session, AccountId? accountId) {
