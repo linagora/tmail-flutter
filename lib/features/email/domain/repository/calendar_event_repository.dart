cc0057bbeda8d185dfe6909efeecb395f7f2e872
dab246 <tdvu@linagora.com>
2023-07-21 16:28:29 +0700

    TF-2035 Add abstract parse calendar event method
    (cherry picked from commit 58eb6ae8075ad4a2f644a0a41e75c145217a96f4)

---
+++ lib/features/email/domain/repository/calendar_event_repository.dart
@@ -0,0 +1,8 @@
+
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
+import 'package:jmap_dart_client/jmap/mail/calendar/calendar_event.dart';
+
+abstract class CalendarEventRepository {
+  Future<List<CalendarEvent>> parse(AccountId accountId, Set<Id> blobIds);
+}
