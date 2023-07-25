1e1693ec6d4b4ceb4c22375a182bfb768e75a4d2
dab246 <tdvu@linagora.com>
2023-07-25 12:11:59 +0700

    TF-2035 Change size padding, text for event banner
    (cherry picked from commit eb6ea34081ebb9790a1cc4e484fb4c07dbfeabf6)

--- lib/features/email/presentation/styles/calendar_event_action_banner_styles.dart
+++ lib/features/email/presentation/styles/calendar_event_action_banner_styles.dart
@@ -1,7 +1,9 @@
 
 class CalendarEventActionBannerStyles {
-  static const double borderRadius = 16;
-  static const double contentPadding = 16;
+  static const double borderRadius = 12;
+  static const double contentPadding = 12;
   static const double viewHorizontalMargin = 16;
-  static const double viewVerticalMargin = 12;
+  static const double titleTextSize = 16;
+  static const double subTileTextSize = 13;
+  static const double iconSize = 20;
 }
