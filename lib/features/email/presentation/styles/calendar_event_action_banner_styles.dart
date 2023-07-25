ecbc296ec685310705186fbd7c27c6ee7ec70009
dab246 <tdvu@linagora.com>
2023-07-25 19:57:46 +0700

    TF-2040 Add vertical margin for CalendarEventActionBanner

--- lib/features/email/presentation/styles/calendar_event_action_banner_styles.dart
+++ lib/features/email/presentation/styles/calendar_event_action_banner_styles.dart
@@ -3,6 +3,7 @@ class CalendarEventActionBannerStyles {
   static const double borderRadius = 12;
   static const double contentPadding = 12;
   static const double viewHorizontalMargin = 16;
+  static const double viewVerticalMargin = 12;
   static const double titleTextSize = 16;
   static const double subTileTextSize = 13;
   static const double iconSize = 20;
