ce284990a101ede7bcc58fd581fef2b03b07217c
dab246 <tdvu@linagora.com>
2023-07-25 19:58:14 +0700

    TF-2040 Add CalendarEventInformationWidget in email view

---
+++ lib/features/email/presentation/styles/calendar_event_information_widget_styles.dart
@@ -0,0 +1,8 @@
+
+class CalendarEventInformationWidgetStyles {
+  static const double borderRadius = 16;
+  static const double contentPadding = 16;
+  static const double verticalMargin = 12;
+  static const double horizontalMargin = 16;
+  static const double calendarDateIconMargin = 16;
+}
