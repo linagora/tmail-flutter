0b8da6cd3ae6a8cb175b076ece554c08b4c4523a
dab246 <tdvu@linagora.com>
2023-07-25 19:57:07 +0700

    TF-2040 Create CalendarDateIconWidget

---
+++ lib/features/email/presentation/styles/calendar_date_icon_widget_styles.dart
@@ -0,0 +1,10 @@
+
+class CalendarIconWidgetStyles {
+  static const double borderRadius = 8;
+  static const double headerVerticalContentPadding = 4;
+  static const double headerHorizontalContentPadding = 8;
+  static const double headerTextSize = 14;
+  static const double bodyDayTextSize = 40;
+  static const double bodyWeekDayTextSize = 14;
+  static const double bodyContentPadding = 8;
+}
