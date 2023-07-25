e6810dc58cc9170aebecddaeba53a703dbb761c0
dab246 <tdvu@linagora.com>
2023-07-25 19:54:56 +0700

    TF-2040 Add LanguageCodeConstants to manage language supported
    (cherry picked from commit de57a6a8a43c18e1deed057d6a0e12c06c2bb1fe)

---
+++ lib/main/localizations/language_code_constants.dart
@@ -0,0 +1,9 @@
+
+class LanguageCodeConstants {
+  static const String english = 'en';
+  static const String french = 'fr';
+  static const String vietnamese = 'vi';
+  static const String italian = 'it';
+  static const String russian = 'ru';
+  static const String arabic = 'ar';
+}
--- lib/main/utils/app_utils.dart
+++ lib/main/utils/app_utils.dart
@@ -2,10 +2,13 @@ import 'package:core/utils/platform_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+import 'package:tmail_ui_user/main/localizations/language_code_constants.dart';
 import 'package:tmail_ui_user/main/utils/app_config.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:intl/intl.dart' as intl;
+import 'package:date_format/date_format.dart' as date_format;
 
 class AppUtils {
 
@@ -47,5 +50,24 @@ class AppUtils {
         SnackBar(content: Text(AppLocalizations.of(context).email_address_copied_to_clipboard))
       );
     });
+  }
+
+  static date_format.DateLocale getCurrentDateLocale() {
+    final currentLanguageCode = Get.locale?.languageCode;
+    if (currentLanguageCode == LanguageCodeConstants.french) {
+      return const date_format.FrenchDateLocale();
+    } else if (currentLanguageCode == LanguageCodeConstants.english) {
+      return const date_format.EnglishDateLocale();
+    } else if (currentLanguageCode == LanguageCodeConstants.vietnamese) {
+      return const date_format.VietnameseDateLocale();
+    } else if (currentLanguageCode == LanguageCodeConstants.russian) {
+      return const date_format.RussianDateLocale();
+    } else if (currentLanguageCode == LanguageCodeConstants.arabic) {
+      return const date_format.ArabicDateLocale();
+    } else if (currentLanguageCode == LanguageCodeConstants.italian) {
+      return const date_format.ItalianDateLocale();
+    } else {
+      return const date_format.EnglishDateLocale();
+    }
   }
 }
