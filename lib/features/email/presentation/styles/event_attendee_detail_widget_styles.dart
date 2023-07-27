1ada7df999106d67ecc55c59fc33b3ecc3d3a566
dab246 <tdvu@linagora.com>
2023-07-28 02:01:50 +0700

    TF-2057 Add see all attendees button
    (cherry picked from commit 9ac78de2505c212d1c14db951a1528d00b91fe26)

--- lib/features/email/presentation/styles/event_attendee_detail_widget_styles.dart
+++ lib/features/email/presentation/styles/event_attendee_detail_widget_styles.dart
@@ -2,4 +2,5 @@
 class EventAttendeeDetailWidgetStyles {
   static const double maxWidth = 100;
   static const double textSize = 16;
+  static const double fieldTopPadding = 8;
 }
---
+++ lib/features/email/presentation/styles/see_all_attendees_button_widget_styles.dart
@@ -0,0 +1,8 @@
+
+class SeeAllAttendeesButtonWidgetStyles {
+  static const double textSize = 16;
+  static const double horizontalPadding = 8;
+  static const double verticalPadding = 4;
+  static const double borderRadius = 20;
+  static const double horizontalMargin = -8;
+}
