7b1abc5eefb20a5980043a5fb94e09e404d09373
dab246 <tdvu@linagora.com>
2023-07-27 16:32:58 +0700

    TF-2057 Add event location detail widget

---
+++ lib/features/email/presentation/widgets/calendar_event/event_location_detail_widget.dart
@@ -0,0 +1,43 @@
+
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:tmail_ui_user/features/email/presentation/styles/event_location_detail_widget_styles.dart';
+import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+
+class EventLocationDetailWidget extends StatelessWidget {
+
+  final String locationEvent;
+
+  const EventLocationDetailWidget({
+    super.key,
+    required this.locationEvent
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: EventLocationDetailWidgetStyles.maxWidth,
+          child: Text(
+            AppLocalizations.of(context).location,
+            style: const TextStyle(
+              fontSize: EventLocationDetailWidgetStyles.textSize,
+              fontWeight: FontWeight.w500,
+              color: AppColor.colorSubTitleEventActionText
+            ),
+          ),
+        ),
+        Expanded(child: Text(
+          locationEvent,
+          style: const TextStyle(
+            fontSize: EventLocationDetailWidgetStyles.textSize,
+            fontWeight: FontWeight.w500,
+            color: Colors.black
+          ),
+        ))
+      ],
+    );
+  }
+}
