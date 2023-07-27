1bbbcc0afc7e98e2607f37caa6f677b30a190896
dab246 <tdvu@linagora.com>
2023-07-27 09:30:56 +0700

    TF-2057 Add event title widget

---
+++ lib/features/email/presentation/widgets/calendar_event/event_title_widget.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:tmail_ui_user/features/email/presentation/styles/event_title_widget_styles.dart';
+
+class EventTitleWidget extends StatelessWidget {
+
+  final String title;
+
+  const EventTitleWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: const TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: EventTitleWidgetStyles.textSize,
+        color: Colors.black
+      )
+    );
+  }
+}
