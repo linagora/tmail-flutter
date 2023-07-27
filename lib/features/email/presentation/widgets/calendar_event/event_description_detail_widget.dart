03426ec638c7d46b3511e45a3429c28c93a3147f
dab246 <tdvu@linagora.com>
2023-07-27 18:41:08 +0700

    TF-2057 Add event description detail widget
    (cherry picked from commit 7f8693755c849e2cf3d4cbb37290da4e20050bc8)

---
+++ lib/features/email/presentation/widgets/calendar_event/event_description_detail_widget.dart
@@ -0,0 +1,47 @@
+
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:core/presentation/resources/image_paths.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/email/presentation/styles/event_description_detail_widget_styles.dart';
+
+class EventDescriptionDetailWidget extends StatelessWidget {
+
+  final String description;
+
+  const EventDescriptionDetailWidget({
+    super.key,
+    required this.description
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final imagePath = Get.find<ImagePaths>();
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(
+        color: AppColor.colorEventDescriptionBackground,
+        borderRadius: BorderRadius.all(Radius.circular(EventDescriptionDetailWidgetStyles.borderRadius)),
+      ),
+      padding: const EdgeInsetsDirectional.all(EventDescriptionDetailWidgetStyles.contentPadding),
+      child: Stack(
+        children: [
+          Text(
+            description,
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: EventDescriptionDetailWidgetStyles.textSize,
+              color: Colors.black
+            )
+          ),
+          PositionedDirectional(
+            top: 0,
+            end: 0,
+            child: SvgPicture.asset(imagePath.icFormatQuote)
+          )
+        ],
+      ),
+    );
+  }
+}
