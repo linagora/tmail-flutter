fe9f219f4f67f7041ca9d113a4821b08b099b525
dab246 <tdvu@linagora.com>
2023-08-01 19:08:55 +0700

    TF-1981 Update UI spam banner on web
    (cherry picked from commit d795fd8989ac09c2d4002ebc3a4bce0e1c619699)

--- lib/features/thread/presentation/widgets/spam_banner/spam_report_banner_widget.dart
+++ lib/features/thread/presentation/widgets/spam_banner/spam_report_banner_widget.dart
@@ -1,11 +1,10 @@
-import 'package:core/presentation/resources/image_paths.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/controller/spam_report_controller.dart';
 import 'package:tmail_ui_user/features/thread/presentation/styles/spam_banner/spam_report_banner_button_styles.dart';
 import 'package:tmail_ui_user/features/thread/presentation/styles/spam_banner/spam_report_banner_styles.dart';
 import 'package:tmail_ui_user/features/thread/presentation/widgets/spam_banner/spam_report_banner_button_widget.dart';
+import 'package:tmail_ui_user/features/thread/presentation/widgets/spam_banner/spam_report_banner_label_widget.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
 
 class SpamReportBannerWidget extends StatelessWidget {
@@ -14,8 +13,7 @@ class SpamReportBannerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     final spamReportController = Get.find<SpamReportController>();
-    final imagePaths = Get.find<ImagePaths>();
-    
+
     return Obx(() {
       if (!spamReportController.enableSpamReport || spamReportController.notShowSpamReportBanner) {
         return const SizedBox.shrink();
@@ -40,25 +38,7 @@ class SpamReportBannerWidget extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  imagePaths.icInfoCircleOutline,
-                  width: SpamReportBannerStyles.iconSize,
-                  height: SpamReportBannerStyles.iconSize
-                ),
-                const SizedBox(width: SpamReportBannerStyles.space),
-                Text(
-                  AppLocalizations.of(context).countNewSpamEmails(spamReportController.numberOfUnreadSpamEmails),
-                  style: const TextStyle(
-                    fontSize: SpamReportBannerStyles.labelTextSize,
-                    color: SpamReportBannerStyles.labelTextColor,
-                    fontWeight: FontWeight.w500
-                  ),
-                ),
-              ],
-            ),
+            SpamReportBannerLabelWidget(countSpamEmailsAsString: spamReportController.numberOfUnreadSpamEmails),
             const SizedBox(height: SpamReportBannerStyles.space),
             Row(
               children: [
