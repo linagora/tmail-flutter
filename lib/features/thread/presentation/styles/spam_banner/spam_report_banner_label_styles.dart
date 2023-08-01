e3841264c3249345690d8c6d4d56a2ef5b051750
dab246 <tdvu@linagora.com>
2023-08-01 18:01:57 +0700

    TF-1981 Add SpamReportBannerLabel widget

---
+++ lib/features/thread/presentation/styles/spam_banner/spam_report_banner_label_styles.dart
@@ -0,0 +1,10 @@
+
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+
+class SpamReportBannerLabelStyles {
+  static const double iconSize = 24;
+  static const double labelTextSize = 16;
+  static const double space = 8;
+  static const Color labelTextColor = AppColor.colorSpamReportBannerLabelColor;
+}
