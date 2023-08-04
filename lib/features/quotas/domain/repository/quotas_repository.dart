a2875ea9210cd4cbdf2910bf18eaa144c7c21446
dab246 <tdvu@linagora.com>
2023-08-04 21:43:33 +0700

    TF-2064 Handle limit storage to show warning banner
    (cherry picked from commit 3e36cebd5cbe880f9832c62463e71979e15e1741)

--- lib/features/quotas/domain/repository/quotas_repository.dart
+++ lib/features/quotas/domain/repository/quotas_repository.dart
@@ -1,6 +1,6 @@
 import 'package:jmap_dart_client/jmap/account_id.dart';
-import 'package:tmail_ui_user/features/quotas/domain/model/quotas_response.dart';
+import 'package:jmap_dart_client/jmap/quotas/quota.dart';
 
 abstract class QuotasRepository {
-  Future<QuotasResponse> getQuotas(AccountId accountId);
+  Future<List<Quota>> getQuotas(AccountId accountId);
 }
