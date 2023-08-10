23c21b5b62ff7e8076a667979d1f58dc0416f747
dab246 <tdvu@linagora.com>
2023-08-10 17:50:52 +0700

    TF-2078 Add abstract get list event action

--- lib/features/email/data/datasource/calendar_event_datasource.dart
+++ lib/features/email/data/datasource/calendar_event_datasource.dart
@@ -2,7 +2,10 @@
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:jmap_dart_client/jmap/mail/calendar/calendar_event.dart';
+import 'package:tmail_ui_user/features/email/domain/model/event_action.dart';
 
 abstract class CalendarEventDataSource {
   Future<List<CalendarEvent>> parse(AccountId accountId, Set<Id> blobIds);
+
+  Future<List<EventAction>> getListEventAction(String emailContents);
 }
