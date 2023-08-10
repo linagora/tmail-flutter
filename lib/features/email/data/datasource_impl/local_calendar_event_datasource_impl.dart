eedc05c0f2b3be2d6beca514e411131fac351986
dab246 <tdvu@linagora.com>
2023-08-10 18:12:58 +0700

    TF-2078 Implement get calendar event action in domain & data layer
    (cherry picked from commit 6c372d57cfebf31e776f071b514e71c637b44257)

---
+++ lib/features/email/data/datasource_impl/local_calendar_event_datasource_impl.dart
@@ -0,0 +1,36 @@
+
+import 'package:collection/collection.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
+import 'package:jmap_dart_client/jmap/mail/calendar/calendar_event.dart';
+import 'package:tmail_ui_user/features/email/data/datasource/calendar_event_datasource.dart';
+import 'package:tmail_ui_user/features/email/data/local/html_analyzer.dart';
+import 'package:tmail_ui_user/features/email/domain/model/event_action.dart';
+import 'package:tmail_ui_user/main/exceptions/exception_thrower.dart';
+
+class LocalCalendarEventDataSourceImpl extends CalendarEventDataSource {
+
+  final HtmlAnalyzer _htmlAnalyzer;
+  final ExceptionThrower _exceptionThrower;
+
+  LocalCalendarEventDataSourceImpl(this._htmlAnalyzer, this._exceptionThrower);
+
+  @override
+  Future<List<CalendarEvent>> parse(AccountId accountId, Set<Id> blobIds) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<EventAction>> getListEventAction(String emailContents) {
+    return Future.sync(() async {
+      final listLink = await _htmlAnalyzer.getListLinkCalendarEvent(emailContents);
+      if (listLink.length >= EventActionType.values.length) {
+        return EventActionType.values
+          .mapIndexed((index, type) => EventAction(type, listLink[index]))
+          .toList();
+      } else {
+        return <EventAction>[];
+      }
+    }).catchError(_exceptionThrower.throwException);
+  }
+}
--- lib/features/email/domain/state/parse_calendar_event_state.dart
+++ lib/features/email/domain/state/parse_calendar_event_state.dart
@@ -1,17 +1,19 @@
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
 import 'package:jmap_dart_client/jmap/mail/calendar/calendar_event.dart';
+import 'package:tmail_ui_user/features/email/domain/model/event_action.dart';
 
 class ParseCalendarEventLoading extends LoadingState {}
 
 class ParseCalendarEventSuccess extends UIState {
 
   final List<CalendarEvent> calendarEventList;
+  final List<EventAction> eventActionList;
 
-  ParseCalendarEventSuccess(this.calendarEventList);
+  ParseCalendarEventSuccess(this.calendarEventList, this.eventActionList);
 
   @override
-  List<Object> get props => [calendarEventList];
+  List<Object> get props => [calendarEventList, eventActionList];
 }
 
 class ParseCalendarEventFailure extends FeatureFailure {
