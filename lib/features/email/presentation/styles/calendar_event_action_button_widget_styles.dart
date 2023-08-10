dbd936a925e88939abe998ed61e38c838636a074
dab246 <tdvu@linagora.com>
2023-08-10 18:14:04 +0700

    TF-2078 Add CalendarEventActionButton widget

---
+++ lib/features/email/presentation/styles/calendar_event_action_button_widget_styles.dart
@@ -0,0 +1,21 @@
+
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+
+class CalendarEventActionButtonWidgetStyles {
+  static const double borderRadius = 10;
+  static const double textSize = 16;
+  static const double space = 16;
+  static const double borderWidth = 1;
+  static const double minWidth = 80;
+
+  static const Color backgroundColor = Colors.transparent;
+  static const Color textColor = AppColor.primaryColor;
+
+  static const FontWeight fontWeight = FontWeight.w500;
+
+  static const EdgeInsetsGeometry buttonPadding = EdgeInsetsDirectional.symmetric(vertical: 8, horizontal: 12);
+  static const EdgeInsetsGeometry paddingMobile = EdgeInsetsDirectional.only(top: 16);
+  static const EdgeInsetsGeometry paddingWeb = EdgeInsetsDirectional.only(start: 100, end: 16, top: 16);
+  static const EdgeInsetsGeometry margin = EdgeInsetsDirectional.only(top: 16);
+}
