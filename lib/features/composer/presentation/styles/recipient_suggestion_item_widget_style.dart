f84f2ce4fd12c3659197c9b26031c9fcfb87b3d7
dab246 <tdvu@linagora.com>
2023-09-06 11:46:38 +0700

    TF-2116 Create RecipientComposer widget and style
    (cherry picked from commit 24be032c8c04e9673953c9dc83d096e7b61fdb04)

---
+++ lib/features/composer/presentation/styles/recipient_suggestion_item_widget_style.dart
@@ -0,0 +1,22 @@
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+
+class RecipientSuggestionItemWidgetStyle {
+  static const double radius = 20;
+  static const double selectedIconSize = 24;
+
+  static const EdgeInsetsGeometry suggestionDuplicatedMargin = EdgeInsets.all(8.0);
+  static const EdgeInsetsGeometry labelDuplicatedPadding = EdgeInsets.symmetric(horizontal: 8.0);
+  static const EdgeInsetsGeometry labelPadding = EdgeInsets.symmetric(horizontal: 16.0);
+
+  static const TextStyle labelTextStyle = TextStyle(
+    color: AppColor.colorHintSearchBar,
+    fontSize: 13,
+    fontWeight: FontWeight.normal
+  );
+  static const TextStyle labelHighlightTextStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 13,
+    fontWeight: FontWeight.bold
+  );
+}
---
+++ lib/features/composer/presentation/styles/recipient_tag_item_widget_style.dart
@@ -0,0 +1,16 @@
+import 'package:core/utils/platform_info.dart';
+import 'package:flutter/material.dart';
+
+class RecipientTagItemWidgetStyle {
+  static const double radius = 10;
+
+  static const EdgeInsetsGeometry padding = EdgeInsetsDirectional.only(start: 4);
+  static const EdgeInsetsGeometry counterPadding = EdgeInsetsDirectional.symmetric(vertical: 5, horizontal: 8);
+  static const EdgeInsetsGeometry counterMargin = EdgeInsetsDirectional.only(top: PlatformInfo.isWeb ? 8 : 0);
+
+  static const TextStyle labelTextStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 17,
+    fontWeight: FontWeight.w400
+  );
+}
