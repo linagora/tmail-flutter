dcf52bdec95af2228d7bee6ee183c8bbf9ded0a8
dab246 <tdvu@linagora.com>
2023-09-06 11:47:04 +0700

    TF-2116 Create TitleComposer widget and style

---
+++ lib/features/composer/presentation/styles/title_composer_widget_style.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class TitleComposerWidgetStyle {
+  static const TextStyle textStyle = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w600,
+    color: Colors.black
+  );
+}
