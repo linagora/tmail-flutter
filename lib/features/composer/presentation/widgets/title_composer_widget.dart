761c2560ff71446fb9e1018bc8e8168368e121c3
dab246 <tdvu@linagora.com>
2023-09-06 11:47:04 +0700

    TF-2116 Create TitleComposer widget and style
    (cherry picked from commit e56b2c28728ddc44e19d8cdbe4865ea0ba5e3d9a)

---
+++ lib/features/composer/presentation/widgets/title_composer_widget.dart
@@ -0,0 +1,29 @@
+import 'package:core/presentation/extensions/capitalize_extension.dart';
+import 'package:core/presentation/utils/style_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:tmail_ui_user/features/composer/presentation/styles/title_composer_widget_style.dart';
+import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+
+class TitleComposerWidget extends StatelessWidget {
+
+  final String emailSubject;
+
+  const TitleComposerWidget({
+    super.key,
+    required this.emailSubject,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      emailSubject.isNotEmpty == true
+        ? emailSubject
+        : AppLocalizations.of(context).new_message.capitalizeFirstEach,
+      maxLines: 1,
+      textAlign: TextAlign.center,
+      overflow: CommonTextStyle.defaultTextOverFlow,
+      softWrap: CommonTextStyle.defaultSoftWrap,
+      style: TitleComposerWidgetStyle.textStyle,
+    );
+  }
+}
