5e00aea331e2e092771809d0955b6099d0473520
hieubt <trunghieu.bui201@gmail.com>
2023-09-06 18:55:56 +0700

    tf-2083 add swipe to remove condition on mobile
    (cherry picked from commit cbb5aeaef04be7efbb0d68ea2f258f70a4ab8a11)

--- lib/features/rules_filter_creator/presentation/widgets/rule_filter_condition_widget.dart
+++ lib/features/rules_filter_creator/presentation/widgets/rule_filter_condition_widget.dart
@@ -1,6 +1,7 @@
 import 'package:core/presentation/extensions/color_extension.dart';
 import 'package:core/presentation/utils/keyboard_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tmail_ui_user/features/base/widget/drop_down_button_widget.dart';
 import 'package:tmail_ui_user/features/rules_filter_creator/presentation/model/rule_filter_condition_type.dart';
@@ -39,25 +40,63 @@ class RuleFilterConditionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: AppColor.colorBackgroundFieldConditionRulesFilter,
-        borderRadius: BorderRadius.circular(12),
+    return Slidable(
+      enabled: ruleFilterConditionScreenType == RuleFilterConditionScreenType.mobile ? true : false,
+      endActionPane: ActionPane(
+      extentRatio: 0.1,
+        motion: const BehindMotion(),
+        children: [
+          CustomSlidableAction(
+            padding: const EdgeInsets.only(right: 12),
+            borderRadius: const BorderRadius.only(topRight: Radius.circular(12), bottomRight: Radius.circular(12)),
+            onPressed: (_) => tapRemoveRuleFilterConditionCallback!(),
+            backgroundColor: AppColor.colorBackgroundFieldConditionRulesFilter,
+            child: CircleAvatar(
+              backgroundColor: AppColor.colorRemoveRuleFilterConditionButton,
+              radius: 110,
+              child: SvgPicture.asset(
+                imagePaths!.icMinimize,
+                fit: BoxFit.fill,
+                colorFilter: AppColor.colorDeletePermanentlyButton.asFilter(),
+              ),
+            )
+          )
+        ]
       ),
-      child: _buildRuleFilterCondition(
-        context,
-        ruleFilterConditionScreenType,
-        ruleCondition,
-        tapRuleConditionFieldCallback,
-        tapRuleConditionComparatorCallback,
-        conditionValueErrorText,
-        conditionValueEditingController,
-        conditionValueFocusNode,
-        conditionValueOnChangeAction,
-        tapRemoveRuleFilterConditionCallback,
-        imagePaths,
-      )
+      child: Builder(builder: (context) {
+        SlidableController? slideController = Slidable.of(context);
+        return ValueListenableBuilder<int>(
+          valueListenable: slideController?.direction ?? ValueNotifier<int>(0),
+          builder: (context, value, _) {
+            var borderRadius = value != -1 ? 
+              BorderRadius.circular(12) : 
+              const BorderRadius.only(
+                bottomLeft: Radius.circular(12),
+                topLeft: Radius.circular(12)
+              );
+            return Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: AppColor.colorBackgroundFieldConditionRulesFilter,
+                borderRadius: borderRadius,
+              ),
+              child: _buildRuleFilterCondition(
+                context,
+                ruleFilterConditionScreenType,
+                ruleCondition,
+                tapRuleConditionFieldCallback,
+                tapRuleConditionComparatorCallback,
+                conditionValueErrorText,
+                conditionValueEditingController,
+                conditionValueFocusNode,
+                conditionValueOnChangeAction,
+                tapRemoveRuleFilterConditionCallback,
+                imagePaths,
+              )
+            );
+          }
+        );
+      })
     );
     
   }
@@ -144,7 +183,11 @@ Widget _buildRuleFilterCondition(
                 editingController: conditionValueEditingController,
                 )
             ),
-            _buildRemoveRuleFilterConditionButton(tapRemoveRuleFilterConditionCallback, imagePaths),
+            Container(
+              padding: const EdgeInsets.only(left: 12),
+              alignment: Alignment.center,
+              child: _buildRemoveRuleFilterConditionButton(tapRemoveRuleFilterConditionCallback, imagePaths)
+            ),
           ],
         );
       default:
@@ -156,20 +199,16 @@ Widget _buildRemoveRuleFilterConditionButton (
   Function()? tapRemoveRuleFilterConditionCallback,
   ImagePaths? imagePath,
 ) {
-  return Container(
-    padding: const EdgeInsets.only(left: 12),
-    alignment: Alignment.center,
-    child: InkWell(
-      onTap: tapRemoveRuleFilterConditionCallback,
-      child: CircleAvatar(
-        backgroundColor: AppColor.colorRemoveRuleFilterConditionButton,
-        radius: 22,
-        child: SvgPicture.asset(
-          imagePath!.icMinimize,
-          fit: BoxFit.fill,
-          colorFilter: AppColor.colorDeletePermanentlyButton.asFilter(),
-        ),
-      )
-    ),
+  return InkWell(
+    onTap: tapRemoveRuleFilterConditionCallback,
+    child: CircleAvatar(
+      backgroundColor: AppColor.colorRemoveRuleFilterConditionButton,
+      radius: 22,
+      child: SvgPicture.asset(
+        imagePath!.icMinimize,
+        fit: BoxFit.fill,
+        colorFilter: AppColor.colorDeletePermanentlyButton.asFilter(),
+      ),
+    )
   );
 }
