bceedfd51a0e19509930650201180d275c4befae
dab246 <tdvu@linagora.com>
2023-09-06 11:52:49 +0700

    TF-2116 Create ContainerViewComposer for all platform on web

---
+++ lib/features/composer/presentation/styles/desktop_container_view_style.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class DesktopContainerViewStyle {
+  static const double radius = 28;
+  static const double margin = 20;
+  static const double elevation = 16;
+
+  static const Color backgroundColor = Colors.white;
+  static const Color outSideBackgroundColor = Colors.black38;
+}
---
+++ lib/features/composer/presentation/styles/mobile_container_view_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MobileContainerViewStyle {
+  static const double radius = 28;
+  static const double elevation = 16;
+
+  static const Color outSideBackgroundColor = Colors.white;
+}
