2e2a75941550e1eddb62787a4511d830459db162
dab246 <dab246@users.noreply.github.com>
2023-09-14 17:58:49 +0000

    deploy: bc43cb77bc2ee99807142f0218449db90cc65434

--- 2142/main.dart.js_1.part.js
+++ 2142/main.dart.js_1.part.js
@@ -157,12 +157,13 @@ $__dart_deferred_initializers__.current = function(hunkHelpers, init, holdersLis
       this.$this = t0;
     }
   },
-  A, C, D, E, J;
+  A, C, D, E, F, J;
   B = hunkHelpers.updateHolder(holdersList[3], B);
   A = holdersList[0];
   C = holdersList[2];
   D = holdersList[15];
   E = holdersList[16];
+  F = holdersList[17];
   J = holdersList[1];
   B._AutofillScopeTextInputConfiguration.prototype = {
     toJson$0() {
@@ -333,14 +334,14 @@ $__dart_deferred_initializers__.current = function(hunkHelpers, init, holdersLis
       A.Localizations_of(context, C.Type_AppLocalizations_swi, t1).toString;
       t2 = A.Text$(A.Intl__message("JMAP-based\ncollaborative team mail solution", _null, "jmapBasedMailSolution", _null, _null), _null, _null, _null, _null, _null, _null, _null, _null, D.TextStyle_oHY20, _null, _null, _null, _null, _null);
       A.Localizations_of(context, C.Type_AppLocalizations_swi, t1).toString;
-      t3 = A.SloganBuilder$(true, _null, _null, "assets/images/ic_jmap_standard.svg", _null, _null, C.EdgeInsets_12_0_0_0, 48, A.Intl__message("JMAP standard", _null, "jmapStandard", _null, _null), _null, D.TextStyle_oHY21);
+      t3 = A.SloganBuilder$(true, _null, _null, "assets/images/ic_jmap_standard.svg", _null, _null, E.EdgeInsets_12_0_0_0, 48, A.Intl__message("JMAP standard", _null, "jmapStandard", _null, _null), _null, D.TextStyle_oHY21);
       A.Localizations_of(context, C.Type_AppLocalizations_swi, t1).toString;
-      t4 = A.SloganBuilder$(true, _null, _null, "assets/images/ic_encrypted.svg", _null, _null, C.EdgeInsets_12_0_0_0, 48, A.Intl__message("Encrypted mailbox", _null, "encryptedMailbox", _null, _null), _null, D.TextStyle_oHY21);
+      t4 = A.SloganBuilder$(true, _null, _null, "assets/images/ic_encrypted.svg", _null, _null, E.EdgeInsets_12_0_0_0, 48, A.Intl__message("Encrypted mailbox", _null, "encryptedMailbox", _null, _null), _null, D.TextStyle_oHY21);
       A.Localizations_of(context, C.Type_AppLocalizations_swi, t1).toString;
-      t5 = A.SloganBuilder$(true, _null, _null, "assets/images/ic_team.svg", _null, _null, C.EdgeInsets_12_0_0_0, 48, A.Intl__message("Manage email as a team", _null, "manageEmailAsATeam", _null, _null), _null, D.TextStyle_oHY21);
+      t5 = A.SloganBuilder$(true, _null, _null, "assets/images/ic_team.svg", _null, _null, E.EdgeInsets_12_0_0_0, 48, A.Intl__message("Manage email as a team", _null, "manageEmailAsATeam", _null, _null), _null, D.TextStyle_oHY21);
       A.Localizations_of(context, C.Type_AppLocalizations_swi, t1).toString;
       t6 = type$.JSArray_Widget;
-      t5 = A.Column$(A._setArrayType([t2, new A.Padding(C.EdgeInsets_0_24_0_0, t3, _null), new A.Padding(C.EdgeInsets_0_16_0_0, t4, _null), new A.Padding(C.EdgeInsets_0_16_0_0, t5, _null), new A.Padding(C.EdgeInsets_0_16_0_0, A.SloganBuilder$(true, _null, _null, "assets/images/ic_integration.svg", _null, _null, C.EdgeInsets_12_0_0_0, 48, A.Intl__message("Multiple integrations", _null, "multipleIntegrations", _null, _null), _null, D.TextStyle_oHY21), _null), new A.Padding(D.EdgeInsets_0_44_0_0, A.SvgPicture$asset("assets/images/ic_login_graphic.svg", C.Alignment_0_0, _null, C.BoxFit_0, _null, _null, _null), _null)], t6), C.CrossAxisAlignment_0, C.MainAxisAlignment_2, C.MainAxisSize_1, C.VerticalDirection_1);
+      t5 = A.Column$(A._setArrayType([t2, new A.Padding(C.EdgeInsets_0_24_0_0, t3, _null), new A.Padding(C.EdgeInsets_0_16_0_0, t4, _null), new A.Padding(C.EdgeInsets_0_16_0_0, t5, _null), new A.Padding(C.EdgeInsets_0_16_0_0, A.SloganBuilder$(true, _null, _null, "assets/images/ic_integration.svg", _null, _null, E.EdgeInsets_12_0_0_0, 48, A.Intl__message("Multiple integrations", _null, "multipleIntegrations", _null, _null), _null, D.TextStyle_oHY21), _null), new A.Padding(D.EdgeInsets_0_44_0_0, A.SvgPicture$asset("assets/images/ic_login_graphic.svg", C.Alignment_0_0, _null, C.BoxFit_0, _null, _null, _null), _null)], t6), C.CrossAxisAlignment_0, C.MainAxisAlignment_2, C.MainAxisSize_1, C.VerticalDirection_1);
       t4 = A.BorderRadius$circular(20);
       t3 = _this._buildAppLogo$1(context);
       A.Localizations_of(context, C.Type_AppLocalizations_swi, t1).toString;
@@ -371,7 +372,7 @@ $__dart_deferred_initializers__.current = function(hunkHelpers, init, holdersLis
       t1 = A.Intl__message("Privacy policy", _null, "privacyPolicy", _null, _null);
       t2 = A.TapGestureRecognizer$(_null, _null);
       t2.onTap = new B.PrivacyLinkWidget_build_closure(this);
-      return A.RichText$(_null, _null, _null, C.TextOverflow_0, _null, _null, true, _null, A.TextSpan$(_null, _null, _null, _null, _null, t2, _null, _null, E.TextStyle_kmC, t1), C.TextAlign_4, _null, _null, 1, C.TextWidthBasis_0);
+      return A.RichText$(_null, _null, _null, C.TextOverflow_0, _null, _null, true, _null, A.TextSpan$(_null, _null, _null, _null, _null, t2, _null, _null, F.TextStyle_kmC, t1), C.TextAlign_4, _null, _null, 1, C.TextWidthBasis_0);
     }
   };
   B.LoginInputDecorationBuilder.prototype = {
@@ -815,4 +816,4 @@ $__dart_deferred_initializers__.current = function(hunkHelpers, init, holdersLis
   })();
 };
 
-$__dart_deferred_initializers__["dz4CcoXpHPJFPvzjvS4k79HAjvE="] = $__dart_deferred_initializers__.current
+$__dart_deferred_initializers__["MBQ8HhJ9hbcMorNmeOMLjA6VsEw="] = $__dart_deferred_initializers__.current
