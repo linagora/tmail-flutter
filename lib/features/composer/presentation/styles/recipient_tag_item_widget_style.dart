323ae9d7c11a82c68b380dcfa99a30116b45f078
dab246 <tdvu@linagora.com>
2023-09-14 12:43:05 +0700

    TF-2119 Implement drag and drop between recipients
    (cherry picked from commit 958940c8a79d4f33eb52fd69718ce1b5c8daaeaa)

--- lib/features/composer/presentation/styles/recipient_tag_item_widget_style.dart
+++ lib/features/composer/presentation/styles/recipient_tag_item_widget_style.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 class RecipientTagItemWidgetStyle {
   static const double radius = 10;
+  static const double avatarIconSize = 24;
+  static const double avatarLabelFontSize = 12;
 
   static const EdgeInsetsGeometry padding = EdgeInsetsDirectional.only(start: 4);
   static const EdgeInsetsGeometry counterPadding = EdgeInsetsDirectional.symmetric(vertical: 5, horizontal: 8);
