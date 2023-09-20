87f00bb9ee8bdbb79ed584795836d3264e74f710
hieubt <trunghieu.bui201@gmail.com>
2023-09-20 12:34:12 +0700

    TF-2085 Add RuleConditionGroup

--- rule_filter/lib/rule_filter/tmail_rule.dart
+++ rule_filter/lib/rule_filter/tmail_rule.dart
@@ -4,6 +4,7 @@ import 'package:rule_filter/rule_filter/converter/rule_id_nullable_converter.dar
 import 'package:rule_filter/rule_filter/rule.dart';
 import 'package:rule_filter/rule_filter/rule_action.dart';
 import 'package:rule_filter/rule_filter/rule_condition.dart';
+import 'package:rule_filter/rule_filter/rule_condition_group.dart';
 import 'package:rule_filter/rule_filter/rule_id.dart';
 
 part 'tmail_rule.g.dart';
@@ -14,26 +15,39 @@ part 'tmail_rule.g.dart';
 class TMailRule extends Rule {
   final RuleId? id;
   final String name;
-  final RuleCondition condition;
+  final RuleCondition? condition;
+  final RuleConditionGroup? conditionGroup;
   final RuleAction action;
 
   TMailRule({
     this.id,
     required this.name,
-    required this.condition,
+    this.condition,
+    this.conditionGroup,
     required this.action,
   });
 
   factory TMailRule.fromJson(Map<String, dynamic> json) =>
       _$TMailRuleFromJson(json);
 
-  Map<String, dynamic> toJson() => _$TMailRuleToJson(this);
+  Map<String, dynamic> toJson() {
+    final result = _$TMailRuleToJson(this);
+
+    if (result['condition'] != null) {
+      result.remove('conditionGroup');
+    } else {
+      result.remove('condition');
+    }
+
+    return result;
+  }
 
   @override
   List<Object?> get props => [
         name,
         condition,
         action,
+        conditionGroup,
       ];
 
   TMailRule copyWith({
@@ -41,12 +55,14 @@ class TMailRule extends Rule {
     String? name,
     RuleCondition? condition,
     RuleAction? action,
+    RuleConditionGroup? conditionGroup,
   }) {
     return TMailRule(
       id: id ?? this.id,
       name: name ?? this.name,
       condition: condition ?? this.condition,
       action: action ?? this.action,
+      conditionGroup: conditionGroup ?? this.conditionGroup,
     );
   }
 }
