b5baf76292d8eaa9c7078f3fefaede925c979f82
hieubt <trunghieu.bui201@gmail.com>
2023-09-24 22:02:22 +0700

    TF-2084 update EmailRuleFilterAction

--- lib/features/rules_filter_creator/presentation/model/email_rule_filter_action.dart
+++ lib/features/rules_filter_creator/presentation/model/email_rule_filter_action.dart
@@ -4,12 +4,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
 
 enum EmailRuleFilterAction {
-  moveMessage;
+  moveMessage,
+  maskAsSeen,
+  startIt,
+  rejectIt,
+  markAsSpam,
+  forwardTo;
 
   String getTitle(BuildContext context) {
     switch(this) {
       case EmailRuleFilterAction.moveMessage:
         return AppLocalizations.of(context).moveMessage;
+      case EmailRuleFilterAction.maskAsSeen:
+        return AppLocalizations.of(context).maskAsSeen;
+      case EmailRuleFilterAction.startIt:
+        return AppLocalizations.of(context).startIt;
+      case EmailRuleFilterAction.rejectIt:
+        return AppLocalizations.of(context).rejectIt;
+      case EmailRuleFilterAction.markAsSpam:
+        return AppLocalizations.of(context).markAsSpam;
+      case EmailRuleFilterAction.forwardTo:
+        return AppLocalizations.of(context).forwardTo;
     }
   }
 }
