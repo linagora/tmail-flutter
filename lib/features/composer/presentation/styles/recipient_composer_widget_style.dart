1b3690f569264bf60e920a1feebecf6d1e9caf73
dab246 <tdvu@linagora.com>
2023-09-29 15:36:36 +0700

    TF-2180 Fix suggestion list being hidden once user use device with small screen

--- lib/features/composer/presentation/styles/recipient_composer_widget_style.dart
+++ lib/features/composer/presentation/styles/recipient_composer_widget_style.dart
@@ -6,10 +6,19 @@ class RecipientComposerWidgetStyle {
   static const double deleteRecipientFieldIconSize = 20;
   static const double space = 8;
   static const double enableBorderRadius = 10;
+  static const double suggestionsBoxElevation = 20.0;
+  static const double suggestionsBoxRadius = 20;
+  static const double suggestionsBoxMaxHeight = 350;
+  static const double suggestionBoxWidth = 300;
+  static const double minTextFieldWidth = 20;
+  static const double tagSpacing = 8;
+
+  static const Duration suggestionDebounceDuration = Duration(milliseconds: 150);
 
   static const Color borderColor = AppColor.colorLineComposer;
   static const Color deleteRecipientFieldIconColor = AppColor.colorCollapseMailbox;
   static const Color enableBorderColor = AppColor.primaryColor;
+  static const Color suggestionsBoxBackgroundColor = Colors.white;
 
   static const EdgeInsetsGeometry deleteRecipientFieldIconPadding = EdgeInsetsDirectional.all(3);
   static const EdgeInsetsGeometry prefixButtonPadding = EdgeInsetsDirectional.symmetric(vertical: 3, horizontal: 5);
