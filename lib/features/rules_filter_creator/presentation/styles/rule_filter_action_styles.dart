ca631d54fc624586bba6277b0415f1c5b4a99ec6
hieubt <trunghieu.bui201@gmail.com>
2023-10-02 17:25:45 +0700

    TF-2084 Add styles for rule filter action
    (cherry picked from commit 9635cfb426a84cd50398ae7e21f04b3bacb83530)

---
+++ lib/features/rules_filter_creator/presentation/styles/rule_filter_action_styles.dart
@@ -0,0 +1,23 @@
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+
+class RuleFilterActionStyles {
+  static const double extentRatio = 0.1;
+  static const double removeButtonRadius = 110.0;
+  static const double mainPadding = 12.0;
+  static const double mainBorderRadius = 12.0;
+  static const int maxLines = 1;
+  static const double fontSize = 16.0;
+  static const Color color = Colors.black;
+  static const TextStyle textStyle = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: fontSize,
+    color: color,
+  );
+  static const double itemDistance = 12.0;
+  static const TextStyle addActionButtonTextStyle = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 17.0,
+    color: AppColor.primaryColor,
+  );
+}
