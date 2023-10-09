8b86ed28f3c1a0a899f6ed5ace7a793e83e34e4b
hieubt <trunghieu.bui201@gmail.com>
2023-10-09 23:44:51 +0700

    TF-1017 Add scrollToTopButtonWidget

---
+++ lib/features/thread/presentation/styles/scroll_to_top_button_widget_styles.dart
@@ -0,0 +1,12 @@
+import 'package:core/core.dart';
+import 'package:flutter/material.dart';
+
+class ScrollToTopButtonWidgetStyles {
+  static const double defaultLimitIndicator = 50.0;
+  static const double defaultElevation = 5.0;
+  static const Color defaultColor = AppColor.colorLabelCancelButton;
+  static const double defaultButtonRadius = 44.0;
+  static const double buttonPadding = 16.0;
+  static const double iconWidth = 28.0;
+  static const double iconHeight = 28.0;
+}
