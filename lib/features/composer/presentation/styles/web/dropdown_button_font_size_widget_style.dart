1aee8b4f7dbde30c7fbf2e9aa722632c9df9cd21
dab246 <tdvu@linagora.com>
2023-10-13 19:35:51 +0700

    TF-1301 Add font size dropdown menu button
    Signed-off-by: dab246 <tdvu@linagora.com>

---
+++ lib/features/composer/presentation/styles/web/dropdown_button_font_size_widget_style.dart
@@ -0,0 +1,22 @@
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+
+class DropdownButtonFontSizeWidgetStyle {
+  static const double borderWidth = 0.5;
+  static const double radius = 8;
+  static const double height = 36;
+  static const double labelRadius = 4;
+  static const double space = 4;
+
+  static const Color borderColor = AppColor.dropdownButtonBorderColor;
+  static const Color labelBackgroundColor = AppColor.dropdownLabelButtonBackgroundColor;
+
+  static const EdgeInsetsGeometry padding = EdgeInsets.all(4);
+  static const EdgeInsetsGeometry labelPadding = EdgeInsets.symmetric(horizontal: 16);
+
+  static const TextStyle labelTextStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppColor.colorLabelRichText
+  );
+}
