0de80dc9374554e57f1cda2184e257459c4d6e17
hieubt <trunghieu.bui201@gmail.com>
2023-10-17 13:39:43 +0700

    TF-1801 Change design of email attachment

--- lib/features/email/presentation/styles/email_attachments_styles.dart
+++ lib/features/email/presentation/styles/email_attachments_styles.dart
@@ -8,16 +8,21 @@ class EmailAttachmentsStyles {
   static const double headerSpace = 4;
   static const double marginHeader = 6;
   static const double buttonTextSize = 13;
+  static const double buttonMoreAttachmentsTextSize = 14;
   static const double buttonBorderRadius = 8;
   static const double listSpace = 12;
-  static const double listHeight = 60;
+  static const double listHeight = 36;
+  static const double mobileListHeight = 100;
+  static const double moreAttachmentsButtonPadding = 8;
 
   static const Color headerTextColor = AppColor.colorTitleHeaderAttachment;
   static const Color headerIconColor = AppColor.colorAttachmentIcon;
   static const Color buttonTextColor = AppColor.primaryColor;
+  static const Color ButtonMoreAttachmentsTextColor = AppColor.colorLabelMoreAttachmentsButton;
 
   static const FontWeight headerFontWeight = FontWeight.w400;
   static const FontWeight buttonFontWeight = FontWeight.w400;
+  static const FontWeight buttonMoreAttachmentsFontWeight = FontWeight.w500;
 
   static const EdgeInsetsGeometry padding = EdgeInsetsDirectional.symmetric(vertical: 12, horizontal: 16);
   static const EdgeInsetsGeometry buttonPadding = EdgeInsets.symmetric(vertical: 8, horizontal: 12);
