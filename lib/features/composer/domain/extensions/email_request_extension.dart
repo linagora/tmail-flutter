4a7dccf86c6c12dcee8a3e2cb1d007a56bffeabd
dab246 <tdvu@linagora.com>
2023-10-20 09:57:48 +0700

    TF-2189 Update sending state in Sending Queue when send email failure on iOS
    Signed-off-by: dab246 <tdvu@linagora.com>
    (cherry picked from commit 295b19620669f59df872d801a5b5e1a79d5ca520)

--- lib/features/composer/domain/extensions/email_request_extension.dart
+++ lib/features/composer/domain/extensions/email_request_extension.dart
@@ -1,10 +1,17 @@
 
 import 'package:tmail_ui_user/features/composer/domain/model/email_request.dart';
 import 'package:tmail_ui_user/features/mailbox/domain/model/create_new_mailbox_request.dart';
+import 'package:tmail_ui_user/features/offline_mode/model/sending_state.dart';
 import 'package:tmail_ui_user/features/sending_queue/domain/model/sending_email.dart';
 
 extension EmailRequestExtension on EmailRequest {
-  SendingEmail toSendingEmail(String sendingId, {CreateNewMailboxRequest? mailboxRequest}) {
+  SendingEmail toSendingEmail(
+    String sendingId,
+    {
+      CreateNewMailboxRequest? mailboxRequest,
+      SendingState newState = SendingState.waiting
+    }
+  ) {
     return SendingEmail(
       sendingId: sendingId,
       email: email,
@@ -15,7 +22,8 @@ extension EmailRequestExtension on EmailRequest {
       emailIdAnsweredOrForwarded: emailIdAnsweredOrForwarded,
       identityId: identityId,
       mailboxNameRequest: mailboxRequest?.newName,
-      creationIdRequest: mailboxRequest?.creationId
+      creationIdRequest: mailboxRequest?.creationId,
+      sendingState: newState
     );
   }
 }
