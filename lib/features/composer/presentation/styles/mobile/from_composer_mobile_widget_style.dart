35aab71aa5b7a9377bff0be449a12122c8369a44
hieubt <trunghieu.bui201@gmail.com>
2023-11-03 16:06:30 +0700

    TF-2196 Add 'From; field fow mobile
    (cherry picked from commit 2e754c1331ce4833b20a21e52f736ccd8fcbb207)

---
+++ lib/features/composer/presentation/styles/mobile/from_composer_mobile_widget_style.dart
@@ -0,0 +1,44 @@
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+
+class FromComposerMobileWidgetStyle {
+  static const double space = 8.0;
+  static const double identityButtonHeight = 32.0;
+  static const double identityButtonWidthMobileLandscape = 421.0;
+
+  static const EdgeInsetsGeometry identityButtonInkWellPadding = EdgeInsets.symmetric(vertical: 8.0);
+  static const EdgeInsetsGeometry identityButtonPadding = EdgeInsets.only(
+    top: 4,
+    bottom: 4,
+    right: 4,
+    left: 8,
+  );
+
+  static const BorderRadius identityButtonInkWellBorderRadius = BorderRadius.all(Radius.circular(10));
+  static const BoxDecoration identityButtonDecoration = BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    color: AppColor.colorComposerAppBar,
+  );
+  static const Border border = Border(
+    bottom: BorderSide(
+      color: AppColor.colorLineComposer,
+      width: 1,
+    )
+  );
+
+  static const TextStyle prefixTextStyle = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColor.colorLabelComposer
+  );
+  static const TextStyle buttonTitleTextStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppColor.colorCalendarEventUnread
+  );
+  static const TextStyle buttonSubTitleTextStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppColor.colorLabelComposer
+  );
+}
