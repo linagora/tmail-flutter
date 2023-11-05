603acaa35acdb973bf904baf19309162c64c662b
david.potoma <davidpotoma1@gmail.com>
2023-11-05 12:49:30 +0100

    Adding functionality for mails to be unread when moved to spam.
    Rebasing
    
    (cherry picked from commit 4a907520bc0b53ff42cb7d6cde40771a649629f5)

--- model/lib/extensions/mailbox_id_extension.dart
+++ model/lib/extensions/mailbox_id_extension.dart
@@ -1,6 +1,8 @@
 import 'package:jmap_dart_client/jmap/core/patch_object.dart';
 import 'package:jmap_dart_client/jmap/core/reference_id.dart';
+import 'package:jmap_dart_client/jmap/mail/email/keyword_identifier.dart';
 import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
+import 'package:model/extensions/keyword_identifier_extension.dart';
 
 extension MailboxIdExtension on MailboxId {
   String generatePath() {
@@ -21,6 +23,14 @@ extension MailboxIdExtension on MailboxId {
   PatchObject generateActionPath() {
     return PatchObject({
       generatePath():  true,
+    });
+  }
+
+  PatchObject generateMoveToSpamActionPath(MailboxId currentMailboxId, MailboxId spamMailboxId) {
+    return PatchObject({
+      currentMailboxId.generatePath():  null,
+      spamMailboxId.generatePath(): true,
+      KeyWordIdentifier.emailSeen.generatePath(): true
     });
   }
 
