7ee35b7349d08aea20445095de383dbd3ed20023
dab246 <tdvu@linagora.com>
2023-11-06 12:32:20 +0700

    TF-2269 Show the report once a day
    Signed-off-by: dab246 <tdvu@linagora.com>
    
    Signed-off-by: dab246 <tdvu@linagora.com>
    
    Signed-off-by: dab246 <tdvu@linagora.com>
    (cherry picked from commit 74bed6e6dfbd32d70baeb131546b490199bb8fbb)

--- lib/features/mailbox_dashboard/domain/utils/mailbox_dashboard_constant.dart
+++
@@ -1,6 +0,0 @@
-
-class MailboxDashboardConstant {
-  static const String keyLastTimeDismissedSpamReported = 'KEY_LAST_TIME_DISMISSED_SPAM_REPORTED';
-  static const String keySpamReportState = 'KEY_SPAM_REPORT_STATE';
-  static const int spamReportBannerDisplayTimeOut = 4;
-}
