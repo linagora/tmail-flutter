71799cf7db9b84770eebd7308a9a95625f486014
hieubt <trunghieu.bui201@gmail.com>
2023-11-07 17:29:33 +0700

    TF-1155 Add text field suggestion for web
    (cherry picked from commit df222e4a0bb60a304b39c870f6099a69f0053cf3)

---
+++ lib/features/mailbox_dashboard/presentation/styles/text_field_autocomplete_email_address_web_style.dart
@@ -0,0 +1,46 @@
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+
+class TextFieldAutoCompleteEmailAddressWebStyles {
+  static const EdgeInsetsGeometry padding = EdgeInsets.symmetric(vertical: 8);
+  static const EdgeInsetsGeometry textInputContentPadding = EdgeInsets.symmetric(vertical: 16);
+  static const EdgeInsets tagEditorPadding = EdgeInsets.symmetric(horizontal: 8);
+
+  static const double borderRadius = 10.0;
+  static const double suggestionBoxRadius = 20.0;
+
+  static const double borderWidth = 2.0;
+  static const double minTextFieldWidth = 20.0;
+
+  static const double suggestionBoxElevation = 20.0;
+  static const double suggestionBoxMaxHeight = 350.0;
+
+  static const double fieldTitleWidth = 112.0;
+  static const double space = 8.0;
+
+  static const InputBorder textInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderSide: BorderSide.none,
+  );
+
+  static const Color textInputFillColor = Colors.white;
+  static const Color focusedBorderColor = AppColor.primaryColor;
+  static const Color enableBorderColor = AppColor.colorInputBorderCreateMailbox;
+  static const Color suggestionBoxBackgroundColor = Colors.white;
+
+  static const TextStyle fieldTitleTextStyle = TextStyle(
+    color: AppColor.colorContentEmail,
+    fontSize: 14,
+  );
+  static const TextStyle textInputHintStyle = TextStyle(
+    fontSize: 16,
+    color: AppColor.colorHintSearchBar,
+  );
+  static const TextStyle textInputTextStyle = TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const Duration debounceDuration = Duration(milliseconds: 150);
+}
