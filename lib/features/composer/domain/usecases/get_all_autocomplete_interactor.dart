c90e7b2508c5ee5f1e04f2d5432624ced79b0f46
dab246 <tdvu@linagora.com>
2023-11-07 11:25:16 +0700

    TF-2302 Rename `GetAutocompleteWithDeviceContactInteractor` to `GetAllAutocompleteInteractor`
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/composer/domain/usecases/get_all_autocomplete_interactor.dart
+++ lib/features/composer/domain/usecases/get_all_autocomplete_interactor.dart
@@ -1,20 +1,23 @@
 
-import 'package:core/core.dart';
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
 import 'package:dartz/dartz.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email_address.dart';
-import 'package:model/model.dart';
+import 'package:model/autocomplete/auto_complete_pattern.dart';
+import 'package:model/contact/contact.dart';
 import 'package:tmail_ui_user/features/composer/domain/state/get_autocomplete_state.dart';
 import 'package:tmail_ui_user/features/composer/domain/state/get_device_contact_suggestions_state.dart';
 import 'package:tmail_ui_user/features/composer/domain/usecases/get_autocomplete_interactor.dart';
 import 'package:tmail_ui_user/features/composer/domain/usecases/get_device_contact_suggestions_interactor.dart';
 
-class GetAutoCompleteWithDeviceContactInteractor {
+class GetAllAutoCompleteInteractor {
   final GetAutoCompleteInteractor _getAutoCompleteInteractor;
   final GetDeviceContactSuggestionsInteractor _getDeviceContactSuggestionsInteractor;
 
-  GetAutoCompleteWithDeviceContactInteractor(
-      this._getAutoCompleteInteractor,
-      this._getDeviceContactSuggestionsInteractor);
+  GetAllAutoCompleteInteractor(
+    this._getAutoCompleteInteractor,
+    this._getDeviceContactSuggestionsInteractor
+  );
 
   Future<Either<Failure, Success>> execute(AutoCompletePattern autoCompletePattern) async {
     try {
