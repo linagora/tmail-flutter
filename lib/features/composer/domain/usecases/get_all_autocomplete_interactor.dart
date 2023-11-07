ff1f28789c620c73cc2cfd8c5ed538de135f4752
dab246 <tdvu@linagora.com>
2023-11-07 12:10:23 +0700

    TF-2302 Get device contact when missing `autocomplete` capability
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/composer/domain/usecases/get_all_autocomplete_interactor.dart
+++ lib/features/composer/domain/usecases/get_all_autocomplete_interactor.dart
@@ -1,10 +1,10 @@
 
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
+import 'package:core/utils/app_logger.dart';
 import 'package:dartz/dartz.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email_address.dart';
 import 'package:model/autocomplete/auto_complete_pattern.dart';
-import 'package:model/contact/contact.dart';
 import 'package:tmail_ui_user/features/composer/domain/state/get_autocomplete_state.dart';
 import 'package:tmail_ui_user/features/composer/domain/state/get_device_contact_suggestions_state.dart';
 import 'package:tmail_ui_user/features/composer/domain/usecases/get_autocomplete_interactor.dart';
@@ -26,16 +26,25 @@ class GetAllAutoCompleteInteractor {
         _getDeviceContactSuggestionsInteractor.execute(autoCompletePattern)
       ]);
 
-      final autoCompleteResults = resultExecutions.first.fold(
-        (failure) => <EmailAddress>[],
-        (success) => success is GetAutoCompleteSuccess ? success.listEmailAddress : <EmailAddress>[]);
+      final autoCompleteResults = List<EmailAddress>.empty(growable: true);
 
-      resultExecutions.last.map((success) {
-          if (success is GetDeviceContactSuggestionsSuccess && success.results.isNotEmpty) {
-            autoCompleteResults.addAll(success.results.map((contact) => contact.toEmailAddress()));
-          }
-        }
+      final listEmailAddressFromServer = resultExecutions[0].fold(
+        (failure) => <EmailAddress>[],
+        (success) => success is GetAutoCompleteSuccess
+          ? success.listEmailAddress
+          : <EmailAddress>[]
       );
+      log('GetAllAutoCompleteInteractor::execute:listEmailAddressFromServerc: $listEmailAddressFromServer');
+      autoCompleteResults.addAll(listEmailAddressFromServer);
+
+      final listEmailAddressFromDevice = resultExecutions[1].fold(
+        (failure) => <EmailAddress>[],
+        (success) => success is GetDeviceContactSuggestionsSuccess
+          ? success.listEmailAddress
+          : <EmailAddress>[]
+      );
+      log('GetAllAutoCompleteInteractor::execute:listEmailAddressFromDevice: $listEmailAddressFromDevice');
+      autoCompleteResults.addAll(listEmailAddressFromDevice);
 
       return Right<Failure, Success>(GetAutoCompleteSuccess(autoCompleteResults));
     } catch (exception) {
