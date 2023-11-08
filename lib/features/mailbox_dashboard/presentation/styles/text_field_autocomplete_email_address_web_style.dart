9116bfec3d9aaa842716a9ee23049ba8e9f9cc2c
dab246 <tdvu@linagora.com>
2023-11-08 15:30:04 +0700

    TF-1155 Update UI for input field
    Signed-off-by: dab246 <tdvu@linagora.com>
    (cherry picked from commit 63a2ec09ae3bd1127e4274fc1104b2b791cb5e43)

--- lib/features/mailbox_dashboard/presentation/styles/text_field_autocomplete_email_address_web_style.dart
+++ lib/features/mailbox_dashboard/presentation/styles/text_field_autocomplete_email_address_web_style.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 
 class TextFieldAutoCompleteEmailAddressWebStyles {
   static const EdgeInsetsGeometry padding = EdgeInsets.symmetric(vertical: 8);
-  static const EdgeInsetsGeometry textInputContentPadding = EdgeInsets.symmetric(vertical: 16);
-  static const EdgeInsets tagEditorPadding = EdgeInsets.symmetric(horizontal: 8);
+  static const EdgeInsetsGeometry textInputContentPadding = EdgeInsetsDirectional.only(top: 16, bottom: 16, start: 12);
+  static const EdgeInsetsGeometry textInputContentPaddingWithSomeTag = EdgeInsetsDirectional.symmetric(vertical: 16);
+  static const EdgeInsets tagEditorPadding = EdgeInsets.symmetric(horizontal: 12);
 
   static const double borderRadius = 10.0;
   static const double suggestionBoxRadius = 20.0;
 
-  static const double borderWidth = 2.0;
-  static const double minTextFieldWidth = 20.0;
+  static const double borderWidth = 1.0;
+  static const double minTextFieldWidth = 40.0;
 
   static const double suggestionBoxElevation = 20.0;
   static const double suggestionBoxMaxHeight = 350.0;
