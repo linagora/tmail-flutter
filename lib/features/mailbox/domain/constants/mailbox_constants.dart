dbdffff3553ac13e67ed05b8c36057d2bc9bcd2b
dab246 <tdvu@linagora.com>
2023-11-09 16:49:20 +0700

    TF-2302 Resolve conflicts on the master branch
    Signed-off-by: dab246 <tdvu@linagora.com>
    (cherry picked from commit 6eb9f32ee0f67d9b78be13a4ecd4b1ae2b4de9a0)

--- lib/features/mailbox/domain/constants/mailbox_constants.dart
+++ lib/features/mailbox/domain/constants/mailbox_constants.dart
@@ -1,5 +1,7 @@
 import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
+import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
 import 'package:model/mailbox/mailbox_property.dart';
+import 'package:model/mailbox/presentation_mailbox.dart';
 
 class MailboxConstants {
   static final propertiesDefault = Properties({
@@ -16,4 +18,14 @@ class MailboxConstants {
     MailboxProperty.myRights,
     MailboxProperty.namespace
   });
+
+  static final List<Role> defaultMailboxRoles = [
+    PresentationMailbox.roleInbox,
+    PresentationMailbox.roleOutbox,
+    PresentationMailbox.roleDrafts,
+    PresentationMailbox.roleSent,
+    PresentationMailbox.roleTrash,
+    PresentationMailbox.roleSpam,
+    PresentationMailbox.roleTemplates,
+  ];
 }
