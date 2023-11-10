eb311420d9f7869af07605064a4e8d442486896a
dab246 <tdvu@linagora.com>
2023-11-10 10:48:05 +0700

    TF-2271 Add `dnsLookupToGetJmapUrl` method in login repository/datasource
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/login/domain/usecases/save_login_url_on_mobile_interactor.dart
+++ lib/features/login/domain/usecases/save_login_url_on_mobile_interactor.dart
@@ -2,17 +2,17 @@ import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
 import 'package:dartz/dartz.dart';
 import 'package:tmail_ui_user/features/login/domain/model/recent_login_url.dart';
-import 'package:tmail_ui_user/features/login/domain/repository/login_url_repository.dart';
+import 'package:tmail_ui_user/features/login/domain/repository/login_repository.dart';
 import 'package:tmail_ui_user/features/login/domain/state/save_recent_login_url_state.dart';
 
 class SaveLoginUrlOnMobileInteractor {
-  final LoginUrlRepository loginUrlRepository;
+  final LoginRepository _loginRepository;
 
-  SaveLoginUrlOnMobileInteractor(this.loginUrlRepository);
+  SaveLoginUrlOnMobileInteractor(this._loginRepository);
 
   Future<Either<Failure, Success>> execute(RecentLoginUrl recentLoginUrl) async {
     try{
-      await loginUrlRepository.saveRecentLoginUrl(recentLoginUrl);
+      await _loginRepository.saveRecentLoginUrl(recentLoginUrl);
       return Right(SaveRecentLoginUrlSuccess());
     } catch(e) {
       return Left(SaveRecentLoginUrlFailed(e));
