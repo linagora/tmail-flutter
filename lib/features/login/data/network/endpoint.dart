210709f35ac7d5154e90af1655f63fe0d32b54f2
dab246 <tdvu@linagora.com>
2023-11-13 19:24:04 +0700

    TF-2311 Set timeout when get session
    Signed-off-by: dab246 <tdvu@linagora.com>
    
    Signed-off-by: dab246 <tdvu@linagora.com>
    
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/login/data/network/endpoint.dart
+++ lib/features/login/data/network/endpoint.dart
@@ -2,4 +2,5 @@ import 'package:core/data/network/config/service_path.dart';
 
 class Endpoint {
   static final ServicePath webFinger = ServicePath('/.well-known/webfinger');
+  static final ServicePath sessionPath = ServicePath('/.well-known/jmap');
 }
