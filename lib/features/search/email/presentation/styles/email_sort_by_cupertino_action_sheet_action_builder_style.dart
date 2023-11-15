5b962fcc42bf92b063d62057b174debfa9c9caa4
hieubt <trunghieu.bui201@gmail.com>
2023-11-15 17:27:41 +0700

    TF-2304 Add action sheet for sort selecting

---
+++ lib/features/search/email/presentation/styles/email_sort_by_cupertino_action_sheet_action_builder_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class EmailSortByCupertinoActionSheetActionBuilderStyle {
+  static const double space = 16.0;
+
+  static const EdgeInsetsDirectional iconPadding = EdgeInsetsDirectional.only(end: space);
+
+  static const backgroundColor = Colors.white;
+}
