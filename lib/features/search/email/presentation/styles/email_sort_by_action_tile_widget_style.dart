1fa0d42aad9d297d800d8e327a5518bee917b55b
hieubt <trunghieu.bui201@gmail.com>
2023-11-15 17:28:16 +0700

    TF-2304 Add pop up title for sort selecting in tablet
    (cherry picked from commit 72d48529992a922d0979a6db0cf223d588197dbb)

---
+++ lib/features/search/email/presentation/styles/email_sort_by_action_tile_widget_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class EmailSortByActionTitleWidgetStyle {
+  static const EdgeInsetsGeometry padding = EdgeInsets.symmetric(horizontal: 24, vertical: 16);
+
+  static const double width = 332.0;
+  static const double space = 12.0;
+  static const double iconSize = 24.0;
+}
