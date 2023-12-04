ae1097cb1261370e14f35ed696f517c5bf552f41
hieubt <trunghieu.bui201@gmail.com>
2023-12-04 09:16:50 +0700

    TF-2346 Update identity creator constant

--- lib/features/identity_creator/presentation/utils/identity_creator_constants.dart
+++ lib/features/identity_creator/presentation/utils/identity_creator_constants.dart
@@ -1,4 +1,8 @@
 
 class IdentityCreatorConstants {
-  static const int maxKBSizeIdentityInlineImage = 16; // Kilobyte
+  static const int maxKBSizeIdentityInlineImage = 128; // Kilobyte
+  static const double maxWidthInlineImageDesktop = 800; // Pixel
+  static const double maxWidthInlineImageOther = 700; // Pixel
+  static const String prefixCompressedInlineImageTemp = 'compressed_';
+  static const int qualityCompressedInlineImage = 50; // 0 - 100
 }
