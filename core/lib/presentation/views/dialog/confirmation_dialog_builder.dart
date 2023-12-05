c9c1575b89cf637ad95d2be4ce8c9c22878ed906
dab246 <tdvu@linagora.com>
2023-12-06 01:28:03 +0700

    Fix padding item email in Thread and Search
    Signed-off-by: dab246 <tdvu@linagora.com>
    (cherry picked from commit 377085ba9054c6eb4c8e3d8388821473b3335962)

--- core/lib/presentation/views/dialog/confirmation_dialog_builder.dart
+++ core/lib/presentation/views/dialog/confirmation_dialog_builder.dart
@@ -1,7 +1,6 @@
 
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 typedef OnConfirmButtonAction = void Function();
 typedef OnCancelButtonAction = void Function();
