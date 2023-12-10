42268482bdf527bada15063eda1e2dd69c4dbeb8
hieubt <trunghieu.bui201@gmail.com>
2023-12-10 21:58:50 +0700

    TF-2100 Add `email_recovery_action/set` method and response and unit test

---
+++ email_recovery/test/method/set_email_recovery_action_method_test.dart
@@ -0,0 +1,109 @@
+import 'package:dio/dio.dart';
+import 'package:email_recovery/email_recovery/email_recovery_action.dart';
+import 'package:email_recovery/email_recovery/email_recovery_action_id.dart';
+import 'package:email_recovery/email_recovery/set/set_email_recovery_action_method.dart';
+import 'package:email_recovery/email_recovery/set/set_email_recovery_action_response.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http_mock_adapter/http_mock_adapter.dart';
+import 'package:jmap_dart_client/http/http_client.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
+import 'package:jmap_dart_client/jmap/core/utc_date.dart';
+import 'package:jmap_dart_client/jmap/jmap_request.dart';
+
+void main() {
+  group('test to json email recovery action set method', () {
+    final expectEmailRecoveryAction = EmailRecoveryAction(
+      id: EmailRecoveryActionId(Id('2034-495-05857-57abcd-0876664'))
+    );
+
+    test('email recovery action set method and response parsing', () async {
+      final baseOption  = BaseOptions(method: 'POST');
+      final dio = Dio(baseOption)..options.baseUrl = 'http://domain.com/jmap';
+      final dioAdapter = DioAdapter(dio: dio);
+      dioAdapter.onPost(
+        '',
+        (server) => server.reply(200, {
+          "sessionState": "2c9f1b12-b35a-43e6-9af2-0106fb53a943",
+          "methodResponses": [
+            [
+              "EmailRecoveryAction/set",
+              {
+                "created": {
+                  "hieubt": {
+                    "id": "2034-495-05857-57abcd-0876664"
+                  }
+                }
+              },
+              "c0"
+            ]
+          ]
+        }),
+        data: {
+          "using": [
+            "urn:ietf:params:jmap:core",
+            "com:linagora:params:jmap:messages:vault"
+          ],
+          "methodCalls": [
+            [
+              "EmailRecoveryAction/set",
+              {
+                "create": {
+                  "hieubt": {
+                    "deletedBefore": "2023-02-09T10:00:00.000Z",
+                    "deletedAfter": "2023-02-09T09:00:00.000Z",
+                    "receivedBefore": "2023-02-09T10:00:00.000Z",
+                    "receivedAfter": "2023-02-09T09:00:00.000Z",
+                    "hasAttachment": "true",
+                    "subject": "Simple topic",
+                    "sender": "bob@domain.tld",
+                    "recipients": ["alice@domain.tld"]
+                  }
+                }
+              },
+              "c0"
+            ]
+          ]
+        },
+        headers: {
+          "accept": "application/json;jmapVersion=rfc-8621",
+          "content-length": 868
+        }
+      );
+
+      final createRequestId = Id('hieubt');
+
+      final emailRecoveryActionSetMethod = SetEmailRecoveryActionMethod()
+        ..addCreate(
+          createRequestId,
+          EmailRecoveryAction(
+            deletedBefore: UTCDate(DateTime.parse('2023-02-09T10:00:00')),
+            deletedAfter: UTCDate(DateTime.parse('2023-02-09T09:00:00')),
+            receivedBefore: UTCDate(DateTime.parse('2023-02-09T10:00:00')),
+            receivedAfter: UTCDate(DateTime.parse('2023-02-09T09:00:00')),
+            hasAttachment: true,
+            subject: 'Simple topic',
+            sender: 'bob@domain.tld',
+            recipients: ['alice@domain.tld']
+          )
+        );
+      
+      final httpClient = HttpClient(dio);
+      final requestBuilder = JmapRequestBuilder(httpClient, ProcessingInvocation());
+      final emailRecoveryActionSetInvocation = requestBuilder.invocation(emailRecoveryActionSetMethod);
+      final response = await (requestBuilder
+          ..usings(emailRecoveryActionSetMethod.requiredCapabilities))
+        .build()
+        .execute();
+      
+      final emailRecoveryActionSetResponse = response.parse<SetEmailRecoveryActionResponse>(
+        emailRecoveryActionSetInvocation.methodCallId,
+        SetEmailRecoveryActionResponse.deserialize
+      );
+
+      expect(
+        emailRecoveryActionSetResponse!.created![createRequestId]!.id,
+        equals(expectEmailRecoveryAction.id)
+      );
+    });
+  });
+}
