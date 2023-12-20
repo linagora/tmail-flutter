9c56e8481c1668ef97f38c1d56afba34fba3e088
hieubt <trunghieu.bui201@gmail.com>
2023-12-20 23:31:26 +0700

    TF-2101 Add `email_recovery` form

---
+++ lib/features/email_recovery/presentation/styles/email_recovery_form_styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class EmailRecoveryFormStyles {
+  static const double iconClosePositionTop = 7;
+  static const double iconClosePositionRight = 12;
+
+  static const EdgeInsetsGeometry padding = EdgeInsets.only(top: 16);
+  static const EdgeInsetsGeometry inputAreaPadding = EdgeInsets.all(20.0);
+  static const EdgeInsetsGeometry bottomAreaPadding = EdgeInsets.symmetric(
+    horizontal: 20,
+    vertical: 12,
+  );
+
+  static const TextStyle titleTextStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+}
