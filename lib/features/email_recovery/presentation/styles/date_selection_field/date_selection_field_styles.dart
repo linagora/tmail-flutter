79dfba0120b18774212b085466d540a2426721d5
hieubt <trunghieu.bui201@gmail.com>
2023-12-21 00:17:08 +0700

    TF-2101 Add date selection field widget

---
+++ lib/features/email_recovery/presentation/styles/date_selection_field/date_selection_field_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DateSelectionFieldStyles {
+  static const double height = 44.0;
+  static const double titleWidth = 112.0;
+  static const double titleSpace = 12.0;
+  static const double titleSpaceMobile = 8.0;
+  static const double icCalendarSize = 24.0;
+  static const double icCalenderSpace = 10.0;
+  static const double borderWidthMobile = 0.5;
+  static const double radius = 12.0;
+
+  static const EdgeInsetsGeometry padding = EdgeInsets.symmetric(
+    vertical: 8.0
+  );
+  static const EdgeInsetsGeometry paddingMobile = EdgeInsets.symmetric(
+    vertical: 7.0
+  );
+  static const EdgeInsetsGeometry inputFieldMobilePadding = EdgeInsets.symmetric(
+    horizontal: 12.0
+  );
+
+  static const BorderRadius borderRadius = BorderRadius.all(Radius.circular(12));
+}
