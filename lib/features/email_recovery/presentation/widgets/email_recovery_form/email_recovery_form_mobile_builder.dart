7996c175dffec3720f22fb1ef3fc1ab13233db02
hieubt <trunghieu.bui201@gmail.com>
2023-12-21 00:33:26 +0700

    TF-2101 Add date selection fields to email recovery form

--- lib/features/email_recovery/presentation/widgets/email_recovery_form/email_recovery_form_mobile_builder.dart
+++ lib/features/email_recovery/presentation/widgets/email_recovery_form/email_recovery_form_mobile_builder.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:tmail_ui_user/features/email_recovery/presentation/email_recovery_controller.dart';
+import 'package:tmail_ui_user/features/email_recovery/presentation/model/email_recovery_field.dart';
 import 'package:tmail_ui_user/features/email_recovery/presentation/styles/email_recovery_form_styles.dart';
+import 'package:tmail_ui_user/features/email_recovery/presentation/widgets/date_selection_field/date_selection_field_mobile_widget.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
 
 class EmailRecoveryFormMobileBuilder extends GetWidget<EmailRecoveryController> {
@@ -25,6 +27,35 @@ class EmailRecoveryFormMobileBuilder extends GetWidget<EmailRecoveryController> 
               child: Text(
                 AppLocalizations.of(context).recoverDeletedMessages,
                 style: EmailRecoveryFormStyles.titleTextStyle,
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const ClampingScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Obx(() => DateSelectionFieldMobileWidget(
+                        field: EmailRecoveryField.deletionDate,
+                        recoveryTimeSelected: controller.deletionDateFieldSelected.value,
+                        imagePaths: controller.imagePaths,
+                        onTap: () => controller.onSelectDeletionDateRange(context),
+                        startDate: controller.startDeletionDate.value,
+                        endDate: controller.endDeletionDate.value,
+                      )),
+                      Obx(() => DateSelectionFieldMobileWidget(
+                        field: EmailRecoveryField.receptionDate,
+                        recoveryTimeSelected: controller.receptionDateFieldSelected.value,
+                        imagePaths: controller.imagePaths,
+                        onTap: () => controller.onSelectReceptionDateRange(context),
+                        startDate: controller.startReceptionDate.value,
+                        endDate: controller.endReceptionDate.value,
+                      )),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
