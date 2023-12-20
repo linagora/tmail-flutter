36a22670a7980c3cc432bc4bb4c82bb84343d580
hieubt <trunghieu.bui201@gmail.com>
2023-12-20 23:49:11 +0700

    TF-2101 Add `email_recovery` dialog

---
+++ lib/features/email_recovery/presentation/styles/email_recovery_view_styles.dart
@@ -0,0 +1,28 @@
+import 'package:core/utils/platform_info.dart';
+import 'package:flutter/material.dart';
+
+class EmailRecoveryViewStyles {
+  static const BoxDecoration backgroundDecorationMobile = BoxDecoration(
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(16),
+      topRight: Radius.circular(16),
+    ),
+    color: Colors.white,
+  );
+  static const BoxDecoration backgroundDecorationDesktop = BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(16)),
+    color: Colors.white,
+  );
+
+  static const EdgeInsetsGeometry backgroundMarginMobile = EdgeInsets.only(top: PlatformInfo.isWeb ? 70 : 0);
+
+  static const BorderRadiusGeometry clipRRectBorderRadiusMobile = BorderRadius.only(
+    topLeft: Radius.circular(16),
+    topRight: Radius.circular(16),
+  );
+  static const BorderRadiusGeometry clipRRectBorderRadiusDesktop = BorderRadius.all(Radius.circular(16));
+
+  static const ShapeBorder shapeBorderDesktop = RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(16)),
+  );
+}
