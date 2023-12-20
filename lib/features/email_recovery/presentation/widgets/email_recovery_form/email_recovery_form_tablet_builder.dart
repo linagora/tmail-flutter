194dc8f4772f4aa178b479217d3814892b32b94e
hieubt <trunghieu.bui201@gmail.com>
2023-12-20 23:31:26 +0700

    TF-2101 Add `email_recovery` form

---
+++ lib/features/email_recovery/presentation/widgets/email_recovery_form/email_recovery_form_tablet_builder.dart
@@ -0,0 +1,47 @@
+import 'package:core/presentation/views/button/icon_button_web.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/email_recovery/presentation/email_recovery_controller.dart';
+import 'package:tmail_ui_user/features/email_recovery/presentation/styles/email_recovery_form_styles.dart';
+import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+
+class EmailRecoveryFormTabletBuilder extends GetWidget<EmailRecoveryController> {
+  @override
+  final controller = Get.find<EmailRecoveryController>();
+
+  EmailRecoveryFormTabletBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EmailRecoveryFormStyles.padding,
+              alignment: Alignment.center,
+              child: Text(
+                AppLocalizations.of(context).recoverDeletedMessages,
+                style: EmailRecoveryFormStyles.titleTextStyle,
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: EmailRecoveryFormStyles.iconClosePositionTop,
+          right: EmailRecoveryFormStyles.iconClosePositionRight,
+          child: buildIconWeb(
+            icon: SvgPicture.asset(
+              controller.imagePaths.icClose,
+              fit: BoxFit.fill,
+            ),
+            tooltip: AppLocalizations.of(context).close,
+            onTap: () => controller.closeView(context),
+          ),
+        )
+      ],
+    );
+  }
+}
