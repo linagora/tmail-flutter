5fd8b9abd6af17e42395f60cb50d0e56666a9c39
hieubt <trunghieu.bui201@gmail.com>
2023-12-21 15:54:57 +0700

    TF-2101 Add text input field widget without suggestion

---
+++ lib/features/email_recovery/presentation/styles/text_input_field/text_input_field_widget_styles.dart
@@ -0,0 +1,50 @@
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+
+class TextInputFieldWidgetStyles {
+  static const double titleWidth = 112.0;
+  static const double borderWidth = 1.0;
+  static const double suffixIconSize = 8.0;
+  static const double space = 12.0;
+  static const double spaceMobile = 8.0;
+  
+  static const EdgeInsetsGeometry padding = EdgeInsets.symmetric(
+    vertical: 8.0
+  );
+  static const EdgeInsetsGeometry suffixIconPadding = EdgeInsets.only(
+    right: 12.0
+  );
+  static const EdgeInsetsGeometry contentPadding = EdgeInsetsDirectional.symmetric(
+    horizontal: 12.0
+  );
+
+  static const BorderRadius borderRadius = BorderRadius.all(Radius.circular(10));
+
+  static const InputBorder border = OutlineInputBorder(
+    borderRadius: borderRadius,
+    borderSide: BorderSide(
+      width: borderWidth,
+      color: AppColor.colorInputBorderCreateMailbox,
+    )
+  );
+  static const InputBorder enableBorder = OutlineInputBorder(
+    borderRadius: borderRadius,
+    borderSide: BorderSide(
+      width: borderWidth,
+      color: AppColor.colorInputBorderCreateMailbox,
+    )
+  );
+  static const InputBorder focusedBorder = OutlineInputBorder(
+    borderRadius: borderRadius,
+    borderSide: BorderSide(
+      width: borderWidth,
+      color: AppColor.primaryColor,
+    )
+  );
+
+  static const TextStyle inputtedTextStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+}
