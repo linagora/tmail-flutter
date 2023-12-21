dbdb0e924cb93b45767a1f0bf26116990136af58
hieubt <trunghieu.bui201@gmail.com>
2023-12-21 15:56:58 +0700

    TF-2101 Add suggestion tag for input field with suggestion
    (cherry picked from commit 9a4847aefae5554710999e6fdf98ed0f7f8eca3c)

---
+++ lib/features/email_recovery/presentation/styles/text_input_field/suggestion_tag_item_widget_styles.dart
@@ -0,0 +1,32 @@
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:flutter/material.dart';
+
+class SuggestionTagItemWidgetStyles {
+  static const double paddingTop = 4.5;
+  static const double paddingEndCollapsed = 40.0;
+  static const double paddingEndExpanded = 0.0;
+  static const double labelPaddingHorizontal = 4.0;
+  static const double labelPaddingVertical = 2.0;
+  static const double collapsedTextBorderRadius = 10.0;
+
+  static const BorderRadius borderRadius = BorderRadius.all(Radius.circular(10));
+
+  static const EdgeInsetsGeometry collapsedTextMargin = EdgeInsets.only(
+    top: 5.5,
+  );
+  static const EdgeInsetsGeometry collapsedTextPadding = EdgeInsetsDirectional.symmetric(
+    vertical: 5,
+    horizontal: 8,
+  );
+
+  static const BorderSide latestTagBorderSide = BorderSide(
+    color: AppColor.primaryColor,
+    width: 1,
+  );
+
+  static const TextStyle labelStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 17,
+    fontWeight: FontWeight.w400
+  );
+}
