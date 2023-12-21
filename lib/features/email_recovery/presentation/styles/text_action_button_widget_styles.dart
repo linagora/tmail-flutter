328a66e3c6f8c4a9cb8d1e2fd896946729cd0650
hieubt <trunghieu.bui201@gmail.com>
2023-12-21 16:50:27 +0700

    TF-2101 Add buttons and `hasAttachment` checkbox

---
+++ lib/features/email_recovery/presentation/styles/text_action_button_widget_styles.dart
@@ -0,0 +1,5 @@
+class TextActionButtonWidgetStyles {
+  static const double radius = 10.0;
+  static const double height = 44.0;
+  static const double fontSize = 17.0;
+}
---
+++ lib/features/email_recovery/presentation/widgets/text_action_button_widget.dart
@@ -0,0 +1,37 @@
+import 'package:core/presentation/views/button/icon_button_web.dart';
+import 'package:flutter/material.dart';
+import 'package:tmail_ui_user/features/email_recovery/presentation/styles/text_action_button_widget_styles.dart';
+
+class TextActionButtonWidget extends StatelessWidget {
+  final Color colorButton;
+  final Color colorText;
+  final String text;
+  final VoidCallback onAction;
+  final double? minWidth;
+  
+  const TextActionButtonWidget({
+    super.key,
+    required this.colorButton,
+    required this.colorText,
+    required this.text,
+    required this.onAction,
+    this.minWidth,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return buildButtonWrapText(
+      text,
+      radius: TextActionButtonWidgetStyles.radius,
+      height: TextActionButtonWidgetStyles.height,
+      minWidth: minWidth,
+      textStyle: TextStyle(
+        fontSize: TextActionButtonWidgetStyles.fontSize,
+        color: colorText,
+        fontWeight: FontWeight.w500
+      ),
+      bgColor: colorButton,
+      onTap: onAction
+    );
+  }
+}
