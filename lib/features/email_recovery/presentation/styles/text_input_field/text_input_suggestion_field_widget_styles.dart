4dd8e7b72792f3c3cc6a61a845b46fdf0a90d804
hieubt <trunghieu.bui201@gmail.com>
2023-12-21 15:57:24 +0700

    TF-2101 Add text input field widget with suggestion
    (cherry picked from commit c0f7c63c3e1aaeb2ded641e1f99329dd47bd76b8)

---
+++ lib/features/email_recovery/presentation/styles/text_input_field/text_input_suggestion_field_widget_styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class TextInputSuggestionFieldWidgetStyles {
+  static const double titleWidth = 112.0;
+  static const double borderRadius = 10.0;
+  static const double borderSize = 1.0;
+  static const double minTextFieldWidth = 40.0;
+  static const double suggestionsBoxElevation = 20.0;
+  static const double suggestionsBoxRadius = 20.0;
+  static const double suggestionsBoxMaxHeight = 350.0;
+  static const double space = 12.0;
+  static const double spaceMobile = 8.0;
+
+  static const EdgeInsetsGeometry padding = EdgeInsets.symmetric(
+    vertical: 8.0
+  );
+  static const EdgeInsetsGeometry contentPadding = EdgeInsetsDirectional.symmetric(
+    vertical: 16.0
+  );
+  static const EdgeInsetsGeometry emptyListContentPadding = EdgeInsetsDirectional.only(
+    top: 16,
+    bottom: 16,
+    start: 12
+  );
+
+  static const EdgeInsets inputFieldPadding = EdgeInsets.symmetric(
+    horizontal: 12.0
+  );
+
+  static const InputBorder border = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderSide: BorderSide.none,
+  );
+
+  static const TextStyle inputFieldTextStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+}
