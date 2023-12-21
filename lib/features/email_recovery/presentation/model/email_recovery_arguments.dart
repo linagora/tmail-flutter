35b51a21297f5350e2eb777fbc410a265a709f05
hieubt <trunghieu.bui201@gmail.com>
2023-12-21 15:58:21 +0700

    TF-2101 Add `email_recovery` arguments
    (cherry picked from commit 0873b6c7718d712d8f3e9954388a7da3dcc2b13c)

---
+++ lib/features/email_recovery/presentation/model/email_recovery_arguments.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
+
+class EmailRecoveryArguments with EquatableMixin {
+  final AccountId accountId;
+  final Session session;
+
+  EmailRecoveryArguments(
+    this.accountId,
+    this.session
+  );
+
+  @override
+  List<Object?> get props => [
+    accountId,
+    session,
+  ];
+  
+}
