4b51b427d97e3fab3382e37847b8672d57ce50ec
dab246 <tdvu@linagora.com>
2023-12-24 18:57:34 +0700

    TF-2384 Merge `Token` with `TokenOIDC` to avoid confusion
    Signed-off-by: dab246 <tdvu@linagora.com>

--- model/lib/oidc/token_oidc.dart
+++ model/lib/oidc/token_oidc.dart
@@ -1,8 +1,14 @@
 
+import 'package:core/utils/app_logger.dart';
 import 'package:equatable/equatable.dart';
-import 'package:model/oidc/token.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:model/oidc/converter/token_id_converter.dart';
 import 'package:model/oidc/token_id.dart';
 
+part 'token_oidc.g.dart';
+
+@TokenIdConverter()
+@JsonSerializable(includeIfNull: false, explicitToJson: true)
 class TokenOIDC with EquatableMixin {
 
   final String token;
@@ -17,6 +23,10 @@ class TokenOIDC with EquatableMixin {
     {this.expiredTime}
   );
 
+  factory TokenOIDC.fromJson(Map<String, dynamic> json) => _$TokenOIDCFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TokenOIDCToJson(this);
+
   @override
   List<Object?> get props => [token, tokenId, expiredTime, refreshToken];
 }
@@ -25,9 +35,15 @@ extension TokenOIDCExtension on TokenOIDC {
 
   bool isTokenValid() => token.isNotEmpty && tokenId.uuid.isNotEmpty;
 
-  Token toToken() {
-    return Token(token, tokenId, refreshToken, expiredTime: expiredTime);
-  }
-
   String get tokenIdHash => tokenId.uuid.hashCode.toString();
+
+  bool get isExpired {
+    if (expiredTime != null) {
+      final now = DateTime.now();
+      log('TokenOIDC::isExpired(): TIME_NOW: $now');
+      log('TokenOIDC::isExpired(): EXPIRED_DATE: $expiredTime');
+      return expiredTime!.isBefore(now);
+    }
+    return false;
+  }
 }
