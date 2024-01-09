5a10316b6566f0a6cea6e3f1a4ae0f971ea7c2b2
dab246 <tdvu@linagora.com>
2024-01-09 11:41:38 +0700

    TF-2420 Move box name to constants class
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/caching/clients/mailbox_cache_client.dart
+++ lib/features/caching/clients/mailbox_cache_client.dart
@@ -1,9 +1,10 @@
 
 import 'package:tmail_ui_user/features/caching/config/hive_cache_client.dart';
+import 'package:tmail_ui_user/features/caching/utils/caching_constants.dart';
 import 'package:tmail_ui_user/features/mailbox/data/model/mailbox_cache.dart';
 
 class MailboxCacheClient extends HiveCacheClient<MailboxCache> {
 
   @override
-  String get tableName => 'MailboxCache';
+  String get tableName => CachingConstants.mailboxCacheBoxName;
 }
--- lib/features/caching/clients/recent_search_cache_client.dart
+++ lib/features/caching/clients/recent_search_cache_client.dart
@@ -1,9 +1,10 @@
 
 import 'package:tmail_ui_user/features/caching/config/hive_cache_client.dart';
+import 'package:tmail_ui_user/features/caching/utils/caching_constants.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/data/model/recent_search_cache.dart';
 
 class RecentSearchCacheClient extends HiveCacheClient<RecentSearchCache> {
 
   @override
-  String get tableName => 'RecentSearchCache';
+  String get tableName => CachingConstants.recentSearchCacheBoxName;
 }
