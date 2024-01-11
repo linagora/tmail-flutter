d45afda2e14d4881ce74701fe0b80d97de6c761a
Dat PHAM HOANG <dphamhoang@linagora.com>
2024-01-11 11:08:48 +0700

    [Android][Disable work manager] Remove unuse interactors in sending queue

--- lib/main/exceptions/send_email_exception_thrower.dart
+++ lib/main/exceptions/send_email_exception_thrower.dart
@@ -4,8 +4,7 @@ import 'package:core/utils/app_logger.dart';
 import 'package:tmail_ui_user/features/network_connection/presentation/network_connection_controller.dart';
 import 'package:tmail_ui_user/main/exceptions/remote_exception.dart';
 import 'package:tmail_ui_user/main/exceptions/remote_exception_thrower.dart';
-
-import '../routes/route_navigation.dart';
+import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 
 class SendEmailExceptionThrower extends RemoteExceptionThrower {
   @override
