fc6f55fc57cc376aa55edc31642bd354221c060f
dab246 <tdvu@linagora.com>
2024-01-19 19:10:31 +0700

    TF-2497 Support HTTP in server url
    Signed-off-by: dab246 <tdvu@linagora.com>

--- core/test/presentation/extensions/uri_extension_test.dart
+++ core/test/presentation/extensions/uri_extension_test.dart
@@ -6,52 +6,62 @@ void main() {
 
   group('method convertToQualifiedUrl test', () {
 
-    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com` and sourceUrl is `https://domain.com/jmap`', () async {
-      final baseUrl =  Uri.parse('https://domain.com');
-      final sourceUrl =  Uri.parse('https://domain.com/jmap');
+    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com` and originUrl is `https://domain.com/jmap`', () async {
+      const baseUrl = 'https://domain.com';
+      final originUrl = Uri.parse('https://domain.com/jmap');
 
       final qualifiedUrlExpected = Uri.parse('https://domain.com/jmap');
-      final qualifiedUrlResult = sourceUrl.toQualifiedUrl(baseUrl: baseUrl);
+      final qualifiedUrlResult = originUrl.toQualifiedUrl(baseUrl);
 
       expect(qualifiedUrlResult, equals(qualifiedUrlExpected));
     });
 
-    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com` and sourceUrl is `/jmap/`', () async {
-      final baseUrl =  Uri.parse('https://domain.com');
-      final sourceUrl =  Uri.parse('/jmap/');
+    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com` and originUrl is `/jmap/`', () async {
+      const baseUrl = 'https://domain.com';
+      final originUrl = Uri.parse('/jmap/');
 
       final qualifiedUrlExpected = Uri.parse('https://domain.com/jmap');
-      final qualifiedUrlResult = sourceUrl.toQualifiedUrl(baseUrl: baseUrl);
+      final qualifiedUrlResult = originUrl.toQualifiedUrl(baseUrl);
 
       expect(qualifiedUrlResult, equals(qualifiedUrlExpected));
     });
 
-    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com` and sourceUrl is `/jmap`', () async {
-      final baseUrl =  Uri.parse('https://domain.com');
-      final sourceUrl =  Uri.parse('/jmap');
+    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com` and originUrl is `/jmap`', () async {
+      const baseUrl = 'https://domain.com';
+      final originUrl = Uri.parse('/jmap');
 
       final qualifiedUrlExpected = Uri.parse('https://domain.com/jmap');
-      final qualifiedUrlResult = sourceUrl.toQualifiedUrl(baseUrl: baseUrl);
+      final qualifiedUrlResult = originUrl.toQualifiedUrl(baseUrl);
 
       expect(qualifiedUrlResult, equals(qualifiedUrlExpected));
     });
 
-    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com/jmap` and sourceUrl is empty', () async {
-      final baseUrl =  Uri.parse('https://domain.com/jmap');
-      final sourceUrl =  Uri.parse('');
+    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com/jmap` and originUrl is empty', () async {
+      const baseUrl = 'https://domain.com/jmap';
+      final originUrl = Uri.parse('');
 
       final qualifiedUrlExpected = Uri.parse('https://domain.com/jmap');
-      final qualifiedUrlResult = sourceUrl.toQualifiedUrl(baseUrl: baseUrl);
+      final qualifiedUrlResult = originUrl.toQualifiedUrl(baseUrl);
 
       expect(qualifiedUrlResult, equals(qualifiedUrlExpected));
     });
 
-    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com/jmap` and sourceUrl is `/`', () async {
-      final baseUrl =  Uri.parse('https://domain.com/jmap');
-      final sourceUrl =  Uri.parse('/');
+    test('convertToQualifiedUrl() should return qualified url when baseUrl is `https://domain.com/jmap` and originUrl is `/`', () async {
+      const baseUrl = 'https://domain.com/jmap';
+      final originUrl = Uri.parse('/');
 
       final qualifiedUrlExpected = Uri.parse('https://domain.com/jmap');
-      final qualifiedUrlResult = sourceUrl.toQualifiedUrl(baseUrl: baseUrl);
+      final qualifiedUrlResult = originUrl.toQualifiedUrl(baseUrl);
+
+      expect(qualifiedUrlResult, equals(qualifiedUrlExpected));
+    });
+
+    test('convertToQualifiedUrl() should return qualified url when baseUrl is `http://domain.com` and originUrl is `http://localhost.com/jmap`', () async {
+      const baseUrl = 'http://domain.com';
+      final originUrl = Uri.parse('http://localhost.com/jmap');
+
+      final qualifiedUrlExpected = Uri.parse('http://localhost.com/jmap');
+      final qualifiedUrlResult = originUrl.toQualifiedUrl(baseUrl);
 
       expect(qualifiedUrlResult, equals(qualifiedUrlExpected));
     });
