0650fdcabce2eb9796a00bfa5edf793a51905531
dab246 <tdvu@linagora.com>
2024-01-26 10:28:05 +0700

    TF-2510 Show PDF icon in composer`
    Signed-off-by: dab246 <tdvu@linagora.com>
    (cherry picked from commit 55b1f99b2835699b5844ceca94ac0bb5371881c2)

--- lib/features/upload/presentation/model/upload_file_state.dart
+++ lib/features/upload/presentation/model/upload_file_state.dart
@@ -1,12 +1,11 @@
 
-import 'package:core/domain/extensions/media_type_extension.dart';
 import 'package:core/presentation/resources/image_paths.dart';
-import 'package:core/utils/app_logger.dart';
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:model/email/attachment.dart';
 import 'package:model/upload/file_info.dart';
+import 'package:tmail_ui_user/features/email/presentation/extensions/attachment_extension.dart';
 import 'package:tmail_ui_user/features/upload/domain/model/upload_task_id.dart';
 import 'package:tmail_ui_user/features/upload/presentation/model/upload_file_status.dart';
 
@@ -77,24 +76,7 @@ class UploadFileState with EquatableMixin {
     if (mediaType == null && file != null) {
       mediaType = MediaType.parse(file!.mimeType);
     }
-    log('AttachmentExtension::getIcon(): mediaType: $mediaType');
-    if (mediaType == null) {
-      return imagePaths.icFileEPup;
-    }
-    if (mediaType.isDocFile()) {
-      return imagePaths.icFileDocx;
-    } else if (mediaType.isExcelFile()) {
-      return imagePaths.icFileXlsx;
-    } else if (mediaType.isPowerPointFile()) {
-      return imagePaths.icFilePptx;
-    } else if (mediaType.isPdfFile()) {
-      return imagePaths.icFilePdf;
-    } else if (mediaType.isZipFile()) {
-      return imagePaths.icFileZip;
-    } else if (mediaType.isImageFile()) {
-      return imagePaths.icFilePng;
-    }
-    return imagePaths.icFileEPup;
+    return attachment?.getIcon(imagePaths, fileMediaType: mediaType) ?? imagePaths.icFileEPup;
   }
 
   @override
