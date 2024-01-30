38702090aa595931909af94fe12c34130d0e3c3f
dab246 <tdvu@linagora.com>
2024-01-30 18:48:04 +0700

    TF-2513 Fix hidden text in mobile web
    Signed-off-by: dab246 <tdvu@linagora.com>

--- core/lib/presentation/utils/style_utils.dart
+++ core/lib/presentation/utils/style_utils.dart
@@ -1,5 +1,4 @@
 import 'package:core/presentation/extensions/color_extension.dart';
-import 'package:core/utils/platform_info.dart';
 import 'package:flutter/material.dart';
 
 class CommonTextStyle {
@@ -10,9 +9,7 @@ class CommonTextStyle {
     fontWeight: FontWeight.normal,
   );
 
-  static const defaultTextOverFlow = PlatformInfo.isWeb
-      ? TextOverflow.fade
-      : TextOverflow.ellipsis;
+  static const defaultTextOverFlow = TextOverflow.ellipsis;
 
-  static const defaultSoftWrap = PlatformInfo.isWeb ? false : true;
+  static const defaultSoftWrap = true;
 }
