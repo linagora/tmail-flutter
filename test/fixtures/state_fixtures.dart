eb3e22b4678da010960ebb384d837ebf295e8670
dab246 <tdvu@linagora.com>
2024-02-06 14:41:03 +0700

    TF-2536 Write unit test for `getAllMailbox` method repository
    Signed-off-by: dab246 <tdvu@linagora.com>

--- test/fixtures/state_fixtures.dart
+++ test/fixtures/state_fixtures.dart
@@ -3,5 +3,6 @@ import 'package:jmap_dart_client/jmap/core/state.dart';
 
 class StateFixtures {
   static final currentMailboxState = State('a1234');
+  static final newMailboxState = State('a2345');
   static final currentEmailState = State('e1234');
 }
