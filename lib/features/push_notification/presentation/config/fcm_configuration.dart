5228f4c348f17c053fb4720edf519e8289e1833a
dab246 <tdvu@linagora.com>
2024-02-22 10:38:05 +0700

    TF-2599 Remove `requestPermissionContact` in AdvancedFilterController due unnecessary and avoid error request only one set of permissions at a time
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/push_notification/presentation/config/fcm_configuration.dart
+++ lib/features/push_notification/presentation/config/fcm_configuration.dart
@@ -1,9 +1,14 @@
 
+import 'package:core/utils/app_logger.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:tmail_ui_user/features/push_notification/presentation/config/firebase_options.dart';
 
 class FcmConfiguration {
   static Future<void> initialize() async {
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    try {
+      await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    } catch (e) {
+      logError('FcmConfiguration::initialize: Exception = $e');
+    }
   }
 }
