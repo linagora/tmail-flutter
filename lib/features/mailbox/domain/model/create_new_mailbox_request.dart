873272e6e479db46580e89da71133333567b001d
dab246 <tdvu@linagora.com>
2024-03-05 18:47:31 +0700

    TF-2667 Move the creation of mailbox request id to the data layer
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/mailbox/domain/model/create_new_mailbox_request.dart
+++ lib/features/mailbox/domain/model/create_new_mailbox_request.dart
@@ -1,17 +1,14 @@
 
 import 'package:equatable/equatable.dart';
-import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
 
 class CreateNewMailboxRequest with EquatableMixin {
 
   final MailboxName newName;
-  final Id creationId;
   final MailboxId? parentId;
   final bool isSubscribed;
 
   CreateNewMailboxRequest(
-    this.creationId,
     this.newName,
     {
       this.parentId,
@@ -21,7 +18,6 @@ class CreateNewMailboxRequest with EquatableMixin {
 
   @override
   List<Object?> get props => [
-    creationId,
     newName,
     parentId,
     isSubscribed
