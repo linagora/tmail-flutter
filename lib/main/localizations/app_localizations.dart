7a56234e5d36cec34ddde400986e047f09ea73c7
DatDang <tddang@linagora.com>
2024-03-08 10:05:02 +0700

    TF-2682 Add alwaysReadReceipt to account menu item model

--- lib/main/localizations/app_localizations.dart
+++ lib/main/localizations/app_localizations.dart
@@ -3833,4 +3833,22 @@ class AppLocalizations {
       name: 'printingFailed'
     );
   }
+
+  String get emailReadReceipts {
+    return Intl.message(
+      'Email read receipts',
+      name: 'emailReadReceipts');
+  }
+
+  String get emailReadReceiptsSettingExplanation {
+    return Intl.message(
+      'Read receipts are notifications that can be sent to and from your users to verify that mail has been read.',
+      name: 'emailReadReceiptsSettingExplanation');
+  }
+
+  String get emailReadReceiptsToggleDescription {
+    return Intl.message(
+      'Always request read receipts with outgoing messages',
+      name: 'emailReadReceiptsToggleDescription');
+  }
 }
