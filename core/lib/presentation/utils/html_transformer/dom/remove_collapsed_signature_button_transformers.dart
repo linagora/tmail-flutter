de6524719f5d446fc2e8f26a8bdf98ca502ed9e8
dab246 <tdvu@linagora.com>
2024-04-08 13:37:22 +0700

    TF-2785 Remove log unnecessary

--- core/lib/presentation/utils/html_transformer/dom/remove_collapsed_signature_button_transformers.dart
+++ core/lib/presentation/utils/html_transformer/dom/remove_collapsed_signature_button_transformers.dart
@@ -1,7 +1,6 @@
 
 import 'package:core/data/network/dio_client.dart';
 import 'package:core/presentation/utils/html_transformer/base/dom_transformer.dart';
-import 'package:core/utils/app_logger.dart';
 import 'package:html/dom.dart';
 
 class RemoveCollapsedSignatureButtonTransformer extends DomTransformer {
@@ -15,10 +14,8 @@ class RemoveCollapsedSignatureButtonTransformer extends DomTransformer {
     Map<String, String>? mapUrlDownloadCID,
   }) async {
     final elements = document.querySelectorAll('.tmail-signature-button');
-    log('RemoveCollapsedSignatureButtonTransformer::process:elements:: ${elements.length}');
     await Future.wait(elements.map((element) async {
       element.remove();
     }));
   }
-
 }
