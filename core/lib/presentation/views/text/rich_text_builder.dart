9d3b74bfc8e509214e37e3485a9945a225db8907
dab246 <tdvu@linagora.com>
2024-04-12 14:38:16 +0700

    TF-2122 Fix highlight and font search result incorrect

--- core/lib/presentation/views/text/rich_text_builder.dart
+++ core/lib/presentation/views/text/rich_text_builder.dart
@@ -1,66 +1,62 @@
-import 'package:core/presentation/utils/style_utils.dart';
 import 'package:flutter/material.dart';
 
-class RichTextBuilder {
+class RichTextBuilder extends StatelessWidget {
 
-  final String _textOrigin;
-  final String _wordToStyle;
-  final TextStyle _styleOrigin;
-  final TextStyle _styleWord;
+  final String textOrigin;
+  final String wordToStyle;
+  final TextStyle styleOrigin;
+  final TextStyle styleWord;
 
-  Key? _key;
-  int? _maxLines;
-  TextOverflow? _overflow;
+  const RichTextBuilder({
+    super.key,
+    required this.textOrigin,
+    required this.wordToStyle,
+    required this.styleOrigin,
+    required this.styleWord,
+  });
 
-  RichTextBuilder(
-    this._textOrigin,
-    this._wordToStyle,
-    this._styleOrigin,
-    this._styleWord,
-  );
-
-  void key(Key key) {
-    _key = key;
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        style: styleOrigin,
+        children: _getSpans(
+          text: textOrigin,
+          word: wordToStyle,
+          styleOrigin: styleOrigin,
+          styleWord: styleWord,
+        )
+      ),
+      style: styleOrigin,
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis
+    );
   }
 
-  void maxLines(int maxLines) {
-    _maxLines = maxLines;
-  }
-
-  void setOverflow(TextOverflow textOverflow) {
-    _overflow = textOverflow;
-  }
-
-  RichText build() {
-    return RichText(
-      key: _key,
-      maxLines: _maxLines ?? 1,
-      softWrap: CommonTextStyle.defaultSoftWrap,
-      overflow: _overflow ?? CommonTextStyle.defaultTextOverFlow,
-      text: TextSpan(
-        style: _styleOrigin,
-        children: _getSpans(_textOrigin, _wordToStyle, _styleWord)));
-  }
-
-  List<TextSpan> _getSpans(String text, String matchWord, TextStyle style) {
+  List<TextSpan> _getSpans({
+    required String text,
+    required String word,
+    required TextStyle styleOrigin,
+    required TextStyle styleWord,
+  }) {
     List<TextSpan> spans = [];
     int spanBoundary = 0;
     do {
       // look for the next match
-      final startIndex = text.toLowerCase().indexOf(matchWord.toLowerCase(), spanBoundary);
+      final startIndex = text.toLowerCase().indexOf(word.toLowerCase(), spanBoundary);
       // if no more matches then add the rest of the string without style
       if (startIndex == -1) {
-        spans.add(TextSpan(text: text.substring(spanBoundary)));
+        spans.add(TextSpan(text: text.substring(spanBoundary), style: styleOrigin));
         return spans;
       }
       // add any unStyled text before the next match
       if (startIndex > spanBoundary) {
-        spans.add(TextSpan(text: text.substring(spanBoundary, startIndex)));
+        spans.add(TextSpan(text: text.substring(spanBoundary, startIndex), style: styleOrigin));
       }
       // style the matched text
-      final endIndex = startIndex + matchWord.length;
+      final endIndex = startIndex + word.length;
       final spanText = text.substring(startIndex, endIndex);
-      spans.add(TextSpan(text: spanText, style: style));
+      spans.add(TextSpan(text: spanText, style: styleWord));
       // mark the boundary to start the next search from
       spanBoundary = endIndex;
       // continue until there are no more matches
