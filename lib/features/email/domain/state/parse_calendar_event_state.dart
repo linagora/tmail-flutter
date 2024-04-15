8ed14711a3c95da30ac8e7df5637327d24a12d3a
DatDang <tddang@linagora.com>
2024-04-15 16:22:22 +0700

    TF-2802 Expose blobId of chosen calendar event after parse

--- lib/features/email/domain/state/parse_calendar_event_state.dart
+++ lib/features/email/domain/state/parse_calendar_event_state.dart
@@ -1,5 +1,6 @@
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:jmap_dart_client/jmap/mail/calendar/calendar_event.dart';
 import 'package:tmail_ui_user/features/email/domain/model/event_action.dart';
 
@@ -7,13 +8,13 @@ class ParseCalendarEventLoading extends LoadingState {}
 
 class ParseCalendarEventSuccess extends UIState {
 
-  final List<CalendarEvent> calendarEventList;
+  final Map<Id, List<CalendarEvent>> calendarEventMap;
   final List<EventAction> eventActionList;
 
-  ParseCalendarEventSuccess(this.calendarEventList, this.eventActionList);
+  ParseCalendarEventSuccess(this.calendarEventMap, this.eventActionList);
 
   @override
-  List<Object> get props => [calendarEventList, eventActionList];
+  List<Object> get props => [calendarEventMap, eventActionList];
 }
 
 class ParseCalendarEventFailure extends FeatureFailure {
