749dbd73e0d275c35a34d40b26a97b76b4e0b615
DatDang <tddang@linagora.com>
2024-04-15 11:18:32 +0700

    TF-2800 Calendar event accept - API and data source implementation

--- lib/features/email/data/datasource_impl/local_calendar_event_datasource_impl.dart
+++ lib/features/email/data/datasource_impl/local_calendar_event_datasource_impl.dart
@@ -2,6 +2,7 @@
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:jmap_dart_client/jmap/mail/calendar/calendar_event.dart';
+import 'package:jmap_dart_client/jmap/mail/calendar/reply/calendar_event_accept_response.dart';
 import 'package:tmail_ui_user/features/email/data/datasource/calendar_event_datasource.dart';
 import 'package:tmail_ui_user/features/email/data/local/html_analyzer.dart';
 import 'package:tmail_ui_user/features/email/domain/model/event_action.dart';
@@ -24,5 +25,10 @@ class LocalCalendarEventDataSourceImpl extends CalendarEventDataSource {
     return Future.sync(() async {
       return await _htmlAnalyzer.getListEventAction(emailContents);
     }).catchError(_exceptionThrower.throwException);
+  }
+  
+  @override
+  Future<CalendarEventAcceptResponse> acceptEventInvitation(AccountId accountId, Set<Id> blobIds) {
+    throw UnimplementedError();
   }
 }
