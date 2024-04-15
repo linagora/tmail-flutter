07d24359936ff59e4adf7ee0ffdd1a69c2a3dec1
DatDang <tddang@linagora.com>
2024-04-15 11:16:41 +0700

    TF-2800 Calendar event accept - abstract data source and repo

--- lib/features/email/domain/repository/calendar_event_repository.dart
+++ lib/features/email/domain/repository/calendar_event_repository.dart
@@ -2,10 +2,13 @@
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:jmap_dart_client/jmap/mail/calendar/calendar_event.dart';
+import 'package:jmap_dart_client/jmap/mail/calendar/reply/calendar_event_accept_response.dart';
 import 'package:tmail_ui_user/features/email/domain/model/event_action.dart';
 
 abstract class CalendarEventRepository {
   Future<Map<Id, List<CalendarEvent>>> parse(AccountId accountId, Set<Id> blobIds);
 
   Future<List<EventAction>> getListEventAction(String emailContents);
+
+  Future<CalendarEventAcceptResponse> acceptEventInvitation(AccountId accountId, Set<Id> blobIds);
 }
