8ec3dec8344ca81334cf2402151168be680edb29
DatDang <tddang@linagora.com>
2024-04-22 10:04:18 +0700

    TF-2819 calendar event maybe abstract data source & repo

--- lib/features/email/domain/repository/calendar_event_repository.dart
+++ lib/features/email/domain/repository/calendar_event_repository.dart
@@ -3,9 +3,12 @@ import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:jmap_dart_client/jmap/mail/calendar/reply/calendar_event_accept_response.dart';
 import 'package:tmail_ui_user/features/email/presentation/model/blob_calendar_event.dart';
+import 'package:jmap_dart_client/jmap/mail/calendar/reply/calendar_event_maybe_response.dart';
 
 abstract class CalendarEventRepository {
   Future<List<BlobCalendarEvent>> parse(AccountId accountId, Set<Id> blobIds);
 
   Future<CalendarEventAcceptResponse> acceptEventInvitation(AccountId accountId, Set<Id> blobIds);
+
+  Future<CalendarEventMaybeResponse> maybeEventInvitation(AccountId accountId, Set<Id> blobIds);
 }
