4de7c398adfa26bd303672fba79e10b612edea67
tddang-linagora <tddang-linagora@users.noreply.github.com>
2024-05-09 08:26:27 +0000

    deploy: 5d69071ebaf17bbd8a669c82f803a300fcf186e8

--- 2826/flutter_service_worker.js
+++ 2826/flutter_service_worker.js
@@ -324,7 +324,7 @@ const RESOURCES = {"splash/img/light-2x.png": "d8ddaeda9f59609cf6e6e3b5719e1352"
 "assets/fonts/MaterialIcons-Regular.otf": "a989b846e9036e366ad999057b61ef0e",
 "assets/AssetManifest.json": "ffe5c159e54f8d32b5f6856dae71e702",
 "main.dart.js_2.part.js": "963f3eab85f94647e536aefe19adc173",
-"main.dart.js": "d0aebafb53281e7eea7567481cd6b4fc",
+"main.dart.js": "df3c24879647dc91397b0c0b21a46162",
 "login-callback.html": "e17f83d4982ea3cce6e692b31c4b4beb",
 "main.dart.js_1.part.js": "01ec4a3954f77f5b6d643e9f5e887cf2",
 "logout-callback.html": "999326dc9a0f37c9a9a4280b4a35dc51",
