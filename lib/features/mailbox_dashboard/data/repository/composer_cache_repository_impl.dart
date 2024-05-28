7e354aceff1acd75159ab5d7041b785dc0bee591
dab246 <tdvu@linagora.com>
2024-05-29 01:31:47 +0700

    TF-2143 Implement store & get composed email in datasource

--- lib/features/mailbox_dashboard/data/repository/composer_cache_repository_impl.dart
+++ lib/features/mailbox_dashboard/data/repository/composer_cache_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/data/datasource/local_storage_browser_datasource.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/data/datasource/session_storage_composer_datasource.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/data/model/composer_cache.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/repository/composer_cache_repository.dart';
@@ -6,8 +7,12 @@ import 'package:tmail_ui_user/features/mailbox_dashboard/domain/repository/compo
 class ComposerCacheRepositoryImpl extends ComposerCacheRepository {
 
   final SessionStorageComposerDatasource composerCacheDataSource;
+  final LocalStorageBrowserDatasource _localStorageBrowserDatasource;
 
-  ComposerCacheRepositoryImpl(this.composerCacheDataSource);
+  ComposerCacheRepositoryImpl(
+    this.composerCacheDataSource,
+    this._localStorageBrowserDatasource,
+  );
 
   @override
   ComposerCache getComposerCacheOnWeb() {
@@ -22,5 +27,15 @@ class ComposerCacheRepositoryImpl extends ComposerCacheRepository {
   @override
   void saveComposerCacheOnWeb(Email email) {
     return composerCacheDataSource.saveComposerCacheOnWeb(email);
+  }
+
+  @override
+  Future<Email> getComposedEmailFromLocalStorageBrowser() {
+    return _localStorageBrowserDatasource.getComposedEmail();
+  }
+
+  @override
+  Future<void> storeComposedEmailToLocalStorageBrowser(Email email) {
+    return _localStorageBrowserDatasource.storeComposedEmail(email);
   }
 }
