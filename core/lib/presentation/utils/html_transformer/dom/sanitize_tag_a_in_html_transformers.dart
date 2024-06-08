83294840dcada7431d9c449903873bb5e55b30da
dab246 <tdvu@linagora.com>
2024-06-09 02:58:02 +0700

    TF-2903 Handling of 3D links

---
+++ core/lib/presentation/utils/html_transformer/dom/sanitize_tag_a_in_html_transformers.dart
@@ -0,0 +1,61 @@
+import 'package:core/data/network/dio_client.dart';
+import 'package:core/presentation/utils/html_transformer/base/dom_transformer.dart';
+import 'package:core/presentation/utils/html_transformer/sanitize_url.dart';
+import 'package:core/utils/html/html_template.dart';
+import 'package:html/dom.dart';
+
+class SanitizeTagAInHtmlTransformer extends DomTransformer {
+  final _sanitizeUrl = SanitizeUrl();
+  final bool useTooltip;
+  
+  SanitizeTagAInHtmlTransformer({this.useTooltip = false});
+
+  @override
+  Future<void> process({
+    required Document document,
+    required DioClient dioClient,
+    Map<String, String>? mapUrlDownloadCID,
+  }) async {
+    final elements = document.querySelectorAll('a');
+    await Future.wait(elements.map((element) async {
+      _addToolTipWhenHoverLink(element);
+      _addBlankForTargetProperty(element);
+      _addNoReferrerForRelProperty(element);
+    }));
+  }
+
+  void _addToolTipWhenHoverLink(Element element) {
+    final url = element.attributes['href'] ?? '';
+
+    final urlSanitized = _sanitizeUrl.process(url);
+    if (urlSanitized.isEmpty) {
+      return;
+    }
+
+    element.attributes['href'] = urlSanitized;
+
+    final text = element.text;
+    final children = element.children;
+    if (children.isEmpty && text.isNotEmpty) {
+      final innerHtml = element.innerHtml;
+      final tagClass = element.attributes['class'];
+      if (tagClass != null) {
+        element.attributes['class'] = '$tagClass ${HtmlTemplate.nameClassToolTip}';
+      } else {
+        element.attributes['class'] = HtmlTemplate.nameClassToolTip;
+      }
+      element.innerHtml = '$innerHtml <span class="tooltiptext">$url</span>';
+    }
+  }
+
+  void _addBlankForTargetProperty(Element element) {
+    element.attributes['target'] = '_blank';
+  }
+
+  void _addNoReferrerForRelProperty(Element element) {
+    final rel = element.attributes['rel'];
+    if (rel == null || (!rel.contains('noopener') && !rel.contains('noreferrer'))) {
+      element.attributes['rel'] = 'noreferrer';
+    }
+  }
+}
