82b51082fcb17e6128db3f54fdf603545806c4df
dab246 <tdvu@linagora.com>
2024-06-10 18:59:24 +0700

    TF-2919 Fix memory leak when attach files in composer

--- lib/features/composer/presentation/widgets/attachment_progress_loading_composer_widget.dart
+++ lib/features/composer/presentation/widgets/attachment_progress_loading_composer_widget.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:tmail_ui_user/features/composer/presentation/styles/attachment_item_composer_widget_style.dart';
 import 'package:tmail_ui_user/features/composer/presentation/styles/attachment_progress_loading_composer_widget_style.dart';
-import 'package:tmail_ui_user/features/upload/presentation/model/upload_file_state.dart';
 import 'package:tmail_ui_user/features/upload/presentation/model/upload_file_status.dart';
 
 class AttachmentProgressLoadingComposerWidget extends StatelessWidget {
 
-  final UploadFileState fileState;
-  final EdgeInsetsGeometry? padding;
+  final UploadFileStatus uploadStatus;
+  final double percentUploading;
 
   const AttachmentProgressLoadingComposerWidget({
     super.key,
-    required this.fileState,
-    this.padding,
+    required this.uploadStatus,
+    required this.percentUploading,
   });
 
   @override
   Widget build(BuildContext context) {
-    switch (fileState.uploadStatus) {
+    switch (uploadStatus) {
       case UploadFileStatus.waiting:
-        return Padding(
-          padding: padding ?? EdgeInsets.zero,
-          child: const LinearProgressIndicator(
+        return const Padding(
+          padding: AttachmentItemComposerWidgetStyle.progressLoadingPadding,
+          child: LinearProgressIndicator(
             color: AttachmentProgressLoadingComposerWidgetStyle.progressColor,
             minHeight: AttachmentProgressLoadingComposerWidgetStyle.height,
             backgroundColor: AttachmentProgressLoadingComposerWidgetStyle.backgroundColor,
@@ -29,13 +29,13 @@ class AttachmentProgressLoadingComposerWidget extends StatelessWidget {
         );
       case UploadFileStatus.uploading:
         return Padding(
-          padding: padding ?? EdgeInsets.zero,
+          padding: AttachmentItemComposerWidgetStyle.progressLoadingPadding,
           child: LinearPercentIndicator(
             padding: EdgeInsets.zero,
             lineHeight:AttachmentProgressLoadingComposerWidgetStyle.height,
-            percent: fileState.percentUploading > 1.0
+            percent: percentUploading > 1.0
               ? 1.0
-              : fileState.percentUploading,
+              : percentUploading,
             barRadius: const Radius.circular(AttachmentProgressLoadingComposerWidgetStyle.radius),
             backgroundColor: AttachmentProgressLoadingComposerWidgetStyle.backgroundColor,
             progressColor: AttachmentProgressLoadingComposerWidgetStyle.progressColor,
