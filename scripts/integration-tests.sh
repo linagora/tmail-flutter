3021ae7ec714800a4ba740a8d34910603fcbed40
Nguyen Thai <tknguyen@linagora.com>
2024-07-10 09:31:46 +0700

    Added local image build for integration test
    Added maven wrapper

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -d -p 2023:80 --name tmail-web -v "$(pwd)/env.file:/usr/share/nginx/html/assets/env.file" linagora/tmail-web
-mvn test
+docker run -d -p 2023:80 --name tmail-web -v "$(pwd)/env.file:/usr/share/nginx/html/assets/env.file" tmail-web:integration-test
+./mvnw test
