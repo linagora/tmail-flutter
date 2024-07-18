61cb519dee21a7774ee0930f653e53638fa1ada8
DatDang <tddang@linagora.com>
2024-07-18 11:08:17 +0700

    Update login scenario

--- tmail_selenium/src/main/java/com/tmail/robots/MailboxDashboardRobot.java
+++ tmail_selenium/src/main/java/com/tmail/robots/MailboxDashboardRobot.java
@@ -1,9 +1,5 @@
 package com.tmail.robots;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.regex.Pattern;
-
 import com.microsoft.playwright.Page;
 import com.tmail.base.CoreRobot;
 
@@ -11,16 +7,6 @@ public class MailboxDashboardRobot extends CoreRobot {
 
     public MailboxDashboardRobot(Page page) {
         super(page);
-    }
-
-    public void clickSentMailbox() {
-        page.getByText("Sent").waitFor();
-        page.waitForTimeout(1000);
-        page.getByText("Sent").click();
-    }
-
-    public void checkIfThereAreMoreThanOneEmailSentBy(String username) {
-        assertTrue(page.getByLabel(Pattern.compile(username)).all().size() > 1);
     }
 
     public void openComposer() {
