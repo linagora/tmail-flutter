bca76ad9d3a712427852fd30b4af0d32ba30b403
tddang-linagora <tddang-linagora@users.noreply.github.com>
2024-07-19 07:24:06 +0000

    deploy: 36843c1be330d2661c0ce802a7f78c3c9a58831a

--- 2993/flutter_bootstrap.js
+++ 2993/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2833770068"
+    serviceWorkerVersion: "3927327744"
   }
 });
