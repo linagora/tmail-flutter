778d522775c21cf463dc134aeef5f9494f5448fa
dab246 <tdvu@linagora.com>
2024-07-26 18:23:16 +0700

    TF-2949 Add Hide attendee button

---
+++ lib/features/email/presentation/widgets/calendar_event/hide_all_attendees_button_widget.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:tmail_ui_user/features/base/widget/material_text_button.dart';
+import 'package:tmail_ui_user/features/email/presentation/styles/see_all_attendees_button_widget_styles.dart';
+import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+
+class HideAllAttendeesButtonWidget extends StatelessWidget {
+
+  final VoidCallback onTap;
+
+  const HideAllAttendeesButtonWidget({
+    super.key,
+    required this.onTap
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+      transform: Matrix4.translationValues(SeeAllAttendeesButtonWidgetStyles.horizontalMargin, 0.0, 0.0),
+      child: MaterialTextButton(
+        label: AppLocalizations.of(context).hide,
+        onTap: onTap,
+        borderRadius: SeeAllAttendeesButtonWidgetStyles.borderRadius,
+        padding: const EdgeInsetsDirectional.symmetric(
+          horizontal: SeeAllAttendeesButtonWidgetStyles.horizontalPadding,
+          vertical: SeeAllAttendeesButtonWidgetStyles.verticalPadding
+        ),
+        customStyle: const TextStyle(
+          fontSize: SeeAllAttendeesButtonWidgetStyles.textSize,
+          color: SeeAllAttendeesButtonWidgetStyles.textColor
+        ),
+      ),
+    );
+  }
+}
