40fa3360d9db822995651582e8da3a2fe5652e89
dab246 <tdvu@linagora.com>
2024-08-05 12:40:26 +0700

    TF-2517 Fix rename/create a folder with special characters do not show error message

--- lib/features/destination_picker/presentation/destination_picker_controller.dart
+++ lib/features/destination_picker/presentation/destination_picker_controller.dart
@@ -30,6 +30,7 @@ import 'package:tmail_ui_user/features/mailbox/presentation/model/mailbox_node.d
 import 'package:tmail_ui_user/features/mailbox/presentation/model/mailbox_tree_builder.dart';
 import 'package:tmail_ui_user/features/mailbox_creator/domain/model/verification/duplicate_name_validator.dart';
 import 'package:tmail_ui_user/features/mailbox_creator/domain/model/verification/empty_name_validator.dart';
+import 'package:tmail_ui_user/features/mailbox_creator/domain/model/verification/special_character_validator.dart';
 import 'package:tmail_ui_user/features/mailbox_creator/domain/state/verify_name_view_state.dart';
 import 'package:tmail_ui_user/features/mailbox_creator/domain/usecases/verify_name_interactor.dart';
 import 'package:tmail_ui_user/features/mailbox_creator/presentation/extensions/validator_failure_extension.dart';
@@ -205,6 +206,7 @@ class DestinationPickerController extends BaseMailboxController {
       [
         EmptyNameValidator(),
         DuplicateNameValidator(listMailboxNameAsStringExist),
+        SpecialCharacterValidator()
       ]
     ).fold(
       (failure) {
--- lib/features/mailbox_creator/domain/extensions/name_validator_string_extension.dart
+++
@@ -1,6 +0,0 @@
-
-extension NameValidatorStringExtension on String {
-  bool hasSpecialCharactersInName() {
-    return  RegExp(r'(?=.*?[#?!@$%^&*)(=+}{:;?/|\\><.,`~])').hasMatch(this);
-  }
-}
