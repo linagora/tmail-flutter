36d225b7917a9110691467135c301e23cb0530e0
hoangdat <hoangdat@users.noreply.github.com>
2024-08-06 11:12:52 +0000

    deploy: ea191a3b39f9cba2c381d55a99c1a68cb1399992

--- 2993/flutter_bootstrap.js
+++ 2993/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1332745240"
+    serviceWorkerVersion: "1674618078"
   }
 });
--- 2993/flutter_service_worker.js
+++ 2993/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "35f68f2eb2c21ab56a991bf1fc23b412",
+const RESOURCES = {"flutter_bootstrap.js": "df910df49bb33d5916a8760616b806bc",
 "logout-callback.html": "999326dc9a0f37c9a9a4280b4a35dc51",
 "assets/assets/images/ic_download_attachment.svg": "b9d3a2f29b73c9a841cc8f716b66785c",
 "assets/assets/images/ic_toast_success_message.svg": "d9b3e17d7e8c451881bc70902368c222",
