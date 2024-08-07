0656fd93909bf4205f486409a2a90ae6c9ba708f
DatDang <tddang@linagora.com>
2024-08-07 11:17:59 +0700

    TF-3040 Create `public_asset` feature

---
+++ lib/features/public_asset/domain/repository/public_asset_repository.dart
@@ -0,0 +1,34 @@
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:jmap_dart_client/jmap/identities/identity.dart';
+import 'package:jmap_dart_client/jmap/mail/extensions/public_asset/public_asset.dart';
+
+abstract class PublicAssetRepository {
+  Future<List<PublicAsset>> getPublicAssetsFromIds(
+    Session session,
+    AccountId accountId,
+    {required List<Id> publicAssetIds}
+  );
+
+  Future<PublicAsset> createPublicAsset(
+    Session session,
+    AccountId accountId,
+    {
+      required Id blobId,
+      required IdentityId? identityId
+    }
+  );
+
+  Future<void> deletePublicAssets(
+    Session session,
+    AccountId accountId,
+    {required List<Id> publicAssetIds}
+  );
+
+  Future<void> updatePublicAssets(
+    Session session,
+    AccountId accountId,
+    {required List<PublicAsset> publicAssets}
+  );
+}
