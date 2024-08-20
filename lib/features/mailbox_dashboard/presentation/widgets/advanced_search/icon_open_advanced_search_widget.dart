6dd453f6c8780900b1cb4dd495db9709d1cacdd3
dab246 <tdvu@linagora.com>
2024-08-20 18:25:28 +0700

    Fix overlap suggestion view with advanced search view

--- lib/features/mailbox_dashboard/presentation/widgets/advanced_search/icon_open_advanced_search_widget.dart
+++ lib/features/mailbox_dashboard/presentation/widgets/advanced_search/icon_open_advanced_search_widget.dart
@@ -1,7 +1,6 @@
 import 'package:core/presentation/extensions/color_extension.dart';
 import 'package:core/presentation/resources/image_paths.dart';
 import 'package:core/presentation/views/button/icon_button_web.dart';
-import 'package:core/utils/app_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -38,7 +37,7 @@ class IconOpenAdvancedSearchWidget extends StatelessWidget {
                 width: 16,
                 height: 16),
             onTap: () {
-              log('IconOpenAdvancedSearchWidget::build(): clicked');
+              FocusScope.of(context).unfocus();
               advancedFilterController.initSearchFilterField(context);
               searchController.openAdvanceSearch();
             }),
