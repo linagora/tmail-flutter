ceb5e2369076c8c079cf22048c22ceb70b71752a
dab246 <tdvu@linagora.com>
2024-08-21 18:22:23 +0700

    Fix input field lost focus when clicked in Search Email View on Tablet/Mobile

--- lib/features/search/email/presentation/search_email_view.dart
+++ lib/features/search/email/presentation/search_email_view.dart
@@ -54,7 +54,9 @@ class SearchEmailView extends GetWidget<SearchEmailController>
 
     return Scaffold(
       body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
+        onTap: PlatformInfo.isWeb
+          ? null
+          : FocusScope.of(context).unfocus,
         child: Container(
           color: Colors.white,
           child: Column(children: [
