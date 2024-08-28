30525fff2b5649e31a32c21eaa8b2841964ce738
DatDang <tddang@linagora.com>
2024-08-28 16:51:05 +0700

    Update workflows with Flutter master branch
    Revert "fixup! Upgrade to Flutter master branch"
    
    This reverts commit 406ce6c3c49af1c8f4534bda090dbbc8c2a774af.

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -9,31 +9,31 @@ flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 ## Install necessary pods
 # cd ../ios
-# flutter pub get && pod install
+# flutter pub upgrade && pod install
 
 cd ../model
-flutter pub get && dart run build_runner build --delete-conflicting-outputs
+flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
 
 cd ../contact
-flutter pub get && dart run build_runner build --delete-conflicting-outputs
+flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
 
 cd ../forward
-flutter pub get && dart run build_runner build --delete-conflicting-outputs
+flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
 
 cd ../rule_filter
-flutter pub get && dart run build_runner build --delete-conflicting-outputs
+flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
 
 cd ../fcm
-flutter pub get && dart run build_runner build --delete-conflicting-outputs
+flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
 
 cd ../email_recovery
-flutter pub get && dart run build_runner build --delete-conflicting-outputs
+flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
 
 cd ../server_settings
-flutter pub get && dart run build_runner build --delete-conflicting-outputs
+flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
 
 cd ..
-flutter pub get \
+flutter pub upgrade \
     && dart run build_runner build --delete-conflicting-outputs \
     && dart run intl_generator:extract_to_arb --output-dir=./lib/l10n lib/main/localizations/app_localizations.dart \
     && dart run intl_generator:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/main/localizations/app_localizations.dart lib/l10n/intl*.arb
