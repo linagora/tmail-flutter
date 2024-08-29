ca902ccf056cbdb10b428e32dfff6f5e501ca3f3
DatDang <tddang@linagora.com>
2024-08-29 11:02:04 +0700

    fixup! Upgrade to Flutter master branch

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -5,35 +5,35 @@ set -e
 set -x
 
 cd core
-flutter pub upgrade
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 ## Install necessary pods
 # cd ../ios
-# flutter pub upgrade && pod install
+# flutter pub get && pod install
 
 cd ../model
-flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../contact
-flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../forward
-flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../rule_filter
-flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../fcm
-flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../email_recovery
-flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ../server_settings
-flutter pub upgrade && dart run build_runner build --delete-conflicting-outputs
+flutter pub get && dart run build_runner build --delete-conflicting-outputs
 
 cd ..
-flutter pub upgrade \
+flutter pub get \
     && dart run build_runner build --delete-conflicting-outputs \
     && dart run intl_generator:extract_to_arb --output-dir=./lib/l10n lib/main/localizations/app_localizations.dart \
     && dart run intl_generator:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/main/localizations/app_localizations.dart lib/l10n/intl*.arb
