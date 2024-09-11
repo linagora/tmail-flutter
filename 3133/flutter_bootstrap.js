6e49a776cd7f8840fb69a7f35559f0dcae63957a
tddang-linagora <tddang-linagora@users.noreply.github.com>
2024-09-11 08:14:45 +0000

    deploy: f8616fde51c0aab89f4b2657a58d685eab1a267a

--- 3133/flutter_bootstrap.js
+++ 3133/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "462847223"
+    serviceWorkerVersion: "3463478882"
   }
 });
--- 3133/flutter_service_worker.js
+++ 3133/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "96df7f80fafebdfe9c3244fe04db1081",
+const RESOURCES = {"main.dart.js": "a8ee74863a8dac65c5bd4d9ec3d8d66a",
 "index.html": "ac0c94bd2396c4848f6db67bd3f1b2a2",
 "/": "ac0c94bd2396c4848f6db67bd3f1b2a2",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
@@ -314,7 +314,7 @@ const RESOURCES = {"main.dart.js": "96df7f80fafebdfe9c3244fe04db1081",
 "assets/configurations/env.fcm": "97d6804936ef04db13fcc840787c67cd",
 "assets/AssetManifest.bin.json": "fcbad1d48d923a963e36d9e3de595d1e",
 "assets/AssetManifest.bin": "f8cbdcf825686bfc3edf8547e66908ae",
-"flutter_bootstrap.js": "4dcdfd8345507e1f877da0eb66668610",
+"flutter_bootstrap.js": "12b67bc523ea67bc41e106f2199c93db",
 "canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "canvaskit/chromium/canvaskit.wasm": "399e2344480862e2dfa26f12fa5891d7",
 "canvaskit/chromium/canvaskit.js": "901bb9e28fac643b7da75ecfd3339f3f",
