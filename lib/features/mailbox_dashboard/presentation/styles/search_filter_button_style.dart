e6eeabedee9defea2c14c3bba285ba018cb491dd
dab246 <tdvu@linagora.com>
2024-09-12 21:31:44 +0700

    TF-3002 [WEB] Add delete button in search filters

--- lib/features/mailbox_dashboard/presentation/styles/search_filter_button_style.dart
+++ lib/features/mailbox_dashboard/presentation/styles/search_filter_button_style.dart
@@ -5,9 +5,14 @@ class SearchFilterButtonStyle {
   static const double iconSize = 16;
   static const double deleteIconSize = 10;
   static const double spaceSize = 4;
-  static const double buttonSize = 35;
 
-  static const EdgeInsetsGeometry buttonPadding = EdgeInsets.symmetric(horizontal: 12, vertical: 8);
+  static EdgeInsetsGeometry getButtonPadding(bool isSelected) {
+    if (isSelected) {
+      return const EdgeInsetsDirectional.only(start: 12, end: 4, top: 4, bottom: 4);
+    } else {
+      return const EdgeInsets.symmetric(horizontal: 12, vertical: 8);
+    }
+  }
   static const EdgeInsetsGeometry elementPadding = EdgeInsetsDirectional.only(start: 8);
   static const BorderRadius borderRadius = BorderRadius.all(Radius.circular(10));
 
