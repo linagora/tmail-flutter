c40dbf11820bc9fa3fc7ea67c11d55527ebd2f2e
DatDang <tddang@linagora.com>
2024-09-16 15:28:42 +0700

    Add semantics to from/cc/bcc fields

--- lib/features/base/key_values/composer_key_values.dart
+++ lib/features/base/key_values/composer_key_values.dart
@@ -5,4 +5,13 @@ class ComposerKeyValues {
   static const String composerSubjectField = 'tmail_composer_subject_field';
   static const String composerContentField = 'tmail_composer_content_field';
   static const String composerSendButton = 'tmail_composer_send_button';
+  // Added on 16/09/2024
+  static const String openWebFromFieldButton = 'tmail_composer_open_web_from_field_button';
+  static const String openWebCcFieldButton = 'tmail_composer_open_web_cc_field_button';
+  static const String openWebBccFieldButton = 'tmail_composer_open_web_bcc_field_button';
+  static const String openMobileSecondaryFieldsButton = 'tmail_composer_open_mobile_secondary_fields_button'; // Mobile only has one button to open all above 3 fields
+  static const String openFromEmailSelector = 'tmail_composer_open_from_email_selector';
+  static const String fromEmailSelectorOption = 'tmail_composer_from_email_selector_option';
+  static const String composerCcField = 'tmail_composer_cc_field';
+  static const String composerBccField = 'tmail_composer_bcc_field';
 }
--- lib/features/composer/presentation/widgets/web/from_composer_drop_down_widget.dart
+++ lib/features/composer/presentation/widgets/web/from_composer_drop_down_widget.dart
@@ -8,6 +8,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jmap_dart_client/jmap/identities/identity.dart';
 import 'package:model/model.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
+import 'package:tmail_ui_user/features/base/key_values/composer_key_values.dart';
 import 'package:tmail_ui_user/features/composer/presentation/extensions/prefix_email_address_extension.dart';
 import 'package:tmail_ui_user/features/composer/presentation/styles/web/from_composer_drop_down_widget_style.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
@@ -63,126 +64,131 @@ class FromComposerDropDownWidget extends StatelessWidget {
             padding: FromComposerDropDownWidgetStyle.dropdownButtonPadding,
             child: DropdownButtonHideUnderline(
               child: PointerInterceptor(
-                child: DropdownButton2<Identity>(
-                  key: dropdownKey,
-                  isExpanded: false,
-                  items: items.map((item) => DropdownMenuItem<Identity>(
-                    value: item,
-                    child: Semantics(
-                      excludeSemantics: true,
-                      child: PointerInterceptor(
-                        child: Container(
-                          color: Colors.transparent,
-                          padding: FromComposerDropDownWidgetStyle.dropdownItemPadding,
-                          child: Row(
-                            children: [
-                              Container(
-                                width: FromComposerDropDownWidgetStyle.avatarSize,
-                                height: FromComposerDropDownWidgetStyle.avatarSize,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: AppColor.avatarColor,
-                                  border: Border.all(
-                                    color: AppColor.colorShadowBgContentEmail,
-                                    width: FromComposerDropDownWidgetStyle.avatarBorderWidth
-                                  )
+                child: Semantics(
+                  container: true,
+                  identifier: ComposerKeyValues.openFromEmailSelector,
+                  child: DropdownButton2<Identity>(
+                    key: dropdownKey,
+                    isExpanded: false,
+                    items: items.map((item) => DropdownMenuItem<Identity>(
+                      value: item,
+                      child: Semantics(
+                        excludeSemantics: true,
+                        identifier: ComposerKeyValues.fromEmailSelectorOption,
+                        child: PointerInterceptor(
+                          child: Container(
+                            color: Colors.transparent,
+                            padding: FromComposerDropDownWidgetStyle.dropdownItemPadding,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: FromComposerDropDownWidgetStyle.avatarSize,
+                                  height: FromComposerDropDownWidgetStyle.avatarSize,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: AppColor.avatarColor,
+                                    border: Border.all(
+                                      color: AppColor.colorShadowBgContentEmail,
+                                      width: FromComposerDropDownWidgetStyle.avatarBorderWidth
+                                    )
+                                  ),
+                                  child: Text(
+                                    item.name!.isNotEmpty
+                                      ? item.name!.firstLetterToUpperCase
+                                      : item.email!.firstLetterToUpperCase,
+                                    style: FromComposerDropDownWidgetStyle.avatarTextStyle,
+                                  ),
                                 ),
-                                child: Text(
-                                  item.name!.isNotEmpty
-                                    ? item.name!.firstLetterToUpperCase
-                                    : item.email!.firstLetterToUpperCase,
-                                  style: FromComposerDropDownWidgetStyle.avatarTextStyle,
-                                ),
-                              ),
-                              const SizedBox(width: FromComposerDropDownWidgetStyle.dropdownItemSpace),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    if (item.name!.isNotEmpty)
-                                      Text(
-                                        item.name!,
-                                        maxLines: 1,
-                                        softWrap: CommonTextStyle.defaultSoftWrap,
-                                        overflow: CommonTextStyle.defaultTextOverFlow,
-                                        style: FromComposerDropDownWidgetStyle.dropdownItemTitleTextStyle,
-                                      ),
-                                    if (item.email!.isNotEmpty)
-                                      Text(
-                                        item.email!,
-                                        maxLines: 1,
-                                        softWrap: CommonTextStyle.defaultSoftWrap,
-                                        overflow: CommonTextStyle.defaultTextOverFlow,
-                                        style: FromComposerDropDownWidgetStyle.dropdownItemSubTitleTextStyle,
-                                      )
-                                  ],
-                                ),
-                              )
-                            ],
+                                const SizedBox(width: FromComposerDropDownWidgetStyle.dropdownItemSpace),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      if (item.name!.isNotEmpty)
+                                        Text(
+                                          item.name!,
+                                          maxLines: 1,
+                                          softWrap: CommonTextStyle.defaultSoftWrap,
+                                          overflow: CommonTextStyle.defaultTextOverFlow,
+                                          style: FromComposerDropDownWidgetStyle.dropdownItemTitleTextStyle,
+                                        ),
+                                      if (item.email!.isNotEmpty)
+                                        Text(
+                                          item.email!,
+                                          maxLines: 1,
+                                          softWrap: CommonTextStyle.defaultSoftWrap,
+                                          overflow: CommonTextStyle.defaultTextOverFlow,
+                                          style: FromComposerDropDownWidgetStyle.dropdownItemSubTitleTextStyle,
+                                        )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  )).toList(),
-                  value: itemSelected,
-                  buttonStyleData: FromComposerDropDownWidgetStyle.buttonStyleData,
-                  dropdownSearchData: DropdownSearchData(
-                    searchInnerWidget: Container(
-                      padding: FromComposerDropDownWidgetStyle.dropdownTopBarPadding,
-                      child: Text(
-                        AppLocalizations.of(context).yourIdentities,
-                        style: FromComposerDropDownWidgetStyle.dropdownTitleTextStyle,
+                    )).toList(),
+                    value: itemSelected,
+                    buttonStyleData: FromComposerDropDownWidgetStyle.buttonStyleData,
+                    dropdownSearchData: DropdownSearchData(
+                      searchInnerWidget: Container(
+                        padding: FromComposerDropDownWidgetStyle.dropdownTopBarPadding,
+                        child: Text(
+                          AppLocalizations.of(context).yourIdentities,
+                          style: FromComposerDropDownWidgetStyle.dropdownTitleTextStyle,
+                        ),
                       ),
+                      searchInnerWidgetHeight: FromComposerDropDownWidgetStyle.dropdownTopBarHeight,
                     ),
-                    searchInnerWidgetHeight: FromComposerDropDownWidgetStyle.dropdownTopBarHeight,
-                  ),
-                  dropdownStyleData: FromComposerDropDownWidgetStyle.dropdownStyleData,
-                  iconStyleData: IconStyleData(
-                    icon: SvgPicture.asset(imagePaths.icDropDown),
-                  ),
-                  menuItemStyleData: FromComposerDropDownWidgetStyle.menuIemStyleData,
-                  customButton: Tooltip(
-                    message: itemSelected != null ? itemSelected!.email : '',
-                    child: Container(
-                      height: FromComposerDropDownWidgetStyle.buttonHeight,
-                      width: FromComposerDropDownWidgetStyle.buttonWidth,
-                      padding: FromComposerDropDownWidgetStyle.buttonPadding,
-                      decoration: FromComposerDropDownWidgetStyle.buttonDecoration,
-                      child: Row(
-                        children: [
-                          if (itemSelected != null)
-                            Expanded(
-                              child: RichText(
-                                maxLines: 1,
-                                softWrap: CommonTextStyle.defaultSoftWrap,
-                                overflow: CommonTextStyle.defaultTextOverFlow,
-                                text: TextSpan(
-                                  children: [
-                                    if (itemSelected!.name!.isNotEmpty)
+                    dropdownStyleData: FromComposerDropDownWidgetStyle.dropdownStyleData,
+                    iconStyleData: IconStyleData(
+                      icon: SvgPicture.asset(imagePaths.icDropDown),
+                    ),
+                    menuItemStyleData: FromComposerDropDownWidgetStyle.menuIemStyleData,
+                    customButton: Tooltip(
+                      message: itemSelected != null ? itemSelected!.email : '',
+                      child: Container(
+                        height: FromComposerDropDownWidgetStyle.buttonHeight,
+                        width: FromComposerDropDownWidgetStyle.buttonWidth,
+                        padding: FromComposerDropDownWidgetStyle.buttonPadding,
+                        decoration: FromComposerDropDownWidgetStyle.buttonDecoration,
+                        child: Row(
+                          children: [
+                            if (itemSelected != null)
+                              Expanded(
+                                child: RichText(
+                                  maxLines: 1,
+                                  softWrap: CommonTextStyle.defaultSoftWrap,
+                                  overflow: CommonTextStyle.defaultTextOverFlow,
+                                  text: TextSpan(
+                                    children: [
+                                      if (itemSelected!.name!.isNotEmpty)
+                                        TextSpan(
+                                          text: '${itemSelected!.name} ',
+                                          style: FromComposerDropDownWidgetStyle.dropdownButtonTitleTextStyle,
+                                        ),
                                       TextSpan(
-                                        text: '${itemSelected!.name} ',
-                                        style: FromComposerDropDownWidgetStyle.dropdownButtonTitleTextStyle,
-                                      ),
-                                    TextSpan(
-                                      text: '(${itemSelected!.email})',
-                                      style: itemSelected!.name!.isNotEmpty
-                                        ? FromComposerDropDownWidgetStyle.dropdownButtonSubTitleTextStyle
-                                        : FromComposerDropDownWidgetStyle.dropdownButtonTitleTextStyle
-                                    )
-                                  ]
-                                ),
+                                        text: '(${itemSelected!.email})',
+                                        style: itemSelected!.name!.isNotEmpty
+                                          ? FromComposerDropDownWidgetStyle.dropdownButtonSubTitleTextStyle
+                                          : FromComposerDropDownWidgetStyle.dropdownButtonTitleTextStyle
+                                      )
+                                    ]
+                                  ),
+                                )
                               )
-                            )
-                          else
-                            const SizedBox.shrink(),
-                          SvgPicture.asset(imagePaths.icDropDown)
-                        ],
+                            else
+                              const SizedBox.shrink(),
+                            SvgPicture.asset(imagePaths.icDropDown)
+                          ],
+                        ),
                       ),
                     ),
+                    onChanged: onChangeIdentity,
                   ),
-                  onChanged: onChangeIdentity,
                 ),
               ),
             ),
