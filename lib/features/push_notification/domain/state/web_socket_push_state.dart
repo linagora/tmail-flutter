718d1c261810e71e27434ee8a2f5fdcef2fd5093
DatDang <tddang@linagora.com>
2024-09-23 13:38:45 +0700

    TF-3157 Implement web socket push

---
+++ lib/features/push_notification/domain/state/web_socket_push_state.dart
@@ -0,0 +1,19 @@
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:jmap_dart_client/jmap/push/state_change.dart';
+
+class InitializingWebSocketPushChannel extends LoadingState {}
+
+class WebSocketPushStateReceived extends UIState {
+  final StateChange stateChange;
+
+  WebSocketPushStateReceived(this.stateChange);
+
+  @override
+  List<Object> get props => [stateChange];
+}
+
+class WebSocketConnectionFailed extends FeatureFailure {
+
+  WebSocketConnectionFailed({super.exception});
+}
---
+++ lib/features/push_notification/presentation/controller/push_base_controller.dart
@@ -0,0 +1,109 @@
+import 'package:collection/collection.dart';
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:core/utils/app_logger.dart';
+import 'package:dartz/dartz.dart';
+import 'package:fcm/model/type_name.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:jmap_dart_client/jmap/core/state.dart' as jmap;
+import 'package:jmap_dart_client/jmap/core/user_name.dart';
+import 'package:tmail_ui_user/features/base/action/ui_action.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/action/push_notification_state_change_action.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/listener/email_change_listener.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/listener/mailbox_change_listener.dart';
+
+abstract class PushBaseController {
+  Session? session;
+  AccountId? accountId;
+
+  void consumeState(Stream<Either<Failure, Success>> newStateStream) {
+    newStateStream.listen(
+      _handleStateStream,
+      onError: (error, stackTrace) {
+        logError('PushBaseController::consumeState():onError:error: $error | stackTrace: $stackTrace');
+      }
+    );
+  }
+
+  void _handleStateStream(Either<Failure, Success> newState) {
+    newState.fold(handleFailureViewState, handleSuccessViewState);
+  }
+
+  void handleFailureViewState(Failure failure);
+
+  void handleSuccessViewState(Success success);
+
+  void initialize({AccountId? accountId, Session? session}) {
+    this.accountId = accountId;
+    this.session = session;
+  }
+
+  void mappingTypeStateToAction(
+    Map<String, dynamic> mapTypeState,
+    AccountId accountId,
+    UserName userName, {
+    bool isForeground = true,
+    Session? session,
+    required EmailChangeListener emailChangeListener,
+    required MailboxChangeListener mailboxChangeListener
+  }) {
+    log('PushBaseController::mappingTypeStateToAction():mapTypeState: $mapTypeState');
+    final listTypeName = mapTypeState.keys
+      .map((value) => TypeName(value))
+      .toList();
+
+    final listEmailActions = listTypeName
+      .where((typeName) => typeName == TypeName.emailType || typeName == TypeName.emailDelivery)
+      .map((typeName) => _toPushNotificationAction(typeName, accountId, userName, mapTypeState, isForeground, session: session))
+      .whereNotNull()
+      .toList();
+
+    log('PushBaseController::mappingTypeStateToAction():listEmailActions: $listEmailActions');
+
+    if (listEmailActions.isNotEmpty) {
+       emailChangeListener.dispatchActions(listEmailActions);
+    }
+
+    final listMailboxActions = listTypeName
+      .where((typeName) => typeName == TypeName.mailboxType)
+      .map((typeName) => _toPushNotificationAction(typeName, accountId, userName, mapTypeState, isForeground))
+      .whereNotNull()
+      .toList();
+
+    log('PushBaseController::mappingTypeStateToAction():listMailboxActions: $listEmailActions');
+
+    if (listMailboxActions.isNotEmpty) {
+      mailboxChangeListener.dispatchActions(listMailboxActions);
+    }
+  }
+
+  PushNotificationStateChangeAction? _toPushNotificationAction(
+    TypeName typeName,
+    AccountId accountId,
+    UserName userName,
+    Map<String, dynamic> mapTypeState,
+    isForeground,
+    {Session? session}
+  ) {
+    final newState = jmap.State(mapTypeState[typeName.value]);
+    if (typeName == TypeName.emailType) {
+      if (isForeground) {
+        return SynchronizeEmailOnForegroundAction(typeName, newState, accountId, session);
+      } else {
+        return StoreEmailStateToRefreshAction(typeName, newState, accountId, userName, session);
+      }
+    } else if (typeName == TypeName.emailDelivery) {
+      if (!isForeground) {
+        return PushNotificationAction(typeName, newState, session, accountId, userName);
+      }
+    } else if (typeName == TypeName.mailboxType) {
+      if (isForeground) {
+        return SynchronizeMailboxOnForegroundAction(typeName, newState, accountId);
+      } else {
+        return StoreMailboxStateToRefreshAction(typeName, newState, accountId, userName);
+      }
+    }
+    return null;
+  }
+}
