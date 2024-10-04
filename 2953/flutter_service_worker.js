6d458a061a1526c704c57f8ed86197adfb18ca8a
tddang-linagora <tddang-linagora@users.noreply.github.com>
2024-10-04 09:14:15 +0000

    deploy: 0533bbeaf3e785f8a250df3e15fbbfcf8eb81dde

--- 2953/flutter_service_worker.js
+++ 2953/flutter_service_worker.js
@@ -302,7 +302,7 @@ const RESOURCES = {"assets/AssetManifest.json": "9215256a67347739bbe0034f72ab8c8
 "icons/Icon-192.png": "009a8eae32218365ab20cce81aa896c3",
 "icons/Icon-maskable-512.png": "3a55e614a03964225fd5b8ca94384524",
 "icons/Icon-maskable-192.png": "009a8eae32218365ab20cce81aa896c3",
-"flutter_bootstrap.js": "ea5e477f2414ec3a18fd65677637e965",
+"flutter_bootstrap.js": "6e05c2d08544146518e3b073eb312ed0",
 "main.dart.js_1.part.js": "66a4ad9ef6102c0b49f49d16bce4664c",
 "firebase-messaging-sw.js": "ed5b30ced7e7646f08edc16e2d5d5ad5",
 "logout-callback.html": "999326dc9a0f37c9a9a4280b4a35dc51",
