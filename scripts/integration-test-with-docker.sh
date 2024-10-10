e51894e5e418023a50bcc6c09eb71ed598626d5a
Nguyen Thai <tknguyen@linagora.com>
2024-10-10 15:44:23 +0700

    Use ngrok for integration tests

--- scripts/integration-test-with-docker.sh
+++ scripts/integration-test-with-docker.sh
@@ -2,23 +2,30 @@
 
 set -eux
 
+# Install ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null &&
+    echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list &&
+    sudo apt update && sudo apt install ngrok
+
 cd backend-docker
 
+# Generate keys for tmail backend
 openssl genpkey -algorithm rsa -pkeyopt rsa_keygen_bits:4096 -out jwt_privatekey
 openssl rsa -in jwt_privatekey -pubout -out jwt_publickey
 
 docker compose up -d
+# Wait till the service is started to add users
 until (docker compose logs tmail-backend | grep -i "JAMES server started"); do
     echo "Waiting for tmail-backend to start..."
     sleep 2
 done
-
 export TEST_USER22_NAME="testuser22"
 export TEST_USER22_PASSWORD="testuser22"
 export TEST_USER21_NAME="testuser21"
 export TEST_USER21_PASSWORD="testuser21"
 docker exec -it tmail-backend james-cli AddUser $TEST_USER22_NAME@lin-saas.dev $TEST_USER22_PASSWORD
 docker exec -it tmail-backend james-cli AddUser $TEST_USER21_NAME@lin-saas.dev $TEST_USER21_PASSWORD
+
 cd ..
 
 ngrok http http://localhost:80 --log=stdout >/dev/null &
@@ -28,7 +35,6 @@ until [[ $(curl localhost:4040/api/status | jq -r ".status") == "online" ]]; do
 done
 
 export BASIC_AUTH_URL=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
-# adb reverse tcp:80 tcp:80
 patrol test \
     --dart-define=USERNAME="$TEST_USER22_NAME" \
     --dart-define=PASSWORD="$TEST_USER22_PASSWORD" \
@@ -36,6 +42,8 @@ patrol test \
     --dart-define=ADDITIONAL_MAIL_RECIPIENT="$TEST_USER21_NAME@lin-saas.dev" \
     --dart-define=BASIC_AUTH_EMAIL="$TEST_USER22_NAME@lin-saas.dev" \
     --dart-define=BASIC_AUTH_URL="$BASIC_AUTH_URL" || true
+
+# Cleanup
 killall ngrok
 
 cd backend-docker
