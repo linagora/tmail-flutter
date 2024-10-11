0614603e7bc4474b68fdf359083dd6a9382cabe3
tk-nguyen <tk-nguyen@users.noreply.github.com>
2024-10-11 03:47:19 +0000

    deploy: 2dbc3415f3f1232f7822b080e064c7a2a2175aea

--- 2953/flutter_bootstrap.js
+++ 2953/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3147331154"
+    serviceWorkerVersion: "58061398"
   }
 });
