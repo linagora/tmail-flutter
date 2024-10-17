9b5d96d4079c95724beee402ba170ecaa148016c
dab246 <tdvu@linagora.com>
2024-10-17 15:55:06 +0700

    TF-2387 Handle sign in SAAS platform

--- lib/features/starting_page/presentation/twake_welcome/twake_welcome_bindings.dart
+++ lib/features/starting_page/presentation/twake_welcome/twake_welcome_bindings.dart
@@ -1,9 +1,60 @@
 import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/base/base_bindings.dart';
+import 'package:tmail_ui_user/features/login/data/network/authentication_client/authentication_client_base.dart';
+import 'package:tmail_ui_user/features/login/domain/repository/account_repository.dart';
+import 'package:tmail_ui_user/features/login/domain/repository/authentication_oidc_repository.dart';
+import 'package:tmail_ui_user/features/login/domain/repository/credential_repository.dart';
+import 'package:tmail_ui_user/features/starting_page/data/datasource/saas_authentication_datasource.dart';
+import 'package:tmail_ui_user/features/starting_page/data/datasource_impl/saas_authentication_datasource_impl.dart';
+import 'package:tmail_ui_user/features/starting_page/data/repository/saas_authentication_repository_impl.dart';
+import 'package:tmail_ui_user/features/starting_page/domain/repository/saas_authentication_repository.dart';
+import 'package:tmail_ui_user/features/starting_page/domain/usecase/sign_in_saas_interactor.dart';
 import 'package:tmail_ui_user/features/starting_page/presentation/twake_welcome/twake_welcome_controller.dart';
+import 'package:tmail_ui_user/main/exceptions/remote_exception_thrower.dart';
 
-class TwakeWelcomeBindings extends Bindings {
+class TwakeWelcomeBindings extends BaseBindings {
+
   @override
-  void dependencies() {
-    Get.lazyPut(() => TwakeWelcomeController());
+  void bindingsController() {
+    Get.lazyPut(() => TwakeWelcomeController(
+      Get.find<SignInSaasInteractor>(),
+    ));
+  }
+
+  @override
+  void bindingsInteractor() {
+    Get.lazyPut(() => SignInSaasInteractor(
+      Get.find<SaasAuthenticationRepository>(),
+      Get.find<AuthenticationOIDCRepository>(),
+      Get.find<AccountRepository>(),
+      Get.find<CredentialRepository>(),
+    ));
+  }
+
+  @override
+  void bindingsDataSourceImpl() {
+    Get.lazyPut(() => SaasAuthenticationDataSourceImpl(
+      Get.find<AuthenticationClientBase>(),
+      Get.find<RemoteExceptionThrower>(),
+    ));
+  }
+
+  @override
+  void bindingsDataSource() {
+    Get.lazyPut<SaasAuthenticationDataSource>(
+      () => Get.find<SaasAuthenticationDataSourceImpl>());
+  }
+
+  @override
+  void bindingsRepository() {
+    Get.lazyPut<SaasAuthenticationRepository>(
+      () => Get.find<SaasAuthenticationRepositoryImpl>());
+  }
+
+  @override
+  void bindingsRepositoryImpl() {
+    Get.lazyPut(() => SaasAuthenticationRepositoryImpl(
+      Get.find<SaasAuthenticationDataSource>(),
+    ));
   }
 }
