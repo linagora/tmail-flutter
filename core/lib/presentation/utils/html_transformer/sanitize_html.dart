342f5e190dad279da505aa7acd6888f98270fd9a
dab246 <tdvu@linagora.com>
2024-10-24 10:53:46 +0700

    fixup! fixup! fixup! fixup! Standardize HTML sanitizing when preview email

--- core/lib/presentation/utils/html_transformer/sanitize_html.dart
+++ core/lib/presentation/utils/html_transformer/sanitize_html.dart
@@ -1,20 +1,16 @@
 import 'package:sanitize_html/sanitize_html.dart';
 
-import '../../../utils/app_logger.dart';
-
 class SanitizeHtml {
   String process({
     required String inputHtml,
     List<String>? allowAttributes,
     List<String>? allowTags,
   }) {
-    log('SanitizeHtml::process:inputHtml = $inputHtml');
     final outputHtml = sanitizeHtml(
       inputHtml,
       allowAttributes: allowAttributes,
       allowTags: allowTags,
     );
-    log('SanitizeHtml::process:outputHtml = $outputHtml');
     return outputHtml;
   }
 }
