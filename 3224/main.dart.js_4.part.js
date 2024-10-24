f71d97b451fae6bfc627d0c53efaa48e2cf4c065
tddang-linagora <tddang-linagora@users.noreply.github.com>
2024-10-24 02:53:25 +0000

    deploy: 5bfbea9f081155f23365bd93d4f3c5cacf46a7cf

--- 3224/main.dart.js_4.part.js
+++ 3224/main.dart.js_4.part.js
@@ -2274,7 +2274,7 @@ $__dart_deferred_initializers__.current = function(hunkHelpers, init, holdersLis
     CalendarEventExtension_get_isDisplayedEventReplyAction(_this) {
       var t1 = _this.method;
       if (t1 != null)
-        if (t1 !== C.EventMethod_2)
+        if (t1 === C.EventMethod_1 || t1 === C.EventMethod_3 || t1 === C.EventMethod_6)
           if (_this.organizer != null) {
             t1 = _this.participants;
             t1 = (t1 == null ? null : t1.length !== 0) === true;
@@ -21554,5 +21554,5 @@ $__dart_deferred_initializers__.current = function(hunkHelpers, init, holdersLis
 ((d, h) => {
   d[h] = d.current;
   d.eventLog.push({p: "main.dart.js_4", e: "endPart", h: h});
-})($__dart_deferred_initializers__, "lMoauR/bupa5yF4tMDHhMmtImJ4=");
+})($__dart_deferred_initializers__, "1hVYAOlSfGwFYR7W59mDGoixgWc=");
 ;
