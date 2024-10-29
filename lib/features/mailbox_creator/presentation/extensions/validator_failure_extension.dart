bb3c28be72279383116401ade171e2d3855580bc
dab246 <tdvu@linagora.com>
2024-10-29 16:16:28 +0700

    TF-3178 Add verify name only contain spaces for rule filter name

--- lib/features/mailbox_creator/presentation/extensions/validator_failure_extension.dart
+++ lib/features/mailbox_creator/presentation/extensions/validator_failure_extension.dart
@@ -21,7 +21,7 @@ extension ValicatorFailureExtension on VerifyNameFailure {
     } else if (exception is SpecialCharacterException) {
       return AppLocalizations.of(context).folderNameCannotContainSpecialCharacters;
     } else if (exception is NameWithSpaceOnlyException) {
-      return AppLocalizations.of(context).this_field_cannot_be_blank;
+      return AppLocalizations.of(context).thisFieldCannotContainOnlySpaces;
     } else {
       return '';
     }
@@ -30,6 +30,8 @@ extension ValicatorFailureExtension on VerifyNameFailure {
   String getMessageIdentity(BuildContext context) {
     if (exception is EmptyNameException) {
       return AppLocalizations.of(context).this_field_cannot_be_blank;
+    } else if (exception is NameWithSpaceOnlyException) {
+      return AppLocalizations.of(context).thisFieldCannotContainOnlySpaces;
     } else if (exception is EmailAddressInvalidException) {
       return AppLocalizations.of(context).thisEmailAddressInvalid;
     } else {
@@ -40,14 +42,8 @@ extension ValicatorFailureExtension on VerifyNameFailure {
   String getMessageRulesFilter(BuildContext context) {
     if (exception is EmptyNameException) {
       return AppLocalizations.of(context).this_field_cannot_be_blank;
-    } else {
-      return '';
-    }
-  }
-
-  String getMessageVacation(BuildContext context) {
-    if (exception is EmptyNameException) {
-      return AppLocalizations.of(context).messageIsRequired;
+    } else if (exception is NameWithSpaceOnlyException) {
+      return AppLocalizations.of(context).thisFieldCannotContainOnlySpaces;
     } else {
       return '';
     }
