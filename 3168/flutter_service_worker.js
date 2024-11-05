813d487d08092333b1a924624d47c0662376038b
tddang-linagora <tddang-linagora@users.noreply.github.com>
2024-11-05 02:48:12 +0000

    deploy: a56cdecc2dac967d696f3bd197afa616a5f97985

--- 3168/flutter_service_worker.js
+++ 3168/flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {"login-callback.html": "e17f83d4982ea3cce6e692b31c4b4beb",
 "i18n/translater.js": "f53aedbd356b679a27a6b0e8e952fb81",
 "i18n/de.json": "51dbc51266ea792eac5f1c8abd8d7b9e",
 "i18n/fr.json": "7617ec5651456d93357805d0b56aa429",
-"main.dart.js": "4e8cac8ef141f3cf69bc32650b9a3092",
+"main.dart.js": "9cdd16d33d9dd4d595524cece439e7ec",
 "main.dart.js_7.part.js": "8929cf7d7e53ad4cc13368b43e075d6d",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "splash/img/light-3x.png": "94074d46280ea96987176264b73677a3",
@@ -350,7 +350,7 @@ const RESOURCES = {"login-callback.html": "e17f83d4982ea3cce6e692b31c4b4beb",
 "icons/Icon-maskable-512.png": "3a55e614a03964225fd5b8ca94384524",
 "favicon.svg": "1734d2c8d930abd57ebba9c158506714",
 "version.json": "e7dfb4a601d4cd0f0774d800aafc9cac",
-"flutter_bootstrap.js": "996f9867c0b8f112b2062683fcf79062"};
+"flutter_bootstrap.js": "ac5b3202fd19a6523d9be16b5054727d"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
