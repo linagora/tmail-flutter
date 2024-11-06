7c90f6d1f4cddf333325ec7b08b1d5bc0184ce42
tddang-linagora <tddang-linagora@users.noreply.github.com>
2024-11-06 02:23:48 +0000

    deploy: 153f34a127bb41fc12f33c2616307051564ce877

--- 3168/flutter_service_worker.js
+++ 3168/flutter_service_worker.js
@@ -349,7 +349,7 @@ const RESOURCES = {"login-callback.html": "e17f83d4982ea3cce6e692b31c4b4beb",
 "icons/Icon-maskable-512.png": "3a55e614a03964225fd5b8ca94384524",
 "favicon.svg": "1734d2c8d930abd57ebba9c158506714",
 "version.json": "e7dfb4a601d4cd0f0774d800aafc9cac",
-"flutter_bootstrap.js": "174152f8632944d22a3e8f3b9cf1afc8"};
+"flutter_bootstrap.js": "b3ad2bf865344307f1c367fca58589cb"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
