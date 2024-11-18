e2f498be5d490de5eb6f05e3b3035249c9265233
florentos17 <florentos17@users.noreply.github.com>
2024-11-18 12:28:24 +0000

    deploy: 34dd511dbbe64027e4a41be737a1cf51d430df3d

--- 3241/flutter_bootstrap.js
+++ 3241/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2782144190"
+    serviceWorkerVersion: "4042173996"
   }
 });
