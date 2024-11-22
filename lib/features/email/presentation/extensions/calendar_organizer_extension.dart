acaf8d4485808d9a1a724b34d8b1ac8a5025ed7b
dab246 <tdvu@linagora.com>
2024-11-23 03:52:01 +0700

    TF-3294 Fix BLUE-BAR mail to attendees duplicated recipients

---
+++ lib/features/email/presentation/extensions/calendar_organizer_extension.dart
@@ -0,0 +1,12 @@
+
+import 'package:jmap_dart_client/jmap/mail/calendar/properties/calendar_organizer.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email_address.dart';
+
+extension CalendarOrganizerExtension on CalendarOrganizer {
+  EmailAddress toEmailAddress() {
+    return EmailAddress(
+       name,
+       mailto?.value,
+    );
+  }
+}
