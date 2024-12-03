69c1730a68acbffd484d4d1b375f0eb767ba95e1
florentos17 <florentos17@users.noreply.github.com>
2024-12-03 10:12:35 +0000

    deploy: 4ad4b81859f25f4f25cb49e9a9c92190ec868230

--- 3241/flutter_service_worker.js
+++ 3241/flutter_service_worker.js
@@ -359,7 +359,7 @@ const RESOURCES = {"login-callback.html": "e17f83d4982ea3cce6e692b31c4b4beb",
 "icons/Icon-maskable-512.png": "3a55e614a03964225fd5b8ca94384524",
 "favicon.svg": "1734d2c8d930abd57ebba9c158506714",
 "version.json": "8bacc5025864b1487f10060b744fb0da",
-"flutter_bootstrap.js": "200b4ce531bbd231680affcd326ece48"};
+"flutter_bootstrap.js": "70ed3031410e8c8f35f46cec943ab560"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
