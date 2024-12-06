b9488ac9a836104957afbb59fc0912ca0c62daae
Florent Azavant <fazavant@linagora.com>
2024-12-06 10:16:40 +0100

    TF-3219 fixups for  and more tests for it

--- lib/features/email_recovery/presentation/model/session_extension.dart
+++ lib/features/email_recovery/presentation/model/session_extension.dart
@@ -1,34 +1,39 @@
 
 import 'package:duration/duration.dart';
 import 'package:email_recovery/email_recovery/capability_deleted_messages_vault.dart';
-import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
-import 'package:jmap_dart_client/jmap/core/capability/capability_properties.dart';
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:model/extensions/session_extension.dart';
 
 extension SessionExtension on Session {
 
   String getRestorationHorizonAsString() {
-    return ((props[0] as Map<CapabilityIdentifier, CapabilityProperties>?)
-        ?[capabilityDeletedMessagesVault]
+    String restorationHorizon = 'restorationHorizon';
+    String defaultHorizon = "15 days";
+
+    return (getCapabilityProperties(accountId, capabilityDeletedMessagesVault)
         ?.props[0] as Map<String, dynamic>?)
-        ?['restorationHorizon']
-        ?? "15 days";
+        ?[restorationHorizon]
+        ?? defaultHorizon;
   }
 
   Duration getRestorationHorizonAsDuration() {
-    String horizonWithCorrectFormat = getRestorationHorizonAsString()
-        .replaceAll(" days", "d")
-        .replaceAll(" day", "d")
-        .replaceAll(" hours", "h")
-        .replaceAll(" hour", "h")
-        .replaceAll(" minutes", "m")
-        .replaceAll(" minute", "m")
-        .replaceAll(" seconds", "s")
-        .replaceAll(" second", "s")
-        .replaceAll(" milliseconds", "ms")
-        .replaceAll(" millisecond", "ms");
+    try {
+      String horizonWithCorrectFormat = getRestorationHorizonAsString()
+          .replaceAll(" days", "d")
+          .replaceAll(" day", "d")
+          .replaceAll(" hours", "h")
+          .replaceAll(" hour", "h")
+          .replaceAll(" minutes", "m")
+          .replaceAll(" minute", "m")
+          .replaceAll(" seconds", "s")
+          .replaceAll(" second", "s")
+          .replaceAll(" milliseconds", "ms")
+          .replaceAll(" millisecond", "ms");
 
-    return parseDuration(horizonWithCorrectFormat, separator: ' ');
+      return parseDuration(horizonWithCorrectFormat, separator: ' ');
+    } catch (e) {
+      return const Duration(days: 15);
+    }
   }
 
   DateTime getRestorationHorizonAsDateTime() {
