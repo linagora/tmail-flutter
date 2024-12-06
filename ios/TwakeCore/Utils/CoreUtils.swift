ed3ebe5b77d229c76c26c38c070fce8805c13274
Dat Dang <tddang@linagora.com>
2024-12-06 13:05:07 +0700

    TF-3275 Fix FCM iOS foreground desync (#3314)

--- ios/TwakeCore/Utils/CoreUtils.swift
+++ ios/TwakeCore/Utils/CoreUtils.swift
@@ -8,6 +8,9 @@ class CoreUtils {
     static let NOTIFICATION_INTERACTION_CHANNEL_NAME = "notification_interaction_channel"
     static let CURRENT_EMAIL_ID_IN_NOTIFICATION_CLICK_WHEN_APP_FOREGROUND_OR_BACKGROUND = "current_email_id_in_notification_click_when_app_foreground_or_background"
     static let CURRENT_EMAIL_ID_IN_NOTIFICATION_CLICK_WHEN_APP_TERMINATED = "current_email_id_in_notification_click_when_app_terminated"
+    static let FCM_METHOD_CHANNEL_NAME = "plugins.flutter.io/firebase_messaging"
+    static let FCM_ON_MESSAGE_METHOD_NAME = "Messaging#onMessage"
+    static let APPLICATION_STATE = "applicationState"
 
     func getCurrentDate() -> Date {
         if #available(iOS 15, *) {
