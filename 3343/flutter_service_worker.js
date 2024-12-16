aa24915c9ecb63f58ab2dd4143c81e6a13169717
tddang-linagora <tddang-linagora@users.noreply.github.com>
2024-12-16 09:42:56 +0000

    deploy: 0aa98a3d312bb140f45bc4484da2cb0c6f341105

--- 3343/flutter_service_worker.js
+++ 3343/flutter_service_worker.js
@@ -340,7 +340,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "009a8eae32218365ab20cce81aa89
 "login-callback.html": "e17f83d4982ea3cce6e692b31c4b4beb",
 "logout-callback.html": "999326dc9a0f37c9a9a4280b4a35dc51",
 "main.dart.js_7.part.js": "f359dbaebff9b45704bb8a2c17d15576",
-"flutter_bootstrap.js": "7a214962397e2de5bafc77058d2e90e4",
+"flutter_bootstrap.js": "baea36c54577baf550d47e28c512a21e",
 "worker_service/img/ic-close-1x.png": "27b9c71ff0cf463da5f1affb11b4c204",
 "worker_service/img/tmail-3x.png": "7b5f8e671c4f39ae5d1a67b46fe494d7",
 "worker_service/img/tmail-2x.png": "55f91555db37edb9ed0608f7ee3f37ee",
