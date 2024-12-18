0a5f0eec1447c01501cc874daf67877ed1a394d5
dab246 <tdvu@linagora.com>
2024-12-18 12:10:53 +0700

    TF-3349 Escape messages when print email

--- lib/features/email/domain/usecases/print_email_interactor.dart
+++ lib/features/email/domain/usecases/print_email_interactor.dart
@@ -1,7 +1,5 @@
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
-import 'package:core/presentation/utils/html_transformer/transform_configuration.dart';
-import 'package:core/utils/app_logger.dart';
 import 'package:dartz/dartz.dart';
 import 'package:tmail_ui_user/features/email/domain/model/email_print.dart';
 import 'package:tmail_ui_user/features/email/domain/repository/email_repository.dart';
@@ -14,24 +12,10 @@ class PrintEmailInteractor {
 
   Stream<Either<Failure, Success>> execute(EmailPrint emailPrint) async* {
     try {
-      final htmlContentTransformed = await _transformHtmlEmailContent(emailPrint.emailContent);
-      final newEmailPrint = emailPrint.fromEmailContent(htmlContentTransformed);
-      await emailRepository.printEmail(newEmailPrint);
+      await emailRepository.printEmail(emailPrint);
       yield Right(PrintEmailSuccess());
     } catch (e) {
       yield Left(PrintEmailFailure(exception: e));
-    }
-  }
-
-  Future<String> _transformHtmlEmailContent(String emailContent) async {
-    try {
-      final htmlContentTransformed = await emailRepository.transformHtmlEmailContent(
-        emailContent,
-        TransformConfiguration.forPrintEmail());
-      return htmlContentTransformed;
-    } catch (e) {
-      logError('PrintEmailInteractor::_transformHtmlEmailContent: Exception: $e');
-      return emailContent;
     }
   }
 }
