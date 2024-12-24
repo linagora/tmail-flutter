efd0816d8c7c84d9a6c13c787082c2abb40abf41
dab246 <tdvu@linagora.com>
2024-12-24 11:06:14 +0700

    TF-3337 Fix open the setting page (Vacation method was call twice)

--- lib/features/manage_account/presentation/vacation/vacation_controller_bindings.dart
+++ lib/features/manage_account/presentation/vacation/vacation_controller_bindings.dart
@@ -1,7 +1,6 @@
 import 'package:get/get.dart';
 import 'package:tmail_ui_user/features/composer/presentation/controller/rich_text_web_controller.dart';
 import 'package:tmail_ui_user/features/mailbox_creator/domain/usecases/verify_name_interactor.dart';
-import 'package:tmail_ui_user/features/manage_account/domain/usecases/get_all_vacation_interactor.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/usecases/update_vacation_interactor.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/vacation/utils/vacation_utils.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/vacation/vacation_controller.dart';
@@ -13,7 +12,6 @@ class VacationControllerBindings extends Bindings {
     Get.lazyPut(() => VerifyNameInteractor());
     Get.lazyPut(() => RichTextWebController(), tag: VacationUtils.vacationTagName);
     Get.put(VacationController(
-      Get.find<GetAllVacationInteractor>(),
       Get.find<UpdateVacationInteractor>(),
       Get.find<RichTextWebController>(tag: VacationUtils.vacationTagName),
     ));
