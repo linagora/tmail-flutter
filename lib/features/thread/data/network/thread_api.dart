2955cbbdd1a22a0ed8b88f0ca79bf6669b2ad98d
Dat PHAM HOANG <dphamhoang@linagora.com>
2024-12-27 14:35:43 +0700

    TF-3372 Add clean mailbox API to ThreadAPI

--- lib/features/thread/data/network/thread_api.dart
+++ lib/features/thread/data/network/thread_api.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:core/utils/app_logger.dart';
 import 'package:jmap_dart_client/http/http_client.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/error/set_error.dart';
 import 'package:jmap_dart_client/jmap/core/filter/filter.dart';
 import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
 import 'package:jmap_dart_client/jmap/core/request/reference_path.dart';
@@ -20,17 +21,22 @@ import 'package:jmap_dart_client/jmap/mail/email/get/get_email_method.dart';
 import 'package:jmap_dart_client/jmap/mail/email/get/get_email_response.dart';
 import 'package:jmap_dart_client/jmap/mail/email/query/query_email_method.dart';
 import 'package:jmap_dart_client/jmap/mail/email/query/query_email_response.dart';
-import 'package:model/extensions/list_email_extension.dart';
-import 'package:tmail_ui_user/features/thread/data/extensions/list_email_extension.dart';
 import 'package:jmap_dart_client/jmap/mail/email/search_snippet/search_snippet.dart';
 import 'package:jmap_dart_client/jmap/mail/email/search_snippet/search_snippet_get_method.dart';
 import 'package:jmap_dart_client/jmap/mail/email/search_snippet/search_snippet_get_response.dart';
+import 'package:jmap_dart_client/jmap/mail/email/set/set_email_method.dart';
+import 'package:jmap_dart_client/jmap/mail/email/set/set_email_response.dart';
+import 'package:model/error_type_handler/set_method_error_handler_mixin.dart';
+import 'package:model/extensions/list_email_extension.dart';
+import 'package:tmail_ui_user/features/base/mixin/handle_error_mixin.dart';
+import 'package:tmail_ui_user/features/thread/data/extensions/list_email_extension.dart';
 import 'package:tmail_ui_user/features/thread/data/model/email_change_response.dart';
 import 'package:tmail_ui_user/features/thread/domain/model/email_response.dart';
+import 'package:tmail_ui_user/features/thread/domain/model/empty_mailbox_response.dart';
 import 'package:tmail_ui_user/features/thread/domain/model/search_emails_response.dart';
 import 'package:tmail_ui_user/main/error/capability_validator.dart';
 
-class ThreadAPI {
+class ThreadAPI with HandleSetErrorMixin {
 
   final HttpClient httpClient;
 
@@ -302,5 +308,92 @@ class ThreadAPI {
       GetEmailResponse.deserialize);
 
     return resultList!.list.first;
+  }
+
+  Future<EmptyMailboxResponse> deleteEmailsBaseOnQuery(
+    Session session,
+    AccountId accountId,
+    {
+      UnsignedInt? limit,
+      int? position,
+      Set<Comparator>? sort,
+      Filter? filter,
+    }
+  ) async {
+    final processingInvocation = ProcessingInvocation();
+
+    final jmapRequestBuilder = JmapRequestBuilder(httpClient, processingInvocation);
+
+    final queryEmailMethod = QueryEmailMethod(accountId);
+
+    if (limit != null) queryEmailMethod.addLimit(limit);
+
+    if (position != null) queryEmailMethod.addPosition(position);
+
+    if (sort != null) queryEmailMethod.addSorts(sort);
+
+    if (filter != null) queryEmailMethod.addFilters(filter);
+
+    final queryEmailInvocation = jmapRequestBuilder.invocation(queryEmailMethod);
+
+    final setEmailMethod = SetEmailMethod(accountId);
+
+    setEmailMethod.addReferenceDestroy(processingInvocation.createResultReference(
+      queryEmailInvocation.methodCallId,
+      ReferencePath.idsPath
+    ));
+
+    final setEmailInvocation = jmapRequestBuilder.invocation(setEmailMethod);
+
+    final capabilities = setEmailMethod.requiredCapabilities;
+
+    final result = await (jmapRequestBuilder
+        ..usings(capabilities))
+      .build()
+      .execute();
+
+    final queryResponse = result.parse<QueryEmailResponse>(
+      queryEmailInvocation.methodCallId, 
+      QueryEmailResponse.deserialize  
+    );
+
+    final setEmailResponse = result.parse<SetEmailResponse>(
+      setEmailInvocation.methodCallId, 
+      SetEmailResponse.deserialize
+    );
+
+    final destroyed = setEmailResponse?.destroyed
+      ?.map((id) => EmailId(id));
+    
+    final queriedCount = queryResponse?.ids.length ?? 0;
+    final destroyedCount = destroyed?.length ?? 0;
+    
+    if (destroyedCount < queriedCount) {
+      final notFoundErrorCounter = _countingNotFoundInNotDestroy(setEmailResponse);
+      final isSuccess = destroyedCount + notFoundErrorCounter >= queriedCount;
+      return EmptyMailboxResponse(isSuccess, destroyedCount + notFoundErrorCounter);
+    }
+    return EmptyMailboxResponse(true, destroyedCount);
+  }
+
+  int _countingNotFoundInNotDestroy(SetEmailResponse? setEmailResponse) {
+    int notFoundCount = 0;
+    final notDestroyedHandlers = <SetMethodErrorHandler>{
+      (entry) {
+        if (entry.value.type == SetError.notFound) {
+          notFoundCount++;
+          return true;
+        }
+        return false;
+      }
+    };
+
+    handleSetErrors(
+      notDestroyedError: setEmailResponse?.notDestroyed,
+      notDestroyedHandlers: notDestroyedHandlers
+    );
+    
+    log('ThreadAPI::_countingNotFoundInNotDestroy: NOT_FOUND_ERRORS_COUNT = $notFoundCount');
+    return notFoundCount;
   }
 }
--- test/features/thread/data/network/thread_api_test.dart
+++ test/features/thread/data/network/thread_api_test.dart
@@ -9,6 +9,7 @@ import 'package:jmap_dart_client/jmap/core/state.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email_filter_condition.dart';
 import 'package:jmap_dart_client/jmap/mail/email/search_snippet/search_snippet.dart';
+import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
 import 'package:tmail_ui_user/features/thread/data/network/thread_api.dart';
 import 'package:tmail_ui_user/features/thread/domain/model/search_email.dart';
 import 'package:tmail_ui_user/features/thread/domain/model/search_emails_response.dart';
@@ -226,6 +227,349 @@ void main() {
             ),
           ),
         );
+      });
+    });
+
+    group('deleteEmailsBaseOnQuery:', () {
+      Map<String, dynamic> generateDeleteRequest({
+        required List<String> emailIds,
+        Filter? filter,
+      }) => {
+        'methodCalls': [
+          [
+            'Email/query',
+            {
+              'accountId': AccountFixtures.aliceAccountId.id.value,
+              if (filter != null) 'filter': filter.toJson(),
+            },
+            'c0'
+          ],
+          [
+            'Email/set',
+            {
+              'accountId': AccountFixtures.aliceAccountId.id.value,
+              '#destroy': {
+                'resultOf': 'c0',
+                'name': 'Email/query',
+                'path': '/ids/*'
+              }
+            },
+            'c1'
+          ]
+        ],
+        'using': [
+          'urn:ietf:params:jmap:mail',
+          'urn:ietf:params:jmap:core'
+        ],
+      };
+
+      test('should send correct JMAP request structure', () async {
+        final emailIds = ['email1', 'email2'];
+        final expectedRequest = generateDeleteRequest(
+          emailIds: emailIds
+        );
+        
+        dioAdapter.onPost(
+          '',
+          (server) => server.reply(
+            200,
+            {
+              "sessionState": sessionState.value,
+              "methodResponses": [
+                [
+                  "Email/query",
+                  {
+                    "accountId": AccountFixtures.aliceAccountId.id.value,
+                    "ids": emailIds,
+                    "queryState": queryState.value,
+                    "canCalculateChanges": true,
+                    "position": 0,
+                  },
+                  "c0"
+                ],
+                [
+                  "Email/set",
+                  {
+                    "accountId": AccountFixtures.aliceAccountId.id.value,
+                    "destroyed": emailIds,
+                    "notDestroyed": null
+                  },
+                  "c1"
+                ]
+              ]
+            },
+          ),
+          queryParameters: {},
+          data: expectedRequest,
+          headers: {
+            "accept": "application/json;jmapVersion=rfc-8621",
+            "content-length": 589,
+          }
+        );
+
+        await threadApi.deleteEmailsBaseOnQuery(
+          SessionFixtures.aliceSession,
+          AccountFixtures.aliceAccountId,
+        );
+      });
+
+      test('should return true when all emails are successfully deleted', () async {
+        final emailIds = ['email1', 'email2'];
+        
+        dioAdapter.onPost(
+          '',
+          (server) => server.reply(
+            200,
+            {
+              "sessionState": sessionState.value,
+              "methodResponses": [
+                [
+                  "Email/query",
+                  {
+                    "accountId": AccountFixtures.aliceAccountId.id.value,
+                    "ids": emailIds,
+                    "queryState": queryState.value,
+                    "canCalculateChanges": false,
+                    "position": 0,
+                  },
+                  "c0"
+                ],
+                [
+                  "Email/set",
+                  {
+                    "accountId": AccountFixtures.aliceAccountId.id.value,
+                    "destroyed": emailIds
+                  },
+                  "c1"
+                ]
+              ]
+            },
+          ),
+          data: {
+          "methodCalls": [
+            [
+              "Email/query",
+              {
+                "accountId": AccountFixtures.aliceAccountId.id.value,
+                "filter": {
+                  "inMailbox": "025b0580-6422-11ef-a702-5d10e1ebf1c3"
+                },
+                "sort": [
+                  {
+                    "isAscending": false,
+                    "property": "receivedAt"
+                  }
+                ]
+              },
+              "c0"
+            ],
+            [
+              "Email/set",
+              {
+                "accountId": AccountFixtures.aliceAccountId.id.value,
+                "#destroy": {
+                  "resultOf": "c0",
+                  "name": "Email/query",
+                  "path": "/ids/*"
+                }
+              },
+              "c1"
+            ]
+          ],
+          "using": [
+            "urn:ietf:params:jmap:core",
+            "urn:ietf:params:jmap:mail"
+          ],
+        },
+          headers: {
+            "accept": "application/json;jmapVersion=rfc-8621",
+            "content-length": 589,
+          }
+        );
+
+        final result = await threadApi.deleteEmailsBaseOnQuery(
+          SessionFixtures.aliceSession,
+          AccountFixtures.aliceAccountId,
+        );
+
+        expect(result, isTrue);
+      });
+
+      test('should return true when some emails are not found but others deleted successfully', () async {
+        final emailIds = ['email1', 'email2', 'email3'];
+        
+        dioAdapter.onPost(
+          '',
+          (server) => server.reply(
+            200,
+            {
+              "sessionState": sessionState.value,
+              "methodResponses": [
+                [
+                  "Email/query",
+                  {
+                    "accountId": AccountFixtures.aliceAccountId.id.value,
+                    "ids": emailIds,
+                    "queryState": queryState.value,
+                    "canCalculateChanges": true,
+                    "position": 0,
+                  },
+                  "c0"
+                ],
+                [
+                  "Email/set",
+                  {
+                    "accountId": AccountFixtures.aliceAccountId.id.value,
+                    "destroyed": ['email1', 'email3'],
+                    "notDestroyed": {
+                      "email2": {
+                        "type": "notFound",
+                        "description": "email not found"
+                      }
+                    }
+                  },
+                  "c1"
+                ]
+              ]
+            },
+          ),
+          data: {
+            "methodCalls": [
+              [
+                "Email/query",
+                {
+                  "accountId": AccountFixtures.aliceAccountId.id.value,
+                  "filter": {
+                    "inMailbox": "025b0580-6422-11ef-a702-5d10e1ebf1c3"
+                  },
+                  "sort": [
+                    {
+                      "isAscending": false,
+                      "property": "receivedAt"
+                    }
+                  ]
+                },
+                "c0"
+              ],
+              [
+                "Email/set",
+                {
+                  "accountId": AccountFixtures.aliceAccountId.id.value,
+                  "#destroy": {
+                    "resultOf": "c0",
+                    "name": "Email/query",
+                    "path": "/ids/*"
+                  }
+                },
+                "c1"
+              ]
+            ],
+            "using": [
+              "urn:ietf:params:jmap:core",
+              "urn:ietf:params:jmap:mail"
+            ],
+          },
+          headers: {
+            "accept": "application/json;jmapVersion=rfc-8621",
+            "content-length": 589,
+          }
+        );
+
+        final result = await threadApi.deleteEmailsBaseOnQuery(
+          SessionFixtures.aliceSession,
+          AccountFixtures.aliceAccountId,
+        );
+
+        expect(result, isTrue);
+      });
+
+      test('should return false when some emails fail to delete for reasons other than not found', () async {
+        final emailIds = ['email1', 'email2', 'email3'];
+        
+        dioAdapter.onPost(
+          '',
+          (server) => server.reply(
+            200,
+            {
+              "sessionState": sessionState.value,
+              "methodResponses": [
+                [
+                  "Email/query",
+                  {
+                    "accountId": AccountFixtures.aliceAccountId.id.value,
+                    "ids": emailIds,
+                    "queryState": queryState.value,
+                    "canCalculateChanges": true,
+                    "position": 0,
+                  },
+                  "c0"
+                ],
+                [
+                  "Email/set",
+                  {
+                    "accountId": AccountFixtures.aliceAccountId.id.value,
+                    "destroyed": ['email1'],
+                    "notDestroyed": {
+                      "email2": {
+                        "type": "forbidden",
+                        "description": "no permission to delete"
+                      },
+                      "email3": {
+                        "type": "notFound",
+                        "description": "email not found"
+                      }
+                    }
+                  },
+                  "c1"
+                ]
+              ]
+            },
+          ),
+          data: {
+            "methodCalls": [
+              [
+                "Email/query",
+                {
+                  "accountId": AccountFixtures.aliceAccountId.id.value,
+                  "filter": {
+                    "inMailbox": "025b0580-6422-11ef-a702-5d10e1ebf1c3"
+                  },
+                },
+                "c0"
+              ],
+              [
+                "Email/set",
+                {
+                  "accountId": AccountFixtures.aliceAccountId.id.value,
+                  "#destroy": {
+                    "resultOf": "c0",
+                    "name": "Email/query",
+                    "path": "/ids/*"
+                  }
+                },
+                "c1"
+              ]
+            ],
+            "using": [
+              "urn:ietf:params:jmap:mail",
+              "urn:ietf:params:jmap:core",
+            ],
+          },
+          headers: {
+            "accept": "application/json;jmapVersion=rfc-8621",
+            "content-length": 679,
+          }
+        );
+
+        final result = await threadApi.deleteEmailsBaseOnQuery(
+          SessionFixtures.aliceSession,
+          AccountFixtures.aliceAccountId,
+          filter: EmailFilterCondition(
+            inMailbox: MailboxId(Id('025b0580-6422-11ef-a702-5d10e1ebf1c3')), 
+          )
+        );
+
+        expect(result, isFalse);
       });
     });
   });
