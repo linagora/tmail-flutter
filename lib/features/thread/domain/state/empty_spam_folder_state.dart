3d8960ef03ff76678267b8f1904ebcc21439975b
Dat PHAM HOANG <dphamhoang@linagora.com>
2024-12-30 22:05:26 +0700

    TF-3372 Worker adds progress state to onProgressController in mobile/web

--- lib/features/thread/domain/state/empty_spam_folder_state.dart
+++ lib/features/thread/domain/state/empty_spam_folder_state.dart
@@ -1,6 +1,7 @@
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
 
 class EmptySpamFolderLoading extends LoadingState {}
 
@@ -17,4 +18,15 @@ class EmptySpamFolderSuccess extends UIState {
 class EmptySpamFolderFailure extends FeatureFailure {
 
   EmptySpamFolderFailure(dynamic exception) : super(exception: exception);
+}
+
+class EmptyingFolderState extends UIState {
+  final MailboxId mailboxId;
+  final int countEmailsDeleted;
+  final int totalEmails;
+
+  EmptyingFolderState(this.mailboxId, this.countEmailsDeleted, this.totalEmails);
+
+  @override
+  List<Object?> get props => [mailboxId, countEmailsDeleted, totalEmails];
 }
