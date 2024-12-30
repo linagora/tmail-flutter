aef7c1921373eedfd37dd3f8854ed8a587aa5777
Dat PHAM HOANG <dphamhoang@linagora.com>
2024-12-30 22:03:42 +0700

    TF-3372 Add onProgressController to get the progress of emptying task

--- lib/features/thread/data/datasource_impl/local_thread_datasource_impl.dart
+++ lib/features/thread/data/datasource_impl/local_thread_datasource_impl.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:dartz/dartz.dart' as dartz;
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/filter/filter.dart';
 import 'package:jmap_dart_client/jmap/core/properties/properties.dart';
@@ -112,6 +115,8 @@ class LocalThreadDataSourceImpl extends ThreadDataSource {
     Session session,
     AccountId accountId,
     MailboxId mailboxId,
+    int totalEmails,
+    StreamController<dartz.Either<Failure, Success>> onProgressController
   ) {
     throw UnimplementedError();
   }
