7a5c1f892594fa18a470c346f8342e5f845a9414
DatDang <tddang@linagora.com>
2025-01-06 16:32:51 +0700

    TF-3385 Fix loading forever when empty <20 items mailbox

--- lib/features/mailbox_dashboard/presentation/extensions/move_emails_to_mailbox_extension.dart
+++ lib/features/mailbox_dashboard/presentation/extensions/move_emails_to_mailbox_extension.dart
@@ -1,15 +1,16 @@
+import 'package:jmap_dart_client/jmap/core/unsigned_int.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email.dart';
 import 'package:jmap_dart_client/jmap/mail/mailbox/mailbox.dart';
 import 'package:model/email/presentation_email.dart';
-import 'package:tmail_ui_user/features/email/domain/model/move_action.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart';
 
 extension MoveEmailsToMailboxExtension on MailboxDashBoardController {
   void handleMoveEmailsToMailbox({
     required Map<MailboxId,List<EmailId>> originalMailboxIdsWithEmailIds,
     required MailboxId destinationMailboxId,
-    required MoveAction moveAction,
   }) {
+    if (destinationMailboxId == selectedMailbox.value?.id) return;
+
     final currentEmails = List<PresentationEmail>.from(
       emailsInCurrentMailbox,
     );
@@ -23,9 +24,18 @@ extension MoveEmailsToMailboxExtension on MailboxDashBoardController {
     final currentEmailsToBeMoved = currentEmails
       .where((email) => movedEmailIds.contains(email.id))
       .toList();
-    if (currentEmailsToBeMoved.isNotEmpty && destinationMailboxId != selectedMailbox.value?.id) {
+    if (currentEmailsToBeMoved.isNotEmpty) {
       currentEmails.removeWhere(currentEmailsToBeMoved.contains);
       updateEmailList(currentEmails);
+    }
+    final currentMailbox = selectedMailbox.value;
+    final currentTotalEmails = currentMailbox?.totalEmails;
+    if (currentMailbox != null && currentTotalEmails != null) {
+      int newTotalEmails = currentTotalEmails.value.value.toInt() - movedEmailIds.length;
+      if (newTotalEmails < 0) newTotalEmails = 0;
+      selectedMailbox.value = currentMailbox.copyWith(
+        totalEmails: TotalEmails(UnsignedInt(newTotalEmails)),
+      );
     }
   }
 }
