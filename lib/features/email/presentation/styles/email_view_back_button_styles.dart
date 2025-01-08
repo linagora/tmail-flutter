b3aaef83a54aeca340f824860522323d5d60f495
dab246 <tdvu@linagora.com>
2025-01-08 16:07:00 +0700

    TF-3410 Update color, dimensions and alignments in EmailView
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/email/presentation/styles/email_view_back_button_styles.dart
+++ lib/features/email/presentation/styles/email_view_back_button_styles.dart
@@ -5,12 +5,12 @@ import 'package:flutter/material.dart';
 class EmailViewBackButtonStyles {
   static const double offsetWidth = 270;
 
-  static const Color iconColor = AppColor.primaryColor;
+  static const Color iconColor = AppColor.steelGrayA540;
 
   static const EdgeInsetsGeometry rtlPadding = EdgeInsetsDirectional.symmetric(horizontal: 8);
 
   static const TextStyle labelTextStyle = TextStyle(
     fontSize: 17,
-    color: AppColor.colorTextButton
+    color: AppColor.steelGrayA540
   );
 }
