c4acd4d16a1f035719620c42eb639a1a9c716630
dab246 <tdvu@linagora.com>
2025-02-03 15:41:13 +0700

    Use custom `mapCapabilitiesConverter` to apply some outside capabilities
    Signed-off-by: dab246 <tdvu@linagora.com>
    (cherry picked from commit f1d9fa65de0224014429b8ed84eeb83f257be86d)

--- lib/features/home/data/extensions/session_hive_obj_extension.dart
+++ lib/features/home/data/extensions/session_hive_obj_extension.dart
@@ -1,9 +1,15 @@
 
 import 'dart:convert';
 
+import 'package:jmap_dart_client/http/converter/capabilities_converter.dart';
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
 import 'package:tmail_ui_user/features/home/data/model/session_hive_obj.dart';
+import 'package:tmail_ui_user/features/home/domain/extensions/session_extensions.dart';
 
 extension SessionHiveObjExtension on SessionHiveObj {
-  Session toSession() => Session.fromJson(jsonDecode(value));
+  Session toSession() => Session.fromJson(
+    jsonDecode(value),
+    converter: CapabilitiesConverter.defaultConverter
+      ..addConverters(SessionExtensions.customMapCapabilitiesConverter),
+  );
 }
