linagora/tmail-ui-user

TF-3427 Collapse side features of the composer in a side menu

lib/features/composer/presentation/composer_view_web.dart
@@ -519,13 +519,18 @@ class ComposerView extends GetWidget<ComposerController> {
                               openRichToolbarAction: controller.richTextWebController!.toggleFormattingOptions,
                               attachFileAction: () => controller.openFilePickerByType(context, FileType.any),
                               insertImageAction: () => controller.insertImage(context, constraints.maxWidth),
-                              showCodeViewAction: controller.richTextWebController!.toggleCodeView,
                               deleteComposerAction: () => controller.handleClickDeleteComposer(context),
                               saveToDraftAction: () => controller.handleClickSaveAsDraftsButton(context),
                               sendMessageAction: () => controller.handleClickSendButton(context),
-                              requestReadReceiptAction: () => controller.toggleRequestReadReceipt(context),
                               isPrintDraftEnabled: controller.isEmailChanged.isTrue,
-                              onPrintDraftAction: () => controller.printDraft(context),
+                              openContextMenuAction: (position) {
+                                controller.openPopupMenuAction(
+                                  context,
+                                  position,
+                                  _createMoreOptionPopupItemsOnDesktop(context),
+                                  radius: ComposerStyle.popupMenuRadius,
+                                );
+                              },
                             )),
                           ],
                         ),
@@ -801,13 +806,18 @@ class ComposerView extends GetWidget<ComposerController> {
                               openRichToolbarAction: controller.richTextWebController!.toggleFormattingOptions,
                               attachFileAction: () => controller.openFilePickerByType(context, FileType.any),
                               insertImageAction: () => controller.insertImage(context, constraints.maxWidth),
-                              showCodeViewAction: controller.richTextWebController!.toggleCodeView,
                               deleteComposerAction: () => controller.handleClickDeleteComposer(context),
                               saveToDraftAction: () => controller.handleClickSaveAsDraftsButton(context),
                               sendMessageAction: () => controller.handleClickSendButton(context),
-                              requestReadReceiptAction: () => controller.toggleRequestReadReceipt(context),
                               isPrintDraftEnabled: controller.isEmailChanged.isTrue,
-                              onPrintDraftAction: () => controller.printDraft(context),
+                              openContextMenuAction: (position) {
+                                controller.openPopupMenuAction(
+                                  context,
+                                  position,
+                                  _createMoreOptionPopupItemsOnDesktop(context),
+                                  radius: ComposerStyle.popupMenuRadius,
+                                );
+                              },
                             )),
                           ],
                         ),
@@ -936,6 +946,7 @@ class ComposerView extends GetWidget<ComposerController> {
         child: PopupItemWidget(
           controller.imagePaths.icDeleteMailbox,
           AppLocalizations.of(context).delete,
+          colorIcon: ComposerStyle.popupItemIconColor,
           styleName: ComposerStyle.popupItemTextStyle,
           padding: ComposerStyle.popupItemPadding,
           onCallbackAction: () {
@@ -944,6 +955,58 @@ class ComposerView extends GetWidget<ComposerController> {
           },
         )
       ),
+    ];
+  }
+
+  List<PopupMenuEntry> _createMoreOptionPopupItemsOnDesktop(BuildContext context) {
+    return [
+      PopupMenuItem(
+        padding: EdgeInsets.zero,
+        child: PopupItemWidget(
+          controller.imagePaths.icStyleCodeView,
+          AppLocalizations.of(context).embedCode,
+          styleName: ComposerStyle.popupItemTextStyle,
+          colorIcon: ComposerStyle.popupItemIconColor,
+          padding: ComposerStyle.popupItemPadding,
+          selectedIcon: controller.imagePaths.icFilterSelected,
+          isSelected: controller.richTextWebController?.codeViewEnabled,
+          onCallbackAction: () {
+            popBack();
+            controller.richTextWebController?.toggleCodeView();
+          },
+        ),
+      ),
+      PopupMenuItem(
+        padding: EdgeInsets.zero,
+        child: PopupItemWidget(
+          controller.imagePaths.icReadReceipt,
+          AppLocalizations.of(context).requestReadReceipt,
+          styleName: ComposerStyle.popupItemTextStyle,
+          padding: ComposerStyle.popupItemPadding,
+          colorIcon: ComposerStyle.popupItemIconColor,
+          selectedIcon: controller.imagePaths.icFilterSelected,
+          isSelected: controller.hasRequestReadReceipt.value,
+          onCallbackAction: () {
+            popBack();
+            controller.toggleRequestReadReceipt(context);
+          },
+        ),
+      ),
+      if (controller.isEmailChanged.isTrue)
+        PopupMenuItem(
+          padding: EdgeInsets.zero,
+          child: PopupItemWidget(
+            controller.imagePaths.icPrinter,
+            AppLocalizations.of(context).printDraft,
+            colorIcon: ComposerStyle.popupItemIconColor,
+            styleName: ComposerStyle.popupItemTextStyle,
+            padding: ComposerStyle.popupItemPadding,
+            onCallbackAction: () {
+              popBack();
+              controller.printDraft(context);
+            },
+          ),
+        ),
     ];
   }
 }

linagora/tmail-flutter

Signed-off-by: dab246 <tdvu@linagora.com>

lib/features/composer/presentation/composer_view_web.dart
@@ -519,13 +519,18 @@ class ComposerView extends GetWidget<ComposerController> {
                               openRichToolbarAction: controller.richTextWebController!.toggleFormattingOptions,
                               attachFileAction: () => controller.openFilePickerByType(context, FileType.any),
                               insertImageAction: () => controller.insertImage(context, constraints.maxWidth),
-                              showCodeViewAction: controller.richTextWebController!.toggleCodeView,
                               deleteComposerAction: () => controller.handleClickDeleteComposer(context),
                               saveToDraftAction: () => controller.handleClickSaveAsDraftsButton(context),
                               sendMessageAction: () => controller.handleClickSendButton(context),
-                              requestReadReceiptAction: () => controller.toggleRequestReadReceipt(context),
                               isPrintDraftEnabled: controller.isEmailChanged.isTrue,
-                              onPrintDraftAction: () => controller.printDraft(context),
+                              openContextMenuAction: (position) {
+                                controller.openPopupMenuAction(
+                                  context,
+                                  position,
+                                  _createMoreOptionPopupItemsOnDesktop(context),
+                                  radius: ComposerStyle.popupMenuRadius,
+                                );
+                              },
                             )),
                           ],
                         ),
@@ -801,13 +806,18 @@ class ComposerView extends GetWidget<ComposerController> {
                               openRichToolbarAction: controller.richTextWebController!.toggleFormattingOptions,
                               attachFileAction: () => controller.openFilePickerByType(context, FileType.any),
                               insertImageAction: () => controller.insertImage(context, constraints.maxWidth),
-                              showCodeViewAction: controller.richTextWebController!.toggleCodeView,
                               deleteComposerAction: () => controller.handleClickDeleteComposer(context),
                               saveToDraftAction: () => controller.handleClickSaveAsDraftsButton(context),
                               sendMessageAction: () => controller.handleClickSendButton(context),
-                              requestReadReceiptAction: () => controller.toggleRequestReadReceipt(context),
                               isPrintDraftEnabled: controller.isEmailChanged.isTrue,
-                              onPrintDraftAction: () => controller.printDraft(context),
+                              openContextMenuAction: (position) {
+                                controller.openPopupMenuAction(
+                                  context,
+                                  position,
+                                  _createMoreOptionPopupItemsOnDesktop(context),
+                                  radius: ComposerStyle.popupMenuRadius,
+                                );
+                              },
                             )),
                           ],
                         ),
@@ -936,6 +946,7 @@ class ComposerView extends GetWidget<ComposerController> {
         child: PopupItemWidget(
           controller.imagePaths.icDeleteMailbox,
           AppLocalizations.of(context).delete,
+          colorIcon: ComposerStyle.popupItemIconColor,
           styleName: ComposerStyle.popupItemTextStyle,
           padding: ComposerStyle.popupItemPadding,
           onCallbackAction: () {
@@ -944,6 +955,58 @@ class ComposerView extends GetWidget<ComposerController> {
           },
         )
       ),
+    ];
+  }
+
+  List<PopupMenuEntry> _createMoreOptionPopupItemsOnDesktop(BuildContext context) {
+    return [
+      PopupMenuItem(
+        padding: EdgeInsets.zero,
+        child: PopupItemWidget(
+          controller.imagePaths.icStyleCodeView,
+          AppLocalizations.of(context).embedCode,
+          styleName: ComposerStyle.popupItemTextStyle,
+          colorIcon: ComposerStyle.popupItemIconColor,
+          padding: ComposerStyle.popupItemPadding,
+          selectedIcon: controller.imagePaths.icFilterSelected,
+          isSelected: controller.richTextWebController?.codeViewEnabled,
+          onCallbackAction: () {
+            popBack();
+            controller.richTextWebController?.toggleCodeView();
+          },
+        ),
+      ),
+      PopupMenuItem(
+        padding: EdgeInsets.zero,
+        child: PopupItemWidget(
+          controller.imagePaths.icReadReceipt,
+          AppLocalizations.of(context).requestReadReceipt,
+          styleName: ComposerStyle.popupItemTextStyle,
+          padding: ComposerStyle.popupItemPadding,
+          colorIcon: ComposerStyle.popupItemIconColor,
+          selectedIcon: controller.imagePaths.icFilterSelected,
+          isSelected: controller.hasRequestReadReceipt.value,
+          onCallbackAction: () {
+            popBack();
+            controller.toggleRequestReadReceipt(context);
+          },
+        ),
+      ),
+      if (controller.isEmailChanged.isTrue)
+        PopupMenuItem(
+          padding: EdgeInsets.zero,
+          child: PopupItemWidget(
+            controller.imagePaths.icPrinter,
+            AppLocalizations.of(context).print,
+            colorIcon: ComposerStyle.popupItemIconColor,
+            styleName: ComposerStyle.popupItemTextStyle,
+            padding: ComposerStyle.popupItemPadding,
+            onCallbackAction: () {
+              popBack();
+              controller.printDraft(context);
+            },
+          ),
+        ),
     ];
   }
 }