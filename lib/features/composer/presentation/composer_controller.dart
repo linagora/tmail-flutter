edc1bfeb467eedc089c79cf2e0b56bc6bfd76676
dab246 <tdvu@linagora.com>
2025-02-14 02:03:22 +0700

    TF-3479 Handle switch state display mode of multiple composer (Fullscreen <-> Normal <-> Minimize)
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/composer/presentation/composer_controller.dart
+++ lib/features/composer/presentation/composer_controller.dart
@@ -65,6 +65,7 @@ import 'package:tmail_ui_user/features/composer/presentation/extensions/get_sent
 import 'package:tmail_ui_user/features/composer/presentation/extensions/handle_message_failure_extension.dart';
 import 'package:tmail_ui_user/features/composer/presentation/extensions/list_identities_extension.dart';
 import 'package:tmail_ui_user/features/composer/presentation/extensions/list_shared_media_file_extension.dart';
+import 'package:tmail_ui_user/features/composer/presentation/extensions/update_screen_display_mode_extension.dart';
 import 'package:tmail_ui_user/features/composer/presentation/mixin/drag_drog_file_mixin.dart';
 import 'package:tmail_ui_user/features/composer/presentation/model/create_email_request.dart';
 import 'package:tmail_ui_user/features/composer/presentation/model/draggable_email_address.dart';
@@ -1616,6 +1617,7 @@ class ComposerController extends BaseController
       richTextWebController!.editorController.setText(textCurrent);
     }
     screenDisplayMode.value = displayMode;
+    updateDisplayModeForComposerQueue(displayMode);
 
     await Future.delayed(
       const Duration(milliseconds: 300),
