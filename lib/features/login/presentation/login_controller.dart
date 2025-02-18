linagora/tmail-ui-user

handle parsing exception for `OIDCHttpClient::checkOIDCIsAvailable()`

lib/features/login/presentation/login_controller.dart
@@ -274,7 +274,7 @@ class LoginController extends ReloadableController {
   }
 
   void _handleCheckOIDCIsAvailableFailure(CheckOIDCIsAvailableFailure failure) {
-    if (failure.exception is CanNotFoundOIDCLinks) {
+    if (failure.exception is CanNotFoundOIDCLinks || failure.exception is FormatException) {
       _handleCommonOIDCFailure();
     } else {
       loginFormType.value = LoginFormType.retry;

linagora/tmail-flutter

lib/features/login/presentation/login_controller.dart
@@ -274,7 +274,7 @@ class LoginController extends ReloadableController {
   }
 
   void _handleCheckOIDCIsAvailableFailure(CheckOIDCIsAvailableFailure failure) {
-    if (failure.exception is CanNotFoundOIDCLinks) {
+    if (failure.exception is CanNotFoundOIDCLinks || failure.exception is InvalidOIDCResponseException) {
       _handleCommonOIDCFailure();
     } else {
       loginFormType.value = LoginFormType.retry;