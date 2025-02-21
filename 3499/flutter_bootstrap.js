4739cfd48f0d5dfebc5bfa590a848d4baac41cc1
dab246 <dab246@users.noreply.github.com>
2025-02-21 02:43:02 +0000

    deploy: 364ce796150a67f4edf2018824cad3d378edd40d

--- 3499/flutter_bootstrap.js
+++ 3499/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "59395678"
+    serviceWorkerVersion: "1435693760"
   }
 });
