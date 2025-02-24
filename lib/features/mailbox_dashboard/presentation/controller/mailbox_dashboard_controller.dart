e595aa02a025d282ec8bda11449095bc1b79c87f
DatDang <tddang@linagora.com>
2025-02-24 10:35:36 +0700

    TF-3487 Handle refreshing all mailbox and email indicator

--- lib/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart
+++ lib/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart
@@ -2674,7 +2674,7 @@ class MailboxDashBoardController extends ReloadableController
       (failure) => false,
       (success) => success is RefreshingAllEmail);
     log('MailboxDashBoardController::isRefreshingAllMailboxAndEmail:isRefreshingMailbox = $isRefreshingMailbox | isRefreshingEmail = $isRefreshingEmail');
-    return isRefreshingMailbox && isRefreshingEmail;
+    return isRefreshingMailbox || isRefreshingEmail;
   }
 
   void selectAllEmailAction() {
