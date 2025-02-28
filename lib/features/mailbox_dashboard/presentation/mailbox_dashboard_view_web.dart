d5e4f9dd66b0c99934624ee074065e37c160aa65
dab246 <tdvu@linagora.com>
2025-02-28 13:43:28 +0700

    [Semantic] Remove import package unnecessary
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/mailbox_dashboard/presentation/mailbox_dashboard_view_web.dart
+++ lib/features/mailbox_dashboard/presentation/mailbox_dashboard_view_web.dart
@@ -2,7 +2,6 @@ import 'package:core/core.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_portal/flutter_portal.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:model/extensions/presentation_mailbox_extension.dart';
 import 'package:model/extensions/session_extension.dart';
