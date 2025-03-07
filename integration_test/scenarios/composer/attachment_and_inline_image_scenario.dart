dbc5f77aa13136b5314fb845d15d9b978abe8c8e
DatDang <tddang@linagora.com>
2025-03-07 15:45:03 +0700

    TF-3535 E2E composer attachment and inline

---
+++ integration_test/scenarios/composer/attachment_and_inline_image_scenario.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:tmail_ui_user/features/composer/presentation/composer_controller.dart';
+import 'package:tmail_ui_user/features/composer/presentation/composer_view.dart';
+import 'package:tmail_ui_user/features/composer/presentation/widgets/attachment_item_composer_widget.dart';
+
+import '../../base/base_test_scenario.dart';
+import '../../resources/test_images.dart';
+import '../../robots/composer_robot.dart';
+import '../../robots/thread_robot.dart';
+
+class ComposerAttachmentAndInlineScenario extends BaseTestScenario {
+  const ComposerAttachmentAndInlineScenario(super.$);
+
+  @override
+  Future<void> runTestLogic() async {
+    const pngName = 'composer-png';
+    final threadRobot = ThreadRobot($);
+    final composerRobot = ComposerRobot($);
+
+    await threadRobot.openComposer();
+    await _expectComposerViewVisible();
+
+    await composerRobot.grantContactPermission();
+
+    final png = await preparingPngWithName(pngName);
+
+    await composerRobot.addAttachment(png);
+    await composerRobot.addInline(png);
+    await $.pumpAndSettle();
+
+    _expectAttachment(pngName);
+    await _expectInline();
+  }
+
+  Future<void> _expectInline() async {
+    final currentHtmlContent = await Get
+      .find<ComposerController>()
+      .getContentInEditor();
+    expect(
+      currentHtmlContent.contains(TestImages.base64),
+      true,
+    );
+  }
+
+  void _expectAttachment(String pngName) {
+    return expect(
+      $(AttachmentItemComposerWidget)
+        .which<AttachmentItemComposerWidget>(
+         (widget) => widget.fileName.contains(pngName)
+        ),
+      findsOneWidget,
+    );
+  }
+  
+  Future<void> _expectComposerViewVisible() => expectViewVisible($(ComposerView));
+}
