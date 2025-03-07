7a64e1fcc246c0c3c2622140edd1b4158e4d62ed
tddang-linagora <tddang-linagora@users.noreply.github.com>
2025-03-07 10:05:03 +0000

    deploy: 202e780f097d50cfc54be0a8825e285635677cb2

--- 3530/flutter_service_worker.js
+++ 3530/flutter_service_worker.js
@@ -358,7 +358,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "login-callback.html": "e17f83d4982ea3cce6e692b31c4b4beb",
 "logout-callback.html": "999326dc9a0f37c9a9a4280b4a35dc51",
 "main.dart.js_1.part.js": "4c81b4a3abfede7e53da37848bc30bfa",
-"flutter_bootstrap.js": "ceb846032e0a794ee77f04e3df008a44"};
+"flutter_bootstrap.js": "75a7efc8824982ef6de99fa3444b0a21"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
