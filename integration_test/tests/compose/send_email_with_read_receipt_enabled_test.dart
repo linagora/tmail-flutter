97eff56ff5d3a6292f1634a6d0eb2efdaee3d9e2
DatDang <tddang@linagora.com>
2025-03-11 13:35:09 +0700

    TF-3543 E2E composer read receipt

---
+++ integration_test/tests/compose/send_email_with_read_receipt_enabled_test.dart
@@ -0,0 +1,11 @@
+import '../../base/test_base.dart';
+import '../../scenarios/composer/send_email_with_read_receipt_enabled_scenario.dart';
+
+void main() {
+  TestBase().runPatrolTest(
+    description: 'Should see read receipt dialog '
+      'when user toggle read receipt '
+      'and open read receipt email',
+    scenarioBuilder: ($) => SendEmailWithReadReceiptEnabledScenario($),
+  );
+}
