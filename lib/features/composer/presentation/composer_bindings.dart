linagora/tmail-ui-user

TF-3478 Use `tag` to inject composer binding
Signed-off-by: dab246 <tdvu@linagora.com>

lib/features/composer/presentation/composer_bindings.dart
@@ -78,6 +78,10 @@ import 'package:worker_manager/worker_manager.dart';
 
 class ComposerBindings extends BaseBindings {
 
+  final String? composerId;
+
+  ComposerBindings({this.composerId});
+
   @override
   void dependencies() {
     _bindingsUtils();
@@ -89,43 +93,52 @@ class ComposerBindings extends BaseBindings {
       Get.find<DioClient>(tag: BindingTag.isolateTag),
       Get.find<Executor>(),
       Get.find<FileUtils>(),
-    ));
+    ), tag: composerId);
   }
 
   @override
   void bindingsDataSourceImpl() {
     Get.lazyPut(() => AttachmentUploadDataSourceImpl(
-      Get.find<FileUploader>(),
+      Get.find<FileUploader>(tag: composerId),
       Get.find<Uuid>(),
-      Get.find<RemoteExceptionThrower>()
-    ));
-    Get.lazyPut(() => ComposerDataSourceImpl(Get.find<DownloadClient>(), Get.find<RemoteExceptionThrower>()));
-    Get.lazyPut(() => ContactDataSourceImpl(Get.find<CacheExceptionThrower>()));
+      Get.find<RemoteExceptionThrower>(),
+    ), tag: composerId);
+    Get.lazyPut(() => ComposerDataSourceImpl(
+      Get.find<DownloadClient>(),
+      Get.find<RemoteExceptionThrower>(),
+    ), tag: composerId);
+    Get.lazyPut(() => ContactDataSourceImpl(
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => MailboxDataSourceImpl(
       Get.find<MailboxAPI>(),
       Get.find<MailboxIsolateWorker>(),
-      Get.find<RemoteExceptionThrower>()));
+      Get.find<RemoteExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => MailboxCacheDataSourceImpl(
-        Get.find<MailboxCacheManager>(),
-        Get.find<CacheExceptionThrower>()));
+      Get.find<MailboxCacheManager>(),
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => EmailDataSourceImpl(
       Get.find<EmailAPI>(),
-      Get.find<RemoteExceptionThrower>()));
+      Get.find<RemoteExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => HtmlDataSourceImpl(
       Get.find<HtmlAnalyzer>(),
-      Get.find<CacheExceptionThrower>()));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => StateDataSourceImpl(
       Get.find<StateCacheManager>(),
       Get.find<IOSSharingManager>(),
-      Get.find<CacheExceptionThrower>()
-    ));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => PrintFileDataSourceImpl(
       Get.find<PrintUtils>(),
       Get.find<ImagePaths>(),
       Get.find<FileUtils>(),
       Get.find<HtmlAnalyzer>(),
-      Get.find<CacheExceptionThrower>()
-    ));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => EmailHiveCacheDataSourceImpl(
       Get.find<NewEmailCacheManager>(),
       Get.find<OpenedEmailCacheManager>(),
@@ -134,136 +147,215 @@ class ComposerBindings extends BaseBindings {
       Get.find<EmailCacheManager>(),
       Get.find<SendingEmailCacheManager>(),
       Get.find<FileUtils>(),
-      Get.find<CacheExceptionThrower>()));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => EmailLocalStorageDataSourceImpl(
       Get.find<LocalStorageManager>(),
-      Get.find<CacheExceptionThrower>()));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => EmailSessionStorageDatasourceImpl(
       Get.find<SessionStorageManager>(),
-      Get.find<CacheExceptionThrower>()));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => RemoteServerSettingsDataSourceImpl(
       Get.find<ServerSettingsAPI>(),
-      Get.find<RemoteExceptionThrower>()));
+      Get.find<RemoteExceptionThrower>(),
+    ), tag: composerId);
   }
 
   @override
   void bindingsDataSource() {
-    Get.lazyPut<AttachmentUploadDataSource>(() => Get.find<AttachmentUploadDataSourceImpl>());
-    Get.lazyPut<ComposerDataSource>(() => Get.find<ComposerDataSourceImpl>());
-    Get.lazyPut<ContactDataSource>(() => Get.find<ContactDataSourceImpl>());
-    Get.lazyPut<MailboxDataSource>(() => Get.find<MailboxDataSourceImpl>());
-    Get.lazyPut<EmailDataSource>(() => Get.find<EmailDataSourceImpl>());
-    Get.lazyPut<HtmlDataSource>(() => Get.find<HtmlDataSourceImpl>());
-    Get.lazyPut<StateDataSource>(() => Get.find<StateDataSourceImpl>());
-    Get.lazyPut<PrintFileDataSource>(() => Get.find<PrintFileDataSourceImpl>());
+    Get.lazyPut<AttachmentUploadDataSource>(
+      () => Get.find<AttachmentUploadDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<ComposerDataSource>(
+      () => Get.find<ComposerDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<ContactDataSource>(
+      () => Get.find<ContactDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<MailboxDataSource>(
+      () => Get.find<MailboxDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<EmailDataSource>(
+      () => Get.find<EmailDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<HtmlDataSource>(
+      () => Get.find<HtmlDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<StateDataSource>(
+      () => Get.find<StateDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<PrintFileDataSource>(
+      () => Get.find<PrintFileDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
     Get.lazyPut<ServerSettingsDataSource>(
-      () => Get.find<RemoteServerSettingsDataSourceImpl>());
+      () => Get.find<RemoteServerSettingsDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
   }
 
   @override
   void bindingsRepositoryImpl() {
     Get.lazyPut(() => ComposerRepositoryImpl(
-      Get.find<AttachmentUploadDataSource>(),
-      Get.find<ComposerDataSource>(),
-      Get.find<HtmlDataSource>(),
+      Get.find<AttachmentUploadDataSource>(tag: composerId),
+      Get.find<ComposerDataSource>(tag: composerId),
+      Get.find<HtmlDataSource>(tag: composerId),
       Get.find<ApplicationManager>(),
       Get.find<Uuid>(),
-    ));
-    Get.lazyPut(() => ContactRepositoryImpl(Get.find<ContactDataSource>()));
+    ), tag: composerId);
+    Get.lazyPut(
+      () => ContactRepositoryImpl(Get.find<ContactDataSource>(tag: composerId)),
+      tag: composerId,
+    );
     Get.lazyPut(() => MailboxRepositoryImpl(
       {
-        DataSourceType.network: Get.find<MailboxDataSource>(),
-        DataSourceType.local: Get.find<MailboxCacheDataSourceImpl>()
+        DataSourceType.network: Get.find<MailboxDataSource>(tag: composerId),
+        DataSourceType.local: Get.find<MailboxCacheDataSourceImpl>(tag: composerId)
       },
-      Get.find<StateDataSource>(),
-    ));
+      Get.find<StateDataSource>(tag: composerId),
+    ), tag: composerId);
     Get.lazyPut(() => EmailRepositoryImpl(
       {
-        DataSourceType.network: Get.find<EmailDataSource>(),
-        DataSourceType.hiveCache: Get.find<EmailHiveCacheDataSourceImpl>(),
-        DataSourceType.local: Get.find<EmailLocalStorageDataSourceImpl>(),
-        DataSourceType.session: Get.find<EmailSessionStorageDatasourceImpl>(),
+        DataSourceType.network: Get.find<EmailDataSource>(tag: composerId),
+        DataSourceType.hiveCache: Get.find<EmailHiveCacheDataSourceImpl>(tag: composerId),
+        DataSourceType.local: Get.find<EmailLocalStorageDataSourceImpl>(tag: composerId),
+        DataSourceType.session: Get.find<EmailSessionStorageDatasourceImpl>(tag: composerId),
       },
-      Get.find<HtmlDataSource>(),
-      Get.find<StateDataSource>(),
-      Get.find<PrintFileDataSource>(),
-    ));
+      Get.find<HtmlDataSource>(tag: composerId),
+      Get.find<StateDataSource>(tag: composerId),
+      Get.find<PrintFileDataSource>(tag: composerId),
+    ), tag: composerId);
     Get.lazyPut(() => ServerSettingsRepositoryImpl(
-      Get.find<ServerSettingsDataSource>(),
-    ));
+      Get.find<ServerSettingsDataSource>(tag: composerId),
+    ), tag: composerId);
   }
 
   @override
   void bindingsRepository() {
-    Get.lazyPut<ComposerRepository>(() => Get.find<ComposerRepositoryImpl>());
-    Get.lazyPut<ContactRepository>(() => Get.find<ContactRepositoryImpl>());
-    Get.lazyPut<MailboxRepository>(() => Get.find<MailboxRepositoryImpl>());
-    Get.lazyPut<EmailRepository>(() => Get.find<EmailRepositoryImpl>());
-    Get.lazyPut<ServerSettingsRepository>(() => Get.find<ServerSettingsRepositoryImpl>());
+    Get.lazyPut<ComposerRepository>(
+      () => Get.find<ComposerRepositoryImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<ContactRepository>(
+      () => Get.find<ContactRepositoryImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<MailboxRepository>(
+      () => Get.find<MailboxRepositoryImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<EmailRepository>(
+      () => Get.find<EmailRepositoryImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<ServerSettingsRepository>(
+      () => Get.find<ServerSettingsRepositoryImpl>(tag: composerId),
+      tag: composerId,
+    );
   }
 
   @override
   void bindingsInteractor() {
-    Get.lazyPut(() => LocalFilePickerInteractor());
-    Get.lazyPut(() => LocalImagePickerInteractor());
-    Get.lazyPut(() => UploadAttachmentInteractor(Get.find<ComposerRepository>()));
-    Get.lazyPut(() => GetEmailContentInteractor(Get.find<EmailRepository>()));
-    Get.lazyPut(() => RemoveComposerCacheOnWebInteractor(Get.find<ComposerCacheRepository>()));
+    Get.lazyPut(
+      () => LocalFilePickerInteractor(),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => LocalImagePickerInteractor(),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => UploadAttachmentInteractor(Get.find<ComposerRepository>(tag: composerId)),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => GetEmailContentInteractor(Get.find<EmailRepository>(tag: composerId)),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => RemoveComposerCacheOnWebInteractor(Get.find<ComposerCacheRepository>(tag: composerId)),
+      tag: composerId,
+    );
     Get.lazyPut(() => SaveComposerCacheOnWebInteractor(
-      Get.find<ComposerCacheRepository>(),
-      Get.find<ComposerRepository>(),
-    ));
-    Get.lazyPut(() => DownloadImageAsBase64Interactor(Get.find<ComposerRepository>()));
-    Get.lazyPut(() => TransformHtmlEmailContentInteractor(Get.find<EmailRepository>()));
-    Get.lazyPut(() => GetAlwaysReadReceiptSettingInteractor(Get.find<ServerSettingsRepository>()));
+      Get.find<ComposerCacheRepository>(tag: composerId),
+      Get.find<ComposerRepository>(tag: composerId),
+    ), tag: composerId);
+    Get.lazyPut(
+      () => DownloadImageAsBase64Interactor(Get.find<ComposerRepository>(tag: composerId)),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => TransformHtmlEmailContentInteractor(Get.find<EmailRepository>(tag: composerId)),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => GetAlwaysReadReceiptSettingInteractor(Get.find<ServerSettingsRepository>(tag: composerId)),
+      tag: composerId,
+    );
     Get.lazyPut(() => CreateNewAndSendEmailInteractor(
-      Get.find<EmailRepository>(),
-      Get.find<ComposerRepository>(),
-    ));
+      Get.find<EmailRepository>(tag: composerId),
+      Get.find<ComposerRepository>(tag: composerId),
+    ), tag: composerId);
     Get.lazyPut(() => CreateNewAndSaveEmailToDraftsInteractor(
-      Get.find<EmailRepository>(),
-      Get.find<ComposerRepository>(),
-    ));
-    Get.lazyPut(() => RestoreEmailInlineImagesInteractor(
-      Get.find<ComposerCacheRepository>()));
-    Get.lazyPut(() => PrintEmailInteractor(
-      Get.find<EmailRepository>()));
+      Get.find<EmailRepository>(tag: composerId),
+      Get.find<ComposerRepository>(tag: composerId),
+    ), tag: composerId);
+    Get.lazyPut(
+      () => RestoreEmailInlineImagesInteractor(Get.find<ComposerCacheRepository>(tag: composerId)),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => PrintEmailInteractor(Get.find<EmailRepository>(tag: composerId)),
+      tag: composerId,
+    );
 
-    IdentityInteractorsBindings().dependencies();
+    IdentityInteractorsBindings(composerId: composerId).dependencies();
   }
 
   @override
   void bindingsController() {
     if (PlatformInfo.isWeb) {
-      Get.lazyPut(() => RichTextWebController());
+      Get.lazyPut(() => RichTextWebController(), tag: composerId);
     } else {
-      Get.lazyPut(() => RichTextMobileTabletController());
+      Get.lazyPut(() => RichTextMobileTabletController(), tag: composerId);
     }
-    Get.lazyPut(() => UploadController(Get.find<UploadAttachmentInteractor>()));
+    Get.lazyPut(
+      () => UploadController(Get.find<UploadAttachmentInteractor>(tag: composerId)),
+      tag: composerId,
+    );
     Get.lazyPut(() => ComposerController(
-      Get.find<LocalFilePickerInteractor>(),
-      Get.find<LocalImagePickerInteractor>(),
-      Get.find<GetEmailContentInteractor>(),
-      Get.find<GetAllIdentitiesInteractor>(),
-      Get.find<UploadController>(),
-      Get.find<RemoveComposerCacheOnWebInteractor>(),
-      Get.find<SaveComposerCacheOnWebInteractor>(),
-      Get.find<DownloadImageAsBase64Interactor>(),
-      Get.find<TransformHtmlEmailContentInteractor>(),
-      Get.find<GetAlwaysReadReceiptSettingInteractor>(),
-      Get.find<CreateNewAndSendEmailInteractor>(),
-      Get.find<CreateNewAndSaveEmailToDraftsInteractor>(),
-      Get.find<PrintEmailInteractor>(),
-    ));
+      Get.find<LocalFilePickerInteractor>(tag: composerId),
+      Get.find<LocalImagePickerInteractor>(tag: composerId),
+      Get.find<GetEmailContentInteractor>(tag: composerId),
+      Get.find<GetAllIdentitiesInteractor>(tag: composerId),
+      Get.find<UploadController>(tag: composerId),
+      Get.find<RemoveComposerCacheOnWebInteractor>(tag: composerId),
+      Get.find<SaveComposerCacheOnWebInteractor>(tag: composerId),
+      Get.find<DownloadImageAsBase64Interactor>(tag: composerId),
+      Get.find<TransformHtmlEmailContentInteractor>(tag: composerId),
+      Get.find<GetAlwaysReadReceiptSettingInteractor>(tag: composerId),
+      Get.find<CreateNewAndSendEmailInteractor>(tag: composerId),
+      Get.find<CreateNewAndSaveEmailToDraftsInteractor>(tag: composerId),
+      Get.find<PrintEmailInteractor>(tag: composerId),
+    ), tag: composerId);
   }
 
   void dispose() {
     if (PlatformInfo.isWeb) {
-      Get.delete<RichTextWebController>();
+      Get.delete<RichTextWebController>(tag: composerId);
     } else {
-      Get.delete<RichTextMobileTabletController>();
+      Get.delete<RichTextMobileTabletController>(tag: composerId);
     }
-    Get.delete<UploadController>();
-    Get.delete<ComposerController>();
+    Get.delete<UploadController>(tag: composerId);
+    Get.delete<ComposerController>(tag: composerId);
   }
 }

linagora/tmail-flutter

lib/features/composer/presentation/composer_bindings.dart
@@ -72,40 +72,53 @@ import 'package:uuid/uuid.dart';
 
 class ComposerBindings extends BaseBindings {
 
+  final String? composerId;
+
+  ComposerBindings({this.composerId});
+
   @override
   void bindingsDataSourceImpl() {
     Get.lazyPut(() => AttachmentUploadDataSourceImpl(
-      Get.find<FileUploader>(),
+      Get.find<FileUploader>(tag: composerId),
       Get.find<Uuid>(),
-      Get.find<RemoteExceptionThrower>()
-    ));
-    Get.lazyPut(() => ComposerDataSourceImpl(Get.find<DownloadClient>(), Get.find<RemoteExceptionThrower>()));
-    Get.lazyPut(() => ContactDataSourceImpl(Get.find<CacheExceptionThrower>()));
+      Get.find<RemoteExceptionThrower>(),
+    ), tag: composerId);
+    Get.lazyPut(() => ComposerDataSourceImpl(
+      Get.find<DownloadClient>(),
+      Get.find<RemoteExceptionThrower>(),
+    ), tag: composerId);
+    Get.lazyPut(() => ContactDataSourceImpl(
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => MailboxDataSourceImpl(
       Get.find<MailboxAPI>(),
       Get.find<MailboxIsolateWorker>(),
-      Get.find<RemoteExceptionThrower>()));
+      Get.find<RemoteExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => MailboxCacheDataSourceImpl(
-        Get.find<MailboxCacheManager>(),
-        Get.find<CacheExceptionThrower>()));
+      Get.find<MailboxCacheManager>(),
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => EmailDataSourceImpl(
       Get.find<EmailAPI>(),
-      Get.find<RemoteExceptionThrower>()));
+      Get.find<RemoteExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => HtmlDataSourceImpl(
       Get.find<HtmlAnalyzer>(),
-      Get.find<CacheExceptionThrower>()));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => StateDataSourceImpl(
       Get.find<StateCacheManager>(),
       Get.find<IOSSharingManager>(),
-      Get.find<CacheExceptionThrower>()
-    ));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => PrintFileDataSourceImpl(
       Get.find<PrintUtils>(),
       Get.find<ImagePaths>(),
       Get.find<FileUtils>(),
       Get.find<HtmlAnalyzer>(),
-      Get.find<CacheExceptionThrower>()
-    ));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => EmailHiveCacheDataSourceImpl(
       Get.find<NewEmailCacheManager>(),
       Get.find<OpenedEmailCacheManager>(),
@@ -114,127 +127,197 @@ class ComposerBindings extends BaseBindings {
       Get.find<EmailCacheManager>(),
       Get.find<SendingEmailCacheManager>(),
       Get.find<FileUtils>(),
-      Get.find<CacheExceptionThrower>()));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => EmailLocalStorageDataSourceImpl(
       Get.find<LocalStorageManager>(),
-      Get.find<CacheExceptionThrower>()));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
     Get.lazyPut(() => EmailSessionStorageDatasourceImpl(
       Get.find<SessionStorageManager>(),
-      Get.find<CacheExceptionThrower>()));
+      Get.find<CacheExceptionThrower>(),
+    ), tag: composerId);
   }
 
   @override
   void bindingsDataSource() {
-    Get.lazyPut<AttachmentUploadDataSource>(() => Get.find<AttachmentUploadDataSourceImpl>());
-    Get.lazyPut<ComposerDataSource>(() => Get.find<ComposerDataSourceImpl>());
-    Get.lazyPut<ContactDataSource>(() => Get.find<ContactDataSourceImpl>());
-    Get.lazyPut<MailboxDataSource>(() => Get.find<MailboxDataSourceImpl>());
-    Get.lazyPut<EmailDataSource>(() => Get.find<EmailDataSourceImpl>());
-    Get.lazyPut<HtmlDataSource>(() => Get.find<HtmlDataSourceImpl>());
-    Get.lazyPut<StateDataSource>(() => Get.find<StateDataSourceImpl>());
-    Get.lazyPut<PrintFileDataSource>(() => Get.find<PrintFileDataSourceImpl>());
+    Get.lazyPut<AttachmentUploadDataSource>(
+      () => Get.find<AttachmentUploadDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<ComposerDataSource>(
+      () => Get.find<ComposerDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<ContactDataSource>(
+      () => Get.find<ContactDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<MailboxDataSource>(
+      () => Get.find<MailboxDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<EmailDataSource>(
+      () => Get.find<EmailDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<HtmlDataSource>(
+      () => Get.find<HtmlDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<StateDataSource>(
+      () => Get.find<StateDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<PrintFileDataSource>(
+      () => Get.find<PrintFileDataSourceImpl>(tag: composerId),
+      tag: composerId,
+    );
   }
 
   @override
   void bindingsRepositoryImpl() {
     Get.lazyPut(() => ComposerRepositoryImpl(
-      Get.find<AttachmentUploadDataSource>(),
-      Get.find<ComposerDataSource>(),
-      Get.find<HtmlDataSource>(),
+      Get.find<AttachmentUploadDataSource>(tag: composerId),
+      Get.find<ComposerDataSource>(tag: composerId),
+      Get.find<HtmlDataSource>(tag: composerId),
       Get.find<ApplicationManager>(),
       Get.find<Uuid>(),
-    ));
-    Get.lazyPut(() => ContactRepositoryImpl(Get.find<ContactDataSource>()));
+    ), tag: composerId);
+    Get.lazyPut(
+      () => ContactRepositoryImpl(Get.find<ContactDataSource>(tag: composerId)),
+      tag: composerId,
+    );
     Get.lazyPut(() => MailboxRepositoryImpl(
       {
-        DataSourceType.network: Get.find<MailboxDataSource>(),
-        DataSourceType.local: Get.find<MailboxCacheDataSourceImpl>()
+        DataSourceType.network: Get.find<MailboxDataSource>(tag: composerId),
+        DataSourceType.local: Get.find<MailboxCacheDataSourceImpl>(tag: composerId)
       },
-      Get.find<StateDataSource>(),
-    ));
+      Get.find<StateDataSource>(tag: composerId),
+    ), tag: composerId);
     Get.lazyPut(() => EmailRepositoryImpl(
       {
-        DataSourceType.network: Get.find<EmailDataSource>(),
-        DataSourceType.hiveCache: Get.find<EmailHiveCacheDataSourceImpl>(),
-        DataSourceType.local: Get.find<EmailLocalStorageDataSourceImpl>(),
-        DataSourceType.session: Get.find<EmailSessionStorageDatasourceImpl>(),
+        DataSourceType.network: Get.find<EmailDataSource>(tag: composerId),
+        DataSourceType.hiveCache: Get.find<EmailHiveCacheDataSourceImpl>(tag: composerId),
+        DataSourceType.local: Get.find<EmailLocalStorageDataSourceImpl>(tag: composerId),
+        DataSourceType.session: Get.find<EmailSessionStorageDatasourceImpl>(tag: composerId),
       },
-      Get.find<HtmlDataSource>(),
-      Get.find<StateDataSource>(),
-      Get.find<PrintFileDataSource>(),
-    ));
+      Get.find<HtmlDataSource>(tag: composerId),
+      Get.find<StateDataSource>(tag: composerId),
+      Get.find<PrintFileDataSource>(tag: composerId),
+    ), tag: composerId);
   }
 
   @override
   void bindingsRepository() {
-    Get.lazyPut<ComposerRepository>(() => Get.find<ComposerRepositoryImpl>());
-    Get.lazyPut<ContactRepository>(() => Get.find<ContactRepositoryImpl>());
-    Get.lazyPut<MailboxRepository>(() => Get.find<MailboxRepositoryImpl>());
-    Get.lazyPut<EmailRepository>(() => Get.find<EmailRepositoryImpl>());
+    Get.lazyPut<ComposerRepository>(
+      () => Get.find<ComposerRepositoryImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<ContactRepository>(
+      () => Get.find<ContactRepositoryImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<MailboxRepository>(
+      () => Get.find<MailboxRepositoryImpl>(tag: composerId),
+      tag: composerId,
+    );
+    Get.lazyPut<EmailRepository>(
+      () => Get.find<EmailRepositoryImpl>(tag: composerId),
+      tag: composerId,
+    );
   }
 
   @override
   void bindingsInteractor() {
-    Get.lazyPut(() => LocalFilePickerInteractor());
-    Get.lazyPut(() => LocalImagePickerInteractor());
-    Get.lazyPut(() => UploadAttachmentInteractor(Get.find<ComposerRepository>()));
-    Get.lazyPut(() => GetEmailContentInteractor(Get.find<EmailRepository>()));
-    Get.lazyPut(() => RemoveComposerCacheOnWebInteractor(Get.find<ComposerCacheRepository>()));
+    Get.lazyPut(
+      () => LocalFilePickerInteractor(),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => LocalImagePickerInteractor(),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => UploadAttachmentInteractor(Get.find<ComposerRepository>(tag: composerId)),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => GetEmailContentInteractor(Get.find<EmailRepository>(tag: composerId)),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => RemoveComposerCacheOnWebInteractor(Get.find<ComposerCacheRepository>(tag: composerId)),
+      tag: composerId,
+    );
     Get.lazyPut(() => SaveComposerCacheOnWebInteractor(
-      Get.find<ComposerCacheRepository>(),
-      Get.find<ComposerRepository>(),
-    ));
-    Get.lazyPut(() => DownloadImageAsBase64Interactor(Get.find<ComposerRepository>()));
-    Get.lazyPut(() => TransformHtmlEmailContentInteractor(Get.find<EmailRepository>()));
+      Get.find<ComposerCacheRepository>(tag: composerId),
+      Get.find<ComposerRepository>(tag: composerId),
+    ), tag: composerId);
+    Get.lazyPut(
+      () => DownloadImageAsBase64Interactor(Get.find<ComposerRepository>(tag: composerId)),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => TransformHtmlEmailContentInteractor(Get.find<EmailRepository>(tag: composerId)),
+      tag: composerId,
+    );
     Get.lazyPut(() => CreateNewAndSendEmailInteractor(
-      Get.find<EmailRepository>(),
-      Get.find<ComposerRepository>(),
-    ));
+      Get.find<EmailRepository>(tag: composerId),
+      Get.find<ComposerRepository>(tag: composerId),
+    ), tag: composerId);
     Get.lazyPut(() => CreateNewAndSaveEmailToDraftsInteractor(
-      Get.find<EmailRepository>(),
-      Get.find<ComposerRepository>(),
-    ));
-    Get.lazyPut(() => RestoreEmailInlineImagesInteractor(
-      Get.find<ComposerCacheRepository>()));
-    Get.lazyPut(() => PrintEmailInteractor(
-      Get.find<EmailRepository>()));
+      Get.find<EmailRepository>(tag: composerId),
+      Get.find<ComposerRepository>(tag: composerId),
+    ), tag: composerId);
+    Get.lazyPut(
+      () => RestoreEmailInlineImagesInteractor(Get.find<ComposerCacheRepository>(tag: composerId)),
+      tag: composerId,
+    );
+    Get.lazyPut(
+      () => PrintEmailInteractor(Get.find<EmailRepository>(tag: composerId)),
+      tag: composerId,
+    );
 
-    IdentityInteractorsBindings().dependencies();
+    IdentityInteractorsBindings(composerId: composerId).dependencies();
     PreferencesInteractorsBindings().dependencies();
   }
 
   @override
   void bindingsController() {
     if (PlatformInfo.isWeb) {
-      Get.lazyPut(() => RichTextWebController());
+      Get.lazyPut(() => RichTextWebController(), tag: composerId);
     } else {
-      Get.lazyPut(() => RichTextMobileTabletController());
+      Get.lazyPut(() => RichTextMobileTabletController(), tag: composerId);
     }
-    Get.lazyPut(() => UploadController(Get.find<UploadAttachmentInteractor>()));
+    Get.lazyPut(
+      () => UploadController(Get.find<UploadAttachmentInteractor>(tag: composerId)),
+      tag: composerId,
+    );
     Get.lazyPut(() => ComposerController(
-      Get.find<LocalFilePickerInteractor>(),
-      Get.find<LocalImagePickerInteractor>(),
-      Get.find<GetEmailContentInteractor>(),
-      Get.find<GetAllIdentitiesInteractor>(),
-      Get.find<UploadController>(),
-      Get.find<RemoveComposerCacheOnWebInteractor>(),
-      Get.find<SaveComposerCacheOnWebInteractor>(),
-      Get.find<DownloadImageAsBase64Interactor>(),
-      Get.find<TransformHtmlEmailContentInteractor>(),
-      Get.find<GetServerSettingInteractor>(),
-      Get.find<CreateNewAndSendEmailInteractor>(),
-      Get.find<CreateNewAndSaveEmailToDraftsInteractor>(),
-      Get.find<PrintEmailInteractor>(),
-    ));
+      Get.find<LocalFilePickerInteractor>(tag: composerId),
+      Get.find<LocalImagePickerInteractor>(tag: composerId),
+      Get.find<GetEmailContentInteractor>(tag: composerId),
+      Get.find<GetAllIdentitiesInteractor>(tag: composerId),
+      Get.find<UploadController>(tag: composerId),
+      Get.find<RemoveComposerCacheOnWebInteractor>(tag: composerId),
+      Get.find<SaveComposerCacheOnWebInteractor>(tag: composerId),
+      Get.find<DownloadImageAsBase64Interactor>(tag: composerId),
+      Get.find<TransformHtmlEmailContentInteractor>(tag: composerId),
+      Get.find<GetServerSettingInteractor>(tag: composerId),
+      Get.find<CreateNewAndSendEmailInteractor>(tag: composerId),
+      Get.find<CreateNewAndSaveEmailToDraftsInteractor>(tag: composerId),
+      Get.find<PrintEmailInteractor>(tag: composerId),
+    ), tag: composerId);
   }
 
   void dispose() {
     if (PlatformInfo.isWeb) {
-      Get.delete<RichTextWebController>();
+      Get.delete<RichTextWebController>(tag: composerId);
     } else {
-      Get.delete<RichTextMobileTabletController>();
+      Get.delete<RichTextMobileTabletController>(tag: composerId);
     }
-    Get.delete<UploadController>();
-    Get.delete<ComposerController>();
+    Get.delete<UploadController>(tag: composerId);
+    Get.delete<ComposerController>(tag: composerId);
   }
 }