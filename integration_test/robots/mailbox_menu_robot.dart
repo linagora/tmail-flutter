0a531f7e13f4c9f7f84f239a82844fbf19b8b69f
DatDang <tddang@linagora.com>
2025-03-18 10:55:08 +0700

    TF-3564 E2E Mailbox count real time update

--- integration_test/robots/mailbox_menu_robot.dart
+++ integration_test/robots/mailbox_menu_robot.dart
@@ -4,12 +4,15 @@ import 'package:core/presentation/views/button/tmail_button_widget.dart';
 import 'package:core/presentation/views/text/text_field_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
 import 'package:tmail_ui_user/features/mailbox/presentation/widgets/label_mailbox_item_widget.dart';
 import 'package:tmail_ui_user/features/mailbox/presentation/widgets/mailbox_item_widget.dart';
 import 'package:tmail_ui_user/features/mailbox_creator/presentation/mailbox_creator_view.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart';
 
 import '../base/core_robot.dart';
+import '../exceptions/mailbox/null_inbox_unread_count_exception.dart';
 
 class MailboxMenuRobot extends CoreRobot {
   MailboxMenuRobot(super.$);
@@ -69,5 +72,21 @@ class MailboxMenuRobot extends CoreRobot {
         return widget.tooltip == AppLocalizations().close;
       })
       .tap();
+  }
+
+  int getCurrentInboxCount() {
+    final inboxCount = Get.find<MailboxDashBoardController>()
+      .selectedMailbox
+      .value
+      ?.unreadEmails
+      ?.value
+      .value
+      .toInt();
+
+    if (inboxCount == null) {
+      throw NullInboxUnreadCountException();
+    }
+
+    return inboxCount;
   }
 }
