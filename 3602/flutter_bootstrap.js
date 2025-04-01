2635df648ffe0185f8ab86206a309b829304f563
dab246 <dab246@users.noreply.github.com>
2025-04-01 02:51:27 +0000

    deploy: c0d1042ceb79856e5501dabf80147ed41ff69bed

--- 3602/flutter_bootstrap.js
+++ 3602/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2811910765"
+    serviceWorkerVersion: "3384726051"
   }
 });
--- 3602/flutter_service_worker.js
+++ 3602/flutter_service_worker.js
@@ -361,7 +361,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "login-callback.html": "e17f83d4982ea3cce6e692b31c4b4beb",
 "logout-callback.html": "999326dc9a0f37c9a9a4280b4a35dc51",
 "main.dart.js_1.part.js": "838b1da9a8a8fccdda1e7d28a730ec6d",
-"flutter_bootstrap.js": "a8e539bf757801212dbc89062dcd79cf"};
+"flutter_bootstrap.js": "ad343a2f24de4155cf6f45f7e4212c9c"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
