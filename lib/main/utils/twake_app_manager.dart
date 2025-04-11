578a7240b7aa3784bfe386b1cc1901c75afcbc3e
dab246 <tdvu@linagora.com>
2025-04-11 15:57:29 +0700

    Avoid displaying duplicate session expiration dialogs
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/main/utils/twake_app_manager.dart
+++ lib/main/utils/twake_app_manager.dart
@@ -1,8 +1,13 @@
 
 class TwakeAppManager {
   bool _hasComposer = false;
+  bool _isSessionExpiresDialogOpened = false;
 
   void setHasComposer(bool value) => _hasComposer = value;
 
   bool get hasComposer => _hasComposer;
+
+  void setSessionExpiresDialogDisplayState(bool value) => _isSessionExpiresDialogOpened = value;
+
+  bool get isSessionExpiresDialogOpened => _isSessionExpiresDialogOpened;
 }
