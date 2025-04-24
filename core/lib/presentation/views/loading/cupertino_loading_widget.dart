7ec08e557f6c3ff6543b7153d6111b0a68049770
dab246 <tdvu@linagora.com>
2025-04-24 14:08:26 +0700

    TF-3678 Support accessibility for `loading icon` in the mail list area
    Signed-off-by: dab246 <tdvu@linagora.com>

--- core/lib/presentation/views/loading/cupertino_loading_widget.dart
+++ core/lib/presentation/views/loading/cupertino_loading_widget.dart
@@ -1,4 +1,5 @@
 import 'package:core/presentation/views/loading/cupertino_loading_widget_styles.dart';
+import 'package:core/presentation/views/semantics/icon_semantics.dart';
 import 'package:flutter/cupertino.dart';
 
 class CupertinoLoadingWidget extends StatelessWidget {
@@ -6,17 +7,19 @@ class CupertinoLoadingWidget extends StatelessWidget {
   final double? size;
   final EdgeInsetsGeometry? padding;
   final bool isCenter;
+  final String? semanticLabel;
 
   const CupertinoLoadingWidget({
     super.key,
     this.size,
     this.padding,
     this.isCenter = true,
+    this.semanticLabel,
   });
 
   @override
   Widget build(BuildContext context) {
-    final item = isCenter
+    Widget item = isCenter
       ? Center(
           child: SizedBox(
             width: size ?? CupertinoLoadingWidgetStyles.size,
@@ -36,6 +39,11 @@ class CupertinoLoadingWidget extends StatelessWidget {
             )
           ),
         );
+
+    if (semanticLabel != null) {
+      item = IconSemantics(label: semanticLabel!, child: item);
+    }
+
     if (padding != null) {
       return Padding(padding: padding!, child: item);
     } else {
