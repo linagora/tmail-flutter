8ff33b6278caab23b3b29df5ce62940e4adc7e12
DatDang <tddang@linagora.com>
2025-05-06 09:18:57 +0700

    fixup! TF-3639 Thread Detail controller

--- lib/features/thread_detail/domain/usecases/get_thread_by_id_interactor.dart
+++ lib/features/thread_detail/domain/usecases/get_thread_by_id_interactor.dart
@@ -4,6 +4,7 @@ import 'package:core/utils/app_logger.dart';
 import 'package:dartz/dartz.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+import 'package:tmail_ui_user/features/thread_detail/domain/exceptions/empty_thread_detail_exception.dart';
 import 'package:tmail_ui_user/features/thread_detail/domain/repository/thread_detail_repository.dart';
 import 'package:tmail_ui_user/features/thread_detail/domain/state/get_thread_by_id_state.dart';
 
@@ -22,6 +23,11 @@ class GetThreadByIdInteractor {
         threadId,
         accountId,
       );
+
+      if (result.isEmpty) {
+        throw EmptyThreadDetailException();
+      }
+
       yield Right(GetThreadByIdSuccess(result));
     } catch (e) {
       logError('GetEmailIdsByThreadIdInteractor::execute(): Exception: $e');
