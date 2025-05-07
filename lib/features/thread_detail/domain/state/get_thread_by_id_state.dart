140e9fc85d1780edcd8fc78237a881c686fe72de
Dat Dang <tddang@linagora.com>
2025-05-07 11:10:39 +0700

    [Thread Detail] [Part 1] TF-3639 data layer and controller (#3645)

---
+++ lib/features/thread_detail/domain/state/get_thread_by_id_state.dart
@@ -0,0 +1,18 @@
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email.dart';
+
+class GettingThreadById extends LoadingState {}
+
+class GetThreadByIdSuccess extends UIState {
+  final List<EmailId> emailIds;
+
+  GetThreadByIdSuccess(this.emailIds);
+
+  @override
+  List<Object> get props => [emailIds];
+}
+
+class GetThreadByIdFailure extends FeatureFailure {
+  GetThreadByIdFailure({super.exception});
+}
