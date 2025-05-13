2b7c058f8360ece572045e79b2dd8c8da1da288e
DatDang <tddang@linagora.com>
2025-05-13 16:26:45 +0700

    TF-3643 Thread Detail refactor toggle collapse expand

---
+++ lib/features/thread_detail/presentation/extension/toggle_thread_detail_collape_expand.dart
@@ -0,0 +1,46 @@
+import 'package:model/email/presentation_email.dart';
+import 'package:tmail_ui_user/features/email/presentation/bindings/email_bindings.dart';
+import 'package:tmail_ui_user/features/email/presentation/controller/single_email_controller.dart';
+import 'package:model/email/email_in_thread_status.dart';
+import 'package:tmail_ui_user/features/thread_detail/presentation/thread_detail_controller.dart';
+import 'package:tmail_ui_user/main/routes/route_navigation.dart';
+
+extension ToggleThreadDetailCollapeExpand on ThreadDetailController {
+  void toggleThreadDetailCollapeExpand(PresentationEmail presentationEmail) {
+    final emailId = presentationEmail.id;
+    final expansionStatus = presentationEmail.emailInThreadStatus;
+    if (emailId == null || expansionStatus == null) return;
+
+    scrollReverse.value = false;
+
+    if (expansionStatus == EmailInThreadStatus.expanded) {
+      emailIdsPresentation[emailId] = presentationEmail.copyWith(
+        emailInThreadStatus: EmailInThreadStatus.collapsed,
+      );
+      return;
+    }
+
+    final isInitialized = getBinding<SingleEmailController>(
+      tag: emailId.id.value,
+    ) != null;
+    if (!isInitialized) {
+      EmailBindings(initialEmail: presentationEmail).dependencies();
+    }
+    for (var key in emailIds) {
+      if (emailIdsPresentation[key] == null) continue;
+
+      if (key == emailId) {
+        emailIdsPresentation[key] = presentationEmail.copyWith(
+          emailInThreadStatus: EmailInThreadStatus.expanded,
+        );
+        continue;
+      }
+
+      if (emailIdsPresentation[key]?.emailInThreadStatus == EmailInThreadStatus.expanded) {
+        emailIdsPresentation[key] = emailIdsPresentation[key]?.copyWith(
+          emailInThreadStatus: EmailInThreadStatus.collapsed,
+        );
+      }
+    }
+  }
+}
