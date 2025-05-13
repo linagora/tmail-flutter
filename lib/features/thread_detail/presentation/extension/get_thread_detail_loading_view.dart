38d8d3ce3089d1d304d610f25357155c4e9f611d
DatDang <tddang@linagora.com>
2025-05-13 14:19:49 +0700

    TF-3643 Thread Detail [Part 2 fixup] optimize getThreadDetailLoadingView

--- lib/features/thread_detail/presentation/extension/get_thread_detail_loading_view.dart
+++ lib/features/thread_detail/presentation/extension/get_thread_detail_loading_view.dart
@@ -1,36 +1,32 @@
 import 'package:core/presentation/views/loading/cupertino_loading_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:tmail_ui_user/features/thread_detail/domain/state/get_thread_by_id_state.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/thread_detail_controller.dart';
 
 extension GetThreadDetailLoadingView on ThreadDetailController {
   Widget getThreadDetailLoadingView({
     required bool isResponsiveDesktop,
   }) {
-    return viewState.value.fold(
-      (failure) => const SizedBox.shrink(),
-      (success) => success is GettingThreadById
-        ? Expanded(
-            child: Container(
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(20),
-                ),
-              ),
-              margin: isResponsiveDesktop
-                ? const EdgeInsetsDirectional.only(end: 16)
-                : null,
-              padding: const EdgeInsets.only(top: 16),
-              child: const SizedBox(
-                width: 24,
-                height: 24,
-                child: CupertinoLoadingWidget(),
-              ),
-            ),
-        )
-        : const SizedBox.shrink(),
+    if (!loadingThreadDetail) return const SizedBox.shrink();
+
+    return Expanded(
+      child: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
+        margin: isResponsiveDesktop
+          ? const EdgeInsetsDirectional.only(end: 16)
+          : null,
+        padding: const EdgeInsets.only(top: 16),
+        child: const SizedBox(
+          width: 24,
+          height: 24,
+          child: CupertinoLoadingWidget(),
+        ),
+      ),
     );
   }
 }
