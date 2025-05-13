c6460a7adc5fe28870f5456f37e7bd5f2ad99c92
DatDang <tddang@linagora.com>
2025-05-13 14:16:52 +0700

    TF-3643 Thread Detail [Part 2 fixup] move isSearchRunning check into ThreadDetailController

--- lib/features/thread_detail/presentation/extension/close_thread_detail_action.dart
+++ lib/features/thread_detail/presentation/extension/close_thread_detail_action.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/instance_manager.dart';
-import 'package:tmail_ui_user/features/email/presentation/controller/email_supervisor_controller.dart';
 import 'package:tmail_ui_user/features/email/presentation/controller/single_email_controller.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/model/dashboard_routes.dart';
-import 'package:tmail_ui_user/features/search/email/presentation/search_email_controller.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/thread_detail_controller.dart';
 import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 
 extension CloseThreadDetailAction on ThreadDetailController {
   void closeThreadDetailAction(BuildContext? context) {
-    if (getBinding<SearchEmailController>()?.searchIsRunning.value == true) {
+    if (isSearchRunning) {
       mailboxDashBoardController.dispatchRoute(DashboardRoutes.searchEmail);
     } else {
       mailboxDashBoardController.dispatchRoute(DashboardRoutes.thread);
