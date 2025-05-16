32cfc713be58fd9943806b781d0cdf679859a36d
DatDang <tddang@linagora.com>
2025-05-16 15:38:59 +0700

    TF-3715 Thread Detail apply actions for collapsed email view

--- lib/features/thread_detail/presentation/extension/initialize_thread_detail_emails.dart
+++ lib/features/thread_detail/presentation/extension/initialize_thread_detail_emails.dart
@@ -28,7 +28,7 @@ extension InitializeThreadDetailEmails on ThreadDetailController {
       properties: EmailUtils.getPropertiesForEmailGetMethod(
         session!,
         accountId!,
-      ),
+      ).union(additionalProperties),
     ));
   }
 }
