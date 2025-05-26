b88e8d21b012126043dc71dc8b1e2a1155c99b85
DatDang <tddang@linagora.com>
2025-05-26 16:46:58 +0700

    TF-3715 Thread Detail fix refresh when mark star selected collapsed email

--- lib/features/thread_detail/presentation/extension/mark_collapsed_email_star_success.dart
+++ lib/features/thread_detail/presentation/extension/mark_collapsed_email_star_success.dart
@@ -2,9 +2,8 @@ import 'package:jmap_dart_client/jmap/mail/email/keyword_identifier.dart';
 import 'package:model/email/mark_star_action.dart';
 import 'package:model/extensions/presentation_email_extension.dart';
 import 'package:tmail_ui_user/features/email/domain/state/mark_as_email_star_state.dart';
-import 'package:tmail_ui_user/features/email/presentation/controller/single_email_controller.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/extensions/update_current_emails_flags_extension.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/thread_detail_controller.dart';
-import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 
 extension MarkCollapsedEmailStarSuccess on ThreadDetailController {
   void markCollapsedEmailStarSuccess(MarkAsStarEmailSuccess success) {
@@ -15,15 +14,11 @@ extension MarkCollapsedEmailStarSuccess on ThreadDetailController {
     if (updatedMarkedEmail == null) return;
 
     if (mailboxDashBoardController.selectedEmail.value?.id == success.emailId) {
-      mailboxDashBoardController.setSelectedEmail(updatedMarkedEmail);
+      mailboxDashBoardController.updateEmailFlagByEmailIds(
+        [success.emailId],
+        markStarAction: success.markStarAction,
+      );
     }
-    final markedEmailController = getBinding<SingleEmailController>(
-      tag: success.emailId.id.value,
-    );
-    if (markedEmailController != null) {
-      markedEmailController.handleSuccessViewState(success);
-    } else {
-      emailIdsPresentation[success.emailId] = updatedMarkedEmail;
-    }
+    emailIdsPresentation[success.emailId] = updatedMarkedEmail;
   }
 }
