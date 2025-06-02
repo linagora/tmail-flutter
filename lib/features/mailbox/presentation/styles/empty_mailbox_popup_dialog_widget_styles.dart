7d9089c21ba5984676ec1182d0c99d2a418804f0
dab246 <tdvu@linagora.com>
2025-06-02 11:27:39 +0700

    TF-3762 Update style for `Clean` button of Trash/Spam folder
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/mailbox/presentation/styles/empty_mailbox_popup_dialog_widget_styles.dart
+++ lib/features/mailbox/presentation/styles/empty_mailbox_popup_dialog_widget_styles.dart
@@ -1,16 +1,8 @@
-import 'package:core/presentation/extensions/color_extension.dart';
 import 'package:flutter/material.dart';
 
 class EmptyMailboxPopupDialogWidgetStyles {
-  static const EdgeInsetsGeometry emptyButtonPadding = EdgeInsetsDirectional.symmetric(vertical: 4, horizontal: 10);
-
-  static const Color emptyButtonBackground = Colors.transparent;
-
-  static const TextStyle emptyButtonTextStyle = TextStyle(
-    fontWeight: FontWeight.w400,
-    fontSize: 15,
-    color: AppColor.colorTextBody
-  );
+  static const EdgeInsetsGeometry emptyButtonPadding =
+      EdgeInsetsDirectional.symmetric(vertical: 4, horizontal: 8);
 
   static const Offset dialogOverlayOffset = Offset(0.0, 50.0);
 }
