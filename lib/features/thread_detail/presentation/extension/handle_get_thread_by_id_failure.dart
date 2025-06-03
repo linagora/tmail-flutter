7e5bb5814c48da36bcab02fd8b2923939aebdf67
DatDang <tddang@linagora.com>
2025-06-03 14:29:49 +0700

    TF-3769 Thread Detail fallback to single email when get thread fails

---
+++ lib/features/thread_detail/presentation/extension/handle_get_thread_by_id_failure.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:tmail_ui_user/features/thread_detail/domain/state/get_emails_by_ids_state.dart';
+import 'package:tmail_ui_user/features/thread_detail/domain/state/get_thread_by_id_state.dart';
+import 'package:tmail_ui_user/features/thread_detail/presentation/thread_detail_controller.dart';
+
+extension HandleGetThreadByIdFailure on ThreadDetailController {
+  void handleGetThreadByIdFailure(GetThreadByIdFailure failure) {
+    final selectedEmail = mailboxDashBoardController.selectedEmail.value;
+    if (selectedEmail != null) {
+      consumeState(Stream.value(Right(GetEmailsByIdsSuccess([selectedEmail]))));
+    } else {
+      showRetryToast(failure);
+    }
+  }
+}
