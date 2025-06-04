f1abfcac62e3b6b7919d1bf93526d1f1b6881a4c
DatDang <tddang@linagora.com>
2025-06-04 17:33:10 +0700

    TF-3712 Update chosen language to server settings

--- lib/features/manage_account/presentation/language_and_region/language_and_region_bindings.dart
+++ lib/features/manage_account/presentation/language_and_region/language_and_region_bindings.dart
@@ -1,33 +1,47 @@
 import 'package:get/get.dart';
 import 'package:tmail_ui_user/features/base/base_bindings.dart';
-import 'package:tmail_ui_user/features/manage_account/domain/repository/preferences_repository.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/usecases/save_language_interactor.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/usecases/save_language_to_server_settings_interactor.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/language_and_region/language_and_region_controller.dart';
+import 'package:tmail_ui_user/features/manage_account/presentation/preferences/bindings/preferences_interactors_bindings.dart';
+import 'package:tmail_ui_user/features/server_settings/domain/repository/server_settings_repository.dart';
 
 class LanguageAndRegionBindings extends BaseBindings {
 
   @override
-  void bindingsController() {
-    Get.lazyPut(() => LanguageAndRegionController(Get.find<SaveLanguageInteractor>()));
-  }
+  void dependencies() {
+    PreferencesInteractorsBindings().dependencies();
 
+    super.dependencies();
+  }
+  
+  @override
+  void bindingsController() {
+    Get.lazyPut(() => LanguageAndRegionController(
+      Get.find<SaveLanguageInteractor>(),
+      Get.find<SaveLanguageToServerSettingsInteractor>(),
+    ));
+  }
+  
   @override
   void bindingsDataSource() {
   }
-
+  
   @override
   void bindingsDataSourceImpl() {
   }
-
+  
   @override
   void bindingsInteractor() {
-    Get.lazyPut(() => SaveLanguageInteractor(Get.find<PreferencesRepository>()));
+    Get.lazyPut(
+      () => SaveLanguageToServerSettingsInteractor(Get.find<ServerSettingsRepository>()),
+    );
   }
-
+  
   @override
   void bindingsRepository() {
   }
-
+  
   @override
   void bindingsRepositoryImpl() {
   }
