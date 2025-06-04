4d67a3fb6a9eedd0fbe1c1f23dd91bd9bcfab59d
DatDang <tddang@linagora.com>
2025-06-04 17:33:10 +0700

    TF-3712 Update chosen language to server settings

--- lib/features/manage_account/presentation/language_and_region/language_and_region_controller.dart
+++ lib/features/manage_account/presentation/language_and_region/language_and_region_controller.dart
@@ -4,20 +4,31 @@ import 'dart:ui';
 import 'package:core/presentation/state/success.dart';
 import 'package:core/utils/app_logger.dart';
 import 'package:get/get.dart';
+import 'package:server_settings/server_settings/capability_server_settings.dart';
 import 'package:tmail_ui_user/features/base/base_controller.dart';
+import 'package:tmail_ui_user/features/home/domain/extensions/session_extensions.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/state/save_language_state.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/usecases/save_language_interactor.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/usecases/save_language_to_server_settings_interactor.dart';
+import 'package:tmail_ui_user/features/manage_account/presentation/manage_account_dashboard_controller.dart';
+import 'package:tmail_ui_user/main/error/capability_validator.dart';
 import 'package:tmail_ui_user/main/localizations/localization_service.dart';
 
 class LanguageAndRegionController extends BaseController {
 
   final SaveLanguageInteractor _saveLanguageInteractor;
+  final SaveLanguageToServerSettingsInteractor saveLanguageToServerSettingsInteractor;
 
   final listSupportedLanguages = <Locale>[].obs;
   final languageSelected = LocalizationService.defaultLocale.obs;
   final isLanguageMenuOverlayOpen = RxBool(false);
 
-  LanguageAndRegionController(this._saveLanguageInteractor);
+  final manageAccountDashBoardController = Get.find<ManageAccountDashBoardController>();
+
+  LanguageAndRegionController(
+    this._saveLanguageInteractor,
+    this.saveLanguageToServerSettingsInteractor,
+  );
 
   @override
   void onReady() {
@@ -53,6 +64,18 @@ class LanguageAndRegionController extends BaseController {
 
   void _saveLanguage(Locale localeCurrent) {
     consumeState(_saveLanguageInteractor.execute(localeCurrent));
+
+    final session = manageAccountDashBoardController.sessionCurrent;
+    final accountId = manageAccountDashBoardController.accountId.value;
+    if (accountId == null ||
+        session == null ||
+        !capabilityServerSettings.isSupported(session, accountId) ||
+        session.isLanguageReadOnly(accountId)) return;
+
+    consumeState(saveLanguageToServerSettingsInteractor.execute(
+      accountId,
+      localeCurrent,
+    ));
   }
 
   void toggleLanguageMenuOverlay() {
