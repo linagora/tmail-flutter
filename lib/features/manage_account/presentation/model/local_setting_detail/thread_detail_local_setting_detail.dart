ba4d789e9559aa579e31441c96dc4852f86e588f
DatDang <tddang@linagora.com>
2025-06-09 17:19:50 +0700

    TF-3769 Thread Detail refactor local setting

--- lib/features/manage_account/presentation/model/local_setting_detail/thread_detail_local_setting_detail.dart
+++ lib/features/manage_account/presentation/model/local_setting_detail/thread_detail_local_setting_detail.dart
@@ -1,19 +1,18 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/model/local_setting_detail/local_setting_detail.dart';
+import 'package:tmail_ui_user/features/manage_account/presentation/model/local_setting_options.dart';
 
 part 'thread_detail_local_setting_detail.g.dart';
 
 @JsonSerializable()
 class ThreadDetailLocalSettingDetail extends LocalSettingDetail<bool> {
-  static const settingType = 'threadDetailLocalSettingDetail';
-
   const ThreadDetailLocalSettingDetail(
     super.value, [
-    this.type = settingType,
+    this.type = SupportedLocalSetting.threadDetail,
   ]);
 
-  @JsonKey(defaultValue: settingType)
-  final String type;
+  @JsonKey(defaultValue: SupportedLocalSetting.threadDetail)
+  final SupportedLocalSetting type;
 
   factory ThreadDetailLocalSettingDetail.fromJson(Map<String, dynamic> json) =>
       _$ThreadDetailLocalSettingDetailFromJson(json);
