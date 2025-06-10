0c65a5f335ef223609d51dcb47e5f9976d52acb5
DatDang <tddang@linagora.com>
2025-06-10 11:17:55 +0700

    TF-3769 Thread Detail fix font height email receiver

--- lib/features/email/presentation/widgets/email_receiver_widget.dart
+++ lib/features/email/presentation/widgets/email_receiver_widget.dart
@@ -241,7 +241,7 @@ class _EmailReceiverWidgetState extends State<EmailReceiverWidget> {
         textStyle: ThemeUtils.textStyleHeadingHeadingSmall(
           color: Colors.black,
           fontWeight: FontWeight.w400,
-        ).copyWith(height: 1, letterSpacing: -0.17),
+        ).copyWith(height: 1, letterSpacing: -0.14),
         padding: const EdgeInsetsDirectional.symmetric(vertical: 2, horizontal: 8),
         backgroundColor: Colors.transparent,
         onTapActionCallback: () => widget.openEmailAddressDetailAction?.call(context, emailAddress),
