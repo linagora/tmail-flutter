20af823b42338a5b022ee5dadfbf2179c126e123
DatDang <tddang@linagora.com>
2025-06-11 16:40:08 +0700

    TF-3769 Thread Detail fix receive time position

--- lib/features/email/presentation/widgets/information_sender_and_receiver_builder.dart
+++ lib/features/email/presentation/widgets/information_sender_and_receiver_builder.dart
@@ -154,7 +154,7 @@ class InformationSenderAndReceiverBuilder extends StatelessWidget {
                           ],
                         ),
                       ),
-                      if (!isInsideThreadDetailView || !responsiveUtils.isMobile(context))
+                      if (!isInsideThreadDetailView)
                         ReceivedTimeBuilder(
                           emailSelected: emailSelected,
                           padding: const EdgeInsetsDirectional.only(start: 16, top: 2),
