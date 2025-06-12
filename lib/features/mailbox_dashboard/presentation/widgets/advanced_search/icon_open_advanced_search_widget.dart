b5caab64659b5ead941564ca0d5a46d44cf5a896
dab246 <tdvu@linagora.com>
2025-06-13 02:31:02 +0700

    TF-3763 Hide `Filter` icon when advanced search view is opened
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/mailbox_dashboard/presentation/widgets/advanced_search/icon_open_advanced_search_widget.dart
+++ lib/features/mailbox_dashboard/presentation/widgets/advanced_search/icon_open_advanced_search_widget.dart
@@ -1,13 +1,12 @@
 import 'package:core/presentation/extensions/color_extension.dart';
 import 'package:core/presentation/resources/image_paths.dart';
-import 'package:core/presentation/views/button/icon_button_web.dart';
+import 'package:core/presentation/views/button/tmail_button_widget.dart';
 import 'package:core/utils/app_logger.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/controller/advanced_filter_controller.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/controller/search_controller.dart'  as search;
-import 'package:tmail_ui_user/main/utils/app_utils.dart';
+import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
 
 class IconOpenAdvancedSearchWidget extends StatelessWidget {
   IconOpenAdvancedSearchWidget({Key? key}) : super(key: key);
@@ -18,31 +17,29 @@ class IconOpenAdvancedSearchWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => Padding(
-        padding: EdgeInsets.only(
-          right: AppUtils.isDirectionRTL(context) ? 0 : 8,
-          left: AppUtils.isDirectionRTL(context) ? 8 : 0,
-        ),
-        child: buildIconWeb(
-            splashRadius: 15,
-            minSize: 40,
-            iconPadding: EdgeInsets.only(
-              right: AppUtils.isDirectionRTL(context) ? 0 : 2,
-              left: AppUtils.isDirectionRTL(context) ? 2 : 0,
-            ),
-            icon: SvgPicture.asset(_imagePaths.icFilterAdvanced,
-                colorFilter: searchController.isAdvancedSearchViewOpen.isTrue || searchController.advancedSearchIsActivated.isTrue
-                    ? AppColor.colorFilterMessageEnabled.asFilter()
-                    : AppColor.colorFilterMessageDisabled.asFilter(),
-                width: 16,
-                height: 16),
-            onTap: () {
-              log('IconOpenAdvancedSearchWidget::build(): clicked');
-              advancedFilterController.initSearchFilterField(context);
-              searchController.openAdvanceSearch();
-            }),
-      ),
-    );
+    return Obx(() {
+      if (searchController.isAdvancedSearchViewOpen.isTrue) {
+        return const SizedBox.shrink();
+      }
+
+      return TMailButtonWidget.fromIcon(
+        icon: _imagePaths.icFilterAdvanced,
+        iconSize: 22,
+        iconColor: searchController.advancedSearchIsActivated.isTrue
+          ? AppColor.primaryMain
+          : AppColor.steelGray400,
+        padding: const EdgeInsets.all(4),
+        margin: const EdgeInsets.symmetric(horizontal: 12),
+        backgroundColor: Colors.transparent,
+        tooltipMessage: AppLocalizations.of(context).advancedSearch,
+        onTapActionCallback: () => _onClickOpenAdvancedSearchView(context),
+      );
+    });
+  }
+
+  void _onClickOpenAdvancedSearchView(BuildContext context) {
+    log('IconOpenAdvancedSearchWidget::_onClickOpenAdvancedSearchView:');
+    advancedFilterController.initSearchFilterField(context);
+    searchController.openAdvanceSearch();
   }
 }
