bf8ce9ca445478204cd1aa4e4248d5a964d3366b
dab246 <tdvu@linagora.com>
2025-06-18 16:06:09 +0700

    TF-3795 Fix cannot send emails with stalwart
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/composer/presentation/extensions/setup_email_request_read_receipt_flag_extension.dart
+++ lib/features/composer/presentation/extensions/setup_email_request_read_receipt_flag_extension.dart
@@ -3,6 +3,7 @@ import 'package:model/email/email_action_type.dart';
 import 'package:server_settings/server_settings/tmail_server_settings_extension.dart';
 import 'package:tmail_ui_user/features/composer/presentation/composer_controller.dart';
 import 'package:tmail_ui_user/features/email/presentation/model/composer_arguments.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/extensions/handle_preferences_setting_extension.dart';
 import 'package:tmail_ui_user/features/server_settings/domain/state/get_server_setting_state.dart';
 
 extension SetupEmailRequestReadReceiptFlagExtension on ComposerController {
@@ -12,6 +13,11 @@ extension SetupEmailRequestReadReceiptFlagExtension on ComposerController {
       hasRequestReadReceipt.value = arguments.hasRequestReadReceipt ?? false;
     } else if (currentEmailActionType != EmailActionType.editDraft &&
         currentEmailActionType != EmailActionType.editAsNewEmail) {
+
+      if (!mailboxDashBoardController.isServerSettingsCapabilitySupported) {
+        return;
+      }
+
       getServerSetting();
     }
   }
