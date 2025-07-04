da8b4b7f58cb4edb23841a49d622b4a27733ad5b
dab246 <tdvu@linagora.com>
2025-07-04 14:18:21 +0700

    Use the correct rich text controller for each platform to avoid errors
    Signed-off-by: dab246 <tdvu@linagora.com>

--- lib/features/manage_account/presentation/vacation/utils/vacation_utils.dart
+++ lib/features/manage_account/presentation/vacation/utils/vacation_utils.dart
@@ -3,8 +3,6 @@ import 'package:core/presentation/utils/responsive_utils.dart';
 import 'package:flutter/material.dart';
 
 class VacationUtils {
-  static const String vacationTagName = 'Vacation';
-
   static EdgeInsets getPaddingView(BuildContext context, ResponsiveUtils responsiveUtils) {
     if (responsiveUtils.isDesktop(context)) {
       return const EdgeInsets.symmetric(vertical: 24, horizontal: 20);
