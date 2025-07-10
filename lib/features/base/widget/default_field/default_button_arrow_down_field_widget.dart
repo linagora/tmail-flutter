58a8d93ed33f5948efe7e2ff95e69a380075a1b1
dab246 <tdvu@linagora.com>
2025-07-11 02:48:26 +0700

    Reuse widgets for advanced search form view
    Signed-off-by: dab246 <tdvu@linagora.com>

---
+++ lib/features/base/widget/default_field/default_button_arrow_down_field_widget.dart
@@ -0,0 +1,55 @@
+import 'package:core/presentation/extensions/color_extension.dart';
+import 'package:core/presentation/utils/theme_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DefaultButtonArrowDownFieldWidget extends StatelessWidget {
+  final String text;
+  final String iconArrowDown;
+  final VoidCallback onTap;
+
+  const DefaultButtonArrowDownFieldWidget({
+    super.key,
+    required this.text,
+    required this.iconArrowDown,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        focusColor: AppColor.colorMailboxHovered,
+        child: Container(
+          height: 40,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            border: Border.all(
+              color: AppColor.m3Neutral90,
+              width: 1,
+            ),
+          ),
+          padding: const EdgeInsetsDirectional.only(start: 12, end: 8),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  text,
+                  style: ThemeUtils.textStyleBodyBody3(
+                    color: AppColor.m3SurfaceBackground,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              SvgPicture.asset(iconArrowDown),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
