f49bd179b0fd31cf18e8d81a947d6501ea49a467
tddang-linagora <tddang-linagora@users.noreply.github.com>
2025-07-18 09:05:47 +0000

    deploy: d858936c3a714ac873fb88dfe7f4f2b54bab01db

--- 3883/flutter_service_worker.js
+++ 3883/flutter_service_worker.js
@@ -307,7 +307,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d80ac80f626c6d75cc7bbb1c8c34c981
 "main.dart.js_6.part.js": "73381785eda53e2e5544b37d7cb2d894",
 "manifest.json": "c2c66c9dd73fa42a8a2c5e95615de7d1",
 "main.dart.js_9.part.js": "9fd4c893a1051bc1a77ef067ae193349",
-"flutter_bootstrap.js": "b10c9e02ffc281bc81ac437117a375a8",
+"flutter_bootstrap.js": "3e998ea0de21edfd2f806a8d295b46e6",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/canvaskit.wasm": "1f237a213d7370cf95f443d896176460",
@@ -353,7 +353,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d80ac80f626c6d75cc7bbb1c8c34c981
 "worker_service/img/ic-close-1x.png": "27b9c71ff0cf463da5f1affb11b4c204",
 "worker_service/style.css": "3ec186c93eb7bdab17e882de738e3123",
 "worker_service/worker_service.js": "c741398f2ab58bd36f7c510a8429fc41",
-"main.dart.js": "232b25d4535f476f918b81bfa964ab57",
+"main.dart.js": "fc1027d376bdebc9b5988989d1617de1",
 "logout-callback.html": "999326dc9a0f37c9a9a4280b4a35dc51",
 "favicon.svg": "1734d2c8d930abd57ebba9c158506714"};
 // The application shell files that are downloaded before a service worker can
