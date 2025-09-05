dc3b4d46dde1243855d5ebbc5082ff54f059538e
dab246 <tdvu@linagora.com>
2025-09-05 16:09:16 +0700

    TF-4004 Change new design for list email rules view

--- lib/features/manage_account/presentation/email_rules/widgets/list_email_rules_widget.dart
+++ lib/features/manage_account/presentation/email_rules/widgets/list_email_rules_widget.dart
@@ -1,81 +1,58 @@
-import 'package:core/presentation/extensions/color_extension.dart';
-import 'package:core/presentation/utils/theme_utils.dart';
-import 'package:core/utils/app_logger.dart';
+import 'package:core/presentation/resources/image_paths.dart';
+import 'package:core/presentation/utils/responsive_utils.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:rule_filter/rule_filter/rule_id.dart';
-import 'package:tmail_ui_user/features/manage_account/presentation/email_rules/email_rules_controller.dart';
+import 'package:rule_filter/rule_filter/tmail_rule.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/email_rules/widgets/email_rule_item_widget.dart';
-import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
 
-class ListEmailRulesWidget extends GetWidget<EmailRulesController> {
-  const ListEmailRulesWidget({Key? key}) : super(key: key);
+class ListEmailRulesWidget extends StatelessWidget {
+  final List<TMailRule> listEmailRule;
+  final ImagePaths imagePaths;
+  final ResponsiveUtils responsiveUtils;
+  final OnEditEmailRuleAction? onEditEmailRuleAction;
+  final OnDeleteEmailRuleAction? onDeleteEmailRuleAction;
+  final OnMoreEmailRuleAction? onMoreEmailRuleAction;
+
+  const ListEmailRulesWidget({
+    Key? key,
+    required this.listEmailRule,
+    required this.imagePaths,
+    required this.responsiveUtils,
+    this.onEditEmailRuleAction,
+    this.onDeleteEmailRuleAction,
+    this.onMoreEmailRuleAction,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: AppColor.colorBackgroundWrapIconStyleCode,
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(
-            width: 1,
-            color: AppColor.colorBorderListRuleFilter)
+    final isMobile = responsiveUtils.isMobile(context);
+
+    return Padding(
+      padding: const EdgeInsetsDirectional.only(
+        top: 12,
       ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  color: AppColor.colorBackgroundHeaderListRuleFilter,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      topRight: Radius.circular(16)),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  vertical: 28,
-                  horizontal: 24,
-                ),
-                child: Text(AppLocalizations.of(context).headerNameOfRules,
-                    style: ThemeUtils.defaultTextStyleInterFont.copyWith(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        color: AppColor.colorTextButtonHeaderThread)),
-              ),
-              Obx(() {
-                if (controller.listEmailRule.isNotEmpty) {
-                  return const Divider();
-                } else {
-                  return const SizedBox.shrink();
-                }
-              }),
-              Obx(() {
-                log('ListEmailRulesWidget::build(): ${controller.listEmailRule}');
-                return ListView.separated(
-                  shrinkWrap: true,
-                  itemCount: controller.listEmailRule.length,
-                  primary: false,
-                  padding: EdgeInsets.zero,
-                  itemBuilder: (context, index) {
-                    final ruleWithId = controller.listEmailRule[index]
-                        .copyWith(id: RuleId(id: Id(index.toString())));
-                    log('ListEmailRulesWidget::build(): $ruleWithId');
-                    return EmailRulesItemWidget(rule: ruleWithId);
-                  },
-                  separatorBuilder: (context, index) {
-                    if (controller.listEmailRule.isNotEmpty) {
-                      return const Divider();
-                    } else {
-                      return const SizedBox.shrink();
-                    }
-                  },
-                );
-              }),
-            ]),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: listEmailRule.length,
+        primary: false,
+        padding: EdgeInsetsDirectional.only(
+          bottom: 16,
+          start: isMobile ? 16 : 0,
+          end: isMobile ? 16 : 0,
+        ),
+        itemBuilder: (context, index) {
+          final ruleWithId = listEmailRule[index].copyWith(
+            id: RuleId.fromString(index.toString()),
+          );
+          return EmailRulesItemWidget(
+            rule: ruleWithId,
+            responsiveUtils: responsiveUtils,
+            imagePaths: imagePaths,
+            onEditEmailRuleAction: onEditEmailRuleAction,
+            onDeleteEmailRuleAction: onDeleteEmailRuleAction,
+            onMoreEmailRuleAction: onMoreEmailRuleAction,
+          );
+        },
       ),
     );
   }
