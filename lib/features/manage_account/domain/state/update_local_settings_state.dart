08e21e944a5098e0986a142fa19cf82797407aeb
dab246 <tdvu@linagora.com>
2025-09-09 11:12:57 +0700

    TF-3977 Separate settings keys for easier in conflict

--- lib/features/manage_account/domain/state/update_local_settings_state.dart
+++ lib/features/manage_account/domain/state/update_local_settings_state.dart
@@ -1,16 +1,16 @@
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
-import 'package:tmail_ui_user/features/manage_account/presentation/model/preferences/preferences_root.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/model/preferences/preferences_setting.dart';
 
 class UpdatingLocalSettingsState extends LoadingState {}
 
 class UpdateLocalSettingsSuccess extends UIState {
-  final PreferencesRoot preferencesRoot;
+  final PreferencesSetting preferencesSetting;
 
-  UpdateLocalSettingsSuccess(this.preferencesRoot);
+  UpdateLocalSettingsSuccess(this.preferencesSetting);
 
   @override
-  List<Object?> get props => [preferencesRoot];
+  List<Object?> get props => [preferencesSetting];
 }
 
 class UpdateLocalSettingsFailure extends FeatureFailure {
