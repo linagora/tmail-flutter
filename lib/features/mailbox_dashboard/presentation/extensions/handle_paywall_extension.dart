f117f37a9243b78b0686e635499bc30ded0b1ac5
dab246 <tdvu@linagora.com>
2025-09-10 14:08:44 +0700

    TF-3985 Write unit test for `isPremiumAvailable` method

--- lib/features/mailbox_dashboard/presentation/extensions/handle_paywall_extension.dart
+++ lib/features/mailbox_dashboard/presentation/extensions/handle_paywall_extension.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tmail_ui_user/features/home/domain/extensions/session_extensions.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart';
 import 'package:tmail_ui_user/features/paywall/domain/model/paywall_url_pattern.dart';
 import 'package:tmail_ui_user/features/paywall/domain/usecases/get_paywall_url_interactor.dart';
@@ -8,16 +7,14 @@ import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 import 'package:tmail_ui_user/main/utils/app_utils.dart';
 
 extension ValidateSaasPremiumAvailableExtension on MailboxDashBoardController {
-  bool get isPremiumAvailable {
+  bool validatePremiumIsAvailable() {
     if (accountId.value == null || sessionCurrent == null) {
       return false;
     }
-
-    final saasAccount = sessionCurrent?.getSaaSAccountCapability(
-      accountId.value!,
+    return isPremiumAvailable(
+      accountId: accountId.value,
+      session: sessionCurrent,
     );
-
-    return saasAccount?.isPremiumAvailable ?? false;
   }
 
   void loadPaywallUrl() {
