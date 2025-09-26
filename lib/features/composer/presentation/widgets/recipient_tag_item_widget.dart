69e5662004edc93cc37246b53844e3c9ec3d1b5b
dab246 <tdvu@linagora.com>
2025-09-26 15:44:19 +0700

    TF-4064 Support drag & drop email addresses in composer on mobile

--- lib/features/composer/presentation/widgets/recipient_tag_item_widget.dart
+++ lib/features/composer/presentation/widgets/recipient_tag_item_widget.dart
@@ -139,23 +139,36 @@ class RecipientTagItemWidget extends StatelessWidget {
     );
 
     if (PlatformInfo.isWeb || isTestingForWeb) {
-      tagWidget = Draggable<DraggableEmailAddress>(
-        data: DraggableEmailAddress(
-          emailAddress: currentEmailAddress,
-          filterField: prefix.filterField,
-          composerId: composerId,
-        ),
-        feedback: DraggableRecipientTagWidget(
-          imagePaths: imagePaths,
-          emailAddress: currentEmailAddress,
-        ),
-        childWhenDragging: DraggableRecipientTagWidget(
-          imagePaths: imagePaths,
-          emailAddress: currentEmailAddress,
-        ),
+      tagWidget = MouseRegion(
+        cursor: SystemMouseCursors.grab,
         child: tagWidget,
       );
     }
+
+    tagWidget = Draggable<DraggableEmailAddress>(
+      data: DraggableEmailAddress(
+        emailAddress: currentEmailAddress,
+        filterField: prefix.filterField,
+        composerId: composerId,
+      ),
+      feedback: DraggableRecipientTagWidget(
+        imagePaths: imagePaths,
+        emailAddress: currentEmailAddress,
+      ),
+      childWhenDragging: PlatformInfo.isMobile
+        ? Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: DraggableRecipientTagWidget(
+              imagePaths: imagePaths,
+              emailAddress: currentEmailAddress,
+            ),
+          )
+        : DraggableRecipientTagWidget(
+            imagePaths: imagePaths,
+            emailAddress: currentEmailAddress,
+          ),
+      child: tagWidget,
+    );
 
     if ((PlatformInfo.isWeb || isTestingForWeb) && PlatformInfo.isCanvasKit) {
       tagWidget = Padding(
