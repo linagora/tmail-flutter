6e9a5f6086055206174c007f360d3b4b71de24b3
dab246 <tdvu@linagora.com>
2025-10-03 13:50:30 +0700

    TF-4084 Fix cannot read or scroll emails when opening them in Twake Mail on Android chrome mobile browser

--- core/lib/utils/platform_info.dart
+++ core/lib/utils/platform_info.dart
@@ -1,6 +1,7 @@
 import 'package:core/utils/app_logger.dart';
 import 'package:core/utils/web_renderer/canvas_kit.dart';
 import 'package:flutter/foundation.dart';
+import 'package:universal_html/html.dart' as universal_html;
 
 abstract class PlatformInfo {
   @visibleForTesting
@@ -44,4 +45,25 @@ abstract class PlatformInfo {
     log('PlatformInfo::platformNameOS(): $platformName');
     return platformName;
   }
+
+  static String get userAgent =>
+      isWeb ? universal_html.window.navigator.userAgent.toLowerCase() : '';
+
+  /// Web browser on a mobile phone
+  static bool get isWebMobile =>
+      isWeb && (userAgent.contains('iphone') ||
+          (userAgent.contains('android') && userAgent.contains('mobile')));
+
+  /// Web browser on a tablet device
+  static bool get isWebTablet =>
+      isWeb && (userAgent.contains('ipad') ||
+          (userAgent.contains('android') && !userAgent.contains('mobile')));
+
+  /// Web browser on a desktop (PC/Mac)
+  static bool get isWebDesktop =>
+      isWeb && !isWebMobile && !isWebTablet;
+
+  /// Touch devices
+  static bool get isWebTouchDevice =>
+      isWeb && (isWebMobile || isWebTablet);
 }
