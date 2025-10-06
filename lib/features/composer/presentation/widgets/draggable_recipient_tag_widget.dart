22b16b09ce059c489e2a5e87955d0869274f19d3
dab246 <tdvu@linagora.com>
2025-10-06 12:46:53 +0700

    TF-4013 Collapse all recipients fields when unfocus

--- lib/features/composer/presentation/widgets/draggable_recipient_tag_widget.dart
+++ lib/features/composer/presentation/widgets/draggable_recipient_tag_widget.dart
@@ -1,25 +1,21 @@
 import 'package:core/presentation/extensions/color_extension.dart';
 import 'package:core/presentation/extensions/string_extension.dart';
 import 'package:core/presentation/resources/image_paths.dart';
+import 'package:core/presentation/utils/theme_utils.dart';
 import 'package:core/presentation/views/avatar/gradient_circle_avatar_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
 import 'package:jmap_dart_client/jmap/mail/email/email_address.dart';
 import 'package:model/extensions/email_address_extension.dart';
-import 'package:tmail_ui_user/features/composer/presentation/styles/draggable_recipient_tag_widget_style.dart';
 
 class DraggableRecipientTagWidget extends StatelessWidget {
-
   final EmailAddress emailAddress;
-  final EdgeInsetsGeometry? padding;
+  final ImagePaths imagePaths;
 
-  final _imagePaths = Get.find<ImagePaths>();
-
-  DraggableRecipientTagWidget({
+  const DraggableRecipientTagWidget({
     super.key,
     required this.emailAddress,
-    this.padding,
+    required this.imagePaths,
   });
 
   @override
@@ -27,34 +23,50 @@ class DraggableRecipientTagWidget extends StatelessWidget {
     return MouseRegion(
       cursor: SystemMouseCursors.grab,
       child: Container(
-        decoration: const ShapeDecoration(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(DraggableRecipientTagWidgetStyle.radius)),
-          ),
-          color: DraggableRecipientTagWidgetStyle.backgroundColor
+        constraints: const BoxConstraints(maxWidth: 267),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: AppColor.primaryColor,
         ),
-        padding: padding ?? DraggableRecipientTagWidgetStyle.padding,
+        height: 32,
+        padding: const EdgeInsetsDirectional.only(start: 8, end: 4),
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            if (emailAddress.emailAddress.isNotEmpty)
-              GradientCircleAvatarIcon(
-                colors: emailAddress.avatarColors,
-                label: emailAddress.emailAddress.firstCharacterToUpperCase,
-                labelFontSize: DraggableRecipientTagWidgetStyle.avatarLabelFontSize,
-                iconSize: DraggableRecipientTagWidgetStyle.avatarIconSize,
+            GradientCircleAvatarIcon(
+              colors: emailAddress.avatarColors,
+              label: emailAddress.asString().firstCharacterToUpperCase,
+              textStyle: ThemeUtils.textStyleInter500().copyWith(
+                color: Colors.white,
+                fontSize: 14,
+                height: 19.2 / 14,
+                letterSpacing: 0.0,
               ),
-            Padding(
-              padding: DraggableRecipientTagWidgetStyle.labelPadding,
-              child: DefaultTextStyle(
-                style: DraggableRecipientTagWidgetStyle.labelTextStyle,
-                child: Text(emailAddress.asString()),
+              iconSize: 20,
+            ),
+            const SizedBox(width: 4),
+            Flexible(
+              child: Text(
+                emailAddress.asString(),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: ThemeUtils.textStyleInter400.copyWith(
+                  color: Colors.white,
+                  fontSize: 17,
+                  height: 1.0,
+                  letterSpacing: -0.17,
+                ),
               ),
             ),
-            SvgPicture.asset(
-              _imagePaths.icClose,
-              colorFilter: DraggableRecipientTagWidgetStyle.deleteIconColor.asFilter(),
-              fit: BoxFit.fill
+            const SizedBox(width: 4),
+            Padding(
+              padding: const EdgeInsets.all(4),
+              child: SvgPicture.asset(
+                imagePaths.icClose,
+                width: 20,
+                height: 20,
+                colorFilter: AppColor.m3SysOutline.asFilter(),
+              ),
             )
           ],
         ),
