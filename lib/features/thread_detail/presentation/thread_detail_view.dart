cd2ca1ec3c5b5160820823905d9f0183536b07e3
dab246 <tdvu@linagora.com>
2025-10-07 01:25:19 +0700

    TF-4075 Jump to attachments list position on mobile

--- lib/features/thread_detail/presentation/thread_detail_view.dart
+++ lib/features/thread_detail/presentation/thread_detail_view.dart
@@ -11,18 +11,18 @@ import 'package:tmail_ui_user/features/email/presentation/action/email_ui_action
 import 'package:tmail_ui_user/features/email/presentation/styles/email_view_app_bar_widget_styles.dart';
 import 'package:tmail_ui_user/features/email/presentation/widgets/email_view_bottom_bar_widget.dart';
 import 'package:tmail_ui_user/features/manage_account/presentation/extensions/vacation_response_extension.dart';
+import 'package:tmail_ui_user/features/manage_account/presentation/vacation/widgets/vacation_notification_message_widget.dart';
 import 'package:tmail_ui_user/features/thread_detail/domain/state/get_thread_by_id_state.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/extension/close_thread_detail_action.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/extension/get_thread_detail_action_status.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/extension/get_thread_details_email_views.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/extension/on_thread_detail_action_click.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/extension/on_thread_page_changed.dart';
+import 'package:tmail_ui_user/features/thread_detail/presentation/extension/parsing_email_opened_properties_extension.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/thread_detail_controller.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/widgets/thread_detail_app_bar.dart';
 import 'package:tmail_ui_user/features/thread_detail/presentation/widgets/thread_detail_cupertino_loading_widget.dart';
 import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
-
-import '../../manage_account/presentation/vacation/widgets/vacation_notification_message_widget.dart';
 
 class ThreadDetailView extends GetWidget<ThreadDetailController> {
   const ThreadDetailView({super.key});
@@ -45,7 +45,9 @@ class ThreadDetailView extends GetWidget<ThreadDetailController> {
             onThreadActionClick: controller.onThreadDetailActionClick,
             onThreadMoreActionClick: controller.onThreadDetailMoreActionClick,
             onOpenAttachmentListAction: controller.isEmailExpandedHasAttachments
-                ? controller.onOpenAttachmentListAction
+                ? () => controller.onOpenAttachmentListAction(
+                    controller.responsiveUtils.getSizeScreenHeight(context),
+                  )
                 : null,
             optionWidgets: [
               if (controller.previousAvailable)
