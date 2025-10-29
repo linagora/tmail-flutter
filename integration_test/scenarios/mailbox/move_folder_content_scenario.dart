a5cc102d0ec53b4235687078c9d5b6da28029638
dab246 <tdvu@linagora.com>
2025-10-29 16:20:10 +0700

    TF-4053 Add E2E test for test case move folder content

---
+++ integration_test/scenarios/mailbox/move_folder_content_scenario.dart
@@ -0,0 +1,83 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:model/extensions/presentation_mailbox_extension.dart';
+import 'package:model/mailbox/presentation_mailbox.dart';
+import 'package:tmail_ui_user/features/mailbox/presentation/widgets/trailing_mailbox_item_widget.dart';
+import 'package:tmail_ui_user/main/localizations/app_localizations.dart';
+
+import '../../base/base_test_scenario.dart';
+import '../../models/provisioning_email.dart';
+import '../../robots/mailbox_menu_robot.dart';
+import '../../robots/thread_robot.dart';
+
+class MoveFolderContentScenario extends BaseTestScenario {
+  const MoveFolderContentScenario(super.$);
+
+  @override
+  Future<void> runTestLogic() async {
+    const email = String.fromEnvironment('BASIC_AUTH_EMAIL');
+    const emailSubject = 'Move folder content';
+
+    final threadRobot = ThreadRobot($);
+    final mailboxMenuRobot = MailboxMenuRobot($);
+    final appLocalizations = AppLocalizations();
+
+    final listEmails = List.generate(
+      40,
+      (_) => ProvisioningEmail(
+        toEmail: email,
+        subject: emailSubject,
+        content: '',
+      ),
+    );
+
+    await provisionEmail(listEmails);
+    await $.pumpAndTrySettle(duration: const Duration(seconds: 2));
+
+    await threadRobot.openMailbox();
+    await $.pumpAndTrySettle();
+    _expectInboxUnreadCountVisible();
+
+    await mailboxMenuRobot.longPressMailboxWithName(
+      appLocalizations.inboxMailboxDisplayName,
+    );
+    await mailboxMenuRobot.tapMoveFolderContentAction(
+      appLocalizations.templatesMailboxDisplayName,
+    );
+    await $.pumpAndTrySettle(duration: const Duration(seconds: 3));
+
+    await threadRobot.openMailbox();
+    await $.pumpAndTrySettle();
+    await mailboxMenuRobot.openFolderByName(
+      appLocalizations.templatesMailboxDisplayName,
+    );
+    await $.pumpAndTrySettle();
+    await _expectEmailWithSubjectVisible(emailSubject);
+
+    await threadRobot.openMailbox();
+    await $.pumpAndTrySettle();
+    await mailboxMenuRobot.openFolderByName(
+      appLocalizations.inboxMailboxDisplayName,
+    );
+    await $.pumpAndTrySettle();
+    await _expectEmailWithSubjectInVisible(emailSubject);
+  }
+
+  Future<void> _expectEmailWithSubjectVisible(String subject) async {
+    await expectViewVisible($(subject));
+  }
+
+  Future<void> _expectEmailWithSubjectInVisible(String subject) async {
+    await expectViewInvisible($(subject));
+  }
+
+  void _expectInboxUnreadCountVisible() {
+    expect(
+      $(TrailingMailboxItemWidget).which<TrailingMailboxItemWidget>((widget) {
+        final mailbox = widget.mailboxNode.item;
+        return mailbox.role == PresentationMailbox.roleInbox &&
+            mailbox.countUnreadEmails > 0;
+      }),
+      findsOneWidget,
+    );
+  }
+}
