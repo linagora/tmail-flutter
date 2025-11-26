724781ca46fe3a802c814edac21e30cc24a0da31
dab246 <tdvu@linagora.com>
2025-11-26 16:32:53 +0700

    TF-4178 Add datasource/repository/interactor for create new label

--- lib/features/labels/data/network/label_api.dart
+++ lib/features/labels/data/network/label_api.dart
@@ -1,12 +1,16 @@
 import 'package:jmap_dart_client/http/http_client.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:jmap_dart_client/jmap/jmap_request.dart';
 import 'package:labels/labels.dart';
+import 'package:tmail_ui_user/features/base/mixin/handle_error_mixin.dart';
+import 'package:uuid/uuid.dart';
 
-class LabelApi {
+class LabelApi with HandleSetErrorMixin {
   final HttpClient _httpClient;
+  final Uuid _uuid;
 
-  LabelApi(this._httpClient);
+  LabelApi(this._httpClient, this._uuid);
 
   Future<List<Label>> getAllLabels(AccountId accountId) async {
     final builder = JmapRequestBuilder(_httpClient, ProcessingInvocation());
@@ -22,5 +26,35 @@ class LabelApi {
     );
 
     return response?.list ?? [];
+  }
+
+  Future<Label> createNewLabel(AccountId accountId, Label labelData) async {
+    final generateCreateId = Id(_uuid.v1());
+
+    final method = SetLabelMethod(accountId)
+      ..addCreate(generateCreateId, labelData);
+
+    final builder = JmapRequestBuilder(_httpClient, ProcessingInvocation());
+    final invocation = builder.invocation(method);
+
+    final result =
+        await (builder..usings(method.requiredCapabilities)).build().execute();
+
+    final response = result.parse<SetLabelResponse>(
+      invocation.methodCallId,
+      SetLabelResponse.deserialize,
+    );
+
+    final labelCreated = response?.created?[generateCreateId];
+
+    if (labelCreated != null) {
+      final newLabelCreated = labelCreated.copyWith(
+        displayName: labelData.displayName,
+        color: labelData.color,
+      );
+      return newLabelCreated;
+    } else {
+      throw parseErrorForSetResponse(response, generateCreateId);
+    }
   }
 }
