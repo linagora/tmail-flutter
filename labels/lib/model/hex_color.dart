af52532e0dc4cd44a91dd637e260306d5124f5f4
dab246 <tdvu@linagora.com>
2025-11-27 10:30:59 +0700

    TF-4169 Add precheck for HexColor and unit test

--- labels/lib/model/hex_color.dart
+++ labels/lib/model/hex_color.dart
@@ -1,9 +1,24 @@
 import 'package:equatable/equatable.dart';
+import 'package:quiver/check.dart';
 
 class HexColor with EquatableMixin {
+  // Only accept #RRGGBB or #AARRGGBB format
+  static final RegExp _hexPattern =
+      RegExp(r'^#[0-9a-fA-F]{6}([0-9a-fA-F]{2})?$');
+
   final String value;
 
-  HexColor(this.value);
+  HexColor(this.value) {
+    checkArgument(value.isNotEmpty, message: 'hex string must not be empty');
+    checkArgument(
+      value.startsWith('#'),
+      message: 'hex string must start with #',
+    );
+    checkArgument(
+      _hexPattern.hasMatch(value),
+      message: 'invalid hex format: expected #RRGGBB or #AARRGGBB',
+    );
+  }
 
   @override
   List<Object> get props => [value];
