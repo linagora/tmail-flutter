74f295d3b6ebc49d3022239e245a675b9185d691
Théo Poizat <hello@zatteo.com>
2025-12-04 17:04:00 +0100

    Add scribe module

---
+++ scribe/lib/scribe.dart
@@ -0,0 +1,15 @@
+library scribe;
+
+export 'scribe/ai/domain/usecases/generate_ai_text_interactor.dart';
+export 'scribe/ai/domain/repository/ai_scribe_repository.dart';
+export 'scribe/ai/domain/model/ai_response.dart';
+export 'scribe/ai/domain/state/generate_ai_text_state.dart';
+export 'scribe/ai/presentation/widgets/ai_scribe.dart';
+export 'scribe/ai/presentation/widgets/ai_scribe_bar.dart';
+export 'scribe/ai/presentation/widgets/ai_scribe_button.dart';
+export 'scribe/ai/presentation/widgets/ai_scribe_menu.dart';
+export 'scribe/ai/presentation/widgets/ai_scribe_suggestion.dart';
+export 'scribe/ai/presentation/model/ai_action.dart';
+export 'scribe/ai/presentation/model/ai_scribe_menu_action.dart';
+export 'scribe/ai/data/repository/ai_repository_impl.dart';
+export 'scribe/ai/data/datasource/ai_datasource.dart';
