253f22c600a13d97ee1f9a2e73e8ab55a734fcd5
Théo Poizat <hello@zatteo.com>
2025-12-11 16:46:51 +0100

    Remove value now unused
    No more API key and url needed because everything is managed by the backend.
    
    Also backend does not accept a model parameter for the moment.

--- scribe/lib/scribe/ai/data/config/ai_config.dart
+++ scribe/lib/scribe/ai/data/config/ai_config.dart
@@ -4,8 +4,4 @@ class AIConfig {
   const AIConfig._();
 
   static bool get isAiEnabled => dotenv.get('AI_ENABLED', fallback: 'false') == 'true';
-
-  static String get aiApiKey => dotenv.get('AI_API_KEY', fallback: '');
-
-  static String get aiApiUrl => dotenv.get('AI_API_URL', fallback: '');
 }
