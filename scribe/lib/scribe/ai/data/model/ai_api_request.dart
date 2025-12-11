a61b9a42c0ddfb4a79a86b6e46b52337a13970b8
Théo Poizat <hello@zatteo.com>
2025-12-11 16:46:51 +0100

    Remove value now unused
    No more API key and url needed because everything is managed by the backend.
    
    Also backend does not accept a model parameter for the moment.

--- scribe/lib/scribe/ai/data/model/ai_api_request.dart
+++ scribe/lib/scribe/ai/data/model/ai_api_request.dart
@@ -1,8 +1,6 @@
 import 'package:scribe/scribe/ai/data/model/ai_message.dart';
 
 class AIAPIRequest {
-  static const String _defaultModel = 'gpt-oss-120b';
-
   final List<AIMessage> messages;
 
   const AIAPIRequest({
@@ -11,7 +9,6 @@ class AIAPIRequest {
 
   Map<String, dynamic> toJson() {
     return {
-      'model': _defaultModel,
       'messages': messages.map((m) => m.toJson()).toList(),
     };
   }
