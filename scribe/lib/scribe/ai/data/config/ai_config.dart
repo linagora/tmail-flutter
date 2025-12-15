a41a205b419eb1e2a9c21956c7ae40904bc71281
Théo Poizat <hello@zatteo.com>
2025-12-15 15:04:41 +0100

    feat: Remove .env file for AI Scribe
    It was used at the beginning for configuration purpose but now we
    have the JMAP capability, no need anymore.

--- scribe/lib/scribe/ai/data/config/ai_config.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-class AIConfig {
-  const AIConfig._();
-
-  static bool get isAiEnabled => dotenv.get('AI_ENABLED', fallback: 'false') == 'true';
-}
