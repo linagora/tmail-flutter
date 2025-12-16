bbbb04a70e2255ec8be2a004e453f9771f89e79b
dab246 <tdvu@linagora.com>
2025-12-16 09:25:01 +0700

    TF-4141 Add AI capability

--- lib/features/home/domain/extensions/session_extensions.dart
+++ lib/features/home/domain/extensions/session_extensions.dart
@@ -28,6 +28,7 @@ extension SessionExtensions on Session {
   static final CapabilityIdentifier linagoraContactSupportCapability = CapabilityIdentifier(Uri.parse('com:linagora:params:jmap:contact:support'));
   static final CapabilityIdentifier linagoraDownloadAllCapability = CapabilityIdentifier(Uri.parse('com:linagora:params:downloadAll'));
   static final CapabilityIdentifier linagoraSaaSCapability = CapabilityIdentifier(Uri.parse('com:linagora:params:saas'));
+  static final CapabilityIdentifier linagoraAICapability = CapabilityIdentifier(Uri.parse('com:linagora:params:jmap:aibot'));
 
   static final Map<CapabilityIdentifier, CapabilityProperties Function(Map<String, dynamic>)> customMapCapabilitiesConverter = {
     linagoraContactSupportCapability: ContactSupportCapability.deserialize,
