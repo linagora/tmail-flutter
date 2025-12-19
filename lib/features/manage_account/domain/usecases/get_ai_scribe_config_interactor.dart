3f392241cbd80eb7297d2c4a1b9e1a038e4ecf9d
Dat H. Pham <dphamhoang@linagora.com>
2025-12-19 10:21:18 +0700

    Interactor getAIScribeConfig

---
+++ lib/features/manage_account/domain/usecases/get_ai_scribe_config_interactor.dart
@@ -0,0 +1,21 @@
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:dartz/dartz.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/repository/manage_account_repository.dart';
+import 'package:tmail_ui_user/features/manage_account/domain/state/get_ai_scribe_config_state.dart';
+
+class GetAIScribeConfigInteractor {
+  const GetAIScribeConfigInteractor(this._manageAccountRepository);
+
+  final ManageAccountRepository _manageAccountRepository;
+
+  Stream<Either<Failure, Success>> execute() async* {
+    try {
+      yield Right(GettingAIScribeConfigState());
+      final preferencesSetting = await _manageAccountRepository.getLocalSettings();
+      yield Right(GetAIScribeConfigSuccess(preferencesSetting.aiScribeConfig));
+    } catch (e) {
+      yield Left(GetAIScribeConfigFailure(exception: e));
+    }
+  }
+}
