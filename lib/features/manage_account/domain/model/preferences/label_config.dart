3d03c8475b5398078f373ddf0c6c89a3a38d0bc5
dab246 <tdvu@linagora.com>
2026-01-06 16:35:33 +0700

    TF-4227 Set default enable Label setting is true in E2E test

--- lib/features/manage_account/domain/model/preferences/label_config.dart
+++ lib/features/manage_account/domain/model/preferences/label_config.dart
@@ -1,3 +1,4 @@
+import 'package:core/utils/platform_info.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:tmail_ui_user/features/manage_account/domain/model/preferences/preferences_config.dart';
 
@@ -9,7 +10,8 @@ class LabelConfig extends PreferencesConfig {
 
   LabelConfig({this.isEnabled = false});
 
-  factory LabelConfig.initial() => LabelConfig();
+  factory LabelConfig.initial() =>
+      LabelConfig(isEnabled: PlatformInfo.isIntegrationTesting);
 
   factory LabelConfig.fromJson(Map<String, dynamic> json) =>
       _$LabelConfigFromJson(json);
