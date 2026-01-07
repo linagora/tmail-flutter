8e06e576bd98e260ad2f41f372664a4596e3758a
dab246 <tdvu@linagora.com>
2026-01-07 13:15:29 +0700

    TF-4227 Fix MissingStubError: 'keywords' in composer_repository_impl_test

--- test/features/composer/data/repository/composer_repository_impl_test.dart
+++ test/features/composer/data/repository/composer_repository_impl_test.dart
@@ -62,6 +62,7 @@ void main() {
       when(mockCreateEmailRequest.identity).thenReturn(mockIdentity);
       when(mockCreateEmailRequest.replyToRecipients).thenReturn({});
       when(mockCreateEmailRequest.ownEmailAddress).thenReturn('test@example.com');
+      when(mockCreateEmailRequest.keywords).thenReturn(null);
 
       repository = ComposerRepositoryImpl(
         mockAttachmentUploadDataSource,
