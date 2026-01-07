d8a723a3d9b0c67d8d3040b2ffecce39ebac4aa3
Théo Poizat <hello@zatteo.com>
2026-01-07 08:38:38 +0100

    Fix selection coordinates in mobile
    Because the HTML editor is different, we need a different selector to get the editable element.
    
    We also need to add a small offset to the coordinates to avoid positionning the overlay icon above native selection UI in mobile. For example, Android adds a marker at the beginning and the end of the selection and we do not want to display the Scribe overlay icon above this marker.

--- core/lib/utils/html/html_utils.dart
+++ core/lib/utils/html/html_utils.dart
@@ -73,11 +73,14 @@ class HtmlUtils {
     name: 'unregisterDropListener');
 
   static ({String name, String script}) registerSelectionChangeListener(
-    String viewId,
-  ) =>
+    String viewId, {
+    bool isWebPlatform = false,
+  }) =>
       (
         script: '''
       let lastSelectedText = '';
+
+      const isWebPlatform = $isWebPlatform;
 
       const sendSelectionChangeMessage = (data) => {
         // When iframe
@@ -101,10 +104,17 @@ class HtmlUtils {
       function getEditableFromSelection(selection) {
         const node = selection?.focusNode || selection?.anchorNode;
         const el = node?.nodeType === Node.ELEMENT_NODE ? node : node?.parentElement;
-        return (
-          el?.closest('.note-editor .note-editable') ||
-          document.querySelector('.note-editor .note-editable')
-        );
+
+        if (isWebPlatform) {
+          return (
+            el?.closest('.note-editor .note-editable') ||
+            document.querySelector('.note-editor .note-editable')
+          );
+        } else {
+          return (
+            el?.closest('#editor')
+          );
+        }
       }
       
       function clamp(v, min, max) {
@@ -142,9 +152,13 @@ class HtmlUtils {
           }
       
           const lastRect = rects[rects.length - 1];
-      
-          let x = lastRect.right - editableRect.left;
-          let y = lastRect.bottom - editableRect.top;
+
+          // Avoid native selection marks in mobile
+          // Offset has been arbitrary determined to avoid selection marks on Android and iOS
+          const buttonOffset = isWebPlatform ? { x: 0, y: 0 } : { x: 22, y: -20 };
+
+          let x = lastRect.right - editableRect.left + buttonOffset.x;
+          let y = lastRect.bottom - editableRect.top + buttonOffset.y;
       
           const isInside =
             lastRect.bottom >= editableRect.top &&
