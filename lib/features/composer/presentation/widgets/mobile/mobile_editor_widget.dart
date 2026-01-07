c9a0f6ff7ccadf0de9003f8a4e6ac886a2fe7c44
Théo Poizat <hello@zatteo.com>
2026-01-07 08:38:38 +0100

    Fix selection coordinates in mobile
    Because the HTML editor is different, we need a different selector to get the editable element.
    
    We also need to add a small offset to the coordinates to avoid positionning the overlay icon above native selection UI in mobile. For example, Android adds a marker at the beginning and the end of the selection and we do not want to display the Scribe overlay icon above this marker.

--- lib/features/composer/presentation/widgets/mobile/mobile_editor_widget.dart
+++ lib/features/composer/presentation/widgets/mobile/mobile_editor_widget.dart
@@ -66,7 +66,7 @@ class _MobileEditorState extends State<MobileEditorWidget> with TextSelectionMix
     _editorController = editorApi.webViewController;
 
     registerSelectionChange =
-        HtmlUtils.registerSelectionChangeListener(_createdViewId);
+        HtmlUtils.registerSelectionChangeListener(_createdViewId, isWebPlatform: PlatformInfo.isWeb);
 
     _editorController?.addJavaScriptHandler(
       handlerName: registerSelectionChange!.name,
