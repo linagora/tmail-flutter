84103e7d53b58bcaad3c37abfb5960d5d452ad7e
dab246 <tdvu@linagora.com>
2026-01-23 09:39:36 +0700

    TF-4268 Update SentryConfig: add sampling and frames tracking

--- lib/main/runner/app_runner_mobile.dart
+++ lib/main/runner/app_runner_mobile.dart
@@ -2,7 +2,7 @@ import 'package:core/utils/config/env_loader.dart';
 import 'package:tmail_ui_user/main/runner/app_runner_base.dart';
 
 Future<void> runAppWithMonitoring(Future<void> Function() runTmail) async {
-  await runWithZoneAndErrorHandling(() async {
+  await runAppGuarded(() async {
     await EnvLoader.loadEnvFile();
 
     await runTmail();
