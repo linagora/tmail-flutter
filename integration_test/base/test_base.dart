266cab2b69b46d2c5b80ed9dc634806ea341cd59
dab246 <tdvu@linagora.com>
2026-01-23 08:53:27 +0700

    TF-4268 Avoid loading environment files multiple times when enabling Sentry

--- integration_test/base/test_base.dart
+++ integration_test/base/test_base.dart
@@ -1,3 +1,4 @@
+import 'package:core/utils/config/env_loader.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
@@ -34,6 +35,7 @@ class TestBase {
   }
 
   Future<void> setupTest() async {
+    await EnvLoader.loadEnvFile();
     await runTmail();
 
     final originalOnError = FlutterError.onError!;
