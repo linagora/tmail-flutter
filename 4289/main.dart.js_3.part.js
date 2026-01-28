f6b74c48330c4b93f05aceb75413447fae6070b0
tddang-linagora <tddang-linagora@users.noreply.github.com>
2026-01-28 09:59:44 +0000

    deploy: bd8635fd3ee7354bcbf4eeebdbfc803aea682ca4

---
+++ 4289/main.dart.js_3.part.js
@@ -0,0 +1,324 @@
+((a,b,c)=>{a[b]=a[b]||{}
+a[b][c]=a[b][c]||[]
+a[b][c].push({p:"main.dart.js_3",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var J,B,C,A={agX:function agX(){},c30:function c30(){},c31:function c31(d,e){this.a=d
+this.b=e},c32:function c32(){},c33:function c33(d,e){this.a=d
+this.b=e},
+eA5(){return new b.G.XMLHttpRequest()},
+eA8(){return b.G.document.createElement("img")},
+dNs(d,e,f){var x=new A.bee(d,B.c([],y.v),B.c([],y.l),B.c([],y.u))
+x.b2K(d,e,f)
+return x},
+a1i:function a1i(d,e,f,g){var _=this
+_.a=d
+_.b=e
+_.c=f
+_.d=g},
+clB:function clB(d,e,f,g){var _=this
+_.a=d
+_.b=e
+_.c=f
+_.d=g},
+clC:function clC(d,e){this.a=d
+this.b=e},
+clz:function clz(d,e,f){this.a=d
+this.b=e
+this.c=f},
+clA:function clA(d,e,f){this.a=d
+this.b=e
+this.c=f},
+bee:function bee(d,e,f,g){var _=this
+_.z=d
+_.Q=!1
+_.at=_.as=$
+_.ax=!1
+_.a=e
+_.b=f
+_.e=_.d=_.c=null
+_.r=_.f=!1
+_.w=0
+_.x=!1
+_.y=g},
+d4t:function d4t(d){this.a=d},
+d4p:function d4p(){},
+d4q:function d4q(d){this.a=d},
+d4r:function d4r(d){this.a=d},
+d4s:function d4s(d){this.a=d},
+d4u:function d4u(d,e){this.a=d
+this.b=e},
+a6a:function a6a(d,e){this.a=d
+this.b=e},
+emE(d,e){return new A.Q6(d,"HTTP request failed, statusCode: "+d+", "+e.l(0),e)},
+cSd:function cSd(d,e){this.a=d
+this.b=e},
+Q6:function Q6(d,e,f){this.a=d
+this.b=e
+this.c=f},
+apK:function apK(d,e,f,g,h){var _=this
+_.c=d
+_.d=e
+_.e=f
+_.a=g
+_.b=h},
+bw6(d,e){var x
+$.q()
+x=$.b
+if(x==null)x=$.b=C.b
+return new A.aBR(x.k(0,null,y.q),e,d,null)},
+aBR:function aBR(d,e,f,g){var _=this
+_.c=d
+_.d=e
+_.f=f
+_.a=g}},D
+J=c[1]
+B=c[0]
+C=c[2]
+A=a.updateHolder(c[11],A)
+D=c[18]
+A.agX.prototype={
+ae5(d,e){var x=this,w=null
+B.y(B.I(x).l(0)+"::buildImage: imagePath = "+d,w,w,C.f,w,!1)
+if(x.aJA(d)&&C.d.fv(d,"svg"))return new B.apL(e,e,C.O,C.t,new A.apK(d,w,w,w,w),new A.c30(),new A.c31(x,e),w,w)
+else if(x.aJA(d))return new B.GJ(B.due(w,w,new A.a1i(d,1,w,D.b5v)),new A.c32(),new A.c33(x,e),e,e,C.O,w)
+else if(C.d.fv(d,"svg"))return B.be(d,C.t,w,C.aB,e,w,w,e)
+else return new B.GJ(B.due(w,w,new B.aa2(d,w,w)),w,w,e,e,C.O,w)},
+aJA(d){return C.d.aK(d,"http")||C.d.aK(d,"https")}}
+A.a1i.prototype={
+RD(d){return new B.eY(this,y.i)},
+JF(d,e){var x=null
+return A.dNs(this.M8(d,e,B.jO(x,x,x,x,!1,y.r)),d.a,x)},
+JG(d,e){var x=null
+return A.dNs(this.M8(d,e,B.jO(x,x,x,x,!1,y.r)),d.a,x)},
+M8(d,e,f){return this.bof(d,e,f)},
+bof(d,e,f){var x=0,w=B.n(y.R),v,u=2,t=[],s=this,r,q,p,o,n
+var $async$M8=B.h(function(g,h){if(g===1){t.push(h)
+x=u}while(true)switch(x){case 0:p=new A.clB(s,e,f,d)
+o=new A.clC(s,d)
+case 3:switch(s.d.a){case 0:x=5
+break
+case 2:x=6
+break
+case 1:x=7
+break
+default:x=4
+break}break
+case 5:v=p.$0()
+x=1
+break
+case 6:v=o.$0()
+x=1
+break
+case 7:u=9
+x=12
+return B.i(p.$0(),$async$M8)
+case 12:r=h
+v=r
+x=1
+break
+u=2
+x=11
+break
+case 9:u=8
+n=t.pop()
+r=o.$0()
+v=r
+x=1
+break
+x=11
+break
+case 8:x=2
+break
+case 11:x=4
+break
+case 4:case 1:return B.l(v,w)
+case 2:return B.k(t.at(-1),w)}})
+return B.m($async$M8,w)},
+MN(d){return this.bb9(d)},
+bb9(d){var x=0,w=B.n(y.p),v,u=this,t,s,r,q,p,o,n
+var $async$MN=B.h(function(e,f){if(e===1)return B.k(f,w)
+while(true)switch(x){case 0:s=u.a
+r=B.qb().b1(s)
+q=new B.aF($.aQ,y.Z)
+p=new B.bc(q,y.x)
+o=A.eA5()
+o.open("GET",s,!0)
+o.responseType="arraybuffer"
+o.addEventListener("load",B.iE(new A.clz(o,p,r)))
+o.addEventListener("error",B.iE(new A.clA(p,o,r)))
+o.send()
+x=3
+return B.i(q,$async$MN)
+case 3:s=o.response
+s.toString
+t=B.aUt(y.o.a(s),0,null)
+if(t.byteLength===0)throw B.v(A.emE(B.aO(o,"status"),r))
+n=d
+x=4
+return B.i(B.agY(t),$async$MN)
+case 4:v=n.$1(f)
+x=1
+break
+case 1:return B.l(v,w)}})
+return B.m($async$MN,w)},
+m(d,e){if(e==null)return!1
+if(J.aR(e)!==B.I(this))return!1
+return e instanceof A.a1i&&e.a===this.a&&e.b===this.b},
+gv(d){return B.aJ(this.a,this.b,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a)},
+l(d){return'NetworkImage("'+this.a+'", scale: '+C.h.bG(this.b,1)+")"}}
+A.bee.prototype={
+b2K(d,e,f){var x=this
+x.e=e
+x.z.jB(0,new A.d4t(x),new A.d4u(x,f),y.P)},
+aiz(){var x,w=this
+if(w.Q){x=w.at
+x===$&&B.d()
+x.p()}w.ax=!0
+w.aXN()}}
+A.a6a.prototype={
+P7(d){return new A.a6a(this.a,this.b)},
+p(){},
+gmV(d){return B.ao(B.bb("Could not create image data for this image because access to it is restricted by the Same-Origin Policy.\nSee https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy"))},
+glP(d){return 1},
+gan1(){var x=this.a
+return C.j.c_(4*x.naturalWidth*x.naturalHeight)},
+$imN:1,
+gpM(){return this.b}}
+A.cSd.prototype={
+K(){return"WebHtmlElementStrategy."+this.b}}
+A.Q6.prototype={
+l(d){return this.b},
+$iay:1}
+A.apK.prototype={
+Kf(d){return this.c04(d)},
+c04(d){var x=0,w=B.n(y.K),v,u=this,t,s,r
+var $async$Kf=B.h(function(e,f){if(e===1)return B.k(f,w)
+while(true)switch(x){case 0:s=u.e
+r=B.dyh()
+s=r==null?new B.VG(new b.G.AbortController()):r
+x=3
+return B.i(s.a4I(0,B.cD(u.c,0,null),u.d),$async$Kf)
+case 3:t=f
+s.ap(0)
+v=t.w
+x=1
+break
+case 1:return B.l(v,w)}})
+return B.m($async$Kf,w)},
+aM3(d){d.toString
+return C.am.a_R(0,d,!0)},
+gv(d){var x=this
+return B.aJ(x.c,x.d,x.a,x.b,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a,C.a)},
+m(d,e){var x
+if(e==null)return!1
+if(e instanceof A.apK)x=e.c===this.c
+else x=!1
+return x},
+l(d){return"SvgNetworkLoader("+this.c+")"}}
+A.aBR.prototype={
+t(d){var x=null,w=$.fV().i0("PLATFORM","other"),v=w.toLowerCase()==="saas"?"assets/images/ic_logo_with_text_beta.svg":"assets/images/ic_logo_with_text.svg",u=this.f
+if(u==null)u=33
+return B.bN(C.q,x,20,x,x,C.q,v,x,u,x,x,1/0,x,this.d,C.K,x,x)}}
+var z=a.updateTypes([])
+A.c30.prototype={
+$1(d){return C.ou},
+$S:2147}
+A.c31.prototype={
+$3(d,e,f){var x=null,w=this.b
+return B.a8(C.t,D.A7,C.k,x,x,x,x,w,x,x,x,x,x,w)},
+$S:2148}
+A.c32.prototype={
+$3(d,e,f){return e},
+$C:"$3",
+$R:3,
+$S:2149}
+A.c33.prototype={
+$3(d,e,f){var x=null,w=this.b
+return B.a8(C.t,D.A7,C.k,x,x,x,x,w,x,x,x,x,x,w)},
+$S:2150}
+A.clB.prototype={
+$0(){var x=0,w=B.n(y.R),v,u=this,t,s,r,q,p
+var $async$$0=B.h(function(d,e){if(d===1)return B.k(e,w)
+while(true)switch(x){case 0:t=u.c
+s=u.d
+r=B
+q=new B.eo(t,B.r(t).h("eo<1>"))
+p=B
+x=3
+return B.i(u.a.MN(u.b),$async$$0)
+case 3:v=r.aUn(q,p.bG(e,y.p),s.a,null,s.b)
+x=1
+break
+case 1:return B.l(v,w)}})
+return B.m($async$$0,w)},
+$S:557}
+A.clC.prototype={
+$0(){var x=0,w=B.n(y.R),v,u=this,t,s,r
+var $async$$0=B.h(function(d,e){if(d===1)return B.k(e,w)
+while(true)switch(x){case 0:s=A.eA8()
+r=u.b.a
+s.src=r
+x=3
+return B.i(B.iu(s.decode(),y.X),$async$$0)
+case 3:t=B.dI0(B.bG(new A.a6a(s,r),y.J),null)
+t.e=r
+v=t
+x=1
+break
+case 1:return B.l(v,w)}})
+return B.m($async$$0,w)},
+$S:557}
+A.clz.prototype={
+$1(d){var x=this.a,w=x.status,v=w>=200&&w<300,u=w>307&&w<400,t=v||w===0||w===304||u,s=this.b
+if(t)s.eg(0,x)
+else{x=this.c
+s.ki(new A.Q6(w,"HTTP request failed, statusCode: "+B.e(w)+", "+x.l(0),x))}},
+$S:51}
+A.clA.prototype={
+$1(d){var x=this.b.status,w=this.c
+return this.a.ki(new A.Q6(x,"HTTP request failed, statusCode: "+B.e(x)+", "+w.l(0),w))},
+$S:10}
+A.d4t.prototype={
+$1(d){var x,w=this.a
+w.Q=!0
+if(w.ax){d.a4(0,new B.nI(new A.d4p(),null,null))
+d.NA()
+return}w.as!==$&&B.cI()
+w.as=d
+if(d.x)B.ao(B.aC("Stream has been disposed.\nAn ImageStream is considered disposed once at least one listener has been added and subsequently all listeners have been removed and no handles are outstanding from the keepAlive method.\nTo resolve this error, maintain at least one listener on the stream, or create an ImageStreamCompleterHandle from the keepAlive method, or create a new stream for the image."))
+x=new B.OH(d)
+x.M7(d)
+w.at!==$&&B.cI()
+w.at=x
+d.a4(0,new B.nI(new A.d4q(w),new A.d4r(w),new A.d4s(w)))},
+$S:2152}
+A.d4p.prototype={
+$2(d,e){},
+$S:314}
+A.d4q.prototype={
+$2(d,e){this.a.a5S(d)},
+$S:314}
+A.d4r.prototype={
+$1(d){this.a.aMP(d)},
+$S:449}
+A.d4s.prototype={
+$2(d,e){this.a.c2u(d,e)},
+$S:337}
+A.d4u.prototype={
+$2(d,e){this.a.Ba(B.dE("resolving an image stream completer"),d,this.b,!0,e)},
+$S:75};(function inheritance(){var x=a.inheritMany,w=a.inherit
+x(B.a3,[A.agX,A.a6a,A.Q6])
+x(B.ph,[A.c30,A.c31,A.c32,A.c33,A.clz,A.clA,A.d4t,A.d4r])
+w(A.a1i,B.nH)
+x(B.vS,[A.clB,A.clC])
+w(A.bee,B.mO)
+x(B.vT,[A.d4p,A.d4q,A.d4s,A.d4u])
+w(A.cSd,B.TP)
+w(A.apK,B.tC)
+w(A.aBR,B.a0)})()
+B.EU(b.typeUniverse,JSON.parse('{"a1i":{"nH":["dtH"],"nH.T":"dtH"},"bee":{"mO":[]},"a6a":{"mN":[]},"dtH":{"nH":["dtH"]},"Q6":{"ay":[]},"apK":{"tC":["dO"],"Ll":[],"tC.T":"dO"},"aBR":{"a0":[],"j":[],"o":[]}}'))
+var y=(function rtii(){var x=B.at
+return{p:x("mG"),r:x("OF"),J:x("mN"),q:x("Cx"),R:x("mO"),v:x("P<nI>"),u:x("P<~()>"),l:x("P<~(a3,e1?)>"),o:x("CT"),P:x("b1"),i:x("eY<a1i>"),x:x("bc<aI>"),Z:x("aF<aI>"),X:x("a3?"),K:x("dO?")}})();(function constants(){D.jl=new B.aH(0,8,0,0)
+D.A7=new B.hu(C.ar7,null,null,null,null)
+D.b5v=new A.cSd(0,"never")})()};
+((a,b)=>{a[b]=a.current
+a.eventLog.push({p:"main.dart.js_3",e:"endPart",h:b})})($__dart_deferred_initializers__,"+WZ3IFM+ngen2Tq1P3X+m/gVta0=");
