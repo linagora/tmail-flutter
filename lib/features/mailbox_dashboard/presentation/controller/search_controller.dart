55104b5d6d0bfae54dac60ea391e62ba865431e1
Dang Dat <tddang@linagora.com>
2026-01-28 17:23:28 +0700

    fixup! TF-4282 Fix sort of web search suggestion

--- lib/features/mailbox_dashboard/presentation/controller/search_controller.dart
+++ lib/features/mailbox_dashboard/presentation/controller/search_controller.dart
@@ -5,23 +5,15 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
-import 'package:jmap_dart_client/jmap/core/filter/filter.dart';
-import 'package:jmap_dart_client/jmap/core/session/session.dart';
-import 'package:jmap_dart_client/jmap/core/unsigned_int.dart';
 import 'package:jmap_dart_client/jmap/core/user_name.dart';
 import 'package:jmap_dart_client/jmap/core/utc_date.dart';
-import 'package:jmap_dart_client/jmap/mail/email/email_filter_condition.dart';
 import 'package:jmap_dart_client/jmap/mail/email/keyword_identifier.dart';
 import 'package:labels/model/label.dart';
-import 'package:model/email/presentation_email.dart';
-import 'package:model/extensions/email_filter_condition_extension.dart';
 import 'package:model/mailbox/presentation_mailbox.dart';
 import 'package:tmail_ui_user/features/base/base_controller.dart';
 import 'package:tmail_ui_user/features/base/mixin/date_range_picker_mixin.dart';
-import 'package:tmail_ui_user/features/email/presentation/utils/email_utils.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/model/recent_search.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/state/get_all_recent_search_latest_state.dart';
-import 'package:tmail_ui_user/features/mailbox_dashboard/domain/state/quick_search_email_state.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/usecases/get_all_recent_search_latest_interactor.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/usecases/quick_search_email_interactor.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/usecases/save_recent_search_interactor.dart';
@@ -35,7 +27,7 @@ import 'package:tmail_ui_user/features/thread/presentation/model/search_state.da
 import 'package:tmail_ui_user/features/thread/presentation/model/search_status.dart';
 
 class SearchController extends BaseController with DateRangePickerMixin {
-  final QuickSearchEmailInteractor _quickSearchEmailInteractor;
+  final QuickSearchEmailInteractor quickSearchEmailInteractor;
   final SaveRecentSearchInteractor _saveRecentSearchInteractor;
   final GetAllRecentSearchLatestInteractor _getAllRecentSearchLatestInteractor;
 
@@ -55,7 +47,7 @@ class SearchController extends BaseController with DateRangePickerMixin {
   String currentSearchText = '';
 
   SearchController(
-    this._quickSearchEmailInteractor,
+    this.quickSearchEmailInteractor,
     this._saveRecentSearchInteractor,
     this._getAllRecentSearchLatestInteractor,
   );
@@ -103,59 +95,6 @@ class SearchController extends BaseController with DateRangePickerMixin {
 
   void deleteQuickSearchFilterFromSuggestionSearchView(QuickSearchFilter searchFilter) {
     listFilterOnSuggestionForm.remove(searchFilter);
-  }
-
-  Future<List<PresentationEmail>> quickSearchEmails({
-    required Session session,
-    required AccountId accountId,
-    required String ownEmailAddress,
-    required String query,
-  }) async {
-    currentSearchText = query;
-    return await _quickSearchEmailInteractor.execute(
-      session,
-      accountId,
-      limit: UnsignedInt(5),
-      sort: searchEmailFilter.value.sortOrderType
-                  .getSortOrder()
-                  .toNullable(),
-      filter: _mappingToFilterOnSuggestionForm(
-        currentUserEmail: ownEmailAddress,
-        query: query,
-      ),
-      properties: EmailUtils.getPropertiesForEmailGetMethod(session, accountId),
-    ).then((result) => result.fold(
-      (failure) => <PresentationEmail>[],
-      (success) => success is QuickSearchEmailSuccess
-        ? success.emailList
-        : <PresentationEmail>[]
-    ));
-  }
-
-  Filter? _mappingToFilterOnSuggestionForm({required String query, required String currentUserEmail}) {
-    log('SearchController::_mappingToFilterOnSuggestionForm():query: $query');
-    final filterCondition = EmailFilterCondition(
-      text: query.isNotEmpty == true ? query : null,
-      after: listFilterOnSuggestionForm.contains(QuickSearchFilter.last7Days)
-        ? EmailReceiveTimeType.last7Days.toOldestUTCDate()
-        : null,
-      before: listFilterOnSuggestionForm.contains(QuickSearchFilter.last7Days)
-        ? EmailReceiveTimeType.last7Days.toLatestUTCDate()
-        : null,
-      hasAttachment: listFilterOnSuggestionForm.contains(QuickSearchFilter.hasAttachment)
-        ? true
-        : null,
-      from: listFilterOnSuggestionForm.contains(QuickSearchFilter.fromMe) && currentUserEmail.isNotEmpty
-        ? currentUserEmail
-        : null,
-      hasKeyword: listFilterOnSuggestionForm.contains(QuickSearchFilter.starred)
-        ? KeyWordIdentifier.emailFlagged.value
-        : null
-    );
-
-    return filterCondition.hasCondition
-      ? filterCondition
-      : null;
   }
 
   void applyFilterSuggestionToSearchFilter(String currentUserEmail) {
---
+++ test/features/mailbox_dashboard/presentation/extensions/quick_search_emails_extension_test.dart
@@ -0,0 +1,101 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/state_manager.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/state/quick_search_email_state.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/domain/usecases/quick_search_email_interactor.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/controller/search_controller.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/extensions/quick_search_emails_extension.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/model/search/email_sort_order_type.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/model/search/quick_search_filter.dart';
+import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/model/search/search_email_filter.dart';
+
+import '../../../../fixtures/account_fixtures.dart';
+import '../../../../fixtures/session_fixtures.dart';
+import 'quick_search_emails_extension_test.mocks.dart';
+
+@GenerateNiceMocks([
+  MockSpec<SearchController>(),
+  MockSpec<QuickSearchEmailInteractor>(),
+])
+void main() {
+  final session = SessionFixtures.aliceSession;
+  final accountId = AccountFixtures.aliceAccountId;
+  const ownEmailAddress = 'alice@linagora.com';
+  const query = 'test';
+
+  EmailSortOrderType? sortOrderType;
+
+  late MockSearchController searchController;
+  late MockQuickSearchEmailInteractor quickSearchEmailInteractor;
+
+  setUp(() {
+    quickSearchEmailInteractor = MockQuickSearchEmailInteractor();
+    searchController = MockSearchController();
+    when(searchController.quickSearchEmailInteractor)
+        .thenReturn(quickSearchEmailInteractor);
+    when(searchController.listFilterOnSuggestionForm)
+        .thenReturn(<QuickSearchFilter>[].obs);
+    when(
+      quickSearchEmailInteractor.execute(
+        any,
+        any,
+        limit: anyNamed('limit'),
+        sort: anyNamed('sort'),
+        filter: anyNamed('filter'),
+        properties: anyNamed('properties'),
+      ),
+    ).thenAnswer((_) => Future.value(Right(QuickSearchEmailSuccess([]))));
+  });
+
+  group('quickSearchEmails', () {
+    test(
+      'should invoke quickSearchEmailInteractor.execute() with SearchController\'s sort '
+      'when sortOrderType is null',
+      () async {
+        sortOrderType = null;
+        when(searchController.searchEmailFilter)
+            .thenReturn(SearchEmailFilter(sortOrderType: sortOrderType).obs);
+        await searchController.quickSearchEmails(
+          session: session,
+          accountId: accountId,
+          ownEmailAddress: ownEmailAddress,
+          query: query,
+        );
+        verify(quickSearchEmailInteractor.execute(
+          session,
+          accountId,
+          limit: anyNamed('limit'),
+          sort: null,
+          filter: anyNamed('filter'),
+          properties: anyNamed('properties'),
+        )).called(1);
+      },
+    );
+
+    test(
+      'should invoke quickSearchEmailInteractor.execute() with SearchController\'s sort '
+      'when sortOrderType is not null',
+      () async {
+        sortOrderType = EmailSortOrderType.oldest;
+        when(searchController.searchEmailFilter)
+            .thenReturn(SearchEmailFilter(sortOrderType: sortOrderType).obs);
+        await searchController.quickSearchEmails(
+          session: session,
+          accountId: accountId,
+          ownEmailAddress: ownEmailAddress,
+          query: query,
+        );
+        verify(quickSearchEmailInteractor.execute(
+          session,
+          accountId,
+          limit: anyNamed('limit'),
+          sort: EmailSortOrderType.oldest.getSortOrder().toNullable(),
+          filter: anyNamed('filter'),
+          properties: anyNamed('properties'),
+        )).called(1);
+      },
+    );
+  });
+}
