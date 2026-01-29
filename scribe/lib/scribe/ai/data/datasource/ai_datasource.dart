46da09a40f5ea1cde1d5a3982ed495311bdf6851
Théo Poizat <hello@zatteo.com>
2026-01-29 15:48:21 +0100

    Manage prompts as a "messages" object during prompt lifecycle
    Before, our prompts were just a string like "Translate this text:
    user text" and just before being sent to the backend, it was
    converted to an "messages" object compatible with OpenAI API like [{
    "role": "user", "content": "Translate this text: user text"}].
    
    Now, we use "messages" object directly when building the prompt
    because in the near future we will create more complex prompts with
    system content and user content so we will need "messages" object
    dirrectly.

--- scribe/lib/scribe/ai/data/datasource/ai_datasource.dart
+++ scribe/lib/scribe/ai/data/datasource/ai_datasource.dart
@@ -1,5 +1,6 @@
+import 'package:scribe/scribe/ai/data/model/ai_message.dart';
 import 'package:scribe/scribe/ai/domain/model/ai_response.dart';
 
 abstract class AIDataSource {
-  Future<AIResponse> generateMessage(String prompt);
+  Future<AIResponse> generateMessage(List<AIMessage> messages);
 }
