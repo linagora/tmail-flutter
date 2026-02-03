b3c7b722952d564829924945e5e8090abdf6029f
dab246 <tdvu@linagora.com>
2026-02-03 18:15:25 +0700

    feat: Add repository/interactor for get label changes

--- lib/features/labels/domain/repository/label_repository.dart
+++ lib/features/labels/domain/repository/label_repository.dart
@@ -1,6 +1,9 @@
 import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:jmap_dart_client/jmap/core/state.dart';
 import 'package:labels/model/label.dart';
 import 'package:tmail_ui_user/features/labels/domain/model/edit_label_request.dart';
+import 'package:tmail_ui_user/features/labels/domain/model/label_changes_result.dart';
 
 abstract class LabelRepository {
   Future<List<Label>> getAllLabels(AccountId accountId);
@@ -10,4 +13,10 @@ abstract class LabelRepository {
   Future<Label> editLabel(AccountId accountId, EditLabelRequest labelRequest);
 
   Future<void> deleteLabel(AccountId accountId, Label label);
+
+  Future<LabelChangesResult> getLabelChanges(
+    Session session,
+    AccountId accountId,
+    State sinceState,
+  );
 }
