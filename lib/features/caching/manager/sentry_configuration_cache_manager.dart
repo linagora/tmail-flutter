45b5e15bc43ac64164fcf4f9dde75091b3c80f32
dab246 <tdvu@linagora.com>
2026-02-03 12:00:38 +0700

    TF-4268 Save Sentry user to cache to re-init in onBackground of FCM

--- lib/features/caching/manager/sentry_configuration_cache_manager.dart
+++ lib/features/caching/manager/sentry_configuration_cache_manager.dart
@@ -1,16 +1,25 @@
 import 'package:tmail_ui_user/features/caching/clients/sentry_configuration_cache_client.dart';
+import 'package:tmail_ui_user/features/caching/clients/sentry_user_cache_client.dart';
 import 'package:tmail_ui_user/features/caching/entries/sentry_configuration_cache.dart';
+import 'package:tmail_ui_user/features/caching/entries/sentry_user_cache.dart';
 import 'package:tmail_ui_user/features/caching/exceptions/local_storage_exception.dart';
 import 'package:tmail_ui_user/features/caching/utils/caching_constants.dart';
 
 class SentryConfigurationCacheManager {
-  final SentryConfigurationCacheClient _cacheClient;
-  final String _cacheKey = CachingConstants.sentryConfigurationCacheKeyName;
+  final SentryConfigurationCacheClient _configurationCacheClient;
+  final SentryUserCacheClient _userCacheClient;
+  final String _configurationCacheKey =
+      CachingConstants.sentryConfigurationCacheKeyName;
+  final String _userCacheKey = CachingConstants.sentryUserCacheKeyName;
 
-  SentryConfigurationCacheManager(this._cacheClient);
+  SentryConfigurationCacheManager(
+    this._configurationCacheClient,
+    this._userCacheClient,
+  );
 
   Future<SentryConfigurationCache> getSentryConfiguration() async {
-    final sentryConfigCache = await _cacheClient.getItem(_cacheKey);
+    final sentryConfigCache =
+        await _configurationCacheClient.getItem(_configurationCacheKey);
     if (sentryConfigCache == null) {
       throw NotFoundSentryConfigurationException();
     } else {
@@ -21,6 +30,24 @@ class SentryConfigurationCacheManager {
   Future<void> saveSentryConfiguration(
     SentryConfigurationCache sentryConfigurationCache,
   ) async {
-    await _cacheClient.insertItem(_cacheKey, sentryConfigurationCache);
+    await _configurationCacheClient.insertItem(
+      _configurationCacheKey,
+      sentryConfigurationCache,
+    );
+  }
+
+  Future<SentryUserCache> getSentryUser() async {
+    final sentryUserCache = await _userCacheClient.getItem(_userCacheKey);
+    if (sentryUserCache == null) {
+      throw NotFoundSentryUserException();
+    } else {
+      return sentryUserCache;
+    }
+  }
+
+  Future<void> saveSentryUser(
+    SentryUserCache sentryUserCache,
+  ) async {
+    await _userCacheClient.insertItem(_userCacheKey, sentryUserCache);
   }
 }
