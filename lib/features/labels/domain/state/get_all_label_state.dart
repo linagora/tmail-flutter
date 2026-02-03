e4972b7e2cbfdb1c5f00be4bfbe6f6c5df766cc8
dab246 <tdvu@linagora.com>
2026-02-03 19:46:24 +0700

    feat: Persist label state after get all labels

--- lib/features/labels/domain/state/get_all_label_state.dart
+++ lib/features/labels/domain/state/get_all_label_state.dart
@@ -1,16 +1,18 @@
 import 'package:core/presentation/state/failure.dart';
 import 'package:core/presentation/state/success.dart';
+import 'package:jmap_dart_client/jmap/core/state.dart';
 import 'package:labels/model/label.dart';
 
 class GettingAllLabel extends LoadingState {}
 
 class GetAllLabelSuccess extends UIState {
   final List<Label> labels;
+  final State? newState;
 
-  GetAllLabelSuccess(this.labels);
+  GetAllLabelSuccess(this.labels, this.newState);
 
   @override
-  List<Object> get props => [labels];
+  List<Object?> get props => [labels, newState];
 }
 
 class GetAllLabelFailure extends FeatureFailure {
