f42667fbeba8f46ff55b69258a61789ab12cd732
zatteo <zatteo@users.noreply.github.com>
2026-02-05 07:35:51 +0000

    deploy: 7f87c9071f4b204a9b352ed1f4ecba67a2992067

--- 4309/flutter_bootstrap.js
+++ 4309/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2120708119"
+    serviceWorkerVersion: "4065587705"
   }
 });
