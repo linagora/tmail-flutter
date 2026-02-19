a90977ecf04142263f4f0e7ff23247aed7a05902
Théo Poizat <hello@zatteo.com>
2026-02-19 15:08:57 +0100

    Fetch prompts from scribePromptUrl
    First, we try to fetch prompts from scribePromptUrl. At fallback, we
    use local prompts.

--- scribe/lib/scribe.dart
+++ scribe/lib/scribe.dart
@@ -4,6 +4,7 @@ export 'scribe/ai/data/datasource/ai_datasource.dart';
 export 'scribe/ai/data/repository/ai_repository_impl.dart';
 export 'scribe/ai/domain/model/ai_response.dart';
 export 'scribe/ai/domain/repository/ai_scribe_repository.dart';
+export 'scribe/ai/domain/service/prompt_service.dart';
 export 'scribe/ai/domain/state/generate_ai_text_state.dart';
 export 'scribe/ai/domain/usecases/generate_ai_text_interactor.dart';
 export 'scribe/ai/localizations/scribe_localizations.dart';
