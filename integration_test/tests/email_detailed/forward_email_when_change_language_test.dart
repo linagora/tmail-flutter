7b1c66a3d589c74df5cc35674bf32bc52eac8085
dab246 <tdvu@linagora.com>
2026-03-10 14:32:44 +0700

    TF-4367 Remove the duplicated closeEmailDetailedView helper from this mixin.

--- integration_test/tests/email_detailed/forward_email_when_change_language_test.dart
+++ integration_test/tests/email_detailed/forward_email_when_change_language_test.dart
@@ -4,7 +4,7 @@ import '../../scenarios/email_detailed/forward_email_when_change_language_scenar
 void main() {
   TestBase().runPatrolTest(
     description:
-        'The forward prefix should be displayed correctly when forwarding to an email.',
+        'The forward prefix should be displayed correctly when forwarding an email.',
     scenarioBuilder: ($) => ForwardEmailWhenChangeLanguageScenario($),
   );
 }
