bdfc6ed58900325d740686d36e6991d6808b29c9
dab246 <tdvu@linagora.com>
2026-04-22 13:27:05 +0700

    refactor(logger): centralize handler registration in registerLogHandlers (ADR-0078)

--- lib/main/runner/app_runner_mobile.dart
+++ lib/main/runner/app_runner_mobile.dart
@@ -1,22 +1,13 @@
 import 'package:core/utils/config/env_loader.dart';
-import 'package:core/utils/logging/app_logger_registry.dart';
 import 'package:core/utils/logging/formatters/mobile_console_formatter.dart';
-import 'package:core/utils/logging/handlers/console_log_handler.dart';
 import 'package:tmail_ui_user/main/runner/app_runner_base.dart';
 
 Future<void> runAppWithMonitoring(Future<void> Function() runTmail) async {
-  _registerLogHandlers();
+  registerLogHandlers(formatter: const MobileConsoleFormatter());
 
   await runAppGuarded(() async {
     await EnvLoader.loadEnvFile();
 
     await runTmail();
   });
-}
-
-void _registerLogHandlers() {
-  AppLoggerRegistry.instance.registerHandler(
-    const ConsoleLogHandler(formatter: MobileConsoleFormatter()),
-  );
-  registerSentryLogHandlers();
 }
