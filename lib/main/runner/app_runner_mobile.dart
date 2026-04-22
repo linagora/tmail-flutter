6b2dfae3b71fdaafd1489889204569026279fe0b
dab246 <tdvu@linagora.com>
2026-04-22 14:10:24 +0700

    feat(logger): register FileLogHandler at mobile startup (ADR-0078)

--- lib/main/runner/app_runner_mobile.dart
+++ lib/main/runner/app_runner_mobile.dart
@@ -1,9 +1,13 @@
 import 'package:core/utils/config/env_loader.dart';
+import 'package:core/utils/logging/app_logger_registry.dart';
 import 'package:core/utils/logging/formatters/mobile_console_formatter.dart';
+import 'package:core/utils/logging/handlers/file_log_handler.dart';
+import 'package:core/utils/logger/log_tracking.dart';
 import 'package:tmail_ui_user/main/runner/app_runner_base.dart';
 
 Future<void> runAppWithMonitoring(Future<void> Function() runTmail) async {
   registerLogHandlers(formatter: const MobileConsoleFormatter());
+  AppLoggerRegistry.instance.registerHandler(FileLogHandler(LogTracking()));
 
   await runAppGuarded(() async {
     await EnvLoader.loadEnvFile();
