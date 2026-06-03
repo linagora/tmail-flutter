659c1a40fcd759d433e2433ce42aca9e0922c890
Rene Cordier <rcordier@linagora.com>
2026-06-03 11:35:32 +0700

    fixup! perf: replace fixed-duration pumps with reactive waits in integration tests => web coverage

--- integration_test/robots/web/web_email_robot.dart
+++ integration_test/robots/web/web_email_robot.dart
@@ -1,13 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import '../mobile/mobile_email_robot.dart';
+import '../../utils/wait_for_condition.dart';
 
 class WebEmailRobot extends MobileEmailRobot {
   WebEmailRobot(super.$);
 
   @override
   Future<void> expectDownloadSaveDialogVisible() async {
-    final downloads = await $.platformAutomator.web.verifyFileDownloads();
-    expect(downloads.any((name) => name.startsWith('TwakeMail-')), isTrue);
+    await waitForCondition(() async {
+      final downloads = await $.platformAutomator.web.verifyFileDownloads();
+      return downloads.any((name) => name.startsWith('TwakeMail-'));
+    });
   }
 }
