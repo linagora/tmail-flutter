df0f913344fe315c264b57a71b9359c5b40888ad
hoangdat <hoangdat@users.noreply.github.com>
2026-06-10 10:11:40 +0000

    deploy: 408a232631668211ae00e059abd03163f0d3a399

--- 4589/flutter_bootstrap.js
+++ 4589/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "592071696"
+    serviceWorkerVersion: "2812732392"
   }
 });
