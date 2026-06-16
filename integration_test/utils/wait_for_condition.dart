345625d2124047b6b3c53bc03da014b54d4d81c4
Dat H. Pham <dphamhoang@linagora.com>
2026-06-16 10:49:09 +0700

    (e2e) fix for draft save flaky test
    (cherry picked from commit 9ac1ad114e840b3096ef37ed75c24ddcf86abca1)

--- integration_test/utils/wait_for_condition.dart
+++ integration_test/utils/wait_for_condition.dart
@@ -5,7 +5,7 @@ import 'test_timeouts.dart';
 Future<void> waitForCondition(
   FutureOr<bool> Function() condition, {
   Duration timeout = TestTimeouts.medium,
-  Duration interval = const Duration(seconds: 1),
+  Duration interval = const Duration(milliseconds: 200),
 }) async {
   await Future.doWhile(() async {
     if (await condition()) {
