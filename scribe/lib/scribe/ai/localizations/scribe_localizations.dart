c23d374ed01990f185e52b90b1c239b5d7ea168e
Benoit TELLIER <btellier@linagora.com>
2026-06-26 10:20:57 +0200

    Add languageMongolian to source catalog and enable mn in Scribe localizations

--- scribe/lib/scribe/ai/localizations/scribe_localizations.dart
+++ scribe/lib/scribe/ai/localizations/scribe_localizations.dart
@@ -4,7 +4,7 @@ import 'package:multiple_localization/multiple_localization.dart';
 import 'package:scribe/scribe/ai/l10n/messages_all.dart';
 
 class ScribeLocalizations {
-  static const List<String> _supportedLanguageCodes = ['en', 'fr', 'ru', 'vi', 'de', 'it', 'ar'];
+  static const List<String> _supportedLanguageCodes = ['en', 'fr', 'ru', 'vi', 'de', 'it', 'ar', 'mn'];
 
   static ScribeLocalizations of(BuildContext context) {
     return Localizations.of<ScribeLocalizations>(context, ScribeLocalizations)!;
