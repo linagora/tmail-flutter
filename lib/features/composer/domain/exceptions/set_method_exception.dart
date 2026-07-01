4293a82d2c131d1923e29bb1828103ca2f8a91fb
Dat H. Pham <dphamhoang@linagora.com>
2026-07-01 16:52:21 +0700

    [Sentry] Fix Calendar error be minified

--- lib/features/composer/domain/exceptions/set_method_exception.dart
+++ lib/features/composer/domain/exceptions/set_method_exception.dart
@@ -1,11 +1,12 @@
 import 'package:jmap_dart_client/jmap/core/error/set_error.dart';
 import 'package:jmap_dart_client/jmap/core/id.dart';
 import 'package:core/domain/exceptions/app_base_exception.dart';
+import 'package:model/extensions/set_error_extension.dart';
 
 class SetMethodException extends AppBaseException {
   final Map<Id, SetError> mapErrors;
 
-  SetMethodException(this.mapErrors) : super('Errors: $mapErrors');
+  SetMethodException(this.mapErrors) : super('Errors: {${mapErrors.toLogMessage()}}');
 
   @override
   String get exceptionName => 'SetMethodException';
