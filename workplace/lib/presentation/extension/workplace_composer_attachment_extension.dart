54e60378efa9b222e2a24eaae3591aaf26034e88
Dang Dat <tddang@linagora.com>
2026-09-08 09:37:15 +0700

    Log a warning when Cozy bridge createIntent falls back to bearer token
    Addresses: 3949007239
    https://github.com/linagora/tmail-flutter/pull/4804#discussion_r3949007239

--- workplace/lib/presentation/extension/workplace_composer_attachment_extension.dart
+++ workplace/lib/presentation/extension/workplace_composer_attachment_extension.dart
@@ -2,6 +2,7 @@ import 'package:core/presentation/extensions/composer_attachment_plugin.dart';
 import 'package:core/presentation/extensions/composer_toolbar_button_style.dart';
 import 'package:core/presentation/resources/image_paths.dart';
 import 'package:core/presentation/state/failure.dart';
+import 'package:core/utils/app_logger.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:workplace/data/bridge/cozy_bridge.dart';
@@ -68,6 +69,10 @@ class WorkplaceComposerAttachmentExtension implements ComposerAttachmentPlugin {
         );
       } catch (_) {
         // fall through to the bearer-token flow below
+        logWarning(
+          'WorkplaceComposerAttachmentExtension::_fetchIntent: Cozy bridge createIntent failed, falling back to bearer-token flow',
+          webConsoleEnabled: true,
+        );
       }
     }
 
