8141de3f4300eeceea35b48a89ed3f230cbee22c
Dang Dat <tddang@linagora.com>
2026-09-17 09:51:41 +0700

    TF-4678 fix(mailbox): drop no-op header icon size, pin the 12dp gap
    Addresses: 4025199924, 4025199889, 4025199881
    https://github.com/linagora/tmail-flutter/pull/4832#discussion_r4025199924
    https://github.com/linagora/tmail-flutter/pull/4832#discussion_r4025199889
    https://github.com/linagora/tmail-flutter/pull/4832#discussion_r4025199881

--- lib/features/mailbox/presentation/styles/sidebar_section_header_action_styles.dart
+++ lib/features/mailbox/presentation/styles/sidebar_section_header_action_styles.dart
@@ -1,9 +1,6 @@
 class SidebarSectionHeaderActionStyles {
   SidebarSectionHeaderActionStyles._();
 
-  /// `magnifier_16_noborder` / `plus_16_noborder` in the sidebar design.
-  static const double iconSize = 16;
-
   /// Gap between the actions of a `Navigation Section Header`.
   static const double actionSpacing = 12;
 }
