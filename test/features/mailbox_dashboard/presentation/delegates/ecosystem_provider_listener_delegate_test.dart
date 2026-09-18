0ef3ca2f0594b47bdb5251852e30ae5429a14173
Dang Dat <tddang@linagora.com>
2026-09-18 17:48:14 +0700

    TF-4838 Cover the Workplace FQDN handler registration in the delegate test
    Addresses: 4045177381
    https://github.com/linagora/tmail-flutter/pull/4841#discussion_r4045177381

--- test/features/mailbox_dashboard/presentation/delegates/ecosystem_provider_listener_delegate_test.dart
+++ test/features/mailbox_dashboard/presentation/delegates/ecosystem_provider_listener_delegate_test.dart
@@ -5,6 +5,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:jmap_dart_client/jmap/account_id.dart';
 import 'package:jmap_dart_client/jmap/core/id.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:jmap_dart_client/jmap/core/state.dart' as jmap;
+import 'package:jmap_dart_client/jmap/core/user_name.dart';
 import 'package:mockito/mockito.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/linagora_ecosystem/linagora_ecosystem.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/domain/linagora_ecosystem/linagora_ecosystem_handler.dart';
@@ -15,6 +18,7 @@ import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/linagora_e
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/providers/active_ecosystem_provider.dart';
 import 'package:tmail_ui_user/features/mailbox_dashboard/presentation/riverpod_widgets/mailbox_dashboard_provider_listener_widget.dart';
 import 'package:tmail_ui_user/features/paywall/presentation/providers/premium_cta_provider.dart';
+import 'package:tmail_ui_user/main/providers/workplace/fqdn/workplace_fqdn_provider.dart';
 
 mockControllerCallback() => InternalFinalCallback<void>(callback: () {});
 
@@ -22,6 +26,9 @@ class _DashboardController extends Mock
     implements MailboxDashBoardController {
   final testAccountId = Rxn<AccountId>();
   int setUpSentryCount = 0;
+
+  @override
+  Session? sessionCurrent;
 
   @override
   final DynamicUrlInterceptors dynamicUrlInterceptors =
@@ -283,6 +290,29 @@ void main() {
       expect(recordingHandler.loaded, [ecosystem]);
       // The default SentryEcosystemHandler would have forwarded this config.
       expect(dashboardController.setUpSentryCount, 0);
+    },
+  );
+
+  // The delegate is the only thing registering the Workplace FQDN handler.
+  testWidgets(
+    'registers the Workplace FQDN handler',
+    (tester) async {
+      final uri = Uri.parse('https://www.google.com');
+      _registerDashboardController('first').sessionCurrent = Session(
+          {}, {}, {}, UserName('alice@example.com'), uri, uri, uri, uri, jmap.State('1'));
+
+      final container = await _pumpDelegate(
+        tester,
+        LinagoraEcosystemHandlerRegistry(),
+      );
+      container.read(_ecosystemStateProvider.notifier).setState(
+            EcosystemAvailable(LinagoraEcosystem.deserialize({
+              'workplaceFqdnFallback': '{localPart}.twake.linagora.com',
+            })),
+          );
+      await tester.pump();
+
+      expect(container.read(workplaceFqdnProvider), 'alice.twake.linagora.com');
     },
   );
 }
